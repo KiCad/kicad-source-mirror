af1a7e07255d6238f280974dac16d9d3975598a3
jerryjacobs <Unknown>
2008-12-19 17:46:32 +0000

    Updated about dialog, also started tabbed indent instead of spaces in sourcecode

--- common/about_kicad.cpp
+++ common/about_kicad.cpp
@@ -10,9 +10,9 @@
 extern wxString g_Main_Title; // Import program title
 
 /**********************************/
-wxString SetMsg( const wxString& msg )
+wxString
+SetMsg( const wxString& msg )
 /**********************************/
-
 /* add \n at the beginning of msg under Windows, and do nothing under other version of wxWidgets
  * Needed under wxWidgets 2.8 because wxGTK and wxMSW do not have the same behavior
  * Add Developer needs \n between names under wxMSW, and nothing under wxGTK
@@ -20,104 +20,110 @@ wxString SetMsg( const wxString& msg )
  * Perhaps depending on wxWidgets versions
  */
 {
-    wxString tmp;
+	wxString message;
 
-//    #ifdef __WINDOWS__
-#if 1
-    tmp = wxT( "\n" );
+#if 1 /* Windows */
+	message = wxT( "\n" );
 #endif
-
-    tmp << msg;
-    return tmp;
+	message << msg;
+	return message;
 }
 
 
 /**************************************************/
-void InitKiCadAbout( wxAboutDialogInfo& info )
+void
+InitKiCadAbout( wxAboutDialogInfo& info )
 /**************************************************/
 {
-    /* Set name and title */
-    info.SetName( g_Main_Title );
+	/* Set name and title */
+	info.SetName( g_Main_Title );
 
-    /* Set description */
-    wxString description;
+	/* Set description */
+	wxString description;
 
-    description << ( _T( "Build: " ) ) << GetAboutBuildVersion();
+	/* KiCad build version */
+	description << ( _T( "Build: " ) ) << GetAboutBuildVersion();
 
-/* Check for unicode */
-    description << ( wxT( "\n\nUsing  " ) );
-    description << ( wxT( "wxWidgets " ) );
-    description << wxMAJOR_VERSION << wxT( "." ) << wxMINOR_VERSION << wxT( "." ) <<
-    wxRELEASE_NUMBER;
+	/* Print for wxversion */
+	description << ( wxT( "\n\nwxWidgets " ) )
+		<< wxMAJOR_VERSION
+		<< wxT( "." )
+		<< wxMINOR_VERSION << wxT( "." )
+		<< wxRELEASE_NUMBER
+
+	/* Show Unicode or Ansi version */
 #if wxUSE_UNICODE
-    description << ( wxT( "  Unicode " ) );
-#else
-    description << ( wxT( "  Ansi " ) );
+		<< ( wxT( " Unicode\n" ) );
+#		else
+		<< ( wxT( " Ansi\n" ) );
 #endif
 
-/* Check for wxMSW */
+	/* Check for wxMSW */
 #if defined __WINDOWS__
     description << ( wxT( "on Windows" ) );
 
-/* Check for wxMAC */
-#elif defined __WXMAC__
-    description << ( wxT( "on Macintosch" ) );
+	/* Check for wxMAC */
+#	elif defined __WXMAC__
+	description << ( wxT( "\non Macintosch" ) );
 
-/* Check for linux and arch */
-#elif defined __LINUX__
-    description << ( wxT( "on GNU/Linux " ) );
+	/* Linux 64 bits */
+#	elif defined _LP64 && __LINUX__
+	description << ( wxT( "on 64 Bits GNU/Linux" ) );
+
+	/* Linux 32 bits */
+#	elif defined __LINUX__
+	description << ( wxT( "on 32 Bits GNU/Linux" ) );
 #endif
-#ifdef _LP64
-    description << ( wxT( " 64 bits" ) );
-#else
-    description << ( wxT( " 32 bits" ) );
-#endif
 
-    description << wxT( "\n\nWeb sites:\n" );
-    description << wxT( "http://iut-tice.ujf-grenoble.fr/kicad/" );
-    description << wxT( "\n" );
-    description << wxT( "http://kicad.sourceforge.net/" );
-    description << wxT( "\n" );
+	/* Websites */
+	description << wxT( "\n\nKiCad on the web\n" );
+	description << wxT( "http://iut-tice.ujf-grenoble.fr/kicad \n" );
+	description << wxT( "http://kicad.sourceforge.net \n" );
+	description << wxT( "http://www.kicadlib.org" );
 
+	/* Set the complete about description */
+	info.SetDescription( description );
 
-    info.SetDescription( description );
+	/* Set copyright dialog */
+	info.SetCopyright( _T( "(C) 1992-2009 KiCad Developers Team" ) );
 
-    /* Set copyright */
-    info.SetCopyright( _T( "(C) 1992-2008 KiCad Developers Team" ) );
+	/* Set license dialog */
+	info.SetLicence( wxString::FromAscii
+	(	"The complete KiCad EDA Suite is released under the\n"
+		"GNU General Public License version 2.\n"
+		"See <http://www.gnu.org/licenses/> for more information."
+	));
 
-    /* Set license */
-    info.SetLicence( wxString::FromAscii
-                    (
-                        "The complete KiCad EDA Suite is released under the following license: \n"
-                        "\n"
-                        "GNU General Public License version 2\n"
-                        "See <http://www.gnu.org/licenses/> for more information"
-                    ) );
+	/* Add developers */
+	info.AddDeveloper( wxT( "Jean-Pierre Charras <jean-pierre.charras@inpg.fr>" ) );
+	info.AddDeveloper( SetMsg( wxT( "Jerry Jacobs <jerkejacobs@gmail.com>" ) ) );
+	info.AddDeveloper( SetMsg( wxT( "Dick Hollenbeck <dick@softplc.com>" ) ) );
+	info.AddDeveloper( SetMsg( wxT( "KBool Library <http://boolean.klaasholwerda.nl/bool.html>" ) ) );
+	info.AddDeveloper( SetMsg( wxT( "Vesa Solonen <vesa.solonen@hut.fi>" ) ) );
+	info.AddDeveloper( SetMsg( wxT( "Wayne Stambaugh <stambaughw@verizon.net>" ) ) );
 
-    /* Add developers */
-    info.AddDeveloper( wxT( "Jean-Pierre Charras <jean-pierre.charras@inpg.fr>" ) );
-    info.AddDeveloper( SetMsg( wxT( "Jerry Jacobs <jerkejacobs@gmail.com>" ) ) );
-    info.AddDeveloper( SetMsg( wxT( "Dick Hollenbeck <dick@softplc.com>" ) ) );
-    info.AddDeveloper( SetMsg( wxT( "KBool Library <http://boolean.klaasholwerda.nl/bool.html>" ) ) );
-    info.AddDeveloper( SetMsg( wxT( "Vesa Solonen <vesa.solonen@hut.fi>" ) ) );
+	/* Add document writers*/
+	info.AddDocWriter( wxT( "Jean-Pierre Charras <jean-pierre.charras@inpg.fr>" ) );
+	info.AddDocWriter( SetMsg( wxT( "Igor Plyatov <plyatov@gmail.com>" ) ) );
 
-    /* Add document writers */
-    info.AddDocWriter( wxT( "Jean-Pierre Charras <jean-pierre.charras@inpg.fr>" ) );
-    info.AddDocWriter( SetMsg( wxT( "Igor Plyatov <plyatov@gmail.com>" ) ) );
+	/* Add translators */
+	info.AddTranslator( wxT( "Czech (CZ) Martin Kratoška <martin@ok1rr.com>" ) );
+	info.AddTranslator( SetMsg( wxT( "Dutch (NL) Jerry Jacobs <jerkejacobs@gmail.com>" ) ) );
+	info.AddTranslator( SetMsg( wxT( "French (FR) Jean-Pierre Charras <jean-pierre.charras@inpg.fr>" ) ) );
+	info.AddTranslator( SetMsg( wxT( "Polish (PL) Mateusz Skowroński <skowri@gmail.com>" ) ) );
+	info.AddTranslator( SetMsg( wxT( "Portuguese (PT) Renie Marquet <reniemarquet@uol.com.br>" ) ) );
+	info.AddTranslator( SetMsg( wxT( "Russian (RU) Igor Plyatov <plyatov@gmail.com>" ) ) );
 
-    /* Add translators */
-    info.AddTranslator( wxT( "Czech (CZ) Martin Kratoška <martin@ok1rr.com>" ) );
-    info.AddTranslator( SetMsg( wxT( "Dutch (NL) Jerry Jacobs <jerkejacobs@gmail.com>" ) ) );
-    info.AddTranslator( SetMsg( wxT(
-                                   "French (FR) Jean-Pierre Charras <jean-pierre.charras@inpg.fr>" ) ) );
-    info.AddTranslator( SetMsg( wxT( "Polish (PL) Mateusz Skowroński <skowri@gmail.com>" ) ) );
-    info.AddTranslator( SetMsg( wxT( "Portuguese (PT) Renie Marquet <reniemarquet@uol.com.br>" ) ) );
-    info.AddTranslator( SetMsg( wxT( "Russian (RU) Igor Plyatov <plyatov@gmail.com>" ) ) );
-    info.AddTranslator( SetMsg( wxT(
-                                   " David Briscoe, Remy Halvick, Boris Barbour, Dominique Laigle, Paul Burke" ) ) );
+	/* TODO are these all russian translators, placed them here now              TODO
+		TODO or else align them below other language maintainer with mail adres   TODO*/
+	info.AddTranslator( SetMsg( wxT( "             Remy Halvick" ) ) );
+	info.AddTranslator( SetMsg( wxT( "             David Briscoe" ) ) );
+	info.AddTranslator( SetMsg( wxT( "             Dominique Laigle" ) ) );
+	info.AddTranslator( SetMsg( wxT( "             Paul Burke" ) ) );
+	info.AddTranslator( SetMsg( wxT( "             Pedro Martin del Valle" ) ) );
+	info.AddTranslator( SetMsg( wxT( "             Iñigo Zuluaga" ) ) );
 
-    info.AddTranslator( SetMsg( wxT( "Pedro Martin del Valle, Inigo Zuluaga")));
-
-    /* Add programm credits for icons */
-    info.AddArtist( wxT( "Icons: Inigo Zuluaga" ) );
+	/* Add programm credits for icons */
+	info.AddArtist( wxT( "Icons by Iñigo Zuluaga" ) );
+	info.AddArtist( wxT( "3D modules by Renie Marquet <reniemarquet@uol.com.br>" ) );
 }
