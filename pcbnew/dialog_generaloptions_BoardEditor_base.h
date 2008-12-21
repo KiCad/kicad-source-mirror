06ca175064d989656cec9dfbefe7c3f6645934aa
charras <Unknown>
2008-12-21 12:11:47 +0000

    pcbnew: Convert dialog_general_option from DialogBlocks to wxFormBuilder and add tips to all options

---
+++ pcbnew/dialog_generaloptions_BoardEditor_base.h
@@ -0,0 +1,86 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Apr 16 2008)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef __dialog_generaloptions_BoardEditor_base__
+#define __dialog_generaloptions_BoardEditor_base__
+
+#include <wx/intl.h>
+
+#include <wx/string.h>
+#include <wx/radiobox.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/spinctrl.h>
+#include <wx/checkbox.h>
+#include <wx/statbox.h>
+#include <wx/button.h>
+#include <wx/dialog.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class DialogGeneralOptionsBoardEditor_base
+///////////////////////////////////////////////////////////////////////////////
+class DialogGeneralOptionsBoardEditor_base : public wxDialog 
+{
+	private:
+	
+	protected:
+		enum
+		{
+			wxID_POLAR_CTRL = 1000,
+			wxID_UNITS,
+			wxID_CURSOR_SHAPE,
+			wxID_LAYER_NUMBER,
+			wxID_DRC_ONOFF,
+			wxID_GENERAL_RATSNEST,
+			wxID_RATSNEST_MODULE,
+			wxID_TRACK_AUTODEL,
+			wxID_TRACKS45,
+			wxID_SEGMENTS45,
+			wxID_AUTOPAN,
+			wxID_MAGNETIC_TRACKS,
+		};
+		
+		wxRadioBox* m_PolarDisplay;
+		wxRadioBox* m_UnitsSelection;
+		wxRadioBox* m_CursorShape;
+		wxRadioBox* m_LayerNumber;
+		wxStaticText* m_staticTextmaxlinks;
+		wxSpinCtrl* m_MaxShowLinks;
+		wxStaticText* m_staticTextautosave;
+		wxSpinCtrl* m_SaveTime;
+		wxCheckBox* m_DrcOn;
+		wxCheckBox* m_ShowGlobalRatsnest;
+		wxCheckBox* m_ShowModuleRatsnest;
+		wxCheckBox* m_TrackAutodel;
+		wxCheckBox* m_Track_45_Only_Ctrl;
+		wxCheckBox* m_Segments_45_Only_Ctrl;
+		wxCheckBox* m_AutoPANOpt;
+		wxCheckBox* m_Track_DoubleSegm_Ctrl;
+		wxRadioBox* m_MagneticPadOptCtrl;
+		wxRadioBox* m_MagneticTrackOptCtrl;
+		wxButton* m_buttonOK;
+		wxButton* m_buttonCANCEL;
+		
+		// Virtual event handlers, overide them in your derived class
+		virtual void OnInitDialog( wxInitDialogEvent& event ){ event.Skip(); }
+		virtual void OnOkClick( wxCommandEvent& event ){ event.Skip(); }
+		virtual void OnCancelClick( wxCommandEvent& event ){ event.Skip(); }
+		
+	
+	public:
+		DialogGeneralOptionsBoardEditor_base( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("General settings"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 585,272 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
+		~DialogGeneralOptionsBoardEditor_base();
+	
+};
+
+#endif //__dialog_generaloptions_BoardEditor_base__
