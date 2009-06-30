92bf0f47b96a4e2dc7b3c1b90663384f0d12d1a5
charras <Unknown>
2009-06-30 19:27:39 +0000

    upload missing file

---
+++ pcbnew/plotdxf.cpp
@@ -0,0 +1,49 @@
+/*******************************/
+/**** Routine de trace HPGL ****/
+/*******************************/
+
+#include "fctsys.h"
+#include "common.h"
+#include "plot_common.h"
+#include "confirm.h"
+#include "pcbnew.h"
+#include "pcbplot.h"
+#include "trigo.h"
+
+#include "protos.h"
+
+/*****************************************************************************/
+void WinEDA_BasePcbFrame::Genere_DXF( const wxString& FullFileName, int Layer,
+                                       GRTraceMode trace_mode )
+/*****************************************************************************/
+{
+    Ki_PageDescr* currentsheet = GetScreen()->m_CurrentSheetDesc;
+
+    MsgPanel->EraseMsgBox();
+
+    FILE* output_file = wxFopen( FullFileName, wxT( "wt" ) );
+    if( output_file == NULL )
+    {
+        wxString msg = _( "Unable to create file " ) + FullFileName;
+        DisplayError( this, msg );
+        return;
+    }
+
+    SetLocaleTo_C_standard();
+    Affiche_1_Parametre( this, 0, _( "File" ), FullFileName, CYAN );
+
+    DXF_Plotter* plotter = new DXF_Plotter();
+    plotter->set_paper_size( currentsheet );
+    plotter->set_viewport( wxPoint(0,0), 1, 0 );
+    plotter->set_creator( wxT( "PCBNEW-DXF" ) );
+    plotter->set_filename( FullFileName );
+    plotter->start_plot( output_file );
+
+    if( g_pcb_plot_options.Plot_Frame_Ref )
+        PlotWorkSheet( plotter, GetScreen() );
+
+    Plot_Layer( plotter, Layer, trace_mode );
+    plotter->end_plot();
+    delete plotter;
+    SetLocaleTo_Default();
+}
