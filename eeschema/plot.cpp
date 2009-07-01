c20d9b9328fc92ac99c8c7e39d698e1eca975c38
charras <Unknown>
2009-07-01 14:59:34 +0000

    minor bugs in plot functions (bad document size in PS header and junction size) fixed

--- eeschema/plot.cpp
+++ eeschema/plot.cpp
@@ -747,7 +747,7 @@ void PlotDrawlist( Plotter* plotter, SCH_ITEM* aDrawlist )
             #undef STRUCT
             #define STRUCT ( (DrawJunctionStruct*) aDrawlist )
             plotter->set_color( ReturnLayerColor( STRUCT->GetLayer() ) );
-            plotter->circle( STRUCT->m_Pos, DRAWJUNCTION_SIZE, FILLED_SHAPE );
+            plotter->circle( STRUCT->m_Pos, DRAWJUNCTION_SIZE*2, FILLED_SHAPE );
             break;
 
         case TYPE_SCH_TEXT:
