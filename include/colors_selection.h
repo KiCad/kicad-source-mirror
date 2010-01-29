ce72c1f91b23924fca889ec58a6953f2af963374
charras <Unknown>
2010-01-29 21:03:12 +0000

    forgotten file

---
+++ include/colors_selection.h
@@ -0,0 +1,10 @@
+/* colors_selection.h */
+
+#ifndef _COLORS_SELECTION_H_
+#define _COLORS_SELECTION_H_
+
+#include "class_colors_design_settings.h"
+// Colors for layers and items
+extern COLORS_DESIGN_SETTINGS g_ColorsSettings;
+
+#endif      //  _COLORS_SELECTION_H_
