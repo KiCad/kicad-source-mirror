43fe809d4dad89c529e4fb81e768148a6a0d7715
Wayne Stambaugh <stambaughw@verizon.net>
2010-11-19 11:45:23 -0500

    Added missing header file from previous commit.

---
+++ eeschema/dialogs/dialog_libedit_dimensions.h
@@ -0,0 +1,23 @@
+
+#ifndef _DIALOG_LIBEDIT_DIMENSIONS_H_
+#define _DIALOG_LIBEDIT_DIMENSIONS_H_
+
+
+#include "dialog_libedit_dimensions_base.h"
+
+
+class LIB_EDIT_FRAME;
+
+
+class DIALOG_LIBEDIT_DIMENSIONS : public DIALOG_LIBEDIT_DIMENSIONS_BASE
+{
+public:
+    DIALOG_LIBEDIT_DIMENSIONS( LIB_EDIT_FRAME* parent );
+    ~DIALOG_LIBEDIT_DIMENSIONS();
+
+private:
+    void initDialog( );
+};
+
+
+#endif    // _DIALOG_LIBEDIT_DIMENSIONS_H_
