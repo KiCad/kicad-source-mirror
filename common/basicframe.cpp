0289b6d550b19e0bbc666bbabeebaf2424c1b55f
jean-pierre charras <jp.charras@wanadoo.fr>
2011-08-04 21:56:30 +0200

    Fix 2.8 compatibility

--- common/basicframe.cpp
+++ common/basicframe.cpp
@@ -268,7 +268,7 @@ void EDA_BASE_FRAME::GetKicadHelp( wxCommandEvent& event )
     {
         // Temporary change the help filename
         wxString tmp = wxGetApp().m_HelpFileName;
-        wxGetApp().m_HelpFileName = "Getting_Started_in_KiCad.pdf";
+        wxGetApp().m_HelpFileName = wxT( "Getting_Started_in_KiCad.pdf" );
         wxString helpFile = wxGetApp().GetHelpFile();
         if( !helpFile )
         {
