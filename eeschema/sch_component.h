d5cbd5d18675db2900da64160024491f5040464c
Wayne Stambaugh <stambaughw@verizon.net>
2011-10-11 16:23:56 -0400

    Eeschema net list object generation improvements.
    * Add net list object creation functions to schematic component object.
    * Remove external variables used by the verify bus label and parse bus
      label functions.
    * Remove redundant net list object include sheet path member assignment.
    * Add license statements to all modified files that required one.

--- eeschema/sch_component.h
+++ eeschema/sch_component.h
@@ -1,3 +1,28 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2009 Jean-Pierre Charras, jaen-pierre.charras@gipsa-lab.inpg.com
+ * Copyright (C) 2011 Wayne Stambaugh <stambaughw@verizon.net>
+ * Copyright (C) 1992-2011 KiCad Developers, see AUTHORS.txt for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you may find one here:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * or you may search the http://www.gnu.org website for the version 2 license,
+ * or you may write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
 /**
  * @file sch_component.h
  * @brief Definition the SCH_COMPONENT class for Eeschema.
@@ -383,6 +408,9 @@ public:
     virtual wxString GetSelectMenuText() const;
 
     virtual BITMAP_DEF GetMenuImage() const { return  add_component_xpm; }
+
+    virtual void GetNetListItem( vector<NETLIST_OBJECT*>& aNetListItems,
+                                 SCH_SHEET_PATH*          aSheetPath );
 
     virtual bool operator <( const SCH_ITEM& aItem ) const;
 
