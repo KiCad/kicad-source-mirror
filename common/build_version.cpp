9d663c58194abade6f2f359748da982e3ed5085d
jean-pierre charras <jp.charras@wanadoo.fr>
2011-11-26 21:02:59 +0100

    Update version number

--- common/build_version.cpp
+++ common/build_version.cpp
@@ -7,9 +7,9 @@
 
 #ifndef KICAD_BUILD_VERSION
 #if defined KICAD_GOST
-#   define KICAD_BUILD_VERSION "(2011-nov-12 GOST)"
+#   define KICAD_BUILD_VERSION "(2011-nov-26 GOST)"
 #else
-#   define KICAD_BUILD_VERSION "(2011-nov-12)"
+#   define KICAD_BUILD_VERSION "(2011-nov-26)"
 #endif
 #endif
 
