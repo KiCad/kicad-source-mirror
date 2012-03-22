31a84b84ecfe15efb17c4159fcd3f2c7692936e9
Dick Hollenbeck <dick@softplc.com>
2012-03-22 02:02:49 -0500

    Add include/dialog_shim.h and common/dialog_shim.cpp for use by wxformbuilder's "subclass a wxDialog" support. This works, but in my version of wxformbuilder there is a bug which does not properly show the subclass property, even though it is still in play.  This happens after saving then loading the *.fbp file. So it is a nuisance bug, but does not affect functionality.

---
+++ common/dialog_shim.cpp
@@ -0,0 +1,90 @@
+
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2012 SoftPLC Corporation, Dick Hollenbeck <dick@softplc.com>
+ * Copyright (C) 2012 KiCad Developers, see CHANGELOG.TXT for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you may find one here:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * or you may search the http://www.gnu.org website for the version 2 license,
+ * or you may write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#include <dialog_shim.h>
+
+
+DIALOG_SHIM::DIALOG_SHIM( wxWindow* aParent, wxWindowID id, const wxString& title,
+        const wxPoint& pos, const wxSize& size, long style, const wxString& name ) :
+    wxDialog( aParent, id, title, pos, size, style, name )
+{
+    // linux wxGTK needed this at one time to allow the ESCAPE key to close a wxDialog window.
+    SetFocus();
+}
+
+
+// our hashtable is an implementation secret, don't need or want it in a header file
+#include <hashtables.h>
+#include <base_struct.h>        // EDA_RECT
+#include <typeinfo>
+
+
+/// hashtable with key: C string and value: EDA_RECT.
+/// The key is the classname of the derived wxformbuilder dialog
+WX_DECLARE_HASH_MAP( char*, EDA_RECT, wxStringHash, wxStringEqual, RECT_MAP );
+static RECT_MAP class_map;
+
+bool DIALOG_SHIM::Show( bool show )
+{
+    bool        ret;
+    const char* classname = typeid(*this).name();
+
+    // Show or hide the window.  If hiding, save current position and size.
+    // If showing, use previous position and size.
+    if( show )
+    {
+        ret = wxDialog::Show( show );
+
+        // classname is key, returns a zeroed out default EDA_RECT if none existed before.
+        EDA_RECT r = class_map[ classname ];
+
+        if( r.GetSize().x != 0 && r.GetSize().y != 0 )
+            SetSize( r.GetPosition().x, r.GetPosition().y, r.GetSize().x, r.GetSize().y, 0 );
+    }
+    else
+    {
+        // Save the dialog's position & size before hiding, using classname as key
+        EDA_RECT  r( wxDialog::GetPosition(), wxDialog::GetSize() );
+        class_map[ classname ] = r;
+
+        ret = wxDialog::Show( show );
+    }
+    return ret;
+}
+
+/*
+const wxSize& DIALOG_SHIM::GetLastSize()
+{
+    const char* classname = typeid(*this).name();
+    return class_map[ classname ].GetSize();
+}
+
+
+const wxPoint& DIALOG_SHIM::GetLastPosition()
+{
+    const char* classname = typeid(*this).name();
+    return class_map[ classname ].GetPosition();
+}
+*/
---
+++ include/dialog_shim.h
@@ -0,0 +1,59 @@
+#ifndef DIALOG_SHIM_
+#define DIALOG_SHIM_
+
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2012 SoftPLC Corporation, Dick Hollenbeck <dick@softplc.com>
+ * Copyright (C) 2012 KiCad Developers, see CHANGELOG.TXT for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you may find one here:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * or you may search the http://www.gnu.org website for the version 2 license,
+ * or you may write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#include <wx/dialog.h>
+#include <hashtables.h>
+
+
+/**
+ * Class DIALOG_SHIM
+ * may sit in the inheritance tree between wxDialog and any class written by
+ * wxFormBuilder.  To put it there, use wxFormBuilder tool and set:
+ * <br> subclass name = DIALOG_SHIM
+ * <br> subclass header = dialog_shim.h
+ * <br>
+ * in the dialog window's properties.
+ **/
+class DIALOG_SHIM : public wxDialog
+{
+public:
+
+    DIALOG_SHIM( wxWindow* aParent, wxWindowID id, const wxString& title,
+            const   wxPoint& pos = wxDefaultPosition,
+            const   wxSize&  size = wxDefaultSize,
+            long    style = wxDEFAULT_DIALOG_STYLE,
+            const   wxString& name = wxDialogNameStr );
+
+    bool Show( bool show );     // overload wxDialog::Show
+
+    /*
+    const wxSize& GetLastSize();
+    const wxPoint& GetLastPosition();
+    */
+};
+
+#endif  // DIALOG_SHIM_
