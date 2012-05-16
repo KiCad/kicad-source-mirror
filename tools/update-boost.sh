879f85aa7c88cce78ef6b3ec5595936229044943
Dick Hollenbeck <dick@softplc.com>
2012-05-16 07:01:40 -0500

    move boost update script to out of way place, fix bug in it

--- tools/update-boost.sh
+++ tools/update-boost.sh
@@ -3,8 +3,8 @@
 # A shell script to update the boost subset within KiCad to newer version.
 
 BOOST_VERSION=1_49_0
-SRC_BOOST=/tmp/boost_$BOOST_VERSION/boost
-KICAD_BASE=/svn/kicad/testing.checkout
+SRC_BOOST="/tmp/boost_$BOOST_VERSION/boost"
+KICAD_BASE="/svn/kicad/testing.checkout"
 DST_BOOST="$KICAD_BASE/include/boost"
 
 # control the subset of boost libs used:
@@ -38,19 +38,16 @@ BOOST_DIRS="\
     utility \
     "
 
-# staging area for non-included dirs:
-# numeric accumulators algorithm
-
 
 # remove all old BOOST include files using bzr
-bzr rm --no-backup --quiet "$DST_BOOST/*"
+eval bzr rm --no-backup --quiet "$DST_BOOST/*"
 
 # copy all *.hpp files in the base boost directory:
 eval cp "$SRC_BOOST/*.hpp" "$DST_BOOST/"
 
 # copy recursively all chosen boost libraries:
 for D in $BOOST_DIRS; do
-    eval cp -r "$SRC_BOOST/$D" "$DST_BOOST"
+    eval cp -r "$SRC_BOOST/$D" "$DST_BOOST/"
 done
 
 # tell bzr about the new files so they become part of the repo.
