39f84d7fe40dde222c4f38b43d90e50b6b10e616
jean-pierre charras <jp.charras@wanadoo.fr>
2012-06-08 11:56:42 +0200

    Pcbnew: Update incorrect or incomplete copyrights in many files. Minor code cleaning in autoroute files.

--- pcbnew/autorouter/autorout.cpp
+++ pcbnew/autorouter/autorout.cpp
@@ -3,6 +3,33 @@
  * @brief Autorouting command and control.
  */
 
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2012 Jean-Pierre Charras, jean-pierre.charras@ujf-grenoble.fr
+ * Copyright (C) 2012 SoftPLC Corporation, Dick Hollenbeck <dick@softplc.com>
+ * Copyright (C) 2011 Wayne Stambaugh <stambaughw@verizon.net>
+ *
+ * Copyright (C) 1992-2012 KiCad Developers, see change_log.txt for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you may find one here:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * or you may search the http://www.gnu.org website for the version 2 license,
+ * or you may write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
 #include <fctsys.h>
 #include <class_drawpanel.h>
 #include <wxPcbStruct.h>
@@ -156,10 +183,10 @@ void PCB_EDIT_FRAME::Autoroute( wxDC* DC, int mode )
     if( Route_Layer_TOP != Route_Layer_BOTTOM )
         Nb_Sides = TWO_SIDES;
 
-    if( RoutingMatrix.InitBoard() < 0 )
+    if( RoutingMatrix.InitRoutingMatrix() < 0 )
     {
         wxMessageBox( _( "No memory for autorouting" ) );
-        RoutingMatrix.UnInitBoard();  /* Free memory. */
+        RoutingMatrix.UnInitRoutingMatrix();  /* Free memory. */
         return;
     }
 
@@ -179,7 +206,7 @@ void PCB_EDIT_FRAME::Autoroute( wxDC* DC, int mode )
     /* Free memory. */
     FreeQueue();
     InitWork();             /* Free memory for the list of router connections. */
-    RoutingMatrix.UnInitBoard();
+    RoutingMatrix.UnInitRoutingMatrix();
     stop = time( NULL ) - start;
     msg.Printf( wxT( "time = %d second%s" ), stop, ( stop == 1 ) ? wxT( "" ) : wxT( "s" ) );
     SetStatusText( msg );
