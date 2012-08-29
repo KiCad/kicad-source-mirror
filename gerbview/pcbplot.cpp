715af96184ec9cd0a1c17ace15dc7e3064be1762
Lorenzo Marcantonio <l.marcantonio@logossrl.com>
2012-08-29 20:43:51 +0200

    Removed g_pcb_plot_options from gerbview (which has no plotting anyway...)

--- gerbview/pcbplot.cpp
+++ gerbview/pcbplot.cpp
@@ -9,10 +9,6 @@
 #include <pcbplot.h>
 
 
-/* The group of plot options - sadly global XXX */
-PCB_Plot_Options g_pcb_plot_options;
-
-
 /**************************************************************/
 /* void PCB_BASE_FRAME::ToPlotter(wxCommandEvent& event) */
 /***************************************************************/
