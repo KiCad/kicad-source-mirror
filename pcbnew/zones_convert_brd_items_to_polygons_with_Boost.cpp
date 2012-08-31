bb73777a4a23ffaaec0965877f962195a675f06d
Marco Mattila <marcom99@gmail.com>
2012-08-31 21:43:09 +0300

    Take pad clearance into account in intersection detection during pcbnew zone filling.

--- pcbnew/zones_convert_brd_items_to_polygons_with_Boost.cpp
+++ pcbnew/zones_convert_brd_items_to_polygons_with_Boost.cpp
@@ -232,6 +232,7 @@ void ZONE_CONTAINER::AddClearanceAreasPolygonsToPolysList( BOARD* aPcb )
             {
                 item_clearance   = pad->GetClearance() + margin;
                 item_boundingbox = pad->GetBoundingBox();
+                item_boundingbox.Inflate( item_clearance );
 
                 if( item_boundingbox.Intersects( zone_boundingbox ) )
                 {
