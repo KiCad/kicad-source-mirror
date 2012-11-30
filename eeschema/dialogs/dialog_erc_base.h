91d5bb5812b462ad7b9252859694e51b622e6b0d
jean-pierre charras <jp.charras@wanadoo.fr>
2012-11-30 13:28:23 +0100

    Eeschema: dialog ERC: minor enhancements and code cleaning.

--- eeschema/dialogs/dialog_erc_base.h
+++ eeschema/dialogs/dialog_erc_base.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Apr 11 2012)
+// C++ code generated with wxFormBuilder (version Apr 10 2012)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO "NOT" EDIT THIS FILE!
@@ -19,12 +19,11 @@
 #include <wx/font.h>
 #include <wx/colour.h>
 #include <wx/settings.h>
-#include <wx/sizer.h>
-#include <wx/statbox.h>
 #include <wx/textctrl.h>
-#include <wx/button.h>
+#include <wx/sizer.h>
 #include <wx/checkbox.h>
-#include <wx/statline.h>
+#include <wx/statbox.h>
+#include <wx/button.h>
 #include <wx/listbox.h>
 #include <wx/panel.h>
 #include <wx/bitmap.h>
@@ -42,15 +41,16 @@ class DIALOG_ERC_BASE : public DIALOG_SHIM
 {
 	DECLARE_EVENT_TABLE()
 	private:
-		
+
 		// Private event handlers
 		void _wxFB_OnErcCmpClick( wxCommandEvent& event ){ OnErcCmpClick( event ); }
 		void _wxFB_OnEraseDrcMarkersClick( wxCommandEvent& event ){ OnEraseDrcMarkersClick( event ); }
 		void _wxFB_OnCancelClick( wxCommandEvent& event ){ OnCancelClick( event ); }
-		void _wxFB_OnLeftDClickMarkersList( wxCommandEvent& event ){ OnLeftDClickMarkersList( event ); }
+		void _wxFB_OnLeftClickMarkersList( wxCommandEvent& event ){ OnLeftClickMarkersList( event ); }
+		void _wxFB_OnLeftDblClickMarkersList( wxCommandEvent& event ){ OnLeftDblClickMarkersList( event ); }
 		void _wxFB_OnResetMatrixClick( wxCommandEvent& event ){ OnResetMatrixClick( event ); }
-		
-	
+
+
 	protected:
 		enum
 		{
@@ -59,43 +59,41 @@ class DIALOG_ERC_BASE : public DIALOG_SHIM
 			ID_MAKER_HTMLLISTBOX,
 			ID_RESET_MATRIX
 		};
-		
+
 		wxNotebook* m_NoteBook;
 		wxPanel* m_PanelERC;
 		wxStaticText* m_ErcTotalErrorsText;
-		wxStaticText* m_TotalErrCount;
+		wxTextCtrl* m_TotalErrCount;
 		wxStaticText* m_WarnErcErrorsText;
-		wxStaticText* m_LastWarningCount;
+		wxTextCtrl* m_LastWarningCount;
 		wxStaticText* m_LastErrCountText;
-		wxStaticText* m_LastErrCount;
+		wxTextCtrl* m_LastErrCount;
+		wxCheckBox* m_WriteResultOpt;
 		wxStaticText* m_titleMessages;
 		wxTextCtrl* m_MessagesList;
 		wxButton* m_buttonERC;
 		wxButton* m_buttondelmarkers;
 		wxButton* m_buttonClose;
-		wxCheckBox* m_WriteResultOpt;
-		wxStaticLine* m_staticline2;
 		wxStaticText* m_textMarkers;
 		ERC_HTML_LISTBOX* m_MarkersList;
 		wxPanel* m_PanelERCOptions;
-		wxBoxSizer* m_PanelMatrixSizer;
 		wxButton* m_ResetOptButton;
-		wxStaticLine* m_staticline1;
-		wxBoxSizer* m_MatrixSizer;
-		
+		wxPanel* m_matrixPanel;
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void OnErcCmpClick( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnEraseDrcMarkersClick( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnCancelClick( wxCommandEvent& event ) { event.Skip(); }
-		virtual void OnLeftDClickMarkersList( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnLeftClickMarkersList( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnLeftDblClickMarkersList( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnResetMatrixClick( wxCommandEvent& event ) { event.Skip(); }
-		
-	
+
+
 	public:
-		
-		DIALOG_ERC_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("EESchema Erc"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+
+		DIALOG_ERC_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("EESchema Erc"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 519,392 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
 		~DIALOG_ERC_BASE();
-	
+
 };
 
 #endif //__DIALOG_ERC_BASE_H__
