dc4b261c686ec686b0148b39821af30ab6e7efaa
Miguel Angel Ajo <miguelangel@nbee.es>
2013-03-15 14:27:18 +0100

    wxT added to the locales fix, message box on python exceptions inside wizard, added some missing copyright notices

--- pcbnew/scripting/pcbnew_footprint_wizards.cpp
+++ pcbnew/scripting/pcbnew_footprint_wizards.cpp
@@ -1,3 +1,27 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2013 NBEE Embedded Systems SL, Miguel Angel Ajo <miguelangel@ajo.es>
+ * Copyright (C) 2013 KiCad Developers, see CHANGELOG.TXT for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you may find one here:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * or you may search the http://www.gnu.org website for the version 2 license,
+ * or you may write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
 /**
  * @file  pcbnew_footprint_wizards.cpp
  * @brief Class PCBNEW_PYTHON_FOOTPRINT_WIZARDS
@@ -39,14 +63,23 @@ PyObject* PYTHON_FOOTPRINT_WIZARD::CallMethod( const char* aMethod, PyObject* aA
 
         if( PyErr_Occurred() )
         {
+            wxString message;
             PyObject* t;
             PyObject* v;
             PyObject* b;
 
             PyErr_Fetch( &t, &v, &b );
-            printf ( "calling %s()\n", aMethod );
-            printf ( "Exception: %s\n", PyString_AsString( PyObject_Str( v ) ) );
-            printf ( "         : %s\n", PyString_AsString( PyObject_Str( b ) ) );
+            message.Printf ( wxT( "calling %s()\n"
+                                  "Exception: %s\n"
+                                  "         : %s\n"),
+                             aMethod,
+                             PyString_AsString( PyObject_Str( v ) ),
+                             PyString_AsString( PyObject_Str( b ) )
+                            );
+
+            wxMessageBox( message,
+                          wxT( "Exception on python footprint wizard code" ),
+                          wxICON_ERROR|wxOK);
         }
 
         if( result )
