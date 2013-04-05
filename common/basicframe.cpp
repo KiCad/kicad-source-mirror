90f2e67ed8637234f0f61997646570c2c741af3f
Lorenzo Marcantonio <l.marcantonio@logossrl.com>
2013-04-05 17:09:04 +0200

    Typo fix

--- common/basicframe.cpp
+++ common/basicframe.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2013 Jean-Pierre Charras, jp.charras at wanadoo.fr
- * Copyright (C) 20131 Wayne Stambaugh <stambaughw@verizon.net>
+ * Copyright (C) 2013 Wayne Stambaugh <stambaughw@verizon.net>
  * Copyright (C) 1992-2013 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
