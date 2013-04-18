97ccebf3552c1ce8220e6e98488df6652171cd09
Matthew Beckler <matthew.beckler@gmail.com>
2013-04-18 23:23:11 +0200

    Fixes to scripting after refactoring & interface cleanups,

--- pcbnew/scripting/examples/listPcbLibrary.py
+++ pcbnew/scripting/examples/listPcbLibrary.py
@@ -4,6 +4,6 @@ lst = FootprintEnumerate("/usr/share/kicad/modules/sockets.mod")
 for name in lst:
 	m = FootprintLoad("/usr/share/kicad/modules/sockets.mod",name)
 	print name,"->",m.GetLibRef(), m.GetReference()
-	for p in m.GetPads():
+	for p in m.Pads():
 		print "\t",p.GetPadName(),p.GetPosition(),p.GetPos0(), p.GetOffset()
 	
