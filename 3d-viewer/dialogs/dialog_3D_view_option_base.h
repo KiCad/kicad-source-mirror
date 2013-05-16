f037cc3c74faad3eb4cbe33c1c25cdfe25ade4de
jean-pierre charras <jp.charras@wanadoo.fr>
2013-05-16 21:04:21 +0200

    3D viewer: Add more options, to show/hide technical layers.

---
+++ 3d-viewer/dialogs/dialog_3D_view_option_base.h
@@ -0,0 +1,82 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Oct  8 2012)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef __DIALOG_3D_VIEW_OPTION_BASE_H__
+#define __DIALOG_3D_VIEW_OPTION_BASE_H__
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/intl.h>
+class DIALOG_SHIM;
+
+#include "dialog_shim.h"
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
+#include <wx/statbmp.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/string.h>
+#include <wx/checkbox.h>
+#include <wx/sizer.h>
+#include <wx/button.h>
+#include <wx/statline.h>
+#include <wx/dialog.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class DIALOG_3D_VIEW_OPTIONS_BASE
+///////////////////////////////////////////////////////////////////////////////
+class DIALOG_3D_VIEW_OPTIONS_BASE : public DIALOG_SHIM
+{
+	private:
+	
+	protected:
+		wxStaticBitmap* m_bitmapCuThickness;
+		wxCheckBox* m_checkBoxCuThickness;
+		wxStaticBitmap* m_bitmap3Dshapes;
+		wxCheckBox* m_checkBox3Dshapes;
+		wxStaticBitmap* m_bitmapAreas;
+		wxCheckBox* m_checkBoxAreas;
+		wxStaticBitmap* m_bitmapSilkscreen;
+		wxCheckBox* m_checkBoxSilkscreen;
+		wxStaticBitmap* m_bitmapSolderMask;
+		wxCheckBox* m_checkBoxSolderMask;
+		wxStaticBitmap* m_bitmapSolderPaste;
+		wxCheckBox* m_checkBoxSolderpaste;
+		wxStaticBitmap* m_bitmapAdhesive;
+		wxCheckBox* m_checkBoxAdhesive;
+		wxStaticBitmap* m_bitmapComments;
+		wxCheckBox* m_checkBoxComments;
+		wxStaticBitmap* m_bitmapECO;
+		wxCheckBox* m_checkBoxECO;
+		wxButton* m_buttonShowAll;
+		wxButton* m_buttonShowNone;
+		wxStaticLine* m_staticline1;
+		wxStdDialogButtonSizer* m_sdbSizer;
+		wxButton* m_sdbSizerOK;
+		wxButton* m_sdbSizerCancel;
+		
+		// Virtual event handlers, overide them in your derived class
+		virtual void OnShowAllClick( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnShowNoneClick( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnCancelClick( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnOKClick( wxCommandEvent& event ) { event.Skip(); }
+		
+	
+	public:
+		
+		DIALOG_3D_VIEW_OPTIONS_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("3D Display Options"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 340,344 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+		~DIALOG_3D_VIEW_OPTIONS_BASE();
+	
+};
+
+#endif //__DIALOG_3D_VIEW_OPTION_BASE_H__
