3fcf44ae32e01b28e854c04da5822cf8a8d0c95d
Dick Hollenbeck <dick@softplc.com>
2013-09-01 12:49:01 -0500

    Clarify, extend, and rename ky*.py to kicad_netlist_reader.py. Significantly enhance the bom_csv_grouped_by_value.py BOM generator. IMO it at this moment, the best BOM production tool for KiCad.

--- scripts/bom-in-python/bom_csv_by_ref_v2.py
+++ scripts/bom-in-python/bom_csv_by_ref_v2.py
@@ -4,21 +4,23 @@
 # Example: Ungrouped (One component per row) CSV output
 #
 
+from __future__ import print_function
+
 # Import the KiCad python helper module
-import ky_generic_netlist_reader
+import kicad_netlist_reader
 import csv
 import sys
 
 # Generate an instance of a generic netlist, and load the netlist tree from
 # the command line option. If the file doesn't exist, execution will stop
-net = ky_generic_netlist_reader.netlist(sys.argv[1])
+net = kicad_netlist_reader.netlist(sys.argv[1])
 
 # Open a file to write to, if the file cannot be opened output to stdout
 # instead
 try:
     f = open(sys.argv[2], 'w')
 except IOError:
-    print >> sys.stderr, __file__, ":", e
+    print(__file__, ":", e, file=sys.stderr)
     f = stdout
 
 # Create a new csv writer object to use as the output formatter
@@ -31,8 +33,10 @@ out.writerow(['Tool:', net.getTool()])
 out.writerow(['Component Count:', len(net.components)])
 out.writerow(['Ref', 'Value', 'Footprint', 'Datasheet', 'Manufacturer', 'Vendor'])
 
+components = net.getInterestingComponents()
+
 # Output all of the component information (One component per row)
-for c in net.components:
+for c in components:
     out.writerow([c.getRef(), c.getValue(), c.getFootprint(), c.getDatasheet(),
         c.getField("Manufacturer"), c.getField("Vendor")])
 
