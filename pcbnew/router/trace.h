abf37168de084c19e15d28fdbd1d594e5126b0b8
tomasz.wlostowski@cern.ch <tomasz.wlostowski@cern.ch>
2013-09-18 19:55:16 +0200

    Initial version of the P&S router. Buggy and crappy.

---
+++ pcbnew/router/trace.h
@@ -0,0 +1,51 @@
+/*
+ * KiRouter - a push-and-(sometimes-)shove PCB router
+ *
+ * Copyright (C) 2013  CERN
+ * Author: Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
+ * 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.or/licenses/>.
+ */
+ 
+#ifndef __TRACE_H
+#define __TRACE_H
+
+
+#ifdef DEBUG
+
+#include <string>
+#include <iostream>
+#include <boost/format.hpp>
+
+static void _trace_print(const char *funcName, int level, const std::string& msg)
+{
+	std::cerr << "trace[" << level << "]: " << funcName << ": " << msg << std::endl;
+}
+
+
+
+#define TRACE(level, fmt, ...) \
+	_trace_print(__FUNCTION__, level, (boost::format(fmt) % __VA_ARGS__).str() );
+
+#define TRACEn(level, msg) \
+	_trace_print(__FUNCTION__, level, std::string(msg));
+
+#else
+
+#define TRACE(level, fmt, ...)
+#define TRACEn(level, msg)
+
+#endif
+
+#endif
