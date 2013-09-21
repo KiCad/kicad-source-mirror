5e53232f0cda2ff35f74af84c3ababe835fb7cb4
Build System <builder@kicadpcb.org>
2013-09-22 02:34:15 +0400

    Fixes typo in doxygen-python, and fixes unitest TestCase().discover on python < 2.7

--- pcbnew/scripting/qa/test.py
+++ pcbnew/scripting/qa/test.py
@@ -1,5 +1,10 @@
 import unittest
+import platform
 
+if platform.python_version() < '2.7':
+    unittest = __import__('unittest2')
+else:
+    import unittest
 
 if __name__ == '__main__':
     testsuite = unittest.TestLoader().discover('testcases',pattern="*.py")
