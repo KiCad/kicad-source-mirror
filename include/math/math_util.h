a6a1af9d753c84e3c566faec3bc4351321d02118
Maciej Sumiński <maciej.suminski@cern.ch>
2013-09-29 21:29:28 +0200

    Restored previous version of math_utils.h. WARNING: Mac OS build is probably broken now.

--- include/math/math_util.h
+++ include/math/math_util.h
@@ -29,7 +29,6 @@
 #include <cmath>
 #include <cstdlib>
 #include <stdint.h>
-#include <climits>
 
 /**
  * Function rescale()
@@ -37,9 +36,63 @@
  * Scales a number (value) by rational (numerator/denominator). Numerator must be <= denominator.
  */
 
-template<typename T> T rescale( T numerator, T value, T denominator )
+template<typename T> static T rescale( T numerator, T value, T denominator )
 {
     return numerator * value / denominator;
 }
+
+
+// explicit specializations for integer types, taking care of overflow.
+template<> int rescale( int numerator, int value, int denominator )
+{
+    return (int) ( (int64_t) numerator * (int64_t) value / (int64_t) denominator );
+}
+
+template<> int64_t rescale( int64_t numerator, int64_t value, int64_t denominator )
+{
+    int64_t r = 0;
+    int64_t sign = ( ( numerator < 0) ? -1 : 1 ) * ( denominator < 0 ? - 1: 1 ) * (value < 0 ? - 1 : 1);
+
+    int64_t a = std::abs( numerator );
+    int64_t b = std::abs( value );
+    int64_t c = std::abs( denominator );
+
+    r = c / 2;
+
+    if( b <= INT_MAX && c <= INT_MAX )
+    {
+        if( a <= INT_MAX )
+            return sign * ( (a * b + r ) / c );
+        else
+            return sign * (a / c * b + (a % c * b + r) / c);
+    } else {
+        uint64_t a0 = a & 0xFFFFFFFF;
+        uint64_t a1 = a >> 32;
+        uint64_t b0 = b & 0xFFFFFFFF;
+        uint64_t b1 = b >> 32;
+        uint64_t t1 = a0 * b1 + a1 * b0;
+        uint64_t t1a = t1 << 32;
+        int i;
+   
+        a0 = a0 * b0 + t1a;
+        a1 = a1 * b1 + (t1 >> 32) + (a0 < t1a);
+        a0 += r;
+        a1 += ((uint64_t)a0) < r;
+
+        for( i = 63; i >= 0; i-- )
+        {
+            a1  += a1 + ( (a0 >> i) & 1 );
+            t1  += t1;
+
+            if( (uint64_t)c <= a1 )
+            {
+                a1 -= c;
+                t1++;
+            }
+        }
+
+        return t1 * sign;
+    }
+};
 
 #endif // __MATH_UTIL_H
