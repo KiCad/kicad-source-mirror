d9174a110b0c99ce6ce8188dd7ac60b47dace7d0
Dick Hollenbeck <dick@softplc.com>
2013-12-09 12:48:42 -0600

    move ${Boost_LIBRARIES} into cvpcb, pcbnew, _pcbnew and out of github, because worker threads used regardless of github

--- include/ki_mutex.h
+++ include/ki_mutex.h
@@ -9,11 +9,9 @@
 /// Using an unnamed resource is easier, providing a textual name for a
 /// constructor is cumbersome, so we make choice on that criteria mostly:
 
-#if 0
+#if 1
 
 // This is a fine choice between the two, but requires linking to ${Boost_LIBRARIES}
-// which currently only happens when GITHUB_PLUGIN goes into pcbnew or _pcbnew.
-// cvpcb is left out.  Changing the 4 CMakeLists.txts fixes all this easily.
 
 #include <boost/interprocess/sync/interprocess_mutex.hpp>
 #include <boost/interprocess/sync/scoped_lock.hpp>
@@ -22,6 +20,8 @@ typedef boost::interprocess::interprocess_mutex     MUTEX;
 typedef boost::interprocess::scoped_lock<MUTEX>     MUTLOCK;
 
 #else
+
+// This choice also works.
 
 #include <wx/thread.h>
 
