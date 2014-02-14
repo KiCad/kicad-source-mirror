33c07934b698d9a9b8897193a3c166607c32fcef
Henner Zeller <h.zeller@acm.org>
2014-02-14 09:05:04 +0100

    PATCH: making component choosing (much!) more usable

---
+++ eeschema/dialogs/dialog_choose_component_base.h
@@ -0,0 +1,67 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Nov  6 2013)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef __DIALOG_CHOOSE_COMPONENT_BASE_H__
+#define __DIALOG_CHOOSE_COMPONENT_BASE_H__
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+class DIALOG_SHIM;
+
+#include "dialog_shim.h"
+#include <wx/string.h>
+#include <wx/stattext.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/textctrl.h>
+#include <wx/sizer.h>
+#include <wx/treectrl.h>
+#include <wx/combobox.h>
+#include <wx/button.h>
+#include <wx/dialog.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class DIALOG_CHOOSE_COMPONENT_BASE
+///////////////////////////////////////////////////////////////////////////////
+class DIALOG_CHOOSE_COMPONENT_BASE : public DIALOG_SHIM
+{
+	private:
+	
+	protected:
+		wxStaticText* m_searchLabel;
+		wxTextCtrl* m_searchBox;
+		wxTreeCtrl* m_libraryComponentTree;
+		wxStaticText* m_componentView;
+		wxTextCtrl* m_componentDetails;
+		wxComboBox* m_unitChoice;
+		wxStdDialogButtonSizer* m_button;
+		wxButton* m_buttonOK;
+		wxButton* m_buttonCancel;
+		
+		// Virtual event handlers, overide them in your derived class
+		virtual void OnInterceptSearchBoxKey( wxKeyEvent& event ) { event.Skip(); }
+		virtual void OnSearchBoxChange( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnSearchBoxEnter( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnTreeMouseUp( wxMouseEvent& event ) { event.Skip(); }
+		virtual void OnDoubleClickTreeSelect( wxTreeEvent& event ) { event.Skip(); }
+		virtual void OnTreeSelect( wxTreeEvent& event ) { event.Skip(); }
+		virtual void OnStartComponentBrowser( wxMouseEvent& event ) { event.Skip(); }
+		
+	
+	public:
+		
+		DIALOG_CHOOSE_COMPONENT_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 450,500 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+		~DIALOG_CHOOSE_COMPONENT_BASE();
+	
+};
+
+#endif //__DIALOG_CHOOSE_COMPONENT_BASE_H__
