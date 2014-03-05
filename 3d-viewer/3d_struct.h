f944c7926952a8f2c1e14cb19a67f39c9bbab017
jean-pierre charras <jp.charras@wanadoo.fr>
2014-03-05 18:40:23 +0100

    Minor code cleanup in 3d viewer (remove dead or useless code). Some minor other fixes.

--- 3d-viewer/3d_struct.h
+++ 3d-viewer/3d_struct.h
@@ -117,12 +117,7 @@ public:
     S3D_MASTER* Next() const { return (S3D_MASTER*) Pnext; }
     S3D_MASTER* Back() const { return (S3D_MASTER*) Pback; }
 
-    void Insert( S3D_MATERIAL* aMaterial )
-    {
-        aMaterial->SetNext( m_Materials );
-        m_Materials = aMaterial;
-    }
-
+    void Insert( S3D_MATERIAL* aMaterial );
 
     void Copy( S3D_MASTER* pattern );
     int  ReadData();
@@ -170,8 +165,6 @@ public:
 
     STRUCT_3D_SHAPE* Next() const { return (STRUCT_3D_SHAPE*) Pnext; }
     STRUCT_3D_SHAPE* Back() const { return (STRUCT_3D_SHAPE*) Pback; }
-
-    int ReadData( FILE* file, int* LineNum );
 
 #if defined(DEBUG)
     void Show( int nestLevel, std::ostream& os ) const { ShowDummy( os ); } // override
