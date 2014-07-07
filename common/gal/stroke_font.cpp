0304598a71316654145d62712988e1d8d95f6ee4
Maciej Suminski <maciej.suminski@cern.ch>
2014-07-07 13:09:44 +0200

    Fixed vertical alignment for strings that contain a newline character at the end (GAL).

--- common/gal/stroke_font.cpp
+++ common/gal/stroke_font.cpp
@@ -40,7 +40,8 @@ STROKE_FONT::STROKE_FONT( GAL* aGal ) :
     m_gal( aGal ),
     m_bold( false ),
     m_italic( false ),
-    m_mirrored( false )
+    m_mirrored( false ),
+    m_overbar( false )
 {
     // Default values
     m_glyphSize = VECTOR2D( 10.0, 10.0 );
@@ -149,6 +150,9 @@ BOX2D STROKE_FONT::computeBoundingBox( const GLYPH& aGLYPH, const VECTOR2D& aGLY
 
 void STROKE_FONT::Draw( const UTF8& aText, const VECTOR2D& aPosition, double aRotationAngle )
 {
+    if( aText.empty() )
+        return;
+
     // Context needs to be saved before any transformations
     m_gal->Save();
 
