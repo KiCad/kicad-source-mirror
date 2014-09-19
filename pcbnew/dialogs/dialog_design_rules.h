12f3fd35def2547e4f1e42c343a46babefea1523
Wayne Stambaugh <stambaughw@verizon.net>
2014-09-19 19:58:32 -0400

    Fix Pcbnew design rules dialog wxGrid tab traversal bug. (fixes lp:1060028)
    * Set wxGrid tab behavior to leave grid when using the tab key.
    * Lots of coding policy fixes.

--- pcbnew/dialogs/dialog_design_rules.h
+++ pcbnew/dialogs/dialog_design_rules.h
@@ -1,6 +1,32 @@
-///////////////////////////////////////////////////////////////////////////////
-/// Class DIALOG_DESIGN_RULES
-///////////////////////////////////////////////////////////////////////////////
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2004-2009 Jean-Pierre Charras, jean-pierre.charras@gpisa-lab.inpg.fr
+ * Copyright (C) 2009 Dick Hollenbeck, dick@softplc.com
+ * Copyright (C) 2009 KiCad Developers, see change_log.txt for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you may find one here:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * or you may search the http://www.gnu.org website for the version 2 license,
+ * or you may write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+
+/**
+ * @file dialog_design_rules.h
+ */
 
 #ifndef __dialog_design_rules_h_
 #define __dialog_design_rules_h_
@@ -36,7 +62,7 @@ class DIALOG_DESIGN_RULES : public DIALOG_DESIGN_RULES_BASE
 
 private:
 
-    static const wxString wildCard;     ///< the name of a ficticious netclass which includes all NETs
+    static const wxString wildCard;     ///< the name of a fictitious netclass which includes all NETs
 
     PCB_EDIT_FRAME*         m_Parent;
     BOARD*                  m_Pcb;
@@ -63,24 +89,81 @@ private:
     void OnOkButtonClick( wxCommandEvent& event );
     void OnAddNetclassClick( wxCommandEvent& event );
     void OnRemoveNetclassClick( wxCommandEvent& event );
+
+    /*
+     * Called on "Move Up" button click
+     * the selected(s) rules are moved up
+     * The default netclass is always the first rule
+     */
     void OnMoveUpSelectedNetClass( wxCommandEvent& event );
+
+    /*
+     * Called on the left Choice Box selection
+     */
     void OnLeftCBSelection( wxCommandEvent& event );
+
+    /*
+     * Called on the Right Choice Box selection
+     */
     void OnRightCBSelection( wxCommandEvent& event );
+
     void OnRightToLeftCopyButton( wxCommandEvent& event );
     void OnLeftToRightCopyButton( wxCommandEvent& event );
+
+    /*
+     * Called on clicking the left "select all" button:
+     * select all items of the left netname list list box
+     */
     void OnLeftSelectAllButton( wxCommandEvent& event );
+
+    /*
+     * Called on clicking the right "select all" button:
+     * select all items of the right netname list list box
+     */
     void OnRightSelectAllButton( wxCommandEvent& event );
+
+    /*
+     * Function TestDataValidity
+     *
+     * Performs a control of data validity
+     * set the background of a bad cell in RED and display an info message
+     * @return true if Ok, false if error
+     */
     bool TestDataValidity( );
+
     void InitDialogRules();
     void InitGlobalRules();
+
+    /**
+     * Function InitRulesList
+     * Fill the grid showing current rules with values
+     */
     void InitRulesList();
+
+    /* Populates the lists of sizes (Tracks width list and Vias diameters & drill list) */
     void InitDimensionsLists();
+
     void InitializeRulesSelectionBoxes();
+
+    /* Copy the rules list from grid to board
+     */
     void CopyRulesListToBoard();
+
     void CopyGlobalRulesToBoard();
     void CopyDimensionsListsToBoard( );
     void SetRoutableLayerStatus();
+
+    /**
+     * Function FillListBoxWithNetNames
+     * populates aListCtrl with net names and class names from m_AllNets in a two column display.
+     */
     void FillListBoxWithNetNames( NETS_LIST_CTRL* aListCtrl, const wxString& aNetClass );
+
+    /**
+     * Function PrintCurrentSettings
+     * Display on m_MessagesList the current global settings:
+     * minimal values for tracks, vias, clearance ...
+     */
     void PrintCurrentSettings( );
 
     /**
