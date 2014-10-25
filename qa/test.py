5034f07a31e9b41a4733fb02ec757f290055ae3a
Nick Østergaard <oe.nick@gmail.com>
2014-10-25 22:26:41 +0100

    * Update the python qa test suite to exit with an error code if any errors fail

--- qa/test.py
+++ qa/test.py
@@ -1,5 +1,6 @@
 import unittest
 import platform
+import sys
 
 if platform.python_version() < '2.7':
     unittest = __import__('unittest2')
@@ -8,6 +9,10 @@ else:
 
 if __name__ == '__main__':
     testsuite = unittest.TestLoader().discover('testcases',pattern="*.py")
-    unittest.TextTestRunner(verbosity=100).run(testsuite)
+    results = unittest.TextTestRunner(verbosity=100).run(testsuite)
+
+    # Return an error code if any of the testsuite tests fail
+    if len(results.errors) > 0:
+        sys.exit(1)
 
 
