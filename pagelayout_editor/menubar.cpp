a285aead3f6bbd04f5b750c28bad141cfed8caf7
Simon Wells <simonrwells@gmail.com>
2015-09-26 05:58:50 +1200

    Fixed order of icons in help menu for gerbview and pagelayout-editor

--- pagelayout_editor/menubar.cpp
+++ pagelayout_editor/menubar.cpp
@@ -150,15 +150,15 @@ void PL_EDITOR_FRAME::ReCreateMenuBar()
                  wxID_HELP,
                  _( "Page Layout Editor &Manual" ),
                  _( "Open the Page Layout Editor Manual" ),
-                 KiBitmap( help_xpm ) );
+                 KiBitmap( online_help_xpm ) );
 
     AddMenuItem( helpMenu,
                  wxID_INDEX,
                  _( "&Getting Started in KiCad" ),
                  _( "Open \"Getting Started in KiCad\" guide for beginners" ),
-                 KiBitmap( online_help_xpm ) );
+                 KiBitmap( help_xpm ) );
 
-    // About GerbView
+    // About Kicad
     AddMenuItem( helpMenu,
                  wxID_ABOUT,
                  _( "&About Kicad" ),
