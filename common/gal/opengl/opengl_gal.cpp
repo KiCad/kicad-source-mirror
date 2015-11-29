c1b46e6ed8cd4e86fe0ebd59a6170925f3823bca
unknown <mark.roszko@gmail.com>
2015-11-29 07:50:25 +0100

    Fix bug Bug #1519590 (gal cannot start on surface book)

--- common/gal/opengl/opengl_gal.cpp
+++ common/gal/opengl/opengl_gal.cpp
@@ -962,7 +962,8 @@ unsigned int OPENGL_GAL::getNewGroupNumber()
 
 bool OPENGL_GAL::runTest()
 {
-    wxDialog dlgtest( GetParent(), -1, wxT( "opengl test" ), wxPoint( 50, 50 ), wxSize( 50, 50 ) );
+    wxDialog dlgtest( GetParent(), -1, wxT( "opengl test" ), wxPoint( 50, 50 ),
+                      wxDLG_UNIT( GetParent(), wxSize( 50, 50 ) ) );
     OPENGL_TEST* test = new OPENGL_TEST( &dlgtest, this );
 
     dlgtest.Raise();         // on Linux, on some windows managers (Unity for instance) this is needed to actually show the dialog
