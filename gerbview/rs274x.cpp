e5a5f9552bd4e79593a61bbcdbbba574922faa58
Chris Pavlina <pavlina.chris@gmail.com>
2016-08-08 13:21:27 -0400

    Fix RS274X Unicode decoding

--- gerbview/rs274x.cpp
+++ gerbview/rs274x.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2007-2016 Jean-Pierre Charras  jp.charras at wanadoo.fr
- * Copyright (C) 1992-2016 KiCad Developers, see AUTHOR.txt for contributors.
+ * Copyright (C) 1992-2016 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -176,7 +176,7 @@ static const wxString fromGerberString( const wxString& aGbrString )
                     digit -= 'a';
                 else digit = 0;
 
-                value += digit && 0xFF;
+                value += digit & 0xFF;
             }
 
             text.Append( wxUniChar( value ) );
