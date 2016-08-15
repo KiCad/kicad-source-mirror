6e4d6c445c154283448df9b04d7b5ca0af435892
Maciej Suminski <maciej.suminski@cern.ch>
2016-08-15 14:39:26 +0200

    Add NGSPICE_ROOT_DIR parameter for libngspice CMake script

--- CMakeModules/Findngspice.cmake
+++ CMakeModules/Findngspice.cmake
@@ -3,12 +3,13 @@
 # Author: Maciej Suminski <maciej.suminski@cern.ch>
 
 find_path( NGSPICE_INCLUDE_DIR ngspice/sharedspice.h
-    PATHS ${NGSPICE_INCLUDE_PATH}
-    PATH_SUFFIXES share/ngspice/include
+    PATHS ${NGSPICE_ROOT_DIR} $ENV{NGSPICE_ROOT_DIR} ${NGSPICE_INCLUDE_PATH}
+    PATH_SUFFIXES src/include share/ngspice/include
 )
 
 find_library( NGSPICE_LIBRARY ngspice
-    PATHS ${NGSPICE_LIBRARY_PATH}
+    PATHS ${NGSPICE_ROOT_DIR} $ENV{NGSPICE_ROOT_DIR} ${NGSPICE_LIBRARY_PATH}
+    PATH_SUFFIXES src/.libs
 )
 
 include( FindPackageHandleStandardArgs )
