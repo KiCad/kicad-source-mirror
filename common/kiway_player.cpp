91c8201b8b6daf3cc75d77282c2c4b1739dd6f5a
Chris Pavlina <pavlina.chris@gmail.com>
2016-09-30 01:34:25 -0400

    Dismiss warning for inconsistent exception spec in ~KIWAY_PLAYER()

--- common/kiway_player.cpp
+++ common/kiway_player.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2014 SoftPLC Corporation, Dick Hollenbeck <dick@softplc.com>
- * Copyright (C) 2014-2015 KiCad Developers, see CHANGELOG.TXT for contributors.
+ * Copyright (C) 2014-2016 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -66,7 +66,7 @@ KIWAY_PLAYER::KIWAY_PLAYER( wxWindow* aParent, wxWindowID aId, const wxString& a
 }
 
 
-KIWAY_PLAYER::~KIWAY_PLAYER(){}
+KIWAY_PLAYER::~KIWAY_PLAYER() throw() {}
 
 
 void KIWAY_PLAYER::KiwayMailIn( KIWAY_EXPRESS& aEvent )
