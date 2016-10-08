f14dc8f2be9289c68ad8ca35bb0bf65d4897c2a9
Wayne Stambaugh <stambaughw@gmail.com>
2016-10-07 21:10:51 -0400

    Pcbnew: fix epic footprint editor DXF import fail.
    The unfortunate combination of using static_cast to promote board items
    to module items then using the assignment operator without any thought as
    to what would happen in MODULE::Add() caused the board objects to not be
    added to the module.  This is expected because board items cannot be in
    modules.  Did this ever work or did someone have a colossal brain cramp?
    If it did work, who ever changed it did not test it because in debug
    builds, you would have gotten and assertion on every object imported.  On
    release builds nothing is imported silently.
    
    Add module object types to the DXF importer and code to choose which type
    of object to import.
    
    Remove offending static casts and assignment operator and pass the correct
    object directly to the MODULE::Add() function when importing DXF in the
    footprint editor.
    
    The usual coding policy fixes.

--- pcbnew/import_dxf/dialog_dxf_import.h
+++ pcbnew/import_dxf/dialog_dxf_import.h
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2013 Jean-Pierre Charras, jp.charras at wanadoo.fr
- * Copyright (C) 1992-2013 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 1992-2016 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -29,7 +29,7 @@
 class DIALOG_DXF_IMPORT : public DIALOG_DXF_IMPORT_BASE
 {
 public:
-    DIALOG_DXF_IMPORT( PCB_BASE_FRAME* aParent );
+    DIALOG_DXF_IMPORT( PCB_BASE_FRAME* aParent, bool aUseModuleItems = false );
     ~DIALOG_DXF_IMPORT();
 
     /**
