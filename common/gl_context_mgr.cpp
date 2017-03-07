KiCad/kicad-source-mirror

commit 11f4622ea557051c0824222bfd76c403311626ad

Fix GL assertion failure during GAL teardown

common/gl_context_mgr.cpp
@@ -2,6 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2016 CERN
+ * Copyright (C) 2017 KiCad Developers, see AUTHORS.txt for contributors.
  * @author Maciej Suminski <maciej.suminski@cern.ch>
  *
  * This program is free software; you can redistribute it and/or
@@ -78,7 +79,12 @@ void GL_CONTEXT_MANAGER::LockCtx( wxGLContext* aContext, wxGLCanvas* aCanvas )
     m_glCtxMutex.lock();
     wxGLCanvas* canvas = aCanvas ? aCanvas : m_glContexts.at( aContext );
 
-    canvas->SetCurrent( *aContext );
+    // Prevent assertion failure in wxGLContext::SetCurrent during GAL teardown
+    if( canvas->GetXWindow() )
+    {
+        canvas->SetCurrent( *aContext );
+    }
+
     m_glCtx = aContext;
 }
 

commit cf373e543f16f5f1b92f865381fcb53ce33a40bb

Fix Windows (and macOS?) build error caused by 11f4622ea

common/gl_context_mgr.cpp
@@ -80,7 +80,9 @@ void GL_CONTEXT_MANAGER::LockCtx( wxGLContext* aContext, wxGLCanvas* aCanvas )
     wxGLCanvas* canvas = aCanvas ? aCanvas : m_glContexts.at( aContext );
 
     // Prevent assertion failure in wxGLContext::SetCurrent during GAL teardown
+#ifdef __WXGTK__
     if( canvas->GetXWindow() )
+#endif
     {
         canvas->SetCurrent( *aContext );
     }