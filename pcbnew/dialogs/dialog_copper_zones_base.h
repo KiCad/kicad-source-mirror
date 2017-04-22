1754b42ba3d6568ac886585d613bda5ec72519bb
jean-pierre charras <jp.charras@wanadoo.fr>
2017-04-22 10:17:59 +0200

    Dialog copper zones: fix a tooltip error

--- pcbnew/dialogs/dialog_copper_zones_base.h
+++ pcbnew/dialogs/dialog_copper_zones_base.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Jun  5 2014)
+// C++ code generated with wxFormBuilder (version May  6 2016)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO "NOT" EDIT THIS FILE!
