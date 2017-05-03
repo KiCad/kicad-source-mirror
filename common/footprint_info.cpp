6a9f38ecc4ccc234f93ed5be090a0e578965d289
Wayne Stambaugh <stambaughw@gmail.com>
2017-05-03 15:27:07 -0400

    Pcbnew: continue loading footprints after first failure.
    The footprint library cache handler would throw an exception on the
    first error it encountered preventing all subsequent footprint files
    from being parsed even if there was nothing wrong with them.  This
    commit queues errors until all of the footprint files in the folder
    have been parsed.
    
    Fixes lp:1416736
    
    https://bugs.launchpad.net/kicad/+bug/1416736

--- common/footprint_info.cpp
+++ common/footprint_info.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2011 Jean-Pierre Charras, <jp.charras@wanadoo.fr>
  * Copyright (C) 2013-2016 SoftPLC Corporation, Dick Hollenbeck <dick@softplc.com>
- * Copyright (C) 1992-2016 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 1992-2017 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -309,6 +309,8 @@ void FOOTPRINT_LIST::DisplayErrors( wxTopLevelWindow* aWindow )
         msg += wxT( "<p>" ) + m_errors[i].errorText + wxT( "</p>" );
     }
 
+    // Preserve new lines in error messages so queued errors don't run together.
+    msg.Replace( "\n", "<BR>" );
     dlg.AddHTML_Text( msg );
 
     dlg.ShowModal();
