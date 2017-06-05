79823d573aa92fe2f7f415bb93103d8d01704e01
jean-pierre charras <jp.charras@wanadoo.fr>
2017-06-05 18:14:37 +0200

    fix incorrect comment

--- pcbnew/menubar_pcbframe.cpp
+++ pcbnew/menubar_pcbframe.cpp
@@ -1,7 +1,7 @@
 /*
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
- * Copyright (C) 2012 Jean-Pierre Charras, jean-pierre.charras@ujf-grenoble.fr
+ * Copyright (C) 2017 Jean-Pierre Charras, jp.charras at wanadoo.fr
  * Copyright (C) 2012 SoftPLC Corporation, Dick Hollenbeck <dick@softplc.com>
  * Copyright (C) 2012-2016 Wayne Stambaugh <stambaughw@verizon.net>
  * Copyright (C) 1992-2017 KiCad Developers, see AUTHORS.txt for contributors.
@@ -389,7 +389,7 @@ void prepareToolsMenu( wxMenu* aParentMenu )
 
     AddMenuItem( aParentMenu, ID_TOOLBARH_PCB_FREEROUTE_ACCESS,
                  _( "&FreeRoute" ),
-                 _( "Fast access to web-based FreeROUTE advanced router" ),
+                 _( "Fast access to the FreeROUTE external advanced router" ),
                  KiBitmap( web_support_xpm ) );
 
 #if defined(KICAD_SCRIPTING_WXPYTHON)
