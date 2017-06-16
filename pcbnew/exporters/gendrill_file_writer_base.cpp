f324c7b16d5795f8498c9abb210ef2fc9bcf37ae
Simon Richter <Simon.Richter@hogyros.de>
2017-06-16 14:15:28 +0200

    Switch another loop to D_PAD iterator

--- pcbnew/exporters/gendrill_file_writer_base.cpp
+++ pcbnew/exporters/gendrill_file_writer_base.cpp
@@ -117,7 +117,7 @@ void GENDRILL_WRITER_BASE::buildHolesList( DRILL_LAYER_PAIR aLayerPair,
         // add holes for thru hole pads
         for( MODULE* module = m_pcb->m_Modules;  module;  module = module->Next() )
         {
-            for( D_PAD* pad = module->Pads();  pad;  pad = pad->Next() )
+            for( auto &pad : module->Pads() )
             {
                 if( !m_merge_PTH_NPTH )
                 {
