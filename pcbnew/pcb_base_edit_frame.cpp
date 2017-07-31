67ae5935d693cfde1c4c2d661f3a9f3a68187ba1
Maciej Suminski <maciej.suminski@cern.ch>
2017-07-31 15:07:55 +0200

    Perform tool reset twice when reloading a board
    The first one is to remove any references to the removed items, the
    second one is to setup the new model.
    
    Fixes: lp:1692257
    * https://bugs.launchpad.net/kicad/+bug/1692257

--- pcbnew/pcb_base_edit_frame.cpp
+++ pcbnew/pcb_base_edit_frame.cpp
@@ -55,13 +55,13 @@ void PCB_BASE_EDIT_FRAME::SetBoard( BOARD* aBoard )
 {
     bool new_board = ( aBoard != m_Pcb );
 
-    // The active tool might store a reference to the BOARD that is about to be deleted.
-    if( m_toolManager )
-        m_toolManager->DeactivateTool();
-
-    // It has to be done before the previous board is destroyed by SetBoard()
     if( new_board )
+    {
+        if( m_toolManager )
+            m_toolManager->ResetTools( TOOL_BASE::MODEL_RELOAD );
+
         GetGalCanvas()->GetView()->Clear();
+    }
 
     PCB_BASE_FRAME::SetBoard( aBoard );
 
