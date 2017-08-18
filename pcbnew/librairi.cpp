d25fdfdb8936cfb536814ea4f346f0a69d3455af
Chris Pavlina <pavlina.chris@gmail.com>
2017-08-17 22:48:05 -0600

    Fix wx assertion for excess "|" in wildcard list

--- pcbnew/librairi.cpp
+++ pcbnew/librairi.cpp
@@ -1,7 +1,7 @@
 /*
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
- * Copyright (C) 1992-2016 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 1992-2017 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -109,7 +109,7 @@ static wxFileName getFootprintFilenameFromUser( wxWindow* aParent, const wxStrin
 
     wildCard << wxGetTranslation( KiCadFootprintLibFileWildcard ) << wxChar( '|' )
              << wxGetTranslation( ModLegacyExportFileWildcard ) << wxChar( '|' )
-             << wxGetTranslation( GedaPcbFootprintLibFileWildcard ) << wxChar( '|' );
+             << wxGetTranslation( GedaPcbFootprintLibFileWildcard );
 
     wxFileDialog dlg( aParent, FMT_IMPORT_MODULE, aLastPath, wxEmptyString, wildCard,
             wxFD_OPEN | wxFD_FILE_MUST_EXIST );
