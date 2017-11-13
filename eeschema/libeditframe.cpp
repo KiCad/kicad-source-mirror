7d45d8b23c0ce0b8d3462b690bcbf3138efe96f8
Maciej Suminski <maciej.suminski@cern.ch>
2017-11-13 23:30:07 +0100

    Library Editor: made the component tree pane resizable

--- eeschema/libeditframe.cpp
+++ eeschema/libeditframe.cpp
@@ -305,8 +305,8 @@ LIB_EDIT_FRAME::LIB_EDIT_FRAME( KIWAY* aKiway, wxWindow* aParent ) :
     m_auimgr.AddPane( m_messagePanel,
                       wxAuiPaneInfo( mesg ).Name( "MsgPanel" ).Bottom().Layer( 10 ) );
 
-    m_auimgr.AddPane( m_treePane,
-                      wxAuiPaneInfo( vert ).Name( "ComponentTree" ).Left().Row( 1 ).Resizable().MinSize( 300, 400 ) );
+    m_auimgr.AddPane( m_treePane, wxAuiPaneInfo().Name( "ComponentTree" ).Left().Row( 1 )
+            .Resizable().MinSize( 250, 400 ).Dock().CloseButton( false ) );
 
     m_auimgr.Update();
 
