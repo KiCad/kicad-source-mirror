dc9d17100bfd7c36a78c3bed418422cd29a61df5
Jeff Young <jeff@rokeby.ie>
2017-12-12 17:16:24 +0000

    Fix update of brightened objects on OSX.
    The OSX implementation needs to be poked to update the
    brightening / unbrightening during running of the
    disambiguation menu.  (FWIW, now that I've finally seen
    it in action, it's a REALLY cool feature.)
    
    Fixes: lp:1715440
    * https://bugs.launchpad.net/kicad/+bug/1715440

--- common/tool/tool_manager.cpp
+++ common/tool/tool_manager.cpp
@@ -739,8 +739,11 @@ bool TOOL_MANAGER::ProcessEvent( const TOOL_EVENT& aEvent )
     {
         auto f = dynamic_cast<EDA_DRAW_FRAME*>( GetEditFrame() );
 
-	if( f )
-	    f->GetGalCanvas()->Refresh();    // fixme: ugly hack, provide a method in TOOL_DISPATCHER.
+    if( f )
+        f->GetGalCanvas()->Refresh();    // fixme: ugly hack, provide a method in TOOL_DISPATCHER.
+#ifdef __WXMAC__
+        wxTheApp->ProcessPendingEvents(); // required for updating brightening behind a popup menu
+#endif
     }
 
     return hotkey_handled;
