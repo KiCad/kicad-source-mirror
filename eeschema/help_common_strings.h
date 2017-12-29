70f516bde43a5ec74b40a592d128bffd417a7e27
Wayne Stambaugh <stambaughw@gmail.com>
2017-12-29 08:46:24 -0500

    Eeschema: changed missed instances of component in UI strings.
    Fixes lp:1740440
    
    https://bugs.launchpad.net/kicad/+bug/1740440

--- eeschema/help_common_strings.h
+++ eeschema/help_common_strings.h
@@ -48,22 +48,22 @@
 #define HELP_DELETE_ITEMS         _( "Delete item" )
 
 // Schematic editor:
-#define HELP_FIND _( "Find components and text" )
+#define HELP_FIND _( "Find symbols and text" )
 #define HELP_REPLACE _( "Find and replace text in schematic items" )
-#define HELP_PLACE_COMPONENTS     _( "Place component" )
+#define HELP_PLACE_COMPONENTS     _( "Place symbol" )
 #define HELP_PLACE_POWERPORT      _( "Place power port" )
 #define HELP_PLACE_WIRE           _( "Place wire" )
 #define HELP_PLACE_BUS            _( "Place bus" )
 #define HELP_PLACE_WIRE2BUS_ENTRY _( "Place wire to bus entry" )
 #define HELP_PLACE_BUS2BUS_ENTRY  _( "Place bus to bus entry" )
-#define HELP_PLACE_NC_FLAG        _( "Place not-connected flag" )
+#define HELP_PLACE_NC_FLAG        _( "Place no connection flag" )
 
-#define HELP_PLACE_NETLABEL _( "Place net name - local label" )
+#define HELP_PLACE_NETLABEL _( "Place net label" )
 #define HELP_PLACE_GLOBALLABEL \
     _(\
         "Place global label.\nWarning: inside global hierarchy , all global labels with same name are connected" )
 #define HELP_PLACE_HIER_LABEL \
-    _( "Place a hierarchical label. Label will be seen as a hierarchical pin in the sheet symbol" )
+    _( "Place a hierarchical label. Label will be seen as a hierarchical pin in the sheet" )
 
 #define HELP_PLACE_JUNCTION     _( "Place junction" )
 #define HELP_PLACE_SHEET        _( "Create hierarchical sheet" )
@@ -73,18 +73,18 @@
 #define HELP_PLACE_GRAPHICLINES _( "Place graphic lines or polygons" )
 #define HELP_PLACE_GRAPHICTEXTS _( "Place text" )
 
-#define HELP_ANNOTATE _( "Annotate schematic components" )
-#define HELP_RUN_LIB_EDITOR _( "Library Editor - Create/edit components" )
-#define HELP_RUN_LIB_VIEWER _( "Library Browser - Browse components" )
+#define HELP_ANNOTATE _( "Annotate schematic symbols" )
+#define HELP_RUN_LIB_EDITOR _( "Create, delete, and edit symbols" )
+#define HELP_RUN_LIB_VIEWER _( "Browse symbol libraries" )
 #define HELP_GENERATE_BOM _( "Generate bill of materials" )
 #define HELP_IMPORT_FOOTPRINTS \
-    _( "Back-import component footprint association fields from the .cmp back import file created by Pcbnew" )
+    _( "Back-import symbol footprint association fields from the .cmp back import file created by Pcbnew" )
 
 // Component editor:
-#define HELP_ADD_PIN _( "Add pins to component" )
-#define HELP_ADD_BODYTEXT _( "Add text to component body" )
-#define HELP_ADD_BODYRECT _( "Add graphic rectangle to component body" )
-#define HELP_ADD_BODYCIRCLE _( "Add circles to component body" )
-#define HELP_ADD_BODYARC _( "Add arcs to component body" )
-#define HELP_ADD_BODYPOLYGON _( "Add lines and polygons to component body" )
+#define HELP_ADD_PIN _( "Add pins to symbol" )
+#define HELP_ADD_BODYTEXT _( "Add text to symbol body" )
+#define HELP_ADD_BODYRECT _( "Add graphic rectangle to symbol body" )
+#define HELP_ADD_BODYCIRCLE _( "Add circles to symbol body" )
+#define HELP_ADD_BODYARC _( "Add arcs to symbol body" )
+#define HELP_ADD_BODYPOLYGON _( "Add lines and polygons to symbol body" )
 #define HELP_PLACE_GRAPHICIMAGES _("Add bitmap image")
