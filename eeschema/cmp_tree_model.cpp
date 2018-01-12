41f5c754e50576cf40d59a71b65e4a10fc5259d5
Wayne Stambaugh <stambaughw@gmail.com>
2018-01-12 11:59:26 -0500

    Eeschema: fix minor component tree model bug.
    The component tree model was always saving the root symbol name which
    prevented the actual alias from be selected in the symbol chooser
    dialog.  This also prevented the correct documentation information
    from being shown in the information panel.
    
    Fixes lp:1740742
    
    https://bugs.launchpad.net/kicad/+bug/1740742

--- eeschema/cmp_tree_model.cpp
+++ eeschema/cmp_tree_model.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2017 Chris Pavlina <pavlina.chris@gmail.com>
  * Copyright (C) 2014 Henner Zeller <h.zeller@acm.org>
- * Copyright (C) 2014-2017 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2014-2018 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -168,6 +168,7 @@ void CMP_TREE_NODE_LIB_ID::Update( LIB_ALIAS* aAlias )
     if( part )
     {
         LibId = part->GetLibId();
+        LibId.SetLibItemName( Name );
         footprint = part->GetFootprintField().GetText();
     }
 
