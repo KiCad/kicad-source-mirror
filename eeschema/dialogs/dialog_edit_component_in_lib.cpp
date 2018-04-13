a41b2d6803ee5969776025ec219f00d9b7ef98c6
Maciej Suminski <maciej.suminski@cern.ch>
2018-04-13 14:24:11 +0200

    Fix names of added aliases

--- eeschema/dialogs/dialog_edit_component_in_lib.cpp
+++ eeschema/dialogs/dialog_edit_component_in_lib.cpp
@@ -332,10 +332,9 @@ void DIALOG_EDIT_COMPONENT_IN_LIBRARY::EditAliasOfPart( wxCommandEvent& aEvent )
     if( dlg.ShowModal() != wxID_OK )
         return; // cancelled by user
 
-    aliasname = dlg.GetValue( );
-    aliasname.Replace( wxT( " " ), wxT( "_" ) );
+    aliasname = LIB_ID::FixIllegalChars( dlg.GetValue(), LIB_ID::ID_SCH );
 
-    if( checkNewAlias( aliasname) )
+    if( checkNewAlias( aliasname ) )
         m_PartAliasListCtrl->SetString( sel, aliasname );
 }
 
@@ -349,8 +348,7 @@ void DIALOG_EDIT_COMPONENT_IN_LIBRARY::AddAliasOfPart( wxCommandEvent& event )
     if( dlg.ShowModal() != wxID_OK )
         return; // cancelled by user
 
-    aliasname = dlg.GetValue( );
-    aliasname.Replace( wxT( " " ), wxT( "_" ) );
+    aliasname = LIB_ID::FixIllegalChars( dlg.GetValue(), LIB_ID::ID_SCH );
 
     if( checkNewAlias( aliasname ) )
         m_PartAliasListCtrl->Append( aliasname );
