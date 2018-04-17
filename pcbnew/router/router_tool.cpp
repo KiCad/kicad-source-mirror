45d1c19eb7ecf677b0f8db90f58ddd0469281e04
Seth Hillbrand <hillbrand@ucdavis.edu>
2018-04-17 08:59:52 -0700

    Add tooltip text to drag options.

--- pcbnew/router/router_tool.cpp
+++ pcbnew/router/router_tool.cpp
@@ -125,13 +125,13 @@ TOOL_ACTION PCB_ACTIONS::breakTrack( "pcbnew.InteractiveRouter.BreakTrack",
 TOOL_ACTION PCB_ACTIONS::drag45Degree( "pcbnew.InteractiveRouter.Drag45Degree",
         AS_GLOBAL, TOOL_ACTION::LegacyHotKey( HK_DRAG_TRACK_KEEP_SLOPE ),
         _( "Drag (45 degree mode)" ),
-        _( "todo" ),
+        _( "Drags the track segment while keeping connected tracks at 45 degrees." ),
         drag_segment_withslope_xpm );
 
 TOOL_ACTION PCB_ACTIONS::dragFreeAngle( "pcbnew.InteractiveRouter.DragFreeAngle",
         AS_GLOBAL, TOOL_ACTION::LegacyHotKey( HK_DRAG_ITEM ),
         _( "Drag (free angle)" ),
-        _( "todo" ),
+        _( "Drags the nearest joint in the track without restricting the track angle." ),
         move_xpm );
 
 static const TOOL_ACTION ACT_NewTrack( "pcbnew.InteractiveRouter.NewTrack", AS_CONTEXT,
