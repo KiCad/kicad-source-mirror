e6bb917ebfeb91ba84c9914bb389109401ec6389
Seth Hillbrand <hillbrand@ucdavis.edu>
2018-04-23 16:03:43 -0700

    Standardize colon in regulator data dialog

--- pcb_calculator/dialogs/dialog_regulator_data_base.cpp
+++ pcb_calculator/dialogs/dialog_regulator_data_base.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Jan  1 2016)
+// C++ code generated with wxFormBuilder (version Apr 19 2018)
 // http://www.wxformbuilder.org/
 //
-// PLEASE DO "NOT" EDIT THIS FILE!
+// PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
 #include "dialog_regulator_data_base.h"
@@ -23,9 +23,9 @@ DIALOG_EDITOR_DATA_BASE::DIALOG_EDITOR_DATA_BASE( wxWindow* parent, wxWindowID i
 	fgSizerPrms->SetFlexibleDirection( wxHORIZONTAL );
 	fgSizerPrms->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
 	
-	m_staticTextName = new wxStaticText( this, wxID_ANY, _("Name"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticTextName = new wxStaticText( this, wxID_ANY, _("Name:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticTextName->Wrap( -1 );
-	fgSizerPrms->Add( m_staticTextName, 0, wxALL, 5 );
+	fgSizerPrms->Add( m_staticTextName, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
 	
 	m_textCtrlName = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	fgSizerPrms->Add( m_textCtrlName, 0, wxALL|wxEXPAND, 5 );
@@ -33,20 +33,20 @@ DIALOG_EDITOR_DATA_BASE::DIALOG_EDITOR_DATA_BASE( wxWindow* parent, wxWindowID i
 	
 	fgSizerPrms->Add( 0, 0, 1, wxEXPAND, 5 );
 	
-	m_staticTextVref = new wxStaticText( this, wxID_ANY, _("Vref"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticTextVref = new wxStaticText( this, wxID_ANY, _("Vref:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticTextVref->Wrap( -1 );
-	fgSizerPrms->Add( m_staticTextVref, 0, wxALL, 5 );
+	fgSizerPrms->Add( m_staticTextVref, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
 	
 	m_textCtrlVref = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	fgSizerPrms->Add( m_textCtrlVref, 0, wxALL|wxEXPAND, 5 );
 	
 	m_staticTextVrefUnit = new wxStaticText( this, wxID_ANY, _("Volt"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticTextVrefUnit->Wrap( -1 );
-	fgSizerPrms->Add( m_staticTextVrefUnit, 0, wxALL, 5 );
+	fgSizerPrms->Add( m_staticTextVrefUnit, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
 	
-	m_staticTextType = new wxStaticText( this, wxID_ANY, _("Type"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticTextType = new wxStaticText( this, wxID_ANY, _("Type:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticTextType->Wrap( -1 );
-	fgSizerPrms->Add( m_staticTextType, 0, wxALL, 5 );
+	fgSizerPrms->Add( m_staticTextType, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
 	
 	wxString m_choiceRegTypeChoices[] = { _("Separate sense pin"), _("3 terminals regulator") };
 	int m_choiceRegTypeNChoices = sizeof( m_choiceRegTypeChoices ) / sizeof( wxString );
@@ -57,16 +57,16 @@ DIALOG_EDITOR_DATA_BASE::DIALOG_EDITOR_DATA_BASE( wxWindow* parent, wxWindowID i
 	
 	fgSizerPrms->Add( 0, 0, 1, wxEXPAND, 5 );
 	
-	m_RegulIadjTitle = new wxStaticText( this, wxID_ANY, _("Iadj"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_RegulIadjTitle = new wxStaticText( this, wxID_ANY, _("Iadj:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_RegulIadjTitle->Wrap( -1 );
-	fgSizerPrms->Add( m_RegulIadjTitle, 0, wxALL, 5 );
+	fgSizerPrms->Add( m_RegulIadjTitle, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
 	
 	m_RegulIadjValue = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	fgSizerPrms->Add( m_RegulIadjValue, 0, wxALL|wxEXPAND, 5 );
 	
 	m_IadjUnitLabel = new wxStaticText( this, wxID_ANY, _("uA"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_IadjUnitLabel->Wrap( -1 );
-	fgSizerPrms->Add( m_IadjUnitLabel, 0, wxALL, 5 );
+	fgSizerPrms->Add( m_IadjUnitLabel, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
 	
 	
 	bSizerMain->Add( fgSizerPrms, 0, wxEXPAND, 5 );
