dfa91752097d25181d28453e307ceacb7492766e
jean-pierre charras <jp.charras@wanadoo.fr>
2018-05-29 13:45:45 +0200

    Fix incorrect mask size for rectangular and trapezoid pads.
    Fixes: lp:1773926
    https://bugs.launchpad.net/kicad/+bug/1773926
    
    Fixes: lp:1773961
    https://bugs.launchpad.net/kicad/+bug/1773961

--- pcbnew/plot_board_layers.cpp
+++ pcbnew/plot_board_layers.cpp
@@ -431,15 +431,17 @@ void PlotStandardLayer( BOARD *aBoard, PLOTTER* aPlotter,
             case PAD_SHAPE_CIRCLE:
             case PAD_SHAPE_OVAL:
                 pad->SetSize( padPlotsSize );
-
                 if( aPlotOpt.GetSkipPlotNPTH_Pads() &&
                     ( pad->GetSize() == pad->GetDrillSize() ) &&
                     ( pad->GetAttribute() == PAD_ATTRIB_HOLE_NOT_PLATED ) )
-                    break;
-                // Fall through:
+
+                itemplotter.PlotPad( pad, color, plotMode );
+                break;
+
             case PAD_SHAPE_TRAPEZOID:
             case PAD_SHAPE_RECT:
             case PAD_SHAPE_ROUNDRECT:
+                pad->SetSize( padPlotsSize );
                 itemplotter.PlotPad( pad, color, plotMode );
                 break;
 
