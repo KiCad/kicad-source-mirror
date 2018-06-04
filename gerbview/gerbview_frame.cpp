e4a6f2e5233374a126aa890b0901171ed5f74634
Maciej Suminski <maciej.suminski@cern.ch>
2018-06-04 11:49:53 +0200

    Switch canvas to Cairo if OpenGL is not supported on the first run

--- gerbview/gerbview_frame.cpp
+++ gerbview/gerbview_frame.cpp
@@ -228,15 +228,20 @@ GERBVIEW_FRAME::GERBVIEW_FRAME( KIWAY* aKiway, wxWindow* aParent ):
 
                 // Switch to OpenGL, which will save the new setting if successful
                 wxCommandEvent evt( wxEVT_MENU, ID_MENU_CANVAS_OPENGL );
-                auto handler = GetEventHandler();
-                handler->ProcessEvent( evt );
+                GetEventHandler()->ProcessEvent( evt );
+
+                // Switch back to Cairo if OpenGL is not supported
+                if( GetGalCanvas()->GetBackend() == EDA_DRAW_PANEL_GAL::GAL_TYPE_NONE )
+                {
+                    wxCommandEvent cairoEvt( wxEVT_MENU, ID_MENU_CANVAS_CAIRO );
+                    GetEventHandler()->ProcessEvent( cairoEvt );
+                }
             }
             else if( canvasType == EDA_DRAW_PANEL_GAL::GAL_TYPE_NONE )
             {
                 // If they were on legacy, switch them to Cairo
                 wxCommandEvent evt( wxEVT_MENU, ID_MENU_CANVAS_CAIRO );
-                auto handler = GetEventHandler();
-                handler->ProcessEvent( evt );
+                GetEventHandler()->ProcessEvent( evt );
             }
         }
 
