246a9286a996c7bafe57d3948021d1852411108a
Jeff Young <jeff@rokeby.ie>
2018-06-28 07:30:44 -0600

    Fix dialogs with wxID_CANCEL as the window ID.
    Fixes: lp:1778990
    * https://bugs.launchpad.net/kicad/+bug/1778990
    
    Fixes: lp:1778991
    * https://bugs.launchpad.net/kicad/+bug/1778991

--- pcbnew/dialogs/dialog_gendrill_base.cpp
+++ pcbnew/dialogs/dialog_gendrill_base.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Apr 19 2018)
+// C++ code generated with wxFormBuilder (version Dec 30 2017)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
--- pcbnew/dialogs/dialog_netlist_base.h
+++ pcbnew/dialogs/dialog_netlist_base.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Apr 19 2018)
+// C++ code generated with wxFormBuilder (version Dec 30 2017)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -79,7 +79,7 @@ class DIALOG_NETLIST_BASE : public DIALOG_SHIM
 	
 	public:
 		
-		DIALOG_NETLIST_BASE( wxWindow* parent, wxWindowID id = wxID_CANCEL, const wxString& title = _("Netlist"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+		DIALOG_NETLIST_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Netlist"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
 		~DIALOG_NETLIST_BASE();
 	
 };
