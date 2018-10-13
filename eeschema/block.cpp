aa622994e18114d4bb4e2bc1bd496a66d84dbd7d
Seth Hillbrand <hillbrand@ucdavis.edu>
2018-10-12 17:07:52 -0700

    eeschema: Ensure is_moved flag is set when moving
    Prior to placing, cleaning routines need to know which schematic items
    are being moved to avoid cleaning them relative to themselves.  The
    IS_MOVED flag communicates this and is cleared by ClearDrawingState()
    
    Fixes: lp:1797576
    * https://bugs.launchpad.net/kicad/+bug/1797576

--- eeschema/block.cpp
+++ eeschema/block.cpp
@@ -140,6 +140,7 @@ void SCH_EDIT_FRAME::HandleBlockPlace( wxDC* DC )
         {
             SCH_ITEM* item = dynamic_cast<SCH_ITEM*>( block->GetItems().GetPickedItem( ii ) );
             item->Move( block->GetMoveVector() );
+            item->SetFlags( IS_MOVED );
             GetCanvas()->GetView()->Update( item, KIGFX::GEOMETRY );
         }
         break;
