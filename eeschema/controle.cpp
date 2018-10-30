49c94fb928d2a18163a73879825c36164a2cd6c2
jean-pierre charras <jp.charras@wanadoo.fr>
2018-10-30 15:58:08 +0100

    Eeschema, library viewer: fix a Kicad assert.

--- eeschema/controle.cpp
+++ eeschema/controle.cpp
@@ -311,12 +311,11 @@ bool LIB_VIEW_FRAME::GeneralControl( wxDC* aDC, const wxPoint& aPosition, EDA_KE
     }
 
     wxPoint pos = aPosition;
-    wxPoint oldpos = GetCrossHairPosition();
     GeneralControlKeyMovement( aHotKey, &pos, true );
 
     // Update cursor position.
+    m_canvas->CrossHairOn( aDC );
     SetCrossHairPosition( pos, true );
-    RefreshCrossHair( oldpos, aPosition, aDC );
 
     if( aHotKey )
     {
