7eac7dcdcb56d92b41cbd8eaa5134b302aeeeca8
jean-pierre charras <jp.charras@wanadoo.fr>
2018-11-26 14:49:06 +0100

    dialog_3D_view_option_base.cpp: fix a minor wxWidgets alert.

--- 3d-viewer/3d_viewer/dialogs/dialog_3D_view_option_base.h
+++ 3d-viewer/3d_viewer/dialogs/dialog_3D_view_option_base.h
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Jul  2 2017)
+// C++ code generated with wxFormBuilder (version Jul 11 2018)
 // http://www.wxformbuilder.org/
 //
-// PLEASE DO "NOT" EDIT THIS FILE!
+// PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
 #ifndef __DIALOG_3D_VIEW_OPTION_BASE_H__
@@ -11,8 +11,6 @@
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
 #include <wx/intl.h>
-class DIALOG_SHIM;
-
 #include "dialog_shim.h"
 #include <wx/string.h>
 #include <wx/stattext.h>
