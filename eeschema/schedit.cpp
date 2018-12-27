72a1052bb993cca2a05e62dbccbde2b0e80c8c94
Jeff Young <jeff@rokeby.ie>
2018-12-27 18:41:14 +0000

    Make sure parents get redrawn to refresh module text items.
    Fixes: lp:1809912
    * https://bugs.launchpad.net/kicad/+bug/1809912

--- eeschema/schedit.cpp
+++ eeschema/schedit.cpp
@@ -741,16 +741,11 @@ static void abortMoveItem( EDA_DRAW_PANEL* aPanel, wxDC* aDC )
         // Never delete existing item, because it can be referenced by an undo/redo command
         // Just restore its data
 
-        view->Remove( item );
+        view->Remove( currentItem );
         currentItem->SwapData( oldItem );
         item->ClearFlags();
-        view->Add( item );
+        view->Add( currentItem );
         view->Hide( item, false );
-
-        // for items managed by their parent, we have to refresh
-        // the parent drawings (scheet or symbol)
-        if( currentItem != item )
-            parent->RefreshItem( currentItem );
     }
 
     screen->TestDanglingEnds();
