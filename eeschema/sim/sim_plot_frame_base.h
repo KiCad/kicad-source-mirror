752cc43fb8f99de3a0f2e84e9646ae50da5125bc
jean-pierre charras <jp.charras@wanadoo.fr>
2019-01-21 19:53:40 +0100

    Fix 2 minor wxWidgets alerts.

--- eeschema/sim/sim_plot_frame_base.h
+++ eeschema/sim/sim_plot_frame_base.h
@@ -1,19 +1,15 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Jul 17 2016)
+// C++ code generated with wxFormBuilder (version Dec  1 2018)
 // http://www.wxformbuilder.org/
 //
-// PLEASE DO "NOT" EDIT THIS FILE!
+// PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
-#ifndef __SIM_PLOT_FRAME_BASE_H__
-#define __SIM_PLOT_FRAME_BASE_H__
+#pragma once
 
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
 #include <wx/intl.h>
-class KIWAY_PLAYER;
-class wxListView;
-
 #include "kiway_player.h"
 #include <wx/string.h>
 #include <wx/bitmap.h>
@@ -44,7 +40,7 @@ class wxListView;
 class SIM_PLOT_FRAME_BASE : public KIWAY_PLAYER
 {
 	private:
-	
+
 	protected:
 		wxMenuBar* m_mainMenu;
 		wxMenu* m_fileMenu;
@@ -83,7 +79,7 @@ class SIM_PLOT_FRAME_BASE : public KIWAY_PLAYER
 		wxPanel* m_tunePanel;
 		wxStaticBoxSizer* m_tuneSizerStb;
 		wxBoxSizer* m_tuneSizer;
-		
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void menuNewPlot( wxCommandEvent& event ) { event.Skip(); }
 		virtual void menuOpenWorkbook( wxCommandEvent& event ) { event.Skip(); }
@@ -102,38 +98,37 @@ class SIM_PLOT_FRAME_BASE : public KIWAY_PLAYER
 		virtual void onPlotClose( wxAuiNotebookEvent& event ) { event.Skip(); }
 		virtual void onSignalDblClick( wxMouseEvent& event ) { event.Skip(); }
 		virtual void onSignalRClick( wxListEvent& event ) { event.Skip(); }
-		
-	
+
+
 	public:
-		
+
 		SIM_PLOT_FRAME_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Spice Simulator"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL, const wxString& name = wxT("SIM_PLOT_FRAME") );
-		
+
 		~SIM_PLOT_FRAME_BASE();
-		
+
 		void m_splitterLeftRightOnIdle( wxIdleEvent& )
 		{
 			m_splitterLeftRight->SetSashPosition( 700 );
 			m_splitterLeftRight->Disconnect( wxEVT_IDLE, wxIdleEventHandler( SIM_PLOT_FRAME_BASE::m_splitterLeftRightOnIdle ), NULL, this );
 		}
-		
+
 		void m_splitterPlotAndConsoleOnIdle( wxIdleEvent& )
 		{
 			m_splitterPlotAndConsole->SetSashPosition( 500 );
 			m_splitterPlotAndConsole->Disconnect( wxEVT_IDLE, wxIdleEventHandler( SIM_PLOT_FRAME_BASE::m_splitterPlotAndConsoleOnIdle ), NULL, this );
 		}
-		
+
 		void m_splitterSignalsOnIdle( wxIdleEvent& )
 		{
 			m_splitterSignals->SetSashPosition( 0 );
 			m_splitterSignals->Disconnect( wxEVT_IDLE, wxIdleEventHandler( SIM_PLOT_FRAME_BASE::m_splitterSignalsOnIdle ), NULL, this );
 		}
-		
+
 		void m_splitterTuneValuesOnIdle( wxIdleEvent& )
 		{
 			m_splitterTuneValues->SetSashPosition( 0 );
 			m_splitterTuneValues->Disconnect( wxEVT_IDLE, wxIdleEventHandler( SIM_PLOT_FRAME_BASE::m_splitterTuneValuesOnIdle ), NULL, this );
 		}
-	
+
 };
 
-#endif //__SIM_PLOT_FRAME_BASE_H__
