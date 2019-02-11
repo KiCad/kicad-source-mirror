c8cb9b3e1c1b68df965b93cfda1324b0a435a724
Seth Hillbrand <hillbrand@ucdavis.edu>
2019-02-11 08:35:23 -0800

    Cleanup licensing
    Add text of BOOST and ISC to source repo and clarify sources under each
    in the README

--- common/system/libcontext.cpp
+++ common/system/libcontext.cpp
@@ -9,7 +9,7 @@
     Minor modifications by Tomasz Wlostowski 2016.
 
  Distributed under the Boost Software License, Version 1.0.
-      (See accompanying file LICENSE_1_0.txt or copy at
+      (See accompanying file LICENSE.BOOSTv1_0.txt or copy at
             http://www.boost.org/LICENSE_1_0.txt)
 
 */
