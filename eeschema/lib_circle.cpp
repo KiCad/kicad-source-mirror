5be59ea8dd4392e9eb794c5b9c4cfccdb70c683c
Seth Hillbrand <hillbrand@ucdavis.edu>
2019-03-10 23:41:00 -0700

    eeschema: Fix plot line width for negative lines
    Negative width lines are used to hide the outline of shapes in eeschema.
    Plot should not show these lines.
    
    (cherry picked from commit 94fa734c614ff1acdf7d4c723706cbf420f72944)

--- eeschema/lib_circle.cpp
+++ eeschema/lib_circle.cpp
@@ -157,8 +157,14 @@ void LIB_CIRCLE::Plot( PLOTTER* aPlotter, const wxPoint& aOffset, bool aFill,
     }
 
     bool already_filled = m_Fill == FILLED_WITH_BG_BODYCOLOR;
-    aPlotter->SetColor( GetLayerColor( LAYER_DEVICE ) );
-    aPlotter->Circle( pos, m_Radius * 2, already_filled ? NO_FILL : m_Fill, GetPenSize() );
+    auto pen_size = GetPenSize();
+
+    if( !already_filled || pen_size > 0 )
+    {
+        pen_size = std::max( 0, pen_size );
+        aPlotter->SetColor( GetLayerColor( LAYER_DEVICE ) );
+        aPlotter->Circle( pos, m_Radius * 2, already_filled ? NO_FILL : m_Fill, GetPenSize() );
+    }
 }
 
 
--- eeschema/lib_rectangle.cpp
+++ eeschema/lib_rectangle.cpp
@@ -147,8 +147,14 @@ void LIB_RECTANGLE::Plot( PLOTTER* aPlotter, const wxPoint& aOffset, bool aFill,
     }
 
     bool already_filled = m_Fill == FILLED_WITH_BG_BODYCOLOR;
-    aPlotter->SetColor( GetLayerColor( LAYER_DEVICE ) );
-    aPlotter->Rect( pos, end, already_filled ? NO_FILL : m_Fill, GetPenSize() );
+    auto pen_size = GetPenSize();
+
+    if( !already_filled || pen_size > 0 )
+    {
+        pen_size = std::max( 0, pen_size );
+        aPlotter->SetColor( GetLayerColor( LAYER_DEVICE ) );
+        aPlotter->Rect( pos, end, already_filled ? NO_FILL : m_Fill, GetPenSize() );
+    }
 }
 
 
