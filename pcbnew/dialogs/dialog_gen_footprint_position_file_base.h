dfee7bddd31f4720bcebe1bc4ca5a70b6586aa6a
Wayne Stambaugh <stambaughw@gmail.com>
2019-04-05 16:15:16 -0400

    Minor footprint position dialog improvements.

--- pcbnew/dialogs/dialog_gen_footprint_position_file_base.h
+++ pcbnew/dialogs/dialog_gen_footprint_position_file_base.h
@@ -1,12 +1,11 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Dec 30 2017)
+// C++ code generated with wxFormBuilder (version Feb 10 2019)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
-#ifndef __DIALOG_GEN_FOOTPRINT_POSITION_FILE_BASE_H__
-#define __DIALOG_GEN_FOOTPRINT_POSITION_FILE_BASE_H__
+#pragma once
 
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
@@ -21,10 +20,10 @@ class WX_HTML_REPORT_PANEL;
 #include <wx/colour.h>
 #include <wx/settings.h>
 #include <wx/textctrl.h>
+#include <wx/bmpbuttn.h>
 #include <wx/bitmap.h>
 #include <wx/image.h>
 #include <wx/icon.h>
-#include <wx/bmpbuttn.h>
 #include <wx/button.h>
 #include <wx/sizer.h>
 #include <wx/radiobox.h>
@@ -40,7 +39,7 @@ class WX_HTML_REPORT_PANEL;
 class DIALOG_GEN_FOOTPRINT_POSITION_BASE : public DIALOG_SHIM
 {
 	private:
-	
+
 	protected:
 		wxBoxSizer* m_MainSizer;
 		wxStaticText* m_staticTextDir;
@@ -54,17 +53,16 @@ class DIALOG_GEN_FOOTPRINT_POSITION_BASE : public DIALOG_SHIM
 		wxStdDialogButtonSizer* m_sdbSizer1;
 		wxButton* m_sdbSizer1OK;
 		wxButton* m_sdbSizer1Cancel;
-		
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void OnOutputDirectoryBrowseClicked( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnGenerate( wxCommandEvent& event ) { event.Skip(); }
-		
-	
+
+
 	public:
-		
-		DIALOG_GEN_FOOTPRINT_POSITION_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Generate Footprint Position Files"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+
+		DIALOG_GEN_FOOTPRINT_POSITION_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Generate Footprint Position Files"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
 		~DIALOG_GEN_FOOTPRINT_POSITION_BASE();
-	
+
 };
 
-#endif //__DIALOG_GEN_FOOTPRINT_POSITION_FILE_BASE_H__
