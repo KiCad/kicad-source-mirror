76e06222226de03925d80215145225527ca23c88
Jeff Young <jeff@rokeby.ie>
2019-04-12 00:13:24 +0100

    Redraw and Position Relative can't share the same hotkey.

--- pcbnew/hotkeys.cpp
+++ pcbnew/hotkeys.cpp
@@ -143,7 +143,7 @@ static EDA_HOTKEY HkMoveItem( _HKI( "Move Item" ), HK_MOVE_ITEM, 'M' );
 static EDA_HOTKEY HkMoveItemExact( _HKI( "Move Item Exactly" ), HK_MOVE_ITEM_EXACT,
                                    'M' + GR_KB_CTRL );
 static EDA_HOTKEY HkPositionItemRelative( _HKI( "Position Item Relative" ), HK_POSITION_RELATIVE,
-                                          'R' + GR_KB_CTRL );
+                                          'P' + GR_KB_SHIFT );
 static EDA_HOTKEY HkDuplicateItem( _HKI( "Duplicate Item" ), HK_DUPLICATE_ITEM, 'D' + GR_KB_CTRL );
 static EDA_HOTKEY HkDuplicateItemAndIncrement( _HKI( "Duplicate Item and Increment" ),
                                                HK_DUPLICATE_ITEM_AND_INCREMENT,
