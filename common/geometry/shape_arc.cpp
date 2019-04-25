KiCad/kicad-source-mirror

commit e312e2b28679dc6d68de682f95f8bb83489801dd

SHAPE_ARC: fix polyline conversion when radius=0
Prevent a divide-by-zero bug in SHAPE_ARC::ConvertToPolyline.

When the radius is zero, just use the initial angle (it makes
no different anyway, the result is the centre point, which is
the start point.

common/geometry/shape_arc.cpp
@@ -256,7 +256,11 @@ const SHAPE_LINE_CHAIN SHAPE_ARC::ConvertToPolyline( double aAccuracy ) const
 
     for( int i = 0; i <= n ; i++ )
     {
-        double a = sa + m_centralAngle * (double) i / (double) n;
+        double a = sa;
+
+        if( n != 0 )
+            sa += m_centralAngle * (double) i / (double) n;
+
         double x = c.x + r * cos( a * M_PI / 180.0 );
         double y = c.y + r * sin( a * M_PI / 180.0 );
 

qa/common/geometry/test_shape_arc.cpp
@@ -314,10 +314,8 @@ bool ArePolylinePointsNearCircle(
     return GEOM_TEST::ArePointsNearCircle( points, aCentre, aRad, aTolEnds );
 }
 
-#ifdef HAVE_EXPECTED_FAILURES
 
-// Failure in zero-radius case
-BOOST_AUTO_TEST_CASE( ArcToPolyline, *boost::unit_test::expected_failures( 1 ) )
+BOOST_AUTO_TEST_CASE( ArcToPolyline )
 {
     const std::vector<ARC_TO_POLYLINE_CASE> cases = {
         {
@@ -372,8 +370,6 @@ BOOST_AUTO_TEST_CASE( ArcToPolyline, *boost::unit_test::expected_failures( 1 ) )
         }
     }
 }
-
-#endif // HAVE_EXPECTED_FAILURES
 
 
 BOOST_AUTO_TEST_SUITE_END()

commit 5813164d159c12c920b969b58ef6318e211758a3

SHAPE_ARC: fixed polyline conversion bug introduced in e312e2b286
Fixes: lp:1826278
* https://bugs.launchpad.net/kicad/+bug/1826278

Fixes: lp:1826270
* https://bugs.launchpad.net/kicad/+bug/1826270

common/geometry/shape_arc.cpp
@@ -259,7 +259,7 @@ const SHAPE_LINE_CHAIN SHAPE_ARC::ConvertToPolyline( double aAccuracy ) const
         double a = sa;
 
         if( n != 0 )
-            sa += m_centralAngle * (double) i / (double) n;
+            a = m_centralAngle * (double) i / (double) n;
 
         double x = c.x + r * cos( a * M_PI / 180.0 );
         double y = c.y + r * sin( a * M_PI / 180.0 );