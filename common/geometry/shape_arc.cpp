KiCad/kicad-source-mirror

commit 5813164d159c12c920b969b58ef6318e211758a3

SHAPE_ARC: fixed polyline conversion bug introduced in e312e2b286
Fixes: lp:1826278
* https://bugs.launchpad.net/kicad/+bug/1826278

Fixes: lp:1826270
* https://bugs.launchpad.net/kicad/+bug/1826270

common/geometry/shape_arc.cpp
@@ -259,7 +259,7 @@ const SHAPE_LINE_CHAIN SHAPE_ARC::ConvertToPolyline( double aAccuracy ) const
         double a = sa;
 
         if( n != 0 )
-            sa += m_centralAngle * (double) i / (double) n;
+            a = m_centralAngle * (double) i / (double) n;
 
         double x = c.x + r * cos( a * M_PI / 180.0 );
         double y = c.y + r * sin( a * M_PI / 180.0 );

commit d89d0e6b7420150dd693554536cccdaa0907875d

SHAPE_ARC: Fix bug introduced in 5813164d1.
This fixes the bug exposed in the previous commit.
The tests which exposed it now pass and expected failures
removed.

common/geometry/shape_arc.cpp
@@ -259,7 +259,7 @@ const SHAPE_LINE_CHAIN SHAPE_ARC::ConvertToPolyline( double aAccuracy ) const
         double a = sa;
 
         if( n != 0 )
-            a = m_centralAngle * (double) i / (double) n;
+            a += m_centralAngle * (double) i / (double) n;
 
         double x = c.x + r * cos( a * M_PI / 180.0 );
         double y = c.y + r * sin( a * M_PI / 180.0 );

qa/common/geometry/test_shape_arc.cpp
@@ -337,10 +337,8 @@ bool ArePolylineMidPointsNearCircle(
     return GEOM_TEST::ArePointsNearCircle( points, aCentre, aRad, aTolMidPts );
 }
 
-#ifdef HAVE_EXPECTED_FAILURES
 
-// Start and end point check fail for the 3 non-zero radius cases
-BOOST_AUTO_TEST_CASE( ArcToPolyline, *boost::unit_test::expected_failures( 3 * 2 ) )
+BOOST_AUTO_TEST_CASE( ArcToPolyline )
 {
     const std::vector<ARC_TO_POLYLINE_CASE> cases = {
         {
@@ -416,6 +414,5 @@ BOOST_AUTO_TEST_CASE( ArcToPolyline, *boost::unit_test::expected_failures( 3 * 2
     }
 }
 
-#endif
 
 BOOST_AUTO_TEST_SUITE_END()