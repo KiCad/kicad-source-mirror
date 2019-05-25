549b76739e89731e461d5acfdcf5943d0f2673ad
Jon Evans <jon@craftyjon.com>
2019-05-25 10:43:55 -0400

    Exclude invisible items from view bounding box for zoom-to-fit
    Fixes: lp:1116457
    * https://bugs.launchpad.net/kicad/+bug/1116457
    
    (cherry picked from commit ede25750181a13de15c8e9606f397f9835ca30a4)

--- pcbnew/class_board.cpp
+++ pcbnew/class_board.cpp
@@ -1104,11 +1104,15 @@ EDA_RECT BOARD::ComputeBoundingBox( bool aBoardEdgesOnly ) const
 {
     bool hasItems = false;
     EDA_RECT area;
+    LSET visible = GetVisibleLayers();
 
     // Check segments, dimensions, texts, and fiducials
     for( BOARD_ITEM* item = m_Drawings;  item;  item = item->Next() )
     {
         if( aBoardEdgesOnly && (item->Type() != PCB_LINE_T || item->GetLayer() != Edge_Cuts ) )
+            continue;
+
+        if( !( item->GetLayerSet() & visible ).any() )
             continue;
 
         if( !hasItems )
@@ -1124,6 +1128,9 @@ EDA_RECT BOARD::ComputeBoundingBox( bool aBoardEdgesOnly ) const
         // Check modules
         for( MODULE* module = m_Modules; module; module = module->Next() )
         {
+            if( !( module->GetLayerSet() & visible ).any() )
+                continue;
+
             if( !hasItems )
                 area = module->GetBoundingBox();
             else
@@ -1135,6 +1142,9 @@ EDA_RECT BOARD::ComputeBoundingBox( bool aBoardEdgesOnly ) const
         // Check tracks
         for( TRACK* track = m_Track; track; track = track->Next() )
         {
+            if( !( track->GetLayerSet() & visible ).any() )
+                continue;
+
             if( !hasItems )
                 area = track->GetBoundingBox();
             else
@@ -1146,6 +1156,9 @@ EDA_RECT BOARD::ComputeBoundingBox( bool aBoardEdgesOnly ) const
         // Check zones
         for( auto aZone : m_ZoneDescriptorList )
         {
+            if( !( aZone->GetLayerSet() & visible ).any() )
+                continue;
+
             if( !hasItems )
                 area = aZone->GetBoundingBox();
             else
