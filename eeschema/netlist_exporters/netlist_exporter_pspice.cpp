b3b32fa96605aa6cc5036a62f21a6b2f99542b17
jean-pierre charras <jp.charras@wanadoo.fr>
2019-07-19 16:13:10 +0200

    Revert "Fix ngspice netlisting when net names contain '/'" This reverts commit eadf6d93bcf8eafb5d1d4383522fae4229c5f9d1.
    because the issue with net names containing '/' is not actually inside the netlist.
    This commit eadf creates issues with ngspice 2.8 and older, and do not fix anything in ngspice

--- eeschema/netlist_exporters/netlist_exporter_pspice.cpp
+++ eeschema/netlist_exporters/netlist_exporter_pspice.cpp
@@ -154,15 +154,6 @@ bool NETLIST_EXPORTER_PSPICE::Format( OUTPUTFORMATTER* aFormatter, unsigned aCtl
                 // unescape net names that contain a escaped sequence ("{slash}"):
                 netName = UnescapeString( netName );
 
-                // Add quotes to nets containing slashes. This isn't added to ReplaceForbidenChars
-                // because this is only necessary for file writing; nets with slashes can be
-                // handled by ngspice after loading.
-                if( netName.Contains( "/" ) )
-                {
-                    netName.Prepend( '"' );
-                    netName.Append( '"' );
-                }
-
                 // Borrow LTSpice's nomenclature for unconnected nets
                 if( netName.IsEmpty() )
                     netName = wxString::Format( wxT( "NC_%.2u" ), NC_counter++ );
