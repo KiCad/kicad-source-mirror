185a8d7582c8a08c92d703151c9cf89812201ed8
jean-pierre charras <jp.charras@wanadoo.fr>
2019-07-28 12:54:59 +0200

    Try to fix UI issues in select layer pair dialog and slect layer dialog.
    Fixes: lp:1838173
    https://bugs.launchpad.net/kicad/+bug/1838173

--- pcbnew/dialogs/dialog_layer_selection_base.cpp
+++ pcbnew/dialogs/dialog_layer_selection_base.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Dec 30 2017)
+// C++ code generated with wxFormBuilder (version Jul 10 2019)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -9,25 +9,25 @@
 
 ///////////////////////////////////////////////////////////////////////////
 
-DIALOG_LAYER_SELECTION_BASE::DIALOG_LAYER_SELECTION_BASE( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
+DIALOG_LAYER_SELECTION_BASE::DIALOG_LAYER_SELECTION_BASE( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : DIALOG_SHIM( parent, id, title, pos, size, style )
 {
 	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
-	
+
 	wxBoxSizer* bSizerMain;
 	bSizerMain = new wxBoxSizer( wxVERTICAL );
-	
+
 	wxBoxSizer* bSizerUpper;
 	bSizerUpper = new wxBoxSizer( wxHORIZONTAL );
-	
+
 	m_leftGridLayers = new wxGrid( this, ID_LEFT_LIST, wxDefaultPosition, wxDefaultSize, 0 );
-	
+
 	// Grid
 	m_leftGridLayers->CreateGrid( 1, 3 );
 	m_leftGridLayers->EnableEditing( false );
 	m_leftGridLayers->EnableGridLines( false );
 	m_leftGridLayers->EnableDragGridSize( false );
 	m_leftGridLayers->SetMargins( 5, 3 );
-	
+
 	// Columns
 	m_leftGridLayers->SetColSize( 0, 24 );
 	m_leftGridLayers->SetColSize( 1, 20 );
@@ -35,30 +35,28 @@ DIALOG_LAYER_SELECTION_BASE::DIALOG_LAYER_SELECTION_BASE( wxWindow* parent, wxWi
 	m_leftGridLayers->EnableDragColMove( false );
 	m_leftGridLayers->EnableDragColSize( false );
 	m_leftGridLayers->SetColLabelSize( 0 );
-	m_leftGridLayers->SetColLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
-	
+	m_leftGridLayers->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+
 	// Rows
 	m_leftGridLayers->EnableDragRowSize( false );
 	m_leftGridLayers->SetRowLabelSize( 0 );
-	m_leftGridLayers->SetRowLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
-	
+	m_leftGridLayers->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+
 	// Label Appearance
-	m_leftGridLayers->SetLabelBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_WINDOW ) );
-	
+
 	// Cell Defaults
-	m_leftGridLayers->SetDefaultCellBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_MENU ) );
 	m_leftGridLayers->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_TOP );
 	bSizerUpper->Add( m_leftGridLayers, 1, wxALL|wxEXPAND, 5 );
-	
+
 	m_rightGridLayers = new wxGrid( this, ID_RIGHT_LIST, wxDefaultPosition, wxDefaultSize, 0 );
-	
+
 	// Grid
 	m_rightGridLayers->CreateGrid( 1, 3 );
 	m_rightGridLayers->EnableEditing( false );
 	m_rightGridLayers->EnableGridLines( false );
 	m_rightGridLayers->EnableDragGridSize( false );
 	m_rightGridLayers->SetMargins( 5, 3 );
-	
+
 	// Columns
 	m_rightGridLayers->SetColSize( 0, 24 );
 	m_rightGridLayers->SetColSize( 1, 20 );
@@ -66,30 +64,29 @@ DIALOG_LAYER_SELECTION_BASE::DIALOG_LAYER_SELECTION_BASE( wxWindow* parent, wxWi
 	m_rightGridLayers->EnableDragColMove( false );
 	m_rightGridLayers->EnableDragColSize( false );
 	m_rightGridLayers->SetColLabelSize( 0 );
-	m_rightGridLayers->SetColLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
-	
+	m_rightGridLayers->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+
 	// Rows
 	m_rightGridLayers->EnableDragRowSize( false );
 	m_rightGridLayers->SetRowLabelSize( 0 );
-	m_rightGridLayers->SetRowLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
-	
+	m_rightGridLayers->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+
 	// Label Appearance
-	
+
 	// Cell Defaults
-	m_rightGridLayers->SetDefaultCellBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_MENU ) );
 	m_rightGridLayers->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_TOP );
 	bSizerUpper->Add( m_rightGridLayers, 1, wxALL|wxEXPAND, 5 );
-	
-	
+
+
 	bSizerMain->Add( bSizerUpper, 1, wxEXPAND|wxALL, 10 );
-	
-	
+
+
 	this->SetSizer( bSizerMain );
 	this->Layout();
 	bSizerMain->Fit( this );
-	
+
 	this->Centre( wxBOTH );
-	
+
 	// Connect Events
 	m_leftGridLayers->Connect( wxEVT_GRID_CELL_LEFT_CLICK, wxGridEventHandler( DIALOG_LAYER_SELECTION_BASE::OnLeftGridCellClick ), NULL, this );
 	m_leftGridLayers->Connect( wxEVT_LEFT_UP, wxMouseEventHandler( DIALOG_LAYER_SELECTION_BASE::OnLeftButtonReleased ), NULL, this );
@@ -104,35 +101,35 @@ DIALOG_LAYER_SELECTION_BASE::~DIALOG_LAYER_SELECTION_BASE()
 	m_leftGridLayers->Disconnect( wxEVT_LEFT_UP, wxMouseEventHandler( DIALOG_LAYER_SELECTION_BASE::OnLeftButtonReleased ), NULL, this );
 	m_rightGridLayers->Disconnect( wxEVT_GRID_CELL_LEFT_CLICK, wxGridEventHandler( DIALOG_LAYER_SELECTION_BASE::OnRightGridCellClick ), NULL, this );
 	m_rightGridLayers->Disconnect( wxEVT_LEFT_UP, wxMouseEventHandler( DIALOG_LAYER_SELECTION_BASE::OnLeftButtonReleased ), NULL, this );
-	
+
 }
 
-DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
+DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : DIALOG_SHIM( parent, id, title, pos, size, style )
 {
 	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
-	
+
 	wxBoxSizer* bSizerMain;
 	bSizerMain = new wxBoxSizer( wxVERTICAL );
-	
+
 	wxBoxSizer* bSizerUpper;
 	bSizerUpper = new wxBoxSizer( wxHORIZONTAL );
-	
+
 	wxBoxSizer* bSizerLeft;
 	bSizerLeft = new wxBoxSizer( wxVERTICAL );
-	
+
 	m_staticTextTopLayer = new wxStaticText( this, wxID_ANY, _("Top/Front layer:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticTextTopLayer->Wrap( -1 );
 	bSizerLeft->Add( m_staticTextTopLayer, 0, wxTOP|wxRIGHT|wxLEFT, 5 );
-	
+
 	m_leftGridLayers = new wxGrid( this, ID_LEFT_LIST, wxDefaultPosition, wxDefaultSize, 0 );
-	
+
 	// Grid
 	m_leftGridLayers->CreateGrid( 1, 3 );
 	m_leftGridLayers->EnableEditing( false );
 	m_leftGridLayers->EnableGridLines( false );
 	m_leftGridLayers->EnableDragGridSize( false );
 	m_leftGridLayers->SetMargins( 3, 3 );
-	
+
 	// Columns
 	m_leftGridLayers->SetColSize( 0, 24 );
 	m_leftGridLayers->SetColSize( 1, 20 );
@@ -140,40 +137,38 @@ DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE
 	m_leftGridLayers->EnableDragColMove( false );
 	m_leftGridLayers->EnableDragColSize( false );
 	m_leftGridLayers->SetColLabelSize( 0 );
-	m_leftGridLayers->SetColLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
-	
+	m_leftGridLayers->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+
 	// Rows
 	m_leftGridLayers->EnableDragRowSize( false );
 	m_leftGridLayers->SetRowLabelSize( 0 );
-	m_leftGridLayers->SetRowLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
-	
+	m_leftGridLayers->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+
 	// Label Appearance
-	m_leftGridLayers->SetLabelBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_WINDOW ) );
-	
+
 	// Cell Defaults
-	m_leftGridLayers->SetDefaultCellBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_MENU ) );
-	m_leftGridLayers->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_CENTRE );
-	bSizerLeft->Add( m_leftGridLayers, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
-	
-	
-	bSizerUpper->Add( bSizerLeft, 0, wxEXPAND|wxRIGHT|wxLEFT, 5 );
-	
+	m_leftGridLayers->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_CENTER );
+	bSizerLeft->Add( m_leftGridLayers, 0, wxALL|wxEXPAND, 5 );
+
+
+	bSizerUpper->Add( bSizerLeft, 1, wxEXPAND|wxRIGHT|wxLEFT, 5 );
+
 	wxBoxSizer* bSizerRight;
 	bSizerRight = new wxBoxSizer( wxVERTICAL );
-	
+
 	m_staticTextBottomLayer = new wxStaticText( this, wxID_ANY, _("Bottom/Back layer:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticTextBottomLayer->Wrap( -1 );
 	bSizerRight->Add( m_staticTextBottomLayer, 0, wxTOP|wxRIGHT|wxLEFT, 5 );
-	
+
 	m_rightGridLayers = new wxGrid( this, ID_RIGHT_LIST, wxDefaultPosition, wxDefaultSize, 0 );
-	
+
 	// Grid
 	m_rightGridLayers->CreateGrid( 1, 3 );
 	m_rightGridLayers->EnableEditing( false );
 	m_rightGridLayers->EnableGridLines( false );
 	m_rightGridLayers->EnableDragGridSize( false );
 	m_rightGridLayers->SetMargins( 3, 3 );
-	
+
 	// Columns
 	m_rightGridLayers->SetColSize( 0, 24 );
 	m_rightGridLayers->SetColSize( 1, 20 );
@@ -181,45 +176,44 @@ DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE
 	m_rightGridLayers->EnableDragColMove( false );
 	m_rightGridLayers->EnableDragColSize( false );
 	m_rightGridLayers->SetColLabelSize( 0 );
-	m_rightGridLayers->SetColLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
-	
+	m_rightGridLayers->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+
 	// Rows
 	m_rightGridLayers->EnableDragRowSize( false );
 	m_rightGridLayers->SetRowLabelSize( 0 );
-	m_rightGridLayers->SetRowLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
-	
+	m_rightGridLayers->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+
 	// Label Appearance
-	
+
 	// Cell Defaults
-	m_rightGridLayers->SetDefaultCellBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_MENU ) );
-	m_rightGridLayers->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_CENTRE );
-	bSizerRight->Add( m_rightGridLayers, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
-	
-	
-	bSizerUpper->Add( bSizerRight, 0, wxEXPAND|wxRIGHT|wxLEFT, 5 );
-	
-	
-	bSizerMain->Add( bSizerUpper, 0, wxALL|wxEXPAND, 5 );
-	
-	m_staticline1 = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
-	bSizerMain->Add( m_staticline1, 0, wxEXPAND | wxALL, 5 );
-	
+	m_rightGridLayers->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_CENTER );
+	bSizerRight->Add( m_rightGridLayers, 0, wxALL|wxEXPAND, 5 );
+
+
+	bSizerUpper->Add( bSizerRight, 1, wxEXPAND|wxRIGHT|wxLEFT, 5 );
+
+
+	bSizerMain->Add( bSizerUpper, 1, wxALL|wxEXPAND, 5 );
+
+	m_staticline = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
+	bSizerMain->Add( m_staticline, 0, wxEXPAND | wxALL, 5 );
+
 	m_sdbSizer = new wxStdDialogButtonSizer();
 	m_sdbSizerOK = new wxButton( this, wxID_OK );
 	m_sdbSizer->AddButton( m_sdbSizerOK );
 	m_sdbSizerCancel = new wxButton( this, wxID_CANCEL );
 	m_sdbSizer->AddButton( m_sdbSizerCancel );
 	m_sdbSizer->Realize();
-	
-	bSizerMain->Add( m_sdbSizer, 0, wxEXPAND|wxBOTTOM|wxRIGHT|wxLEFT, 5 );
-	
-	
+
+	bSizerMain->Add( m_sdbSizer, 0, wxBOTTOM|wxRIGHT|wxLEFT|wxALIGN_RIGHT, 5 );
+
+
 	this->SetSizer( bSizerMain );
 	this->Layout();
 	bSizerMain->Fit( this );
-	
+
 	this->Centre( wxBOTH );
-	
+
 	// Connect Events
 	m_leftGridLayers->Connect( wxEVT_GRID_CELL_LEFT_CLICK, wxGridEventHandler( DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::OnLeftGridCellClick ), NULL, this );
 	m_rightGridLayers->Connect( wxEVT_GRID_CELL_LEFT_CLICK, wxGridEventHandler( DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::OnRightGridCellClick ), NULL, this );
@@ -230,5 +224,5 @@ DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::~DIALOG_COPPER_LAYER_PAIR_SELECTION_BAS
 	// Disconnect Events
 	m_leftGridLayers->Disconnect( wxEVT_GRID_CELL_LEFT_CLICK, wxGridEventHandler( DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::OnLeftGridCellClick ), NULL, this );
 	m_rightGridLayers->Disconnect( wxEVT_GRID_CELL_LEFT_CLICK, wxGridEventHandler( DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE::OnRightGridCellClick ), NULL, this );
-	
+
 }
--- pcbnew/dialogs/dialog_layer_selection_base.h
+++ pcbnew/dialogs/dialog_layer_selection_base.h
@@ -1,16 +1,16 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Dec 30 2017)
+// C++ code generated with wxFormBuilder (version Jul 10 2019)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
-#ifndef __DIALOG_LAYER_SELECTION_BASE_H__
-#define __DIALOG_LAYER_SELECTION_BASE_H__
+#pragma once
 
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
 #include <wx/intl.h>
+#include "dialog_shim.h"
 #include <wx/colour.h>
 #include <wx/settings.h>
 #include <wx/string.h>
@@ -31,54 +31,53 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// Class DIALOG_LAYER_SELECTION_BASE
 ///////////////////////////////////////////////////////////////////////////////
-class DIALOG_LAYER_SELECTION_BASE : public wxDialog 
+class DIALOG_LAYER_SELECTION_BASE : public DIALOG_SHIM
 {
 	private:
-	
+
 	protected:
 		wxGrid* m_leftGridLayers;
 		wxGrid* m_rightGridLayers;
-		
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void OnLeftGridCellClick( wxGridEvent& event ) { event.Skip(); }
 		virtual void OnLeftButtonReleased( wxMouseEvent& event ) { event.Skip(); }
 		virtual void OnRightGridCellClick( wxGridEvent& event ) { event.Skip(); }
-		
-	
+
+
 	public:
-		
-		DIALOG_LAYER_SELECTION_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Select Layer"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+
+		DIALOG_LAYER_SELECTION_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Select Layer"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
 		~DIALOG_LAYER_SELECTION_BASE();
-	
+
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Class DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE
 ///////////////////////////////////////////////////////////////////////////////
-class DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE : public wxDialog 
+class DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE : public DIALOG_SHIM
 {
 	private:
-	
+
 	protected:
 		wxStaticText* m_staticTextTopLayer;
 		wxGrid* m_leftGridLayers;
 		wxStaticText* m_staticTextBottomLayer;
 		wxGrid* m_rightGridLayers;
-		wxStaticLine* m_staticline1;
+		wxStaticLine* m_staticline;
 		wxStdDialogButtonSizer* m_sdbSizer;
 		wxButton* m_sdbSizerOK;
 		wxButton* m_sdbSizerCancel;
-		
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void OnLeftGridCellClick( wxGridEvent& event ) { event.Skip(); }
 		virtual void OnRightGridCellClick( wxGridEvent& event ) { event.Skip(); }
-		
-	
+
+
 	public:
-		
-		DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Select Copper Layer Pair"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+
+		DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Select Copper Layer Pair"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
 		~DIALOG_COPPER_LAYER_PAIR_SELECTION_BASE();
-	
+
 };
 
-#endif //__DIALOG_LAYER_SELECTION_BASE_H__
