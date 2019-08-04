c60118b6a462f86e005c715df62acd858e3dbeb4
Jeff Young <jeff@rokeby.ie>
2019-08-03 22:16:35 -0600

    Ativate delete tool so it can display the selection clarification menu when needed.
    Fixes: lp:1835371
    * https://bugs.launchpad.net/kicad/+bug/1835371

--- pcbnew/tools/edit_tool.cpp
+++ pcbnew/tools/edit_tool.cpp
@@ -805,6 +805,7 @@ int EDIT_TOOL::Remove( const TOOL_EVENT& aEvent )
         return 1;
 
     std::vector<BOARD_ITEM*> lockedItems;
+    Activate();
 
     // get a copy instead of reference (as we're going to clear the selection before removing items)
     PCBNEW_SELECTION selectionCopy;
