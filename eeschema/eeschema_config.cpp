ab44cd95b8d764cda59c12aae4e7413dcaf5b2d1
Franck Jullien <franck.jullien@gmail.com>
2019-10-24 19:07:01 +0200

    eeschema: Allow hierarchy navigator to stay open
    ADD: User can now decide to keep the hierarchy navigator open while working
    on a schematic.
    
    This behavior can be configured in eeschema->preferences->eeschema->Editing options.
    
    Signed-off-by: Franck Jullien <franck.jullien@gmail.com>

--- eeschema/eeschema_config.cpp
+++ eeschema/eeschema_config.cpp
@@ -294,6 +294,7 @@ const wxChar AutoplaceJustifyEntry[] =              wxT( "AutoplaceJustify" );
 const wxChar AutoplaceAlignEntry[] =                wxT( "AutoplaceAlign" );
 static const wxChar DragActionIsMoveEntry[] =       wxT( "DragActionIsMove" );
 static const wxChar FootprintPreviewEntry[] =       wxT( "FootprintPreview" );
+static const wxChar NavigatorStaysOpenEntry[] =     wxT( "NavigatorStaysOpen" );
 static const wxChar DefaultBusWidthEntry[] =        wxT( "DefaultBusWidth" );
 static const wxChar DefaultWireWidthEntry[] =       wxT( "DefaultWireWidth" );
 static const wxChar DefaultDrawLineWidthEntry[] =   wxT( "DefaultDrawLineWidth" );
@@ -401,6 +402,7 @@ void SCH_EDIT_FRAME::LoadSettings( wxConfigBase* aCfg )
     aCfg->Read( AutoplaceJustifyEntry, &m_autoplaceJustify, true );
     aCfg->Read( AutoplaceAlignEntry, &m_autoplaceAlign, false );
     aCfg->Read( FootprintPreviewEntry, &m_footprintPreview, false );
+    aCfg->Read( NavigatorStaysOpenEntry, &m_navigatorStaysOpen, false );
 
     wxString templateFieldNames = aCfg->Read( FieldNamesEntry, wxEmptyString );
 
@@ -447,6 +449,7 @@ void SCH_EDIT_FRAME::SaveSettings( wxConfigBase* aCfg )
     aCfg->Write( boxedSelectedText, GetSelectionTextAsBox() );
     aCfg->Write( drawSelectedChildren, GetSelectionDrawChildItems() );
     aCfg->Write( selectionFillShapes, GetSelectionFillShapes() );
+    aCfg->Write( NavigatorStaysOpenEntry, m_navigatorStaysOpen );
 
     // Save template fieldnames
     STRING_FORMATTER sf;
