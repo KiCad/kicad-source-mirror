ffcf3b01fce98f1bcbdf3b76fbc88228126be965
Seth Hillbrand <seth@kipro-pcb.com>
2019-11-26 23:13:01 -0800

    Fixup issue with adding junctions
    Segfault when we pass a component to the routine as it expects to see a
    selection.

--- eeschema/tools/sch_drawing_tools.cpp
+++ eeschema/tools/sch_drawing_tools.cpp
@@ -191,7 +191,10 @@ int SCH_DRAWING_TOOLS::PlaceComponent(  const TOOL_EVENT& aEvent  )
                 m_view->ClearPreview();
                 m_frame->AddItemToScreenAndUndoList( component );
 
-                m_toolMgr->RunAction( EE_ACTIONS::addNeededJunctions, true, component );
+                EE_SELECTION new_sel;
+                new_sel.Add( component );
+
+                m_toolMgr->RunAction( EE_ACTIONS::addNeededJunctions, true, &new_sel );
                 m_frame->OnModify();
 
                 if( m_frame->GetUseAllUnits() || m_frame->GetRepeatComponent() )
