a9da0c6ecf382e5ba840fd188dd1a0c3ea832a33
Jeff Young <jeff@rokeby.ie>
2019-12-02 14:55:00 +0000

    Update local coords when editing EDGE_MODULEs.
    Fixes: lp:1853726
    * https://bugs.launchpad.net/kicad/+bug/1853726

--- pcbnew/tools/point_editor.cpp
+++ pcbnew/tools/point_editor.cpp
@@ -1048,6 +1048,9 @@ int POINT_EDITOR::addCorner( const TOOL_EVENT& aEvent )
         // Move the end of the line to the break point..
         graphicItem->SetEnd( wxPoint( nearestPoint.x, nearestPoint.y ) );
 
+        if( graphicItem->Type() == PCB_MODULE_EDGE_T )
+            static_cast<EDGE_MODULE*>( graphicItem )->SetLocalCoord();
+
         // and add another one starting from the break point
         DRAWSEGMENT* newSegment;
 
@@ -1065,6 +1068,9 @@ int POINT_EDITOR::addCorner( const TOOL_EVENT& aEvent )
         newSegment->ClearSelected();
         newSegment->SetStart( wxPoint( nearestPoint.x, nearestPoint.y ) );
         newSegment->SetEnd( wxPoint( seg.B.x, seg.B.y ) );
+
+        if( newSegment->Type() == PCB_MODULE_EDGE_T )
+            static_cast<EDGE_MODULE*>( newSegment )->SetLocalCoord();
 
         commit.Add( newSegment );
         commit.Push( _( "Split segment" ) );
