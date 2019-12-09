KiCad/kicad-source-mirror

commit 54f066fed7196dd3ffaf98f1e2a57165f5ca2983

Implement simple inheritance for library symbols.
This change completely removes the LIB_ALIAS design pattern an replaces
it by allowing LIB_PART objects to inherit from other LIB_PART objects.
The initial implementation only allows for single inheritance and only
supports the mandatory fields in the derived part because that is all
that the current symbol library file format will support.  Once the new
file format is implemented and saving to the old file format is deprecated,
more complex inheritance will be added.  The LIB_ALIAS information saved
in the document files was move into the LIB_PART object.  This change
impacts virtually every part of the schematic and symbol library editor
code so this commit message is woefully incomplete.

REMOVE: Removed the symbol aliases concept from the schematic and symbol
editors and the symbol viewer.

NEW: Replace the symbol alias concept with simple inheritance that allows
a library symbol to be derived from another library symbol.

eeschema/project_rescue.cpp
@@ -139,8 +139,8 @@ void RESCUE_CASE_CANDIDATE::FindRescues( RESCUER& aRescuer,
     candidate_map_t candidate_map;
     // Remember the list of components is sorted by part name.
     // So a search in libraries is made only once by group
-    LIB_ALIAS* case_sensitive_match = nullptr;
-    std::vector<LIB_ALIAS*> case_insensitive_matches;
+    LIB_PART* case_sensitive_match = nullptr;
+    std::vector<LIB_PART*> case_insensitive_matches;
 
     wxString last_part_name;
 
@@ -157,7 +157,7 @@ void RESCUE_CASE_CANDIDATE::FindRescues( RESCUER& aRescuer,
 
             LIB_ID id( wxEmptyString, part_name );
 
-            case_sensitive_match = aRescuer.GetPrj()->SchLibs()->FindLibraryAlias( id );
+            case_sensitive_match = aRescuer.GetPrj()->SchLibs()->FindLibPart( id );
 
             if( !case_sensitive_match )
                 // the case sensitive match failed. Try a case insensitive match
@@ -169,7 +169,7 @@ void RESCUE_CASE_CANDIDATE::FindRescues( RESCUER& aRescuer,
             continue;
 
         RESCUE_CASE_CANDIDATE candidate( part_name, case_insensitive_matches[0]->GetName(),
-                                         case_insensitive_matches[0]->GetPart() );
+                                         case_insensitive_matches[0] );
 
         candidate_map[part_name] = candidate;
     }
@@ -310,7 +310,6 @@ bool RESCUE_CACHE_CANDIDATE::PerformAction( RESCUER* aRescuer )
 
     LIB_PART new_part( *tmp );
     new_part.SetName( m_new_name );
-    new_part.RemoveAllAliases();
     aRescuer->AddPart( &new_part );
 
     for( SCH_COMPONENT* each_component : *aRescuer->GetComponents() )
@@ -446,7 +445,6 @@ bool RESCUE_SYMBOL_LIB_TABLE_CANDIDATE::PerformAction( RESCUER* aRescuer )
     LIB_PART new_part( *tmp );
     new_part.SetLibId( m_new_id );
     new_part.SetName( m_new_id.GetLibItemName() );
-    new_part.RemoveAllAliases();
     aRescuer->AddPart( &new_part );
 
     for( SCH_COMPONENT* each_component : *aRescuer->GetComponents() )
@@ -654,17 +652,14 @@ void LEGACY_RESCUER::OpenRescueLibrary()
     if( rescueLib )
     {
         // For items in the rescue library, aliases are the root symbol.
-        std::vector< LIB_ALIAS* > aliases;
+        std::vector< LIB_PART* > symbols;
 
-        rescueLib->GetAliases( aliases );
+        rescueLib->GetParts( symbols );
 
-        for( auto alias : aliases )
+        for( auto symbol : symbols )
         {
-            LIB_PART* part = alias->GetPart();
-
-            wxCHECK2( part, continue );
-
-            m_rescue_lib->AddPart( new LIB_PART( *part, m_rescue_lib.get() ) );
+            // The LIB_PART copy constructor flattens derived symbols (formerly known as aliases).
+            m_rescue_lib->AddPart( new LIB_PART( *symbol, m_rescue_lib.get() ) );
         }
     }
 }

commit 30da2b31ea1b13f180cea294a045fb039a48bd3b

Eeschema: fix multiple symbol rescue bug caused by commit 54f066fe.
Use the root symbol when comparing against the cached symbol when
checking to see if a symbol changed.  When the original symbol is
a derived symbol, the test will always fail.

Force a symbol link refresh on a rescue to prevent stale links from
crashing the connection graph refresh when running the project rescue
on demand.

Force a symbol link refresh on project load to prevent stale links from
symbol link changes from crashing the connection graph when the project
rescue in invoked.

Fixes kicad/code/kicad#3645

eeschema/files-io.cpp
@@ -388,7 +388,9 @@ bool SCH_EDIT_FRAME::OpenProjectFiles( const std::vector<wxString>& aFileSet, in
                 RescueSymbolLibTableProject( false );
         }
 
-        schematic.UpdateSymbolLinks();      // Update all symbol library links for all sheets.
+        schematic.UpdateSymbolLinks( true );      // Update all symbol library links for all sheets.
+        g_ConnectionGraph->Reset();
+        RecalculateConnections( GLOBAL_CLEANUP );
         SetScreen( g_CurrentSheet->LastScreen() );
 
         // Migrate conflicting bus definitions

eeschema/project_rescue.cpp
@@ -25,6 +25,7 @@
 #include <sch_draw_panel.h>
 #include <class_library.h>
 #include <confirm.h>
+#include <connection_graph.h>
 #include <invoke_sch_dialog.h>
 #include <kiway.h>
 #include <project_rescue.h>
@@ -373,13 +374,34 @@ void RESCUE_SYMBOL_LIB_TABLE_CANDIDATE::FindRescues(
             // A new part name is found (a new group starts here).
             // Search the symbol names candidates only once for this group:
             old_part_id = part_id;
+
+            // Get the library symbol from the cache library.  It will be a flattened
+            // symbol by default (no inheritance).
             cache_match = find_component( part_id.Format().wx_str(), aRescuer.GetPrj()->SchLibs(),
                                           true );
 
+            // Get the library symbol from the symbol library table.
             lib_match = SchGetLibPart( part_id, aRescuer.GetPrj()->SchSymbolLibTable() );
 
             if( !cache_match && !lib_match )
                 continue;
+
+            PART_SPTR lib_match_parent;
+
+            // If it's a derive symbol, use the parent symbol to perform the pin test.
+            if( lib_match && lib_match->IsAlias() )
+            {
+                lib_match_parent = lib_match->GetParent().lock();
+
+                if( !lib_match_parent )
+                {
+                    lib_match = nullptr;
+                }
+                else
+                {
+                    lib_match = lib_match_parent.get();
+                }
+            }
 
             // Test whether there is a conflict or if the symbol can only be found in the cache.
             if( LIB_ID::HasIllegalChars( part_id.GetLibItemName(), LIB_ID::ID_SCH ) == -1 )
@@ -534,6 +556,15 @@ bool SCH_EDIT_FRAME::rescueProject( RESCUER& aRescuer, bool aRunningOnDemand )
 
         if( viewer )
             viewer->ReCreateListLib();
+
+        if( aRunningOnDemand )
+        {
+            SCH_SCREENS schematic;
+
+            schematic.UpdateSymbolLinks( true );
+            g_ConnectionGraph->Reset();
+            RecalculateConnections( GLOBAL_CLEANUP );
+        }
 
         GetScreen()->ClearUndoORRedoList( GetScreen()->m_UndoList, 1 );
         SyncView();