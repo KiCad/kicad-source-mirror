8464203395165ace39f88f0c79e04229ee6ec6b0
Mark Roszko <mark.roszko@gmail.com>
2019-12-27 23:39:40 -0500

    Update dialog_edit_line_style_base via formbuilder version upda te

--- eeschema/dialogs/dialog_edit_line_style_base.cpp
+++ eeschema/dialogs/dialog_edit_line_style_base.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Apr 19 2018)
+// C++ code generated with wxFormBuilder (version Oct 26 2018)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -17,63 +17,65 @@ END_EVENT_TABLE()
 DIALOG_EDIT_LINE_STYLE_BASE::DIALOG_EDIT_LINE_STYLE_BASE( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : DIALOG_SHIM( parent, id, title, pos, size, style )
 {
 	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
-	
+
 	wxBoxSizer* mainSizer;
 	mainSizer = new wxBoxSizer( wxVERTICAL );
-	
+
 	wxBoxSizer* bSizerUpper;
 	bSizerUpper = new wxBoxSizer( wxHORIZONTAL );
-	
+
 	wxStaticBoxSizer* sbSizerGeneral;
 	sbSizerGeneral = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, _("General:") ), wxVERTICAL );
-	
+
 	wxFlexGridSizer* fgSizerGeneral;
 	fgSizerGeneral = new wxFlexGridSizer( 0, 3, 0, 0 );
 	fgSizerGeneral->AddGrowableCol( 1 );
 	fgSizerGeneral->SetFlexibleDirection( wxBOTH );
 	fgSizerGeneral->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
-	
+
 	m_staticTextWidth = new wxStaticText( sbSizerGeneral->GetStaticBox(), wxID_ANY, _("Width:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticTextWidth->Wrap( -1 );
 	fgSizerGeneral->Add( m_staticTextWidth, 0, wxALIGN_CENTER_VERTICAL|wxALL, 3 );
-	
+
 	m_lineWidth = new wxTextCtrl( sbSizerGeneral->GetStaticBox(), wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize( -1,-1 ), 0 );
 	m_lineWidth->SetMinSize( wxSize( 80,-1 ) );
-	
+
 	fgSizerGeneral->Add( m_lineWidth, 0, wxALIGN_CENTER_VERTICAL|wxALL|wxEXPAND, 3 );
-	
+
 	m_staticWidthUnits = new wxStaticText( sbSizerGeneral->GetStaticBox(), wxID_ANY, _("unit"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticWidthUnits->Wrap( -1 );
 	fgSizerGeneral->Add( m_staticWidthUnits, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1 );
-	
+
 	m_staticText5 = new wxStaticText( sbSizerGeneral->GetStaticBox(), wxID_ANY, _("Color:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText5->Wrap( -1 );
 	fgSizerGeneral->Add( m_staticText5, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
-	
-	m_colorButton = new wxBitmapButton( sbSizerGeneral->GetStaticBox(), idColorBtn, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW );
+
+	m_colorButton = new wxBitmapButton( sbSizerGeneral->GetStaticBox(), idColorBtn, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW|0 );
+
+	m_colorButton->SetBitmap( wxNullBitmap );
 	fgSizerGeneral->Add( m_colorButton, 0, wxALL|wxALIGN_CENTER_VERTICAL|wxEXPAND, 5 );
-	
-	
+
+
 	sbSizerGeneral->Add( fgSizerGeneral, 3, wxEXPAND, 5 );
-	
-	
+
+
 	bSizerUpper->Add( sbSizerGeneral, 3, wxALL|wxEXPAND, 5 );
-	
+
 	wxString m_lineStyleChoices[] = { _("Solid"), _("Dashed"), _("Dotted"), _("Dash-Dot") };
 	int m_lineStyleNChoices = sizeof( m_lineStyleChoices ) / sizeof( wxString );
 	m_lineStyle = new wxRadioBox( this, wxID_ANY, _("Line Style:"), wxDefaultPosition, wxDefaultSize, m_lineStyleNChoices, m_lineStyleChoices, 4, wxRA_SPECIFY_ROWS );
 	m_lineStyle->SetSelection( 1 );
 	bSizerUpper->Add( m_lineStyle, 2, wxALL|wxEXPAND, 5 );
-	
-	
+
+
 	mainSizer->Add( bSizerUpper, 0, wxEXPAND, 5 );
-	
-	
+
+
 	mainSizer->Add( 0, 0, 1, wxALL|wxEXPAND, 1 );
-	
+
 	m_staticline = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
 	mainSizer->Add( m_staticline, 0, wxEXPAND | wxALL, 5 );
-	
+
 	m_sdbSizer = new wxStdDialogButtonSizer();
 	m_sdbSizerOK = new wxButton( this, wxID_OK );
 	m_sdbSizer->AddButton( m_sdbSizerOK );
@@ -82,13 +84,13 @@ DIALOG_EDIT_LINE_STYLE_BASE::DIALOG_EDIT_LINE_STYLE_BASE( wxWindow* parent, wxWi
 	m_sdbSizerCancel = new wxButton( this, wxID_CANCEL );
 	m_sdbSizer->AddButton( m_sdbSizerCancel );
 	m_sdbSizer->Realize();
-	
+
 	mainSizer->Add( m_sdbSizer, 0, wxALL|wxALIGN_RIGHT, 5 );
-	
-	
+
+
 	this->SetSizer( mainSizer );
 	this->Layout();
-	
+
 	this->Centre( wxBOTH );
 }
 
--- eeschema/dialogs/dialog_edit_line_style_base.h
+++ eeschema/dialogs/dialog_edit_line_style_base.h
@@ -1,12 +1,11 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Apr 19 2018)
+// C++ code generated with wxFormBuilder (version Oct 26 2018)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
-#ifndef __DIALOG_EDIT_LINE_STYLE_BASE_H__
-#define __DIALOG_EDIT_LINE_STYLE_BASE_H__
+#pragma once
 
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
@@ -19,10 +18,10 @@
 #include <wx/colour.h>
 #include <wx/settings.h>
 #include <wx/textctrl.h>
+#include <wx/bmpbuttn.h>
 #include <wx/bitmap.h>
 #include <wx/image.h>
 #include <wx/icon.h>
-#include <wx/bmpbuttn.h>
 #include <wx/button.h>
 #include <wx/sizer.h>
 #include <wx/statbox.h>
@@ -39,18 +38,18 @@ class DIALOG_EDIT_LINE_STYLE_BASE : public DIALOG_SHIM
 {
 	DECLARE_EVENT_TABLE()
 	private:
-		
+
 		// Private event handlers
 		void _wxFB_onColorButtonClicked( wxCommandEvent& event ){ onColorButtonClicked( event ); }
 		void _wxFB_resetDefaults( wxCommandEvent& event ){ resetDefaults( event ); }
-		
-	
+
+
 	protected:
 		enum
 		{
 			idColorBtn = 1000
 		};
-		
+
 		wxStaticText* m_staticTextWidth;
 		wxTextCtrl* m_lineWidth;
 		wxStaticText* m_staticWidthUnits;
@@ -62,18 +61,17 @@ class DIALOG_EDIT_LINE_STYLE_BASE : public DIALOG_SHIM
 		wxButton* m_sdbSizerOK;
 		wxButton* m_sdbSizerApply;
 		wxButton* m_sdbSizerCancel;
-		
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void onColorButtonClicked( wxCommandEvent& event ) { event.Skip(); }
 		virtual void resetDefaults( wxCommandEvent& event ) { event.Skip(); }
-		
-	
+
+
 	public:
-		bool m_isValid; 
-		
-		DIALOG_EDIT_LINE_STYLE_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Line Style"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 298,204 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+		bool m_isValid;
+
+		DIALOG_EDIT_LINE_STYLE_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Line Style"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 298,204 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
 		~DIALOG_EDIT_LINE_STYLE_BASE();
-	
+
 };
 
-#endif //__DIALOG_EDIT_LINE_STYLE_BASE_H__
