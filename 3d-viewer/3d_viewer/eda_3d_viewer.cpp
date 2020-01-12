5eecd8987e48b8d3c50a858599ba3e0f0dab61a0
Fabien Corona <f.corona@laposte.net>
2020-01-12 17:21:43 +0100

    fix #include paths

--- 3d-viewer/3d_viewer/eda_3d_viewer.cpp
+++ 3d-viewer/3d_viewer/eda_3d_viewer.cpp
@@ -24,13 +24,14 @@
 
 #include "eda_3d_viewer.h"
 
-#include "../../pcbnew/board_stackup_manager/class_board_stackup.h"
-#include "../../pcbnew/board_stackup_manager/stackup_predefined_prms.h"
-#include "../../pcbnew/class_board.h"
+
 #include "../3d_viewer_id.h"
 #include "../common_ogl/cogl_att_list.h"
 #include <3d_actions.h>
 #include <bitmaps.h>
+#include <board_stackup_manager/class_board_stackup.h>
+#include <board_stackup_manager/stackup_predefined_prms.h>
+#include <class_board.h>
 #include <dpi_scaling.h>
 #include <gestfich.h>
 #include <hotkeys_basic.h>
