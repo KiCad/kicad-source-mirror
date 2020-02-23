a8e905a1e8d0a2a4988d71fe2630bd40fb034e77
jean-pierre charras <jp.charras@wanadoo.fr>
2020-02-23 20:15:53 +0100

    QA: fix a python test issue due to code changes in board.h
    FindModule() does not exist anymore. Use FindModuleByReference() instead.
    
    Fixes #3927
    https://gitlab.com/kicad/code/kicad/issues/3927

--- qa/testcases/test_002_board_class.py
+++ qa/testcases/test_002_board_class.py
@@ -23,7 +23,7 @@ class TestBoardClass(unittest.TestCase):
         self.FILENAME=tempfile.mktemp()+".kicad_pcb"
 
     def test_pcb_find_module(self):
-        module = self.pcb.FindModule('P1')
+        module = self.pcb.FindModuleByReference('P1')
         self.assertEqual(module.GetReference(),'P1')
 
     def test_pcb_get_track_count(self):
@@ -43,9 +43,6 @@ class TestBoardClass(unittest.TestCase):
         pcb = BOARD()
         track = TRACK(pcb)
         pcb.Add(track)
-
-        #track.SetStartEnd(wxPointMM(10.0, 10.0),
-        #                  wxPointMM(20.0, 30.0))
 
         track.SetStart(wxPointMM(10.0, 10.0))
         track.SetEnd(wxPointMM(20.0, 30.0))
