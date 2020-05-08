d645c22411cd9ac8ccb77814bcbd83c1d57a9355
Jeff Young <jeff@rokeby.ie>
2020-05-08 23:57:23 +0100

    Allow path separators in the sheet filename validator.
    Fixes https://gitlab.com/kicad/code/kicad/issues/4374

--- eeschema/sch_validators.cpp
+++ eeschema/sch_validators.cpp
@@ -47,9 +47,7 @@ SCH_FIELD_VALIDATOR::SCH_FIELD_VALIDATOR(  bool aIsLibEditor, int aFieldId, wxSt
     {
         excludes += " ";
     }
-    else if( ( aFieldId == VALUE && m_isLibEditor )
-                || aFieldId == SHEETNAME_V
-                || aFieldId == SHEETFILENAME_V )
+    else if( ( aFieldId == VALUE && m_isLibEditor ) || aFieldId == SHEETNAME_V )
     {
         excludes += " :/\\";
     }
