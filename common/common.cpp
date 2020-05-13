3a9746c65747740344f1eea1a0f8ce9e23d4d615
Jeff Young <jeff@rokeby.ie>
2020-05-13 23:45:04 +0100

    Add ~ processing in env vars.
    Fixes https://gitlab.com/kicad/code/kicad/issues/1829

--- common/common.cpp
+++ common/common.cpp
@@ -554,6 +554,11 @@ wxString KIwxExpandEnvVars( const wxString& str, const PROJECT* aProject )
         }
     }
 
+#ifndef __WINDOWS__
+    if( strResult.StartsWith( "~" ) )
+        strResult.Replace( "~", wxGetHomeDir(), false );
+#endif // __WINDOWS__
+
     return strResult;
 }
 
