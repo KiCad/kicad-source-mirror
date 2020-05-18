5a9e69b146569ec782ac490ad9d03ad121b25ba2
Jon Evans <jon@craftyjon.com>
2020-05-18 17:47:22 -0400

    Use the eeschema color theme for the symbol preview canvas
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/4484

--- eeschema/widgets/symbol_preview_widget.cpp
+++ eeschema/widgets/symbol_preview_widget.cpp
@@ -66,6 +66,10 @@ SYMBOL_PREVIEW_WIDGET::SYMBOL_PREVIEW_WIDGET( wxWindow* aParent, KIWAY& aKiway,
     // before any OnPaint event is fired for the canvas using a wrong bg color
     KIGFX::VIEW* view = m_preview->GetView();
     auto settings = static_cast<KIGFX::SCH_RENDER_SETTINGS*>( view->GetPainter()->GetSettings() );
+
+    if( auto* theme = Pgm().GetSettingsManager().GetColorSettings( app_settings->m_ColorTheme ) )
+        settings->LoadColors( theme );
+
     m_preview->GetGAL()->SetClearColor( settings->GetBackgroundColor() );
 
     SetBackgroundColour( *wxWHITE );
