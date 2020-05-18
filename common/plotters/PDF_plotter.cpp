d8a74c13b88f0b96b28c1442c1347a75078401c4
Jeff Young <jeff@rokeby.ie>
2020-05-18 01:42:04 +0100

    Fix a syntax error in the PDF generator.
    The /Trapped key needs to be the name False, which is not the same
    as the boolean false.  Thanks PDF, that's clear as mud.

--- common/plotters/PDF_plotter.cpp
+++ common/plotters/PDF_plotter.cpp
@@ -752,7 +752,7 @@ bool PDF_PLOTTER::EndPlot()
              "/CreationDate (%s)\n"
              "/Creator (%s)\n"
              "/Title (%s)\n"
-             "/Trapped No\n",
+             "/Trapped False\n",
              date_buf,
              TO_UTF8( creator ),
              TO_UTF8( title ) );
