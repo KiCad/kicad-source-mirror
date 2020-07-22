85aecc15eedb35495aad5e69c6f6d0a06ecd0d2d
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2020-07-23 01:04:52 +0200

    router: fix mistake in 45/free-angle mode selection when dragging corners

--- pcbnew/router/pns_dragger.cpp
+++ pcbnew/router/pns_dragger.cpp
@@ -362,9 +362,9 @@ bool DRAGGER::dragWalkaround( const VECTOR2I& aP )
         LINE origLine( m_draggedLine );
         
         if( m_mode == DM_SEGMENT )
-            dragged.DragSegment( aP, m_draggedSegmentIndex, false );
+            dragged.DragSegment( aP, m_draggedSegmentIndex );
         else
-            dragged.DragCorner( aP, m_draggedSegmentIndex, thresh );
+            dragged.DragCorner( aP, m_draggedSegmentIndex );
 
         if ( m_world->CheckColliding( &dragged ) )
         {
