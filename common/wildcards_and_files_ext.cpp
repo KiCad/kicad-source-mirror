a1b79298a4c534fc51b0f63176a28bfffff0ac9c
PJM <Me@NotReal.com>
2020-07-23 16:25:31 -0700

    GerbView #2124 : Alternative drill file extension .txt
    '.txt' is a common file extension for drill files.  This Merge
    Request adds it to the list of extensions used in the File Open dialog
    for Excellon Drill files.
    
    Fixes issue https://gitlab.com/kicad/code/kicad/-/issues/2124

--- common/wildcards_and_files_ext.cpp
+++ common/wildcards_and_files_ext.cpp
@@ -342,7 +342,7 @@ wxString ComponentFileWildcard()
 // Wildcard for reports and fabrication documents
 wxString DrillFileWildcard()
 {
-    return _( "Drill files" ) + AddFileExtListToFilter( { "drl", "nc", "xnc" } );
+    return _( "Drill files" ) + AddFileExtListToFilter( { "drl", "nc", "xnc", "txt" } );
 }
 
 
