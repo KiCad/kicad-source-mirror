3674064b9e5afc7909dd845883bd35b9b9512b00
Marek Roszko <mark.roszko@gmail.com>
2020-08-15 14:38:01 -0400

    Fix double msgpanel refresh and use the new moved event

--- eeschema/tools/sch_move_tool.cpp
+++ eeschema/tools/sch_move_tool.cpp
@@ -344,8 +344,7 @@ int SCH_MOVE_TOOL::Main( const TOOL_EVENT& aEvent )
                 updateView( item );
             }
 
-            m_toolMgr->PostEvent( EVENTS::SelectedItemsModified );
-            m_frame->UpdateMsgPanel();
+            m_toolMgr->PostEvent( EVENTS::SelectedItemsMoved );
         }
         //------------------------------------------------------------------------
         // Handle cancel
