884bed7e12ca1fbd960ffe2fc41c0194fb6150e1
Dominik Wernberger <dominik.wernberger@gmx.de>
2020-09-20 17:42:00 +0200

    Fix that all file types are shown in file dialog

--- pcbnew/dialogs/dialog_import_settings.cpp
+++ pcbnew/dialogs/dialog_import_settings.cpp
@@ -111,7 +111,6 @@ bool DIALOG_IMPORT_SETTINGS::TransferDataToWindow()
 void DIALOG_IMPORT_SETTINGS::OnBrowseClicked( wxCommandEvent& event )
 {
     wxFileName fn = m_frame->GetBoard()->GetFileName();
-    fn.SetExt( ProjectFileExtension );
 
     wxFileDialog dlg( this, _( "Import Settings From" ), fn.GetPath(), fn.GetFullName(),
                       PcbFileWildcard(), wxFD_OPEN | wxFD_FILE_MUST_EXIST | wxFD_CHANGE_DIR );
