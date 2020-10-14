02787784a46661cf38f142688fb8a057e205fd42
Seth Hillbrand <seth@kipro-pcb.com>
2020-10-14 08:47:36 -0700

    Bump OCE Plugin version
    We modified how we search for color information.  Changing the version
    data will for models to reload instead of keeping the cached data with
    (possibly) missing color information
    
    (cherry picked from commit b51ca1ab0e8be01271e179a66844d4d6e101aeac)

--- plugins/3d/oce/oce.cpp
+++ plugins/3d/oce/oce.cpp
@@ -33,8 +33,8 @@
 SCENEGRAPH* LoadModel( char const* filename );
 
 #define PLUGIN_OCE_MAJOR 1
-#define PLUGIN_OCE_MINOR 1
-#define PLUGIN_OCE_PATCH 1
+#define PLUGIN_OCE_MINOR 2
+#define PLUGIN_OCE_PATCH 0
 #define PLUGIN_OCE_REVNO 0
 
 
