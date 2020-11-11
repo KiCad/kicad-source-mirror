6570cd5850d601c52396a0796455d1bc3233f8a0
jean-pierre charras <jp.charras@wanadoo.fr>
2020-11-11 12:02:23 +0100

    minor compil warning fix

--- 3d-viewer/3d_rendering/3d_render_raytracing/accelerators/ccontainer2d.h
+++ 3d-viewer/3d_rendering/3d_render_raytracing/accelerators/ccontainer2d.h
@@ -1,8 +1,8 @@
 /*
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
- * Copyright (C) 2015-2016 Mario Luzeiro <mrluzeiro@ua.pt>
- * Copyright (C) 1992-2016 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2015-2020 Mario Luzeiro <mrluzeiro@ua.pt>
+ * Copyright (C) 1992-2020 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -119,7 +119,8 @@ public:
 
     void BuildBVH();
 
-    void Clear();
+    void Clear() override;
+
 private:
     bool m_isInitialized;
     std::list<BVH_CONTAINER_NODE_2D *> m_elements_to_delete;
