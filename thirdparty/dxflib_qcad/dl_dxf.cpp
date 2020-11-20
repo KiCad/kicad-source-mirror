baebf7036f80fee81056fa71f3e6f3725fe8cc4d
Marek Roszko <mark.roszko@gmail.com>
2020-11-19 19:01:22 -0500

    dxflib_qcad: Saving dimension with custom text position
    upstream commit b4c74180a6857c7b63f7a71e1236930e85c15134

--- thirdparty/dxflib_qcad/dl_dxf.cpp
+++ thirdparty/dxflib_qcad/dl_dxf.cpp
@@ -3512,6 +3512,12 @@ void DL_Dxf::writeDimStyleOverrides( DL_WriterA& dw,
         dw.dxfString( 1001, "ACAD" );
         dw.dxfString( 1000, "DSTYLE" );
         dw.dxfString( 1002, "{" );
+        if( data.type & 128 )
+        {
+            // custom text position:
+            dw.dxfInt( 1070, 279 );
+            dw.dxfInt( 1070, 2 );
+        }
         dw.dxfInt( 1070, 144 );
         dw.dxfReal( 1040, data.linearFactor );
         dw.dxfInt( 1070, 40 );
