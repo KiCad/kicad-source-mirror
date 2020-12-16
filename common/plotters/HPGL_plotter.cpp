a59cab24b83164db402acc1f4c20e57acc57fb93
Alexis Lockwood <alexislockwood@fastmail.com>
2020-12-15 18:33:55 -0700

    HPGL: Fix missing point in custom pad outline

--- common/plotters/HPGL_plotter.cpp
+++ common/plotters/HPGL_plotter.cpp
@@ -670,7 +670,7 @@ void HPGL_PLOTTER::FlashPadCustom( const wxPoint& aPadPos, const wxSize& aSize,
         cornerList.clear();
         cornerList.reserve( poly.PointCount() );
 
-        for( int ii = 1; ii < poly.PointCount(); ++ii )
+        for( int ii = 0; ii < poly.PointCount(); ++ii )
             cornerList.emplace_back( poly.CPoint( ii ).x, poly.CPoint( ii ).y );
 
         if( cornerList.back() != cornerList.front() )
