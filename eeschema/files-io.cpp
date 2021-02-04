2c04d76ff42423dff19d6b78852b502edd177203
Roberto Fernandez Bautista <roberto.fer.bau@gmail.com>
2021-02-04 17:46:07 +0000

    Non-Kicad Schematic Importers: Do not reload all symbols after import This is required to allow handling of pin swaps in the CADSTAR importer

--- eeschema/files-io.cpp
+++ eeschema/files-io.cpp
@@ -954,28 +954,6 @@ bool SCH_EDIT_FRAME::importFile( const wxString& aFileName, int aFileType )
             SaveProjectSettings();
 
             UpdateFileHistory( aFileName );
-            SCH_SCREENS schematic( Schematic().Root() );
-            schematic.UpdateSymbolLinks();      // Update all symbol library links for all sheets.
-
-            for( SCH_SCREEN* screen = schematic.GetFirst(); screen; screen = schematic.GetNext() )
-            {
-                for( auto item : screen->Items().OfType( SCH_COMPONENT_T ) )
-                {
-                    std::vector<wxPoint> pts;
-                    SCH_COMPONENT*       cmp = static_cast<SCH_COMPONENT*>( item );
-
-                    // Update footprint LIB_ID to point to the imported Eagle library
-                    SCH_FIELD* fpField = cmp->GetField( FOOTPRINT_FIELD );
-
-                    if( !fpField->GetText().IsEmpty() )
-                    {
-                        LIB_ID fpId;
-                        fpId.Parse( fpField->GetText(), true );
-                        fpId.SetLibNickname( newfilename.GetName() );
-                        fpField->SetText( fpId.Format() );
-                    }
-                }
-            }
 
             // Only perform the dangling end test on root sheet.
             GetScreen()->TestDanglingEnds();
