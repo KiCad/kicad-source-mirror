KiCad/kicad-source-mirror

commit 7688fdc94fa98e7c084c18fc687d1f568403a4e4

Hack to prevent multiple simulation commands from stacking up.
Can happen if someone hammers away on the probe and/or tune tools
without ever mousing over the schematic in between.

*May* fix the below bug.

Fixes https://gitlab.com/kicad/code/kicad/issues/6876

include/tool/tool_manager.h
@@ -266,6 +266,11 @@ public:
      */
     inline void PostEvent( const TOOL_EVENT& aEvent )
     {
+        // Horrific hack, but it's a crash bug.  Don't let inter-frame commands stack up
+        // waiting to be processed.
+        if( aEvent.IsSimulator() && m_eventQueue.back().IsSimulator() )
+            m_eventQueue.pop_back();
+
         m_eventQueue.push_back( aEvent );
     }
 

common/tool/tool_event.cpp
@@ -219,3 +219,10 @@ bool TOOL_EVENT::IsMoveTool() const
     return( m_commandStr.is_initialized()
                 && m_commandStr.get().find( "InteractiveMove" ) != GetCommandStr()->npos );
 }
+
+
+bool TOOL_EVENT::IsSimulator() const
+{
+    return( m_commandStr.is_initialized()
+                && m_commandStr.get().find( "Simulation" ) != GetCommandStr()->npos );
+}

include/tool/tool_event.h
@@ -419,6 +419,11 @@ public:
     bool IsMoveTool() const;
 
     /**
+     * Indicate if the event is from the simulator.
+     */
+    bool IsSimulator() const;
+
+    /**
      * Return a non-standard parameter assigned to the event. Its meaning depends on the
      * target tool.
      */

commit 572ba8984af7cb09c27429dc005f103b73a90bb6

Fix the crash bug introduced by 7688fdc94fa98
Fixes #7556

include/tool/tool_manager.h
@@ -268,7 +268,7 @@ public:
     {
         // Horrific hack, but it's a crash bug.  Don't let inter-frame commands stack up
         // waiting to be processed.
-        if( aEvent.IsSimulator() && m_eventQueue.back().IsSimulator() )
+        if( aEvent.IsSimulator() && m_eventQueue.size() > 0 && m_eventQueue.back().IsSimulator() )
             m_eventQueue.pop_back();
 
         m_eventQueue.push_back( aEvent );