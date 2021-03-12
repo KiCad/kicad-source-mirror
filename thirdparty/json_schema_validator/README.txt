1d17e123717f2a85b1782af0f4dbdbe54c7606ed
qu1ck <anlutsenko@gmail.com>
2021-03-11 21:59:23 -0800

    Add json-schema-validator library

---
+++ thirdparty/json_schema_validator/README.txt
@@ -0,0 +1,7 @@
+This directory contains the pboettch/json-schema-validator project
+from https://github.com/pboettch/json-schema-validator
+with a patch to allow validating objects against specific sub definitions
+in a schema from
+https://github.com/pboettch/json-schema-validator/pull/154
+
+The project is licensed under MIT, with the license text in this directory.
---
+++ thirdparty/json_schema_validator/json-patch.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace nlohmann
+{
+class JsonPatchFormatException : public std::exception
+{
+public:
+	explicit JsonPatchFormatException(std::string msg)
+	    : ex_{std::move(msg)} {}
+
+	inline const char *what() const noexcept override final { return ex_.c_str(); }
+
+private:
+	std::string ex_;
+};
+
+class json_patch
+{
+public:
+	json_patch() = default;
+	json_patch(json &&patch);
+	json_patch(const json &patch);
+
+	json_patch &add(const json::json_pointer &, json value);
+	json_patch &replace(const json::json_pointer &, json value);
+	json_patch &remove(const json::json_pointer &);
+
+	operator json() const { return j_; }
+
+private:
+	json j_;
+
+	static void validateJsonPatch(json const &patch);
+};
+} // namespace nlohmann
