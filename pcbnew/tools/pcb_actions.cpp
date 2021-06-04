6e2724294ae7dd01582b75e1e814e99561130d14
Jon Evans <jon@craftyjon.com>
2021-06-03 21:27:15 -0400

    Fix typo in action description

--- pcbnew/tools/pcb_actions.cpp
+++ pcbnew/tools/pcb_actions.cpp
@@ -1051,7 +1051,7 @@ TOOL_ACTION PCB_ACTIONS::layerAlphaDec( "pcbnew.Control.layerAlphaDec",
         AS_GLOBAL,
         // Don't be tempted to remove "Modern Toolset only".  It's in the legacy property name.
         '{', LEGACY_HK_NAME( "Decrement Layer Transparency (Modern Toolset only)" ),
-        _( "Decrease Layer Opacity" ), _( "Make the current layer more transparent" ),
+        _( "Decrease Layer Opacity" ), _( "Make the current layer less transparent" ),
         BITMAPS::contrast_mode );
 
 TOOL_ACTION PCB_ACTIONS::layerChanged( "pcbnew.Control.layerChanged",
