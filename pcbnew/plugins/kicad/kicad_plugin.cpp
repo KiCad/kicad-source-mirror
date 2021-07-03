ab38dfd7ae87691cef099ee3287b3142d89d113c
Jon Evans <jon@craftyjon.com>
2021-07-03 19:47:58 -0400

    Fix build

--- pcbnew/plugins/kicad/kicad_plugin.cpp
+++ pcbnew/plugins/kicad/kicad_plugin.cpp
@@ -855,7 +855,7 @@ void PCB_IO::format( const PCB_SHAPE* aShape, int aNestLevel ) const
                 }
                 else
                 {
-                    SHAPE_ARC& arc = outline.Arc( ind );
+                    const SHAPE_ARC& arc = outline.Arc( ind );
                     m_out->Print( aNestLevel, "%s(arc (start %s) (mid %s) (end %s)",
                             nestLevel ? "" : " ",
                             FormatInternalUnits( arc.GetP0() ).c_str(),
