905a8366c5c5f1835affee72652b6a40f085b370
Mike Williams <mike@mikebwilliams.com>
2021-08-17 14:03:49 -0400

    Gerbview: keep highlighted and selected layers above active layer
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/7201

--- gerbview/gerbview_draw_panel_gal.cpp
+++ gerbview/gerbview_draw_panel_gal.cpp
@@ -189,8 +189,9 @@ void GERBVIEW_DRAW_PANEL_GAL::SetTopLayer( int aLayer )
 
     for( int i = 0; i < GERBER_DRAWLAYERS_COUNT; ++i )
     {
-        m_view->SetLayerOrder( GERBER_DCODE_LAYER( GERBER_DRAW_LAYER( i ) ), 2 * i );
-        m_view->SetLayerOrder( GERBER_DRAW_LAYER( i ), ( 2 * i ) + 1 );
+        m_view->SetLayerOrder( GERBER_DCODE_LAYER( GERBER_DRAW_LAYER( i ) ),
+                               GERBER_DRAW_LAYER( 2 * i ) );
+        m_view->SetLayerOrder( GERBER_DRAW_LAYER( i ), GERBER_DRAW_LAYER( ( 2 * i ) + 1 ) );
     }
 
     m_view->SetTopLayer( aLayer );
