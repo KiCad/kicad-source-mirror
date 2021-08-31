345f506f0c119a39860cdf94e4a219d73afbed45
Seth Hillbrand <seth@kipro-pcb.com>
2021-08-31 14:31:56 -0700

    Ignore STARTPOINT/ENDPOINT in multiple item rot
    Rotating multiple items does not allow for individual end points to be
    selected.  This can happen when we narrowSelection by hovering on end
    points.

--- eeschema/tools/sch_edit_tool.cpp
+++ eeschema/tools/sch_edit_tool.cpp
@@ -577,10 +577,12 @@ int SCH_EDIT_TOOL::Rotate( const TOOL_EVENT& aEvent )
             {
                 SCH_LINE* line = (SCH_LINE*) item;
 
-                if( item->HasFlag( STARTPOINT ) )
+                // If we are rotating more than one item, we do not have start/end
+                // points separately selected
+                if( item->HasFlag( STARTPOINT ) || principalItemCount > 1 )
                     line->RotateStart( rotPoint );
 
-                if( item->HasFlag( ENDPOINT ) )
+                if( item->HasFlag( ENDPOINT ) || principalItemCount > 1 )
                     line->RotateEnd( rotPoint );
             }
             else if( item->Type() == SCH_SHEET_PIN_T )
