cf8618a0d557bc81a7fc51ed686c900daef64b4b
jean-pierre charras <jp.charras@wanadoo.fr>
2021-09-06 16:40:30 +0200

    A very ugly hack to fix an issue on Linux: if the wxbase315u_xml_gcc_custom.so is used **only** in PCM (in Kicad manager), it is not found in some cases at run time. So just use it in the main module to avoid a not found issue for the wxbase315u_xml_gcc_custom shared object when launching Kicad (the cost is very low)

--- kicad/kicad_manager_frame.cpp
+++ kicad/kicad_manager_frame.cpp
@@ -97,6 +97,11 @@ BEGIN_EVENT_TABLE( KICAD_MANAGER_FRAME, EDA_BASE_FRAME )
 END_EVENT_TABLE()
 
 
+#ifdef KICAD_PCM
+// See below the purpose of this include
+#include <wx/xml/xml.h>
+#endif
+
 KICAD_MANAGER_FRAME::KICAD_MANAGER_FRAME( wxWindow* parent, const wxString& title,
                                           const wxPoint& pos, const wxSize&   size ) :
         EDA_BASE_FRAME( parent, KICAD_MAIN_FRAME_T, title, pos, size,
@@ -108,6 +113,14 @@ KICAD_MANAGER_FRAME::KICAD_MANAGER_FRAME( wxWindow* parent, const wxString& titl
     m_active_project = false;
     m_leftWinWidth = 250;       // Default value
     m_aboutTitle = "KiCad";
+
+#ifdef KICAD_PCM
+    // JPC: A very ugly hack to fix an issue on Linux: if the wxbase315u_xml_gcc_custom.so is
+    // used **only** in PCM, it is not found in some cases at run time.
+    // So just use it in the main module to avoid a not found issue
+    // wxbase315u_xml_gcc_custom shared object when launching Kicad
+    wxXmlDocument dummy;
+#endif
 
     // Create the status line (bottom of the frame)
     static const int dims[2] = { -1, -1 };
