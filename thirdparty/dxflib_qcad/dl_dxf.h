fc3fb86cb52817108b8b9804be85aa59254e7167
Marek Roszko <mark.roszko@gmail.com>
2021-10-23 08:18:06 -0400

    Bump the dxflib version string to match upstream

--- thirdparty/dxflib_qcad/dl_dxf.h
+++ thirdparty/dxflib_qcad/dl_dxf.h
@@ -56,11 +56,11 @@ class DL_CreationInterface;
 class DL_WriterA;
 
 
-#define DL_VERSION                  "3.17.0.0"
+#define DL_VERSION                  "3.26.4.0"
 
 #define DL_VERSION_MAJOR            3
-#define DL_VERSION_MINOR            17
-#define DL_VERSION_REV              0
+#define DL_VERSION_MINOR            26
+#define DL_VERSION_REV              4
 #define DL_VERSION_BUILD            0
 
 #define DL_UNKNOWN                  0
