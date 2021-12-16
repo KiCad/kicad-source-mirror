6f711f8ce30569115217d84262b2c18451b1ddd1
Wayne Stambaugh <stambaughw@gmail.com>
2021-12-16 11:14:42 -0500

    Begin final push to stable release 6.0.0 round 2.

--- CMakeModules/KiCadVersion.cmake
+++ CMakeModules/KiCadVersion.cmake
@@ -37,7 +37,7 @@
 # KiCad.
 #
 # Note: This version string should follow the semantic versioning system
-set( KICAD_SEMANTIC_VERSION "6.0.0-rc2" )
+set( KICAD_SEMANTIC_VERSION "6.0.0-rc2-unknown" )
 
 # Default the version to the semantic version.
 # This is overridden by the git repository tag though (if using git)
