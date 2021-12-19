5644f236d86160c7a4c16b76a16d1cc899ba4e0d
jean-pierre charras <jp.charras@wanadoo.fr>
2021-12-19 09:09:25 +0100

    Allow teardrops only if advanced config define AllowTeardrops=1

--- pcbnew/menubar_pcb_editor.cpp
+++ pcbnew/menubar_pcb_editor.cpp
@@ -402,12 +402,16 @@ void PCB_EDIT_FRAME::ReCreateMenuBar()
     toolsMenu->Add( ACTIONS::showFootprintEditor );
     toolsMenu->Add( PCB_ACTIONS::updateFootprints );
 
-
-    toolsMenu->AppendSeparator();
-    toolsMenu->Add( _( "Add Teardrops" ), "",
-                    ID_RUN_TEARDROP_TOOL, BITMAPS::via );
-    toolsMenu->Add( _( "Remove Teardrops" ), "",
-                    ID_REMOVE_TEARDROP_TOOL, BITMAPS::via );
+    // Currently: available only with advanced_config m_AllowTeardrops = true
+    // (AllowTeardrops = 1)
+    if(  ADVANCED_CFG::GetCfg().m_AllowTeardrops )
+    {
+        toolsMenu->AppendSeparator();
+        toolsMenu->Add( _( "Add Teardrops" ), "",
+                        ID_RUN_TEARDROP_TOOL, BITMAPS::via );
+        toolsMenu->Add( _( "Remove Teardrops" ), "",
+                        ID_REMOVE_TEARDROP_TOOL, BITMAPS::via );
+    }
 
     toolsMenu->AppendSeparator();
     toolsMenu->Add( PCB_ACTIONS::cleanupTracksAndVias );
