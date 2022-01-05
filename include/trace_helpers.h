1ad1cf83746be984e05d8a9f8e778594c7b0d617
Ian McInerney <ian.s.mcinerney@ieee.org>
2022-01-05 19:23:02 +0000

    Fix compile error with UTF8 unicode wxWidgets build
    Fixes https://gitlab.com/kicad/code/kicad/issues/10210
    
    (Cherry-picked from bd828f598c87a152b7ae422732cc8d93e0bc9933)

--- include/trace_helpers.h
+++ include/trace_helpers.h
@@ -242,7 +242,7 @@ public:
     void DoTraceUtf8( const wxString aWhat, const wxChar* aFmt, ... )
     {
         va_list argptr;
-        va_start( argptr, format );
+        va_start( argptr, aFmt );
         traceV( aWhat, aFmt, argptr );
         va_end( argptr );
     }
