b8a7a66a0ab949d24d7fe6a1ad995ff013e31846
Seth Hillbrand <seth@kipro-pcb.com>
2022-01-05 16:50:24 -0800

    Keep symbol synced with edit tool
    Undo/Redo can change the symbol referenced by the symbol drawing tool.
    This updates the current symbol before important steps to keep the
    system synced
    
    Fixes https://gitlab.com/kicad/code/kicad/issues/10125

--- eeschema/symbol_editor/symbol_editor.cpp
+++ eeschema/symbol_editor/symbol_editor.cpp
@@ -385,6 +385,8 @@ bool SYMBOL_EDIT_FRAME::LoadOneLibrarySymbolAux( LIB_SYMBOL* aEntry, const wxStr
     RebuildSymbolUnitsList();
     SetShowDeMorgan( GetCurSymbol()->HasConversion() );
 
+    ClearUndoRedoList();
+
     // Display the document information based on the entry selected just in
     // case the entry is an alias.
     UpdateMsgPanel();
