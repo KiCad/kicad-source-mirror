22ff66f1426df407d7e0b76b6819a9e7380054ff
Rafael Silva <silvagracarafael@gmail.com>
2022-01-22 18:22:58 +0000

    add nightly icons

--- bitmaps_png/mk_icn_res.sh
+++ bitmaps_png/mk_icn_res.sh
@@ -2,55 +2,71 @@
 #
 # make icons using Inkscape
 
+ICON="kicad
+      pcbnew
+      eeschema
+      gerbview
+      bitmap2component
+      pcbcalculator"
+
+SIZE="16x16
+      24x24
+      32x32
+      48x48
+      64x64
+      128x128"
+
 # create output directories
-rm -r ../resources/linux/mime/icons/hicolor/*
-mkdir -p ../resources/linux/mime/icons/hicolor/scalable
-cd ../resources/linux/mime/icons/hicolor
+rm -r ../resources/linux/icons/hicolor/*
+rm -r ../resources/linux/icons-nightly/hicolor/*
+
+mkdir -p ../resources/linux/icons/hicolor/scalable/mimetypes ../resources/linux/icons/hicolor/scalable/apps
+mkdir -p ../resources/linux/icons-nightly/hicolor/scalable/apps
+
 echo -e '\E[0;34m'"Directory \"scalable\" for .svg icons was created."
+tput sgr0
 
 # copy sources to the scalable dir
-cd ../../../../../bitmaps_png/sources
-cp icon_kicad.svg ../../resources/linux/mime/icons/hicolor/scalable/kicad.svg
-cp icon_pcbnew.svg ../../resources/linux/mime/icons/hicolor/scalable/pcbnew.svg
-cp icon_eeschema.svg ../../resources/linux/mime/icons/hicolor/scalable/eeschema.svg
-echo -e '\E[0;34m'"Sources of icons was copied."
+cp ./sources/light/icon_kicad.svg ../resources/linux/icons/hicolor/scalable/mimetypes/application-x-kicad-project.svg
+cp ./sources/light/icon_pcbnew.svg ../resources/linux/icons/hicolor/scalable/mimetypes/application-x-kicad-pcb.svg
+cp ./sources/light/icon_eeschema.svg ../resources/linux/icons/hicolor/scalable/mimetypes/application-x-kicad-schematic.svg
 
-# convert .svg files into .png files
-cd ../../resources/linux/mime/icons/hicolor/scalable
-
-SIZES="16x16
-       22x22
-       24x24
-       32x32
-       48x48"
-
-for size in $SIZES
+for icon in $ICON
 do
-    sz=${size%x*}
-    echo -e '\E[0;32m'"\nMaking the mimetypes icons with size $size."
-    mkdir ../$size
-    echo -e '\E[0;34m'"Directory $size was created."
-    mkdir ../$size/mimetypes
-    echo -e '\E[0;34m'"Subdirectory \"mimetypes\" was created."
-    tput sgr0
-
-    inkscape -f kicad.svg -e ../$size/mimetypes/application-x-kicad-project.png -w $sz -h $sz --export-area-snap
-    inkscape -f eeschema.svg -e ../$size/mimetypes/application-x-kicad-eeschema.png -w $sz -h $sz --export-area-snap
-    inkscape -f pcbnew.svg -e ../$size/mimetypes/application-x-kicad-pcbnew.png -w $sz -h $sz --export-area-snap
-    echo -e '\E[0;34m'"Icons with size $size was created."
-
-    if [ $sz -eq 48 ]
-    then
-        echo -e '\E[0;32m'"\nMaking the applications icons with size $size."
-        mkdir ../$size/apps
-        echo -e '\E[0;34m'"Subdirectory \"apps\" was created."
-        tput sgr0
-
-        inkscape -f kicad.svg -e ../$size/apps/kicad.png -w $sz -h $sz --export-area-snap
-        inkscape -f eeschema.svg -e ../$size/apps/eeschema.png -w $sz -h $sz --export-area-snap
-        inkscape -f pcbnew.svg -e ../$size/apps/pcbnew.png -w $sz -h $sz --export-area-snap
-        echo -e '\E[0;34m'"Icons with size $size was created."
-        tput sgr0
-    fi
+    cp ./sources/light/icon_$icon.svg ../resources/linux/icons/hicolor/scalable/apps/$icon.svg
 done
 
+cp ./sources/light/icon_kicad_nightly.svg ../resources/linux/icons-nightly/hicolor/scalable/apps/kicad.svg
+
+echo -e '\E[0;34m'"Sources of icons were copied."
+tput sgr0
+
+# convert .svg files into .png files
+for size in $SIZE
+do
+    sz=${size%x*}
+
+    source_sz=""
+
+    if [ $sz -le 32 ]
+    then
+        source_sz="_$sz"
+    fi
+
+	echo -e '\E[0;32m'"\nMaking icons with size $size from \"source$source_sz\"."
+    tput sgr0
+
+    mkdir -p ../resources/linux/icons/hicolor/$size/mimetypes ../resources/linux/icons/hicolor/$size/apps
+    mkdir -p ../resources/linux/icons-nightly/hicolor/$size/apps
+
+    inkscape -o ../resources/linux/icons/hicolor/$size/mimetypes/application-x-kicad-project.png -w $sz -h $sz --export-area-snap ./sources/light/icon_kicad${source_sz}.svg
+    inkscape -o ../resources/linux/icons/hicolor/$size/mimetypes/application-x-kicad-pcb.png -w $sz -h $sz --export-area-snap ./sources/light/icon_pcbnew${source_sz}.svg
+    inkscape -o ../resources/linux/icons/hicolor/$size/mimetypes/application-x-kicad-schematic.png -w $sz -h $sz --export-area-snap ./sources/light/icon_eeschema${source_sz}.svg
+
+    for icon in $ICON
+    do
+        inkscape -o ../resources/linux/icons/hicolor/$size/apps/$icon.png -w $sz -h $sz --export-area-snap ./sources/light/icon_${icon}${source_sz}.svg
+    done
+    inkscape -o ../resources/linux/icons-nightly/hicolor/$size/apps/kicad.png -w $sz -h $sz --export-area-snap ./sources/light/icon_kicad_nightly${source_sz}.svg
+
+done
