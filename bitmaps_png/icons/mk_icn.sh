a5022e1e596ee3c453ff305d265b35d67dc5154d
Marek Roszko <mark.roszko@gmail.com>
2022-01-22 21:08:01 -0500

    Repair paths in mk_icn.sh

--- bitmaps_png/icons/mk_icn.sh
+++ bitmaps_png/icons/mk_icn.sh
@@ -10,14 +10,15 @@ rm icon_*.png
 mkdir tmp
 mkdir tmp/22 tmp/26 tmp/32 tmp/48 tmp/64 tmp/128 tmp/256 tmp/ico
 echo "all directories created."
-cd ../sources
+cd ../sources/light
 
 ICON_FILES="icon_3d.svg
             icon_gerbview.svg
             icon_kicad.svg
+            icon_kicad_nightly.svg
             icon_bitmap2component.svg
             icon_pcbcalculator.svg
-            icon_pl_editor.svg
+            icon_pagelayout_editor.svg
             icon_cvpcb.svg
             icon_pcbnew.svg
             icon_eeschema.svg
@@ -27,19 +28,19 @@ ICON_FILES="icon_3d.svg
 for fl in $ICON_FILES
 do
    NAME=${fl%.*} # strip the file extension
-	inkscape -f $fl -e ../icons/tmp/22/$NAME.png -w 22 -h 22 --export-area-snap
-	inkscape -f $fl -e ../icons/tmp/26/$NAME.png -w 26 -h 26 --export-area-snap
-	inkscape -f $fl -e ../icons/tmp/32/$NAME.png -w 32 -h 32 --export-area-snap
-	inkscape -f $fl -e ../icons/tmp/48/$NAME.png -w 48 -h 48 --export-area-snap
-	inkscape -f $fl -e ../icons/tmp/64/$NAME.png -w 64 -h 64 --export-area-snap
-	inkscape -f $fl -e ../icons/tmp/128/$NAME.png -w 128 -h 128 --export-area-snap
-	inkscape -f $fl -e ../icons/tmp/256/$NAME.png -w 256 -h 256 --export-area-snap
+	inkscape -f $fl -e ../../icons/tmp/22/$NAME.png -w 22 -h 22 --export-area-snap
+	inkscape -f $fl -e ../../icons/tmp/26/$NAME.png -w 26 -h 26 --export-area-snap
+	inkscape -f $fl -e ../../icons/tmp/32/$NAME.png -w 32 -h 32 --export-area-snap
+	inkscape -f $fl -e ../../icons/tmp/48/$NAME.png -w 48 -h 48 --export-area-snap
+	inkscape -f $fl -e ../../icons/tmp/64/$NAME.png -w 64 -h 64 --export-area-snap
+	inkscape -f $fl -e ../../icons/tmp/128/$NAME.png -w 128 -h 128 --export-area-snap
+	inkscape -f $fl -e ../../icons/tmp/256/$NAME.png -w 256 -h 256 --export-area-snap
 	echo "file $fl converted."
 done
 
 # convert .png files into .ico files using "icotool" from icoutils
 # (see http://www.nongnu.org/icoutils/)
-cd ../icons
+cd ../../icons
 for fl in $ICON_FILES
 do
     NAME=${fl%.*} # strip the file extension
