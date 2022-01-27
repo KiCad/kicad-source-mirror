2345e09a12e627b8fe67e7eccaaa78ae634eb5e6
jean-pierre charras <jp.charras@wanadoo.fr>
2022-01-27 13:03:00 +0100

    Pcbnew, export svg: Fix incorrect saving of layer set.

--- pcbnew/dialogs/dialog_export_svg.cpp
+++ pcbnew/dialogs/dialog_export_svg.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2012 Jean-Pierre Charras, jp.charras at wanadoo.fr
- * Copyright (C) 2012 KiCad Developers, see change_log.txt for contributors.
+ * Copyright (C) 2022 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -131,7 +131,7 @@ DIALOG_EXPORT_SVG::~DIALOG_EXPORT_SVG()
             continue;
 
         if( m_boxSelectLayer[layer].first->IsChecked( m_boxSelectLayer[layer].second ) )
-            cfg->m_ExportSvg.layers.push_back( m_boxSelectLayer[layer].second );
+            cfg->m_ExportSvg.layers.push_back( layer );
     }
 }
 
