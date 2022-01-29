8753051db66e93cdb9c5c3c22292931f1a013341
Seth Hillbrand <seth@kipro-pcb.com>
2022-01-28 16:31:41 -0800

    Don't count zero-length as duplicate
    We were checking for duplicate tracks by looking to see if the two
    tracks had two shared points.  A null track always matched this case,
    which removed the valid track.  We solve this by avoiding null tracks in
    the duplicate checker.  They are removed separately in the null track
    stage.
    
    This also fixes a GTK-specific tree issue where we require the
    BeforeReset()/AfterReset() calls instead of Cleared() to prevent GTK
    from dereferencing a parent after freeing
    
    Fixes https://gitlab.com/kicad/code/kicad/issues/10624

--- common/rc_item.cpp
+++ common/rc_item.cpp
@@ -210,6 +210,8 @@ void RC_TREE_MODEL::rebuildModel( RC_ITEMS_PROVIDER* aProvider, int aSeverities 
         m_view->UnselectAll();
     }
 
+    BeforeReset();
+
     if( aProvider != m_rcItemsProvider )
     {
         delete m_rcItemsProvider;
@@ -255,7 +257,7 @@ void RC_TREE_MODEL::rebuildModel( RC_ITEMS_PROVIDER* aProvider, int aSeverities 
 
     // Must be called after a significant change of items to force the
     // wxDataViewModel to reread all of them, repopulating itself entirely.
-    Cleared();
+    AfterReset();
 
 #ifdef __WXGTK__
     // The fastest method to update wxDataViewCtrl is to rebuild from
