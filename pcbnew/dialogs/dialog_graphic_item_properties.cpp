ef94f31204048f44d2dddb409a3b8c27a87a564d
Seth Hillbrand <seth@kipro-pcb.com>
2022-02-03 14:30:44 -0800

    Update error message to match new arc dialog
    We now provide arc end points and interior angle, so we cannot check for
    null radius in the same fashion.  Instead, the starpoint==endpoint
    indicates a zero radius
    
    Fixes https://gitlab.com/kicad/code/kicad/issues/10714

--- pcbnew/dialogs/dialog_graphic_item_properties.cpp
+++ pcbnew/dialogs/dialog_graphic_item_properties.cpp
@@ -370,7 +370,10 @@ bool DIALOG_GRAPHIC_ITEM_PROPERTIES::Validate()
         if( m_angle.GetValue() == 0 )
             error_msgs.Add( _( "The arc angle cannot be zero." ) );
 
-        KI_FALLTHROUGH;
+        if( m_startX.GetValue() == m_endX.GetValue() && m_startY.GetValue() == m_endY.GetValue() )
+            error_msgs.Add( _( "The radius cannot be zero." ) );
+
+        break;
 
     case SHAPE_T::CIRCLE:
         // Check radius.
