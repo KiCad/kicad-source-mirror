b8b9f646c6d3a6374198cf822fa90485d1edaf18
Jeff Young <jeff@rokeby.ie>
2022-02-06 17:45:17 +0000

    Show pin dangling symbols in Symbol Editor.
    Even though we don't have connections they're a still a good visual
    indication of which end of the pin is which.
    
    Fixes https://gitlab.com/kicad/code/kicad/issues/10742
    
    (cherry picked from commit aedfe08badb8df504e7cebef319d4db797aa05c5)

--- eeschema/lib_pin.cpp
+++ eeschema/lib_pin.cpp
@@ -1061,6 +1061,17 @@ void LIB_PIN::GetMsgPanelInfo( EDA_DRAW_FRAME* aFrame, std::vector<MSG_PANEL_ITE
 }
 
 
+void LIB_PIN::ViewGetLayers( int aLayers[], int& aCount ) const
+{
+    aCount     = 3;
+    aLayers[0] = LAYER_DANGLING;    // We don't really show dangling vs non-dangling (since there
+                                    // are no connections in the symbol editor), but it's still
+                                    // a good visual indication of which end of the pin is which.
+    aLayers[1] = LAYER_DEVICE;
+    aLayers[2] = LAYER_SELECTION_SHADOWS;
+}
+
+
 const EDA_RECT LIB_PIN::GetBoundingBox( bool aIncludeInvisibles, bool aPinOnly ) const
 {
     EDA_RECT       bbox;
