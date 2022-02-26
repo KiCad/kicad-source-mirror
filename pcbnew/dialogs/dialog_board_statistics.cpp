05110b7bdc24fc8613f9ec991b557a7b1b1023dd
jean-pierre charras <jp.charras@wanadoo.fr>
2022-02-26 16:58:30 +0100

    DIALOG_BOARD_STATISTICS: Reset the drill count list before recalculations. Fixes #10984 https://gitlab.com/kicad/code/kicad/issues/10984

--- pcbnew/dialogs/dialog_board_statistics.cpp
+++ pcbnew/dialogs/dialog_board_statistics.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2019 Alexander Shuklin, jasuramme@gmail.com
- * Copyright (C) 2019-2021 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2019-2022 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -203,6 +203,7 @@ bool DIALOG_BOARD_STATISTICS::TransferDataToWindow()
 void DIALOG_BOARD_STATISTICS::getDataFromPCB()
 {
     BOARD* board = m_parentFrame->GetBoard();
+    m_drillTypes.clear();
 
     // Get footprints and pads count
     for( FOOTPRINT* footprint : board->Footprints() )
