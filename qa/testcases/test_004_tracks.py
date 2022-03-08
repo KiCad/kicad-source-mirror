da480aba476b48da8b40eca684e4d00ccab327a6
Seth Hillbrand <seth@kipro-pcb.com>
2022-03-08 12:36:08 -0800

    Fix one more test case for arc approx

--- qa/testcases/test_004_tracks.py
+++ qa/testcases/test_004_tracks.py
@@ -21,7 +21,7 @@ class TestTracks(unittest.TestCase):
         arc = sorted(arcs, key=lambda t: [t.GetStart()[0], t.GetStart()[1]])[0]
         self.assertEqual( [29414200, 26500000], [arc.GetCenter()[0], arc.GetCenter()[1]] )
         self.assertEqual([1800, 2250], [round(arc.GetArcAngleStart()), round(arc.GetArcAngleEnd())])
-        self.assertEqual(2414211, round(arc.GetRadius()))
+        self.assertEqual( 2414200, round( arc.GetRadius() ) )
 
     def test_vias(self):
         vias = [t.Cast() for t in self.pcb.Tracks() if t.GetClass() == 'PCB_VIA']
