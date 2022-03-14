cbb6ca8500c200b712511700d98e5068097a5d37
Jeff Young <jeff@rokeby.ie>
2022-03-14 22:55:57 +0000

    Import specctra track locking.
    Fixes https://gitlab.com/kicad/code/kicad/issues/11070

--- pcbnew/specctra_import_export/specctra_import.cpp
+++ pcbnew/specctra_import_export/specctra_import.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2007-2013 SoftPLC Corporation, Dick Hollenbeck <dick@softplc.com>
- * Copyright (C) 2007 KiCad Developers, see change_log.txt for contributors.
+ * Copyright (C) 2007-2022 KiCad Developers, see change_log.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -455,6 +455,7 @@ void SPECCTRA_DB::FromSESSION( BOARD* aBoard )
                 for( unsigned pt=0;  pt<path->points.size()-1;  ++pt )
                 {
                     PCB_TRACK* track = makeTRACK( path, pt, netoutCode );
+                    track->SetLocked( wire->wire_type == T_protect );
                     aBoard->Add( track );
                 }
             }
