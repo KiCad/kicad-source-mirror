6971e409bfcfcf7d7bb9ca7d4dcb5be1f53acb52
jean-pierre charras <jp.charras@wanadoo.fr>
2022-03-21 18:10:07 +0100

    Pcbnew: fix lost of focus after clicking on a wxChoice in toolbar. After changing track width, via size, grid or zoom from the toolbar, the canvas has no longer the focus on Windows. So we force the focus after clicking on these widgets. From Master branch Fixes #11103 https://gitlab.com/kicad/code/kicad/issues/11103

--- common/eda_draw_frame.cpp
+++ common/eda_draw_frame.cpp
@@ -387,6 +387,9 @@ void EDA_DRAW_FRAME::OnSelectGrid( wxCommandEvent& event )
 
     UpdateStatusBar();
     m_canvas->Refresh();
+    // Needed on Windows because clicking on m_gridSelectBox remove the focus from m_canvas
+    // (Windows specific
+    m_canvas->SetFocus();
 }
 
 
@@ -463,6 +466,9 @@ void EDA_DRAW_FRAME::OnSelectZoom( wxCommandEvent& event )
     m_toolManager->RunAction( ACTIONS::zoomPreset, true, static_cast<intptr_t>( id ) );
     UpdateStatusBar();
     m_canvas->Refresh();
+    // Needed on Windows because clicking on m_zoomSelectBox remove the focus from m_canvas
+    // (Windows specific
+    m_canvas->SetFocus();
 }
 
 
