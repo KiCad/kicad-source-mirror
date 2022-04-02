056748e0df716f15096e0a3e2e09c46c443ea3da
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2022-04-02 18:13:52 +0200

    altium: Add testcase to verify that "PCB Binary Version 5.0" is also supported

--- qa/unittests/pcbnew/plugins/altium/test_altium_pcblib_import.cpp
+++ qa/unittests/pcbnew/plugins/altium/test_altium_pcblib_import.cpp
@@ -65,7 +65,8 @@ BOOST_FIXTURE_TEST_SUITE( AltiumPcbLibImport, ALTIUM_PCBLIB_IMPORT_FIXTURE )
 BOOST_AUTO_TEST_CASE( AltiumPcbLibImport )
 {
     std::vector<std::pair<wxString, wxString>> tests = {
-        { "Tracks.PcbLib", "Tracks.pretty" },
+        { "Tracks.v5.PcbLib", "Tracks.pretty" },
+        { "Tracks.v6.PcbLib", "Tracks.pretty" },
         { "Espressif ESP32-WROOM-32.PcbLib", "Espressif ESP32-WROOM-32.pretty" }
     };
 
