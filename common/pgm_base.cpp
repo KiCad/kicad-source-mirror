c0a9a02591b6cd64b4915ec3f6c2cd86f9df1060
Seth Hillbrand <seth@kipro-pcb.com>
2022-05-25 16:56:54 -0700

    Handle settings sequencing
    Avoids crash when window close ordering places 3d viewer after the
    common settings are closed

--- common/pgm_base.cpp
+++ common/pgm_base.cpp
@@ -559,7 +559,7 @@ void PGM_BASE::SaveCommonSettings()
 
 COMMON_SETTINGS* PGM_BASE::GetCommonSettings() const
 {
-    return m_settings_manager ? GetSettingsManager().GetCommonSettings() : nullptr;
+    return m_settings_manager ? m_settings_manager->GetCommonSettings() : nullptr;
 }
 
 
