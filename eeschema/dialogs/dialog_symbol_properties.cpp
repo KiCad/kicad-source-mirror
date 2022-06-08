41a9293106dba1d0e258598f85dd82f9dce9275f
Jeff Young <jeff@rokeby.ie>
2022-06-08 13:48:19 +0100

    Update grid attributes after sorting.
    Fixes https://gitlab.com/kicad/code/kicad/issues/11753
    
    (cherry picked from commit 46fd32b7385f7c7a9de8901b9f6ec4be3a34db4f)

--- eeschema/dialogs/dialog_symbol_properties.cpp
+++ eeschema/dialogs/dialog_symbol_properties.cpp
@@ -89,6 +89,14 @@ public:
 
     void BuildAttrs()
     {
+        for( wxGridCellAttr* attr : m_nameAttrs )
+            attr->DecRef();
+
+        m_nameAttrs.clear();
+
+        if( m_readOnlyAttr )
+            m_readOnlyAttr->DecRef();
+
         m_readOnlyAttr = new wxGridCellAttr;
         m_readOnlyAttr->SetReadOnly( true );
 
@@ -117,10 +125,16 @@ public:
             m_nameAttrs.push_back( attr );
         }
 
+        if( m_typeAttr )
+            m_typeAttr->DecRef();
+
         m_typeAttr = new wxGridCellAttr;
         m_typeAttr->SetRenderer( new GRID_CELL_ICON_TEXT_RENDERER( PinTypeIcons(),
                                                                    PinTypeNames() ) );
         m_typeAttr->SetReadOnly( true );
+
+        if( m_shapeAttr )
+            m_shapeAttr->DecRef();
 
         m_shapeAttr = new wxGridCellAttr;
         m_shapeAttr->SetRenderer( new GRID_CELL_ICON_TEXT_RENDERER( PinShapeIcons(),
@@ -1024,6 +1038,7 @@ void DIALOG_SYMBOL_PROPERTIES::OnPinTableColSort( wxGridEvent& aEvent )
         ascending = true;
 
     m_dataModel->SortRows( sortCol, ascending );
+    m_dataModel->BuildAttrs();
 }
 
 
