cca7f1dbda0c91e3b9372d874b8d96cc050f6e93
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2022-07-21 16:30:28 +0200

    router: use actual snap point position for routability checks of a segment instead of the segment endpoint(s).
    Explanation:
    - place a long segment A with something violating the rules close to its endpoint
    - try to start routing in the 2/3 of the length of A (on the obstacle side)
    - bummer - the router picks the nearest endpoint of A for the collision check (even though we don't care about this collision since we want to route far away from it)
    
    (cherry picked from commit e5588938514aec064543e152b6a25a8943ef62b2)

--- pcbnew/router/pns_node.cpp
+++ pcbnew/router/pns_node.cpp
@@ -484,7 +484,7 @@ NODE::OPT_OBSTACLE NODE::CheckColliding( const ITEM* aItemA, int aKindMask )
         if( line->EndsWithVia() )
         {
             n += QueryColliding( &line->Via(), obs, aKindMask, 1 );
-
+            
             if( n )
                 return OPT_OBSTACLE( obs[0] );
         }
