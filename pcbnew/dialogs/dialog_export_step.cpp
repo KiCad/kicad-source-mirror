KiCad/kicad-source-mirror

commit 8456d6cea9f0f0911614f5df4bc9ef04b8225656

Corrected syntax in kicad2step's help message regarding user-specified origin.

utils/kicad2step/kicad2step_app.cpp
@@ -30,12 +30,15 @@
 #include <wx/filename.h>
 #include <sstream>
 #include <iostream>
-#include <sstream>
+#include <regex>
 
 #include "kicad2step.h"
 #include "kicad2step_frame_base.h"
 #include <Standard_Failure.hxx> // In open cascade
 
+#define REGEX_QUANTITY "([\\s]*[+-]?[\\d]*[.]?[\\d]*)"
+#define REGEX_DELIMITER "(?:[\\s]*x)"
+#define REGEX_UNIT "([m]{2}|(?:in))"
 
 class KICAD2MCAD_APP : public wxApp
 {
@@ -48,8 +51,8 @@ public:
     virtual bool OnCmdLineParsed( wxCmdLineParser& parser ) override;
 
 private:
-    KICAD2STEP*      m_converter;
-    KICAD2MCAD_PRMS  m_params;
+    KICAD2STEP*     m_converter;
+    KICAD2MCAD_PRMS m_params;
 };
 
 
@@ -146,48 +149,30 @@ bool KICAD2MCAD_APP::OnCmdLineParsed( wxCmdLineParser& parser )
 
     if( parser.Found( wxT( "user-origin" ), &tstr ) )
     {
-        std::istringstream istr;
-        istr.str( std::string( tstr.ToUTF8() ) );
-        istr >> m_params.m_xOrigin;
+        std::regex  re_pattern( REGEX_QUANTITY REGEX_DELIMITER REGEX_QUANTITY REGEX_UNIT,
+                                std::regex_constants::icase );
+        std::smatch sm;
+        std::string str( tstr.ToUTF8() );
+        std::regex_search( str, sm, re_pattern );
+        m_params.m_xOrigin = atof( sm.str( 1 ).c_str() );
+        m_params.m_yOrigin = atof( sm.str( 2 ).c_str() );
+        std::string tunit( sm[3] );
 
-        if( istr.fail() )
+        if( ( !sm.str( 1 ).compare( " " ) || !sm.str( 2 ).compare( " " ) ) || ( sm.size() != 4 ) )
         {
             parser.Usage();
             return false;
         }
 
-        char tmpc;
-        istr >> tmpc;
-
-        if( istr.fail() || ( tmpc != 'x' && tmpc != 'X' ) )
+        if( !tunit.compare( "in" ) || !tunit.compare( "inch" ) )
+        {
+            m_params.m_xOrigin *= 25.4;
+            m_params.m_yOrigin *= 25.4;
+        }
+        else if( tunit.compare( "mm" ) )
         {
             parser.Usage();
             return false;
-        }
-
-        istr >> m_params.m_yOrigin;
-
-        if( istr.fail() )
-        {
-            parser.Usage();
-            return false;
-        }
-
-        if( !istr.eof() )
-        {
-            std::string tunit;
-            istr >> tunit;
-
-            if( !tunit.compare( "in" ) || !tunit.compare( "inch" ) )
-            {
-                m_params.m_xOrigin *= 25.4;
-                m_params.m_yOrigin *= 25.4;
-            }
-            else if( tunit.compare( "mm" ) )
-            {
-                parser.Usage();
-                return false;
-            }
         }
     }
 

commit 06e612661c0d858857ae233b3a77db0f46192d74

Kicad2step: fix STEP export from Pcbnew, broken by changes in commit 8456d6c also: DIALOG_EXPORT_STEP: remember tolerance option during a session. From Master branch. Fixes #12119 https://gitlab.com/kicad/code/kicad/issues/12119

pcbnew/dialogs/dialog_export_step.cpp
@@ -68,6 +68,7 @@ private:
     double m_XOrg;          // remember last User Origin X value
     double m_YOrg;          // remember last User Origin Y value
     wxString m_boardPath;   // path to the exported board file
+    static int      m_toleranceLastChoice;  // Store m_tolerance option during a session
 
 protected:
     void onUpdateUnits( wxUpdateUIEvent& aEvent ) override;
@@ -112,6 +113,7 @@ public:
     ~DIALOG_EXPORT_STEP();
 };
 
+int DIALOG_EXPORT_STEP::m_toleranceLastChoice = -1;     // Use default
 
 DIALOG_EXPORT_STEP::DIALOG_EXPORT_STEP( PCB_EDIT_FRAME* aParent, const wxString& aBoardPath ) :
     DIALOG_EXPORT_STEP_BASE( aParent )
@@ -220,6 +222,9 @@ DIALOG_EXPORT_STEP::DIALOG_EXPORT_STEP( PCB_EDIT_FRAME* aParent, const wxString&
         if( msgDlg.DoNotShowAgain() )
             Pgm().GetCommonSettings()->m_DoNotShowAgain.scaled_3d_models_warning = true;
     }
+    if( m_toleranceLastChoice >= 0 )
+        m_tolerance->SetSelection( m_toleranceLastChoice );
+
     // Now all widgets have the size fixed, call FinishDialogSettings
     finishDialogSettings();
 }
@@ -245,6 +250,8 @@ DIALOG_EXPORT_STEP::~DIALOG_EXPORT_STEP()
     cfg->m_ExportStep.origin_y = val;
 
     cfg->m_ExportStep.no_virtual = m_cbRemoveVirtual->GetValue();
+
+    m_toleranceLastChoice = m_tolerance->GetSelection();
 }
 
 
@@ -310,6 +317,7 @@ void DIALOG_EXPORT_STEP::onExportButton( wxCommandEvent& aEvent )
     m_parent->SetLastPath( LAST_PATH_STEP, m_filePickerSTEP->GetPath() );
 
     double tolerance = 0.01;   // default value in mm
+    m_toleranceLastChoice = m_tolerance->GetSelection();
 
     switch( m_tolerance->GetSelection() )
     {
@@ -420,7 +428,7 @@ void DIALOG_EXPORT_STEP::onExportButton( wxCommandEvent& aEvent )
             }
 
             LOCALE_IO dummy;
-            cmdK2S.Append( wxString::Format( wxT( " --user-origin=%c%.6f x %.6f%c" ),
+            cmdK2S.Append( wxString::Format( wxT( " --user-origin=%c%.6fx%.6fmm%c" ),
                                              quote, xOrg, yOrg, quote ) );
         }
             break;
@@ -431,7 +439,7 @@ void DIALOG_EXPORT_STEP::onExportButton( wxCommandEvent& aEvent )
             xOrg = Iu2Millimeter( bbox.GetCenter().x );
             yOrg = Iu2Millimeter( bbox.GetCenter().y );
             LOCALE_IO dummy;
-            cmdK2S.Append( wxString::Format( wxT( " --user-origin=%c%.6f x %.6f%c" ),
+            cmdK2S.Append( wxString::Format( wxT( " --user-origin=%c%.6fx%.6fmm%c" ),
                                              quote, xOrg, yOrg, quote ) );
         }
             break;
@@ -439,7 +447,7 @@ void DIALOG_EXPORT_STEP::onExportButton( wxCommandEvent& aEvent )
 
     {
         LOCALE_IO dummy;
-        cmdK2S.Append( wxString::Format( wxT( " --min-distance=%c%.3f mm%c" ),
+        cmdK2S.Append( wxString::Format( wxT( " --min-distance=%c%.3fmm%c" ),
                                          quote, tolerance, quote ) );
     }
 

utils/kicad2step/kicad2step_app.cpp
@@ -86,7 +86,7 @@ static const wxCmdLineEntryDesc cmdLineDesc[] = {
       _( "Substitute STEP or IGS models with the same name in place of VRML models" ).mb_str(),
       wxCMD_LINE_VAL_NONE, wxCMD_LINE_PARAM_OPTIONAL },
     { wxCMD_LINE_OPTION, NULL, "min-distance",
-      _( "Minimum distance between points to treat them as separate ones (default 0.01 mm)" )
+      _( "Minimum distance between points to treat them as separate ones (default 0.01mm)" )
               .mb_str(),
       wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL },
     { wxCMD_LINE_SWITCH, "h", NULL, _( "display this message" ).mb_str(), wxCMD_LINE_VAL_NONE,
@@ -156,23 +156,28 @@ bool KICAD2MCAD_APP::OnCmdLineParsed( wxCmdLineParser& parser )
         std::regex_search( str, sm, re_pattern );
         m_params.m_xOrigin = atof( sm.str( 1 ).c_str() );
         m_params.m_yOrigin = atof( sm.str( 2 ).c_str() );
+
         std::string tunit( sm[3] );
 
-        if( ( !sm.str( 1 ).compare( " " ) || !sm.str( 2 ).compare( " " ) ) || ( sm.size() != 4 ) )
+        if( tunit.size() > 0 ) // No unit accepted ( default = mm )
         {
-            parser.Usage();
-            return false;
-        }
+            if( ( !sm.str( 1 ).compare( " " ) || !sm.str( 2 ).compare( " " ) ) || ( sm.size() != 4 ) )
+            {
+                parser.Usage();
+                return false;
+            }
 
-        if( !tunit.compare( "in" ) || !tunit.compare( "inch" ) )
-        {
-            m_params.m_xOrigin *= 25.4;
-            m_params.m_yOrigin *= 25.4;
-        }
-        else if( tunit.compare( "mm" ) )
-        {
-            parser.Usage();
-            return false;
+            // only in, inch and mm are valid:
+            if( !tunit.compare( "in" ) || !tunit.compare( "inch" ) )
+            {
+                m_params.m_xOrigin *= 25.4;
+                m_params.m_yOrigin *= 25.4;
+            }
+            else if( tunit.compare( "mm" ) )
+            {
+                parser.Usage();
+                return false;
+            }
         }
     }
 