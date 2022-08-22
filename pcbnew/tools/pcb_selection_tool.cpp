1e68c353f17c32d30ba853e06f755c797e073600
Jeff Young <jeff@rokeby.ie>
2022-08-22 22:32:42 +0100

    Account for groups in undo of Position Relative To.
    Also fixes a bug were pads weren't correctly being found for anchoring
    the selection before the Position Relative To.
    
    Fixes https://gitlab.com/kicad/code/kicad/issues/11793

--- pcbnew/tools/pcb_selection_tool.cpp
+++ pcbnew/tools/pcb_selection_tool.cpp
@@ -2819,7 +2819,7 @@ void PCB_SELECTION_TOOL::FilterCollectorForHierarchy( GENERAL_COLLECTOR& aCollec
     std::unordered_set<BOARD_ITEM*> toAdd;
 
     // Set CANDIDATE on all parents which are included in the GENERAL_COLLECTOR.  This
-    // algorithm is O3n, whereas checking for the parent inclusion could potentially be On^2.
+    // algorithm is O(3n), whereas checking for the parent inclusion could potentially be O(n^2).
     for( int j = 0; j < aCollector.GetCount(); j++ )
     {
         if( aCollector[j]->GetParent() )
