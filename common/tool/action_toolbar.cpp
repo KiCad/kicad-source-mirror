3a297093e65cbe1da6a5118baf4c5359d834e47a
Ian McInerney <ian.s.mcinerney@ieee.org>
2022-09-25 01:58:45 +0100

    Fix lambda capture in ACTION_TOOLBAR

--- common/tool/action_toolbar.cpp
+++ common/tool/action_toolbar.cpp
@@ -594,7 +594,7 @@ void ACTION_TOOLBAR::onPaletteEvent( wxCommandEvent& aEvent )
 
     // Find the action corresponding to the button press
     auto actionIt = std::find_if( group->GetActions().begin(), group->GetActions().end(),
-                                  [=]( const TOOL_ACTION* aAction )
+                                  [&]( const TOOL_ACTION* aAction )
                                   {
                                       return aAction->GetUIId() == aEvent.GetId();
                                   } );
