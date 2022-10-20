da520f83ee63eef471c1529f2de75cab5560e184
Jeff Young <jeff@rokeby.ie>
2022-10-20 23:58:03 +0100

    Use comma to separate diff pair nets/netclassse rather than /.
    ("/" is problematic because it's more likely to appear in netnames.)
    
    Fixes https://gitlab.com/kicad/code/kicad/issues/12592
    
    (cherry picked from commit 6fd195c38908b57c7c7f1404871b5366afac131a)

--- pcbnew/router/router_tool.cpp
+++ pcbnew/router/router_tool.cpp
@@ -2156,7 +2156,7 @@ void ROUTER_TOOL::UpdateMessagePanel()
         wxASSERT( netB );
 
         description = wxString::Format( _( "Routing Diff Pair: %s" ),
-                                        netA->GetNetname() + wxT( "/" ) + netB->GetNetname() );
+                                        netA->GetNetname() + wxT( ", " ) + netB->GetNetname() );
 
         wxString  netclass;
         NETCLASS* netclassA = netA->GetNetClass();
@@ -2165,7 +2165,7 @@ void ROUTER_TOOL::UpdateMessagePanel()
         if( netclassA == netclassB )
             netclass = netclassA->GetName();
         else
-            netclass = netclassA->GetName() + wxT( "/" ) + netclassB->GetName();
+            netclass = netclassA->GetName() + wxT( ", " ) + netclassB->GetName();
 
         secondary = wxString::Format( _( "Net Class: %s" ),
                                       UnescapeString( netclass ) );
