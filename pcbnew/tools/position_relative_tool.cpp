cefc5268abd71bf79d84753b557ae3995c653d63
Jeff Young <jeff@rokeby.ie>
2022-11-16 18:36:00 +0000

    Allow pads in selection and filter them for free-pads later.
    Fixes https://gitlab.com/kicad/code/kicad/issues/12912

--- pcbnew/tools/position_relative_tool.cpp
+++ pcbnew/tools/position_relative_tool.cpp
@@ -73,7 +73,6 @@ int POSITION_RELATIVE_TOOL::PositionRelative( const TOOL_EVENT& aEvent )
             {
                 sTool->FilterCollectorForHierarchy( aCollector, true );
                 sTool->FilterCollectorForMarkers( aCollector );
-                sTool->FilterCollectorForFreePads( aCollector );
             },
             !m_isFootprintEditor /* prompt user regarding locked items */ );
 
