f0527fac6773553b205121d0379a27794393e1dc
jean-pierre charras <jp.charras@wanadoo.fr>
2022-11-22 08:39:16 +0100

    Gerbview, read zip archive: do not generate a error message for directory names. A directory is not a file, and is now just skipped.

--- gerbview/files.cpp
+++ gerbview/files.cpp
@@ -463,6 +463,9 @@ bool GERBVIEW_FRAME::unarchiveFiles( const wxString& aFullFileName, REPORTER* aR
 
     while( ( entry = zipArchive.GetNextEntry() ) )
     {
+        if( entry->IsDir() )
+            continue;
+
         wxString   fname = entry->GetName();
         wxFileName uzfn = fname;
         wxString   curr_ext = uzfn.GetExt().Lower();
