8c324f1fc8f7fcdb5e8f494da3d45997001fab4a
jean-pierre charras <jp.charras@wanadoo.fr>
2022-12-20 12:46:19 +0100

    Fix a missing include. Fixes #13216 https://gitlab.com/kicad/code/kicad/issues/13216

--- pcb_calculator/eserie.h
+++ pcb_calculator/eserie.h
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2020 <janvi@veith.net>
- * Copyright (C) 2020-2021 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2020-2022 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -21,6 +21,7 @@
 #include <array>
 #include <vector>
 #include <string>
+#include <cstdint>
 
 /**
  * E-Values derived from a geometric sequence formula by Charles Renard were already
