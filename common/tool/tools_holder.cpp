316087c468667a42f22198859caef862f1e43972
jean-pierre charras <jp.charras@wanadoo.fr>
2023-01-02 15:42:49 +0100

    Protect against null pointer use. Fixes #13349 https://gitlab.com/kicad/code/kicad/issues/13349

--- common/tool/tools_holder.cpp
+++ common/tool/tools_holder.cpp
@@ -1,7 +1,7 @@
 /*
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
- * Copyright (C) 2020-2021 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2020-2022 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -117,8 +117,11 @@ bool TOOLS_HOLDER::IsCurrentTool( const TOOL_ACTION& aAction ) const
 
 void TOOLS_HOLDER::ShowChangedLanguage()
 {
+    if( !GetToolManager() )
+        return;
+
     std::string  actionName = CurrentToolName();
-    TOOL_ACTION* action = m_toolManager->GetActionManager()->FindAction( actionName );
+    TOOL_ACTION* action = GetToolManager()->GetActionManager()->FindAction( actionName );
 
     if( action )
         DisplayToolMsg( action->GetLabel() );
