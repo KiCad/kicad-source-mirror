b222e4fd078163868982f670c5d26452ab7710de
Seth Hillbrand <seth@kipro-pcb.com>
2023-01-12 14:08:44 -0800

    Remove unused file

--- eeschema/cmp_library_lexer.cpp
+++
@@ -1,6 +0,0 @@
-/*
- * Do not delete this file.  It will eventually become the new symbol
- * library file DSN lexer which will replace the current library and library
- * document file formats.
- */
-#include <cmp_library_lexer.h>
