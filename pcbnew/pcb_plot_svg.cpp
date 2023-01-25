ae812bdf3934ba74d0fe13dcae5c505f73f56b4f
Marek Roszko <mark.roszko@gmail.com>
2023-01-24 22:57:28 -0500

    Try to use the theme setting for the cli pcb svg export

--- pcbnew/pcb_plot_svg.cpp
+++ pcbnew/pcb_plot_svg.cpp
@@ -77,7 +77,7 @@ bool PCB_PLOT_SVG::Plot( BOARD* aBoard, const PCB_PLOT_SVG_OPTIONS& aSvgPlotOpti
     SETTINGS_MANAGER& mgr = Pgm().GetSettingsManager();
     PCBNEW_SETTINGS*  cfg = mgr.GetAppSettings<PCBNEW_SETTINGS>();
 
-    plot_opts.SetColorSettings( mgr.GetColorSettings( cfg->m_ColorTheme ) );
+    plot_opts.SetColorSettings( mgr.GetColorSettings( aSvgPlotOptions.m_colorTheme ) );
 
     LOCALE_IO toggle;
 
