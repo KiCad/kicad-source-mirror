b1018e8eebc7253583f54044512004e37c1dd38f
Jeff Young <jeff@rokeby.ie>
2023-01-28 10:40:28 +0000

    Relax no-zone-commands constraint on interactive placer tools.
    These tools are initialized with an item (via/pad/microwave) so there's
    no way to tell between during-placement and idle.  Besides, they don't
    update the ratsnest so they probably won't fall afoul of the original bug
    (https://gitlab.com/kicad/code/kicad/-/issues/12736).
    
    Fixes https://gitlab.com/kicad/code/kicad/issues/13686

--- pcbnew/tools/pcb_tool_base.cpp
+++ pcbnew/tools/pcb_tool_base.cpp
@@ -1,7 +1,7 @@
 /*
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
- * Copyright (C) 2017-2022 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2017-2023 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -237,10 +237,6 @@ void PCB_TOOL_BASE::doInteractiveItemPlacement( const TOOL_EVENT&        aTool,
 
                 // Notify other tools of the changes
                 m_toolMgr->ProcessEvent( EVENTS::SelectedItemsModified );
-            }
-            else if( ZONE_FILLER_TOOL::IsZoneFillAction( evt ) )
-            {
-                wxBell();
             }
             else if( evt->IsAction( &ACTIONS::refreshPreview ) )
             {
