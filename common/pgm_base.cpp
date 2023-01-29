KiCad/kicad-source-mirror

commit 98d55ce82f43aec2c2e577a842b89e15bd8c5119

Avoid rebuilding with each commit
We shouldn't include the kicad_build_info.h outside of the wrapper
build_info.h.  Also adds an error directive to prevent re-introduction
of define dependencies

common/pgm_base.cpp
@@ -68,7 +68,7 @@
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <sentry.h>
-#include <kicad_build_version.h>
+#include <build_version.h>
 #endif
 
 /**
@@ -325,11 +325,10 @@ void PGM_BASE::sentryInit()
         sentry_options_set_symbolize_stacktraces( options, true );
         sentry_options_set_auto_session_tracking( options, false );
 
-#if !KICAD_IS_NIGHTLY
-        sentry_options_set_release( options, KICAD_SEMANTIC_VERSION );
-#else
-        sentry_options_set_release( options, KICAD_COMMIT_HASH );
-#endif
+        if( IsNightlyVersion() )
+            sentry_options_set_release( options, GetSemanticVersion().ToStdString().c_str() );
+        else
+            sentry_options_set_release( options, GetCommitHash().ToStdString().c_str() );
 
         sentry_init( options );
 
@@ -337,7 +336,7 @@ void PGM_BASE::sentryInit()
         sentry_value_set_by_key( user, "id", sentry_value_new_string( m_sentryUid.c_str() ) );
         sentry_set_user( user );
 
-        sentry_set_tag( "kicad.version", KICAD_VERSION_FULL );
+        sentry_set_tag( "kicad.version", GetBuildVersion().ToStdString().c_str() );
     }
 }
 

cmake/BuildSteps/WriteVersionHeader.cmake
@@ -67,6 +67,10 @@ set( _wvh_new_version_text
 #ifndef __KICAD_VERSION_H__
 #define __KICAD_VERSION_H__
 
+#ifndef INCLUDE_KICAD_VERSION
+#error Do not include kicad_build_version.h directly.  Include build_version.h instead.
+#endif
+
 #define KICAD_COMMIT_HASH               \"${KICAD_COMMIT_HASH}\"
 #define KICAD_VERSION_FULL              \"${KICAD_VERSION_FULL}\"
 #define KICAD_SEMANTIC_VERSION          \"${KICAD_SEMANTIC_VERSION}\"

common/build_version.cpp
@@ -29,6 +29,8 @@
 #include <kiplatform/app.h>
 #include <font/outline_font.h>
 
+#include <tuple>
+
 // kicad_curl.h must be included before wx headers, to avoid
 // conflicts for some defines, at least on Windows
 // kicad_curl.h can create conflicts for some defines, at least on Windows
@@ -46,8 +48,9 @@ extern std::string GetCurlLibVersion();
 // The include file version.h is always created even if the repo version cannot be
 // determined.  In this case KICAD_VERSION_FULL will default to the KICAD_VERSION
 // that is set in KiCadVersion.cmake.
+#define INCLUDE_KICAD_VERSION
 #include <kicad_build_version.h>
-
+#undef INCLUDE_KICAD_VERSION
 
 wxString GetPlatformGetBitnessName()
 {
@@ -62,6 +65,12 @@ wxString GetPlatformGetBitnessName()
 #else
     return platform.GetArchName();
 #endif
+}
+
+
+bool IsNightlyVersion()
+{
+    return !!KICAD_IS_NIGHTLY;
 }
 
 
@@ -90,6 +99,27 @@ wxString GetMajorMinorVersion()
 {
     wxString msg = wxString::Format( wxT( "%s" ), wxT( KICAD_MAJOR_MINOR_VERSION ) );
     return msg;
+}
+
+
+wxString GetCommitHash()
+{
+    wxString msg = wxString::Format( wxT( "%s" ), wxT( KICAD_COMMIT_HASH ) );
+    return msg;
+}
+
+
+wxString GetMajorMinorPatchVersion()
+{
+    wxString msg = wxString::Format( wxT( "%s" ), wxT( KICAD_MAJOR_MINOR_PATCH_VERSION ) );
+    return msg;
+}
+
+const std::tuple<int,int,int>& GetMajorMinorPatchTuple()
+{
+    static std::tuple<int, int, int> retval = KICAD_MAJOR_MINOR_PATCH_TUPLE;
+
+    return retval;
 }
 
 

common/dialog_about/dialog_about.cpp
@@ -26,7 +26,7 @@
 #include <config.h>
 #include <string>
 
-#include <kicad_build_version.h>
+#include <build_version.h>
 
 #include <wx/clipbrd.h>
 #include <wx/msgdlg.h>
@@ -79,11 +79,12 @@ DIALOG_ABOUT::DIALOG_ABOUT( EDA_BASE_FRAME *aParent, ABOUT_APP_INFO& aAppInfo )
     else
     {
         wxIcon icon;
-#if KICAD_IS_NIGHTLY
-        icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_nightly ) );
-#else
-        icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad ) );
-#endif
+
+        if( IsNightlyVersion() )
+            icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_nightly ) );
+        else
+            icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad ) );
+
         SetIcon( icon );
         m_bitmapApp->SetBitmap( icon );
     }

include/build_version.h
@@ -27,8 +27,9 @@
 #ifndef KICAD_BUILD_VERSION_H
 #define KICAD_BUILD_VERSION_H
 
-class wxString;
+#include <tuple>
 
+class wxString;
 
 /**
  * Get the full KiCad version string. This string contains platform-specific information
@@ -60,12 +61,39 @@ wxString GetSemanticVersion();
 wxString GetMajorMinorVersion();
 
 /**
+ * Get the major, minor and patch version in a string major.minor.patch
+ * This is extracted by CMake from the KICAD_SEMANTIC_VERSION variable.
+ *
+ * @return the major.minor.patch version as a string
+ */
+wxString GetMajorMinorPatchVersion();
+
+/**
  * Get the build date as a string.
  *
  * @return the build date string
  */
 wxString GetBuildDate();
 
+/**
+ * Get the commit hash as a string.
+ *
+ * @return the commit hash string
+ */
+wxString GetCommitHash();
+
+/**
+ * Get the build version numbers as a tuple
+ *
+ * @return A tuple with three ints for major/minor/patch revisions
+ */
+const std::tuple<int,int,int>& GetMajorMinorPatchTuple();
+
+/**
+ * Check if the build is meant to be nightly
+ * @return true if running nightly build
+ */
+bool IsNightlyVersion();
 
 /**
  * Create a version info string for bug reports and the about dialog

kicad/cli/command_version.cpp
@@ -21,7 +21,7 @@
 #include "command_version.h"
 #include <cli/exit_codes.h>
 #include <wx/crt.h>
-#include <kicad_build_version.h>
+#include <build_version.h>
 
 #include <macros.h>
 #include <build_version.h>
@@ -41,11 +41,11 @@ int CLI::VERSION_COMMAND::doPerform( KIWAY& aKiway )
     wxString format = FROM_UTF8( m_argParser.get<std::string>( ARG_FORMAT ).c_str() );
     if( format == wxS( "plain" ) )
     {
-        wxPrintf( KICAD_MAJOR_MINOR_PATCH_VERSION );
+        wxPrintf( GetMajorMinorPatchVersion() );
     }
     else if( format == wxS( "commit" ) )
     {
-        wxPrintf( KICAD_COMMIT_HASH );
+        wxPrintf( GetCommitHash() );
     }
     else if( format == wxS( "about" ) )
     {

kicad/kicad_cli.cpp
@@ -43,7 +43,7 @@
 #include "pgm_kicad.h"
 #include "kicad_manager_frame.h"
 
-#include <kicad_build_version.h>
+#include <build_version.h>
 #include <kiplatform/app.h>
 #include <kiplatform/environment.h>
 
@@ -289,7 +289,7 @@ bool PGM_KICAD::OnPgmInit()
 
 int PGM_KICAD::OnPgmRun()
 {
-    argparse::ArgumentParser argParser( std::string( "kicad-cli" ), KICAD_MAJOR_MINOR_VERSION,
+    argparse::ArgumentParser argParser( std::string( "kicad-cli" ), GetMajorMinorVersion().ToStdString(),
                                         argparse::default_arguments::none );
 
     argParser.add_argument( "-v", ARG_VERSION )

kicad/kicad_manager_frame.cpp
@@ -40,7 +40,7 @@
 #include <gestfich.h>
 #include <kiplatform/app.h>
 #include <kiplatform/policy.h>
-#include <kicad_build_version.h>
+#include <build_version.h>
 #include <kiway.h>
 #include <kiway_express.h>
 #include <launch_ext.h>
@@ -141,21 +141,24 @@ KICAD_MANAGER_FRAME::KICAD_MANAGER_FRAME( wxWindow* parent, const wxString& titl
     wxIcon icon;
     wxIconBundle icon_bundle;
 
-#if KICAD_IS_NIGHTLY
-    icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_nightly ) );
-    icon_bundle.AddIcon( icon );
-    icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_nightly_32 ) );
-    icon_bundle.AddIcon( icon );
-    icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_nightly_16 ) );
-    icon_bundle.AddIcon( icon );
-#else
-    icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad ) );
-    icon_bundle.AddIcon( icon );
-    icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_32 ) );
-    icon_bundle.AddIcon( icon );
-    icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_16 ) );
-    icon_bundle.AddIcon( icon );
-#endif
+    if( IsNightlyVersion())
+    {
+        icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_nightly ) );
+        icon_bundle.AddIcon( icon );
+        icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_nightly_32 ) );
+        icon_bundle.AddIcon( icon );
+        icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_nightly_16 ) );
+        icon_bundle.AddIcon( icon );
+    }
+    else
+    {
+        icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad ) );
+        icon_bundle.AddIcon( icon );
+        icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_32 ) );
+        icon_bundle.AddIcon( icon );
+        icon.CopyFromBitmap( KiBitmap( BITMAPS::icon_kicad_16 ) );
+        icon_bundle.AddIcon( icon );
+    }
 
     SetIcons( icon_bundle );
 

commit 80b182609588424d5782586fcfc03cb4bd58c329

Fix sentry release tagging broken by 98d55ce8

common/pgm_base.cpp
@@ -327,7 +327,7 @@ void PGM_BASE::sentryInit()
         sentry_options_set_symbolize_stacktraces( options, true );
         sentry_options_set_auto_session_tracking( options, false );
 
-        if( IsNightlyVersion() )
+        if( !IsNightlyVersion() )
             sentry_options_set_release( options, GetSemanticVersion().ToStdString().c_str() );
         else
             sentry_options_set_release( options, GetCommitHash().ToStdString().c_str() );