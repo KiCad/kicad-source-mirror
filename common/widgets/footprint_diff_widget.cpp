1058d36315c8f2eccbc4801fac80389729941730
Jeff Young <jeff@rokeby.ie>
2023-03-13 22:35:45 +0000

    Unrotate/unflip board footprint to match library copy.
    Fixes https://gitlab.com/kicad/code/kicad/issues/14276

--- common/widgets/footprint_diff_widget.cpp
+++ common/widgets/footprint_diff_widget.cpp
@@ -73,6 +73,12 @@ void FOOTPRINT_DIFF_WIDGET::DisplayDiff( FOOTPRINT* aBoardFootprint,
 
     m_boardItemCopy->Move( -m_boardItemCopy->GetPosition() );
 
+    if( m_boardItemCopy->IsFlipped() )
+        m_boardItemCopy->Flip( {0,0}, false );
+
+    if( m_boardItemCopy->GetOrientation() != ANGLE_0 )
+        m_boardItemCopy->Rotate( {0,0}, -m_boardItemCopy->GetOrientation() );
+
     m_libraryItem = aLibFootprint;
 
     DisplayFootprints( m_boardItemCopy, m_libraryItem );
