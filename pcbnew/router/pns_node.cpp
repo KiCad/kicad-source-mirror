b06f90335b2106207f3d370935593703228cd4a2
Jon Evans <jon@craftyjon.com>
2023-05-07 22:12:11 -0400

    PNS: Remove holes when removing items with holes
    Related to https://gitlab.com/kicad/code/kicad/-/issues/14702

--- pcbnew/router/pns_node.cpp
+++ pcbnew/router/pns_node.cpp
@@ -840,8 +840,15 @@ void NODE::Remove( ITEM* aItem )
         break;
 
     case ITEM::SOLID_T:
+    {
+        SOLID* solid = static_cast<SOLID*>( aItem );
+
+        if( solid->HasHole() )
+            Remove( solid->Hole() );
+
         Remove( static_cast<SOLID*>( aItem ) );
         break;
+    }
 
     case ITEM::SEGMENT_T:
         Remove( static_cast<SEGMENT*>( aItem ) );
@@ -858,8 +865,15 @@ void NODE::Remove( ITEM* aItem )
     }
 
     case ITEM::VIA_T:
+    {
+        VIA* via = static_cast<VIA*>( aItem );
+
+        if( via->HasHole() )
+            Remove( via->Hole() );
+
         Remove( static_cast<VIA*>( aItem ) );
         break;
+    }
 
     default:
         break;
