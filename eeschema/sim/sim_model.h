03486443c7ed1902f1f7f5cb015a29265ebb39c9
Jeff Young <jeff@rokeby.ie>
2023-05-22 11:45:59 +0100

    Fix more SPICE case-insensitivity bugs.
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/14793

--- eeschema/sim/sim_model.h
+++ eeschema/sim/sim_model.h
@@ -362,6 +362,8 @@ public:
                 enumValues( std::move( aEnumValues ) )
             {}
 
+            bool Matches( const std::string& aName ) const;
+
             std::string              name;
             unsigned                 id;
             DIR                      dir;
@@ -377,12 +379,17 @@ public:
             std::vector<std::string> enumValues;
         };
 
-        std::string  value;
-        const INFO&  info;
-
         PARAM( const INFO& aInfo ) :
                 info( aInfo )
         {}
+
+        bool Matches( const std::string& aName ) const
+        {
+            return info.Matches( aName );
+        }
+
+        std::string  value;
+        const INFO&  info;
     };
 
 
