KiCad/kicad-source-mirror

commit 942ee13cd1e61463b605c6c020a6de43d72bc480

PNS: Allow width-caused DRC violations at start of route
Also fix an issue where DRC violations introduced by the
width change action could be committed in walk/shove mode.

Fixes https://gitlab.com/kicad/code/kicad/-/issues/14805

pcbnew/router/pns_line_placer.cpp
@@ -1156,6 +1156,16 @@ void LINE_PLACER::routeStep( const VECTOR2I& aP )
         {
             m_tail = prevTail;
             m_head = prevHead;
+
+            // If we fail to walk out of the initial point (no tail), instead of returning an empty
+            // line, return a zero-length line so that the user gets some feedback that routing is
+            // happening.  This will get pruned later.
+            if( m_tail.PointCount() == 0 )
+            {
+                m_tail.Line().Append( m_p_start );
+                m_tail.Line().Append( m_p_start, true );
+            }
+
             fail = true;
         }
 
@@ -1224,10 +1234,10 @@ bool LINE_PLACER::route( const VECTOR2I& aP )
 {
     routeStep( aP );
 
-    if (!m_head.PointCount() )
+    if( !m_head.PointCount() )
         return false;
 
-    return m_head.CPoint(-1) == aP;
+    return m_head.CPoint( -1 ) == aP;
 }
 
 
@@ -1235,7 +1245,12 @@ const LINE LINE_PLACER::Trace() const
 {
     SHAPE_LINE_CHAIN l( m_tail.CLine() );
     l.Append( m_head.CLine() );
-    l.Simplify();
+
+    // Only simplify if we have more than two points, because if we have a zero-length seg as the
+    // only part of the trace, we don't want it to be removed at this stage (will be the case if
+    // the routing start point violates DRC due to track width in shove/walk mode, for example).
+    if( l.PointCount() > 2 )
+        l.Simplify();
 
     LINE tmp( m_head );
 
@@ -1515,11 +1530,13 @@ bool LINE_PLACER::FixRoute( const VECTOR2I& aP, ITEM* aEndItem, bool aForceFinis
                 aEndItem->SetNet( m_currentNet );
             }
         }
-
-        // Collisions still prevent fixing unless "Allow DRC violations" is checked
-        if( !Settings().AllowDRCViolations() && m_world->CheckColliding( &pl ) )
-            return false;
     }
+
+    // Collisions still prevent fixing unless "Allow DRC violations" is checked
+    // Note that collisions can occur even in walk/shove modes if the beginning of the trace
+    // collides (for example if the starting track width is too high)
+    if( !Settings().AllowDRCViolations() && m_world->CheckColliding( &pl ) )
+        return false;
 
     const SHAPE_LINE_CHAIN& l = pl.CLine();
 

pcbnew/router/pns_kicad_iface.cpp
@@ -619,6 +619,7 @@ bool PNS_KICAD_IFACE_BASE::ImportSizes( PNS::SIZES_SETTINGS& aSizes, PNS::ITEM* 
     }
 
     aSizes.SetTrackWidth( trackWidth );
+    aSizes.SetBoardMinTrackWidth( bds.m_TrackMinWidth );
     aSizes.SetTrackWidthIsExplicit( !bds.m_UseConnectedTrackWidth || bds.m_TempOverrideTrackWidth );
 
     int viaDiameter = bds.m_ViasMinSize;

pcbnew/router/pns_router.cpp
@@ -318,16 +318,23 @@ bool ROUTER::isStartingPointRoutable( const VECTOR2I& aWhere, ITEM* aStartItem, 
 
         if( m_world->CheckColliding( &dummyStartLine, ITEM::ANY_T ) )
         {
-            ITEM_SET          dummyStartSet( &dummyStartLine );
-            NODE::ITEM_VECTOR highlightedItems;
+            // If the only reason we collide is track width; it's better to allow the user to start
+            // anyway and just highlight the resulting collisions, so they can change width later.
+            dummyStartLine.SetWidth( m_sizes.BoardMinTrackWidth() );
 
-            markViolations( m_world.get(), dummyStartSet, highlightedItems );
+            if( m_world->CheckColliding( &dummyStartLine, ITEM::ANY_T ) )
+            {
+                ITEM_SET dummyStartSet( &dummyStartLine );
+                NODE::ITEM_VECTOR highlightedItems;
 
-            for( ITEM* item : highlightedItems )
-                m_iface->HideItem( item );
+                markViolations( m_world.get(), dummyStartSet, highlightedItems );
 
-            SetFailureReason( _( "The routing start point violates DRC." ) );
-            return false;
+                for( ITEM* item : highlightedItems )
+                    m_iface->HideItem( item );
+
+                SetFailureReason( _( "The routing start point violates DRC." ) );
+                return false;
+            }
         }
     }
     else if( m_mode == PNS_MODE_ROUTE_DIFF_PAIR )
@@ -372,18 +379,27 @@ bool ROUTER::isStartingPointRoutable( const VECTOR2I& aWhere, ITEM* aStartItem, 
         if( m_world->CheckColliding( &dummyStartLineA, ITEM::ANY_T )
                 || m_world->CheckColliding( &dummyStartLineB, ITEM::ANY_T ) )
         {
-            ITEM_SET          dummyStartSet;
-            NODE::ITEM_VECTOR highlightedItems;
+            // If the only reason we collide is track width; it's better to allow the user to start
+            // anyway and just highlight the resulting collisions, so they can change width later.
+            dummyStartLineA.SetWidth( m_sizes.BoardMinTrackWidth() );
+            dummyStartLineB.SetWidth( m_sizes.BoardMinTrackWidth() );
 
-            dummyStartSet.Add( dummyStartLineA );
-            dummyStartSet.Add( dummyStartLineB );
-            markViolations( m_world.get(), dummyStartSet, highlightedItems );
+            if( m_world->CheckColliding( &dummyStartLineA, ITEM::ANY_T )
+                || m_world->CheckColliding( &dummyStartLineB, ITEM::ANY_T ) )
+            {
+                ITEM_SET          dummyStartSet;
+                NODE::ITEM_VECTOR highlightedItems;
 
-            for( ITEM* item : highlightedItems )
-                m_iface->HideItem( item );
+                dummyStartSet.Add( dummyStartLineA );
+                dummyStartSet.Add( dummyStartLineB );
+                markViolations( m_world.get(), dummyStartSet, highlightedItems );
 
-            SetFailureReason( _( "The routing start point violates DRC." ) );
-            return false;
+                for( ITEM* item : highlightedItems )
+                    m_iface->HideItem( item );
+
+                SetFailureReason( _( "The routing start point violates DRC." ) );
+                return false;
+            }
         }
     }
 
@@ -696,8 +712,7 @@ void ROUTER::updateView( NODE* aNode, ITEM_SET& aCurrent, bool aDragging )
     if( !aNode )
         return;
 
-    if( Settings().Mode() == RM_MarkObstacles || m_forceMarkObstaclesMode )
-        markViolations( aNode, aCurrent, removed );
+    markViolations( aNode, aCurrent, removed );
 
     aNode->GetUpdatedItems( removed, added );
 

pcbnew/router/pns_sizes_settings.h
@@ -73,6 +73,9 @@ public:
     bool TrackWidthIsExplicit() const { return m_trackWidthIsExplicit; }
     void SetTrackWidthIsExplicit( bool aIsExplicit ) { m_trackWidthIsExplicit = aIsExplicit; }
 
+    int BoardMinTrackWidth() const { return m_boardMinTrackWidth; }
+    void SetBoardMinTrackWidth( int aWidth ) { m_boardMinTrackWidth = aWidth; }
+
     int DiffPairWidth() const { return m_diffPairWidth; }
     int DiffPairGap() const { return m_diffPairGap; }
 
@@ -135,6 +138,7 @@ private:
     int     m_minClearance;
     int     m_trackWidth;
     bool    m_trackWidthIsExplicit;
+    int     m_boardMinTrackWidth;
 
     VIATYPE m_viaType;
     int     m_viaDiameter;

pcbnew/router/router_preview_item.cpp
@@ -214,7 +214,22 @@ void ROUTER_PREVIEW_ITEM::drawLineChain( const SHAPE_LINE_CHAIN_BASE* aL, KIGFX:
     gal->SetIsFill( false );
 
     for( int s = 0; s < aL->GetSegmentCount(); s++ )
-        gal->DrawLine( aL->GetSegment( s ).A, aL->GetSegment( s ).B );
+    {
+        SEG seg = aL->GetSegment( s );
+
+        if( seg.A == seg.B )
+        {
+            gal->SetIsFill( true );
+            gal->SetIsStroke( false );
+            gal->DrawCircle( seg.A, gal->GetLineWidth() / 2 );
+            gal->SetIsFill( false );
+            gal->SetIsStroke( true );
+        }
+        else
+        {
+            gal->DrawLine( seg.A, seg.B );
+        }
+    }
 
     const SHAPE_LINE_CHAIN* lineChain = dynamic_cast<const SHAPE_LINE_CHAIN*>( aL );
 

commit 141cc85ff35c7242ca1690ed8be044666607a6f0

PNS: Work around collision checking issues in shove mode
Need to special-case shove mode since 942ee13c introduced
collision checking in all modes.

Fixes https://gitlab.com/kicad/code/kicad/-/issues/14836

pcbnew/router/pns_line_placer.cpp
@@ -1534,9 +1534,21 @@ bool LINE_PLACER::FixRoute( const VECTOR2I& aP, ITEM* aEndItem, bool aForceFinis
 
     // Collisions still prevent fixing unless "Allow DRC violations" is checked
     // Note that collisions can occur even in walk/shove modes if the beginning of the trace
-    // collides (for example if the starting track width is too high)
-    if( !Settings().AllowDRCViolations() && m_world->CheckColliding( &pl ) )
-        return false;
+    // collides (for example if the starting track width is too high).
+
+    if( !Settings().AllowDRCViolations() )
+    {
+        NODE* checkNode = ( Settings().Mode() == RM_Shove ) ? m_shove->CurrentNode() : m_world;
+        std::optional<OBSTACLE> obs = checkNode->CheckColliding( &pl );
+
+        if( obs )
+        {
+            // TODO: Determine why the shove node sometimes reports collisions against shoved objects.
+            // For now, to work around this issue, we consider only solids in shove mode.
+            if( Settings().Mode() != RM_Shove || obs->m_item->OfKind( ITEM::SOLID_T ) )
+                return false;
+        }
+    }
 
     const SHAPE_LINE_CHAIN& l = pl.CLine();
 