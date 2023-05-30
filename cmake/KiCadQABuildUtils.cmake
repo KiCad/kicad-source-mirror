8381677f206cfeb22792d01722e46416bfc48ecf
Marek Roszko <mark.roszko@gmail.com>
2023-05-29 22:23:44 -0400

    Remove --report_level=no

--- cmake/KiCadQABuildUtils.cmake
+++ cmake/KiCadQABuildUtils.cmake
@@ -62,7 +62,7 @@ if( KICAD_TEST_XML_OUTPUT )
     # Due to Boost issue in 1.62, have to use the --logger parameter, rather than
     # separate --log_format, --log_sink, etc parameter
     # https://svn.boost.org/trac10/ticket/12507
-    set(BOOST_TEST_PARAMS --logger=JUNIT,all,${TEST_NAME}.boost-results.xml --report_level=no --result_code=yes)
+    set(BOOST_TEST_PARAMS --logger=JUNIT,all,${TEST_NAME}.boost-results.xml)
 endif()
 
 # Add the test to the CTest registry
