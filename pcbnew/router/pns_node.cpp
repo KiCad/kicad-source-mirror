3c3517cc36c760c7f25524568208b8a386dc173e
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2023-06-01 22:44:24 +0200

    router: can't add a clone of a VIA's hole when adding the via to INDEX - this will result in bogus colliding objects
    This commit partially reverts deb3cbf8. VIA and SOLID operator='s introduced in it do a good job but the cloning of holes in addHole() causes bogus collisions.

--- pcbnew/router/pns_node.cpp
+++ pcbnew/router/pns_node.cpp
@@ -518,11 +518,8 @@ void NODE::addHole( HOLE* aHole )
     // do we need holes in the connection graph?
     //linkJoint( aHole->Pos(), aHole->Layers(), aHole->Net(), aHole );
 
-    HOLE* hole = new HOLE( aHole->Shape()->Clone() );
-    hole->SetLayers( aHole->Layers() );
-
-    hole->SetOwner( this );
-    m_index->Add( hole );
+    aHole->SetOwner( this );
+    m_index->Add( aHole );
 }
 
 
