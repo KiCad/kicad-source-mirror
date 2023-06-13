137640ac28ae000373c30a8cc2110a456bfd5701
Ian McInerney <ian.s.mcinerney@ieee.org>
2023-06-14 00:26:46 +0100

    Remove group association before plotting board outline in drill map
    The plotter doesn't care about the grouping, and we expect the item to
    have no group on deletion, so temporary items like this shouldn't be
    part of one.
    
    (Sentry issue KICAD-24Y)

--- pcbnew/exporters/gen_drill_report_files.cpp
+++ pcbnew/exporters/gen_drill_report_files.cpp
@@ -225,6 +225,7 @@ bool GENDRILL_WRITER_BASE::genDrillMapFile( const wxString& aFullFileName, PLOT_
             {
             PCB_SHAPE dummy_shape( *static_cast<PCB_SHAPE*>( item ) );
             dummy_shape.SetLayer( Dwgs_User );
+            dummy_shape.SetParentGroup( nullptr );      // Remove group association, not needed for plotting
             itemplotter.PlotShape( &dummy_shape );
             }
             break;
