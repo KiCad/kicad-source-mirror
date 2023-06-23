f56ea370be7b2706feb308e15c901f6ff49e8256
jean-pierre charras <jp.charras@wanadoo.fr>
2023-06-23 18:48:11 +0200

    PCB_FIELD::GetCanonicalName() must return the canonical name (not a translation)

--- pcbnew/pcb_field.cpp
+++ pcbnew/pcb_field.cpp
@@ -73,7 +73,7 @@ wxString PCB_FIELD::GetCanonicalName() const
     if( m_parent && m_parent->Type() == PCB_FOOTPRINT_T )
     {
         if( m_id < MANDATORY_FIELDS )
-            return TEMPLATE_FIELDNAME::GetDefaultFieldName( m_id, true );
+            return TEMPLATE_FIELDNAME::GetDefaultFieldName( m_id, false );
         else
             return m_name;
     }
