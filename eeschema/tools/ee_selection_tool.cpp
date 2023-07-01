55b31030c0fef9baee893bdbbbbd3ea0f1e4f431
Alex Shvartzkop <dudesuchamazing@gmail.com>
2023-07-01 05:39:41 +0300

    Perf: don't search for niluuid items when moving cursor in eeschema.
    Reduces CPU usage by 60% in some cases.

--- eeschema/tools/ee_selection_tool.cpp
+++ eeschema/tools/ee_selection_tool.cpp
@@ -670,9 +670,11 @@ int EE_SELECTION_TOOL::Main( const TOOL_EVENT& aEvent )
             }
         }
 
-        if( EDA_ITEM* item = m_frame->GetItem( rolloverItem ) )
+        if( rolloverItem != niluuid )
         {
-            if( !( item->GetFlags() & IS_ROLLOVER ) )
+            EDA_ITEM* item = m_frame->GetItem( rolloverItem );
+
+            if( item && !( item->GetFlags() & IS_ROLLOVER ) )
             {
                 item->SetFlags( IS_ROLLOVER );
                 lastRolloverItem = rolloverItem;
