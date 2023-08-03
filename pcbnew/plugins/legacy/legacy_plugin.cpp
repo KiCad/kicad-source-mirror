573d2f94de72a8a3779a53f060feb4c87663978d
Roberto Fernandez Bautista <roberto.fer.bau@gmail.com>
2023-08-03 12:29:05 +0200

    Fix loading of via drill default sizes and other settings from *.brd kicad files

--- pcbnew/plugins/legacy/legacy_plugin.cpp
+++ pcbnew/plugins/legacy/legacy_plugin.cpp
@@ -850,6 +850,7 @@ void LEGACY_PLUGIN::loadSETUP()
     char*                     saveptr;
 
     m_board->m_LegacyDesignSettingsLoaded = true;
+    m_board->m_LegacyNetclassesLoaded = true;
 
     while( ( line = READLINE( m_reader ) ) != nullptr )
     {
