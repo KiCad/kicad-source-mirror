ea65a5a80bac300455019e9af652fb3d42a9b495
Alex Shvartzkop <dudesuchamazing@gmail.com>
2023-08-16 08:23:27 +0300

    Tent vias by default.

--- pcbnew/pcb_plot_params.cpp
+++ pcbnew/pcb_plot_params.cpp
@@ -101,8 +101,8 @@ PCB_PLOT_PARAMS::PCB_PLOT_PARAMS()
 
     // we used 0.1mils for SVG step before, but nm precision is more accurate, so we use nm
     m_svgPrecision               = SVG_PRECISION_DEFAULT;
-    m_plotDrawingSheet = false;
-    m_plotViaOnMaskLayer         = true;
+    m_plotDrawingSheet           = false;
+    m_plotViaOnMaskLayer         = false;
     m_plotMode                   = FILLED;
     m_DXFPolygonMode = true;
     m_DXFUnits = DXF_UNITS::INCHES;
