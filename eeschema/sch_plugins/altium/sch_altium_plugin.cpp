de2b645fc8215192d87ad358be9f2154004b96b2
Alex Shvartzkop <dudesuchamazing@gmail.com>
2023-09-11 10:02:43 +0300

    Altium schematic: hide power symbol pin.

--- eeschema/sch_plugins/altium/sch_altium_plugin.cpp
+++ eeschema/sch_plugins/altium/sch_altium_plugin.cpp
@@ -2920,6 +2920,7 @@ void SCH_ALTIUM_PLUGIN::ParsePowerPort( const std::map<wxString, wxString>& aPro
         pin->SetPosition( { 0, 0 } );
         pin->SetLength( 0 );
         pin->SetType( ELECTRICAL_PINTYPE::PT_POWER_IN );
+        pin->SetVisible( false );
 
         VECTOR2I valueFieldPos =
                 HelperGeneratePowerPortGraphics( libSymbol, elem.style, m_reporter );
