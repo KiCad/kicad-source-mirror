f5edac547e520852837eb6a1df0fb22c4487db82
Jeff Young <jeff@rokeby.ie>
2023-10-17 13:04:38 +0100

    More modern look & feel for tuning status popup.
    (Also includes min and max info, and move string processing out
    of router.)

--- include/status_popup.h
+++ include/status_popup.h
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2014-2015 CERN
- * Copyright (C) 2020-2021 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2020-2023 KiCad Developers, see AUTHORS.txt for contributors.
  * Author: Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
  *
  * This program is free software; you can redistribute it and/or
