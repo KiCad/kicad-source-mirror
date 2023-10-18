599a8304a6c1e747b80bb845981d022b460416cc
jean-pierre charras <jp.charras@wanadoo.fr>
2023-10-18 11:14:43 +0200

    Pcbnew, ConnectBoardShapes(): do not add 0 size segments to outlines. Add also our standard copyright text.

--- pcbnew/fix_board_shape.cpp
+++ pcbnew/fix_board_shape.cpp
@@ -1,3 +1,27 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2023 Alex Shvartzkop <dudesuchamazing@gmail.com>
+ * Copyright (C) 2023 KiCad Developers, see AUTHORS.txt for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you may find one here:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * or you may search the http://www.gnu.org website for the version 2 license,
+ * or you may write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
 #include "fix_board_shape.h"
 
 #include <vector>
@@ -88,6 +112,10 @@ void ConnectBoardShapes( std::vector<PCB_SHAPE*>&                 aShapeList,
 
     auto addSegment = [&]( const VECTOR2I start, const VECTOR2I end, int width, PCB_LAYER_ID layer )
     {
+        // Ensure null shapes are not added
+        if( start == end )
+            return;
+
         std::unique_ptr<PCB_SHAPE> seg = std::make_unique<PCB_SHAPE>( nullptr, SHAPE_T::SEGMENT );
         seg->SetStart( start );
         seg->SetEnd( end );
@@ -206,7 +234,6 @@ void ConnectBoardShapes( std::vector<PCB_SHAPE*>&                 aShapeList,
                         arcShape->SetArcGeometry( circProj, arc.GetArcMid(), arc.GetP1() );
                     else
                         arcShape->SetArcGeometry( arc.GetP0(), arc.GetArcMid(), circProj );
-
 
                     addSegment( circProj, lineProj, segShape->GetWidth(), segShape->GetLayer() );
                     success = true;
