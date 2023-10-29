e334f11f1215c8d8087542e692be75e74b55f1fc
jean-pierre charras <jp.charras@wanadoo.fr>
2023-10-29 18:34:28 +0100

    kicad manager: fix broken Rename file command.

--- kicad/project_tree_item.cpp
+++ kicad/project_tree_item.cpp
@@ -123,10 +123,11 @@ bool PROJECT_TREE_ITEM::Rename( const wxString& name, bool check )
     if( newFile == GetFileName() )
         return false;
 
+    // If required, prompt the user if the filename extension has changed:
     wxString ext = PROJECT_TREE_PANE::GetFileExt( GetType() );
-    wxRegEx  reg( wxT( "^.*\\" ) + ext + wxT( "$" ), wxRE_ICASE );
+    wxString full_ext = wxT( "." ) + ext;
 
-    if( check && !ext.IsEmpty() && !reg.Matches( newFile ) )
+    if( check && !ext.IsEmpty() && !newFile.EndsWith( full_ext ) )
     {
         wxMessageDialog dialog( m_parent, _( "Changing file extension will change file type.\n"
                                              "Do you want to continue ?" ),
