acd244f873a503e26ab8821fa9c6ae45cb0bf063
Alex Shvartzkop <dudesuchamazing@gmail.com>
2023-11-21 00:47:26 +0300

    STEP export: Do not set BRepBuilderAPI precision.
    This caused issues when loading some STEP models.
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/15485
    
    (cherry picked from commit f3fdfa73e6032609f93ae14452e18dd702d4a076)

--- pcbnew/exporters/step/step_pcb_model.cpp
+++ pcbnew/exporters/step/step_pcb_model.cpp
@@ -189,7 +189,6 @@ STEP_PCB_MODEL::STEP_PCB_MODEL( const wxString& aPcbName )
     m_mergeOCCMaxDist = OCC_MAX_DISTANCE_TO_MERGE_POINTS;
     m_minx = 1.0e10;    // absurdly large number; any valid PCB X value will be smaller
     m_pcbName = aPcbName;
-    BRepBuilderAPI::Precision( m_mergeOCCMaxDist );
     m_maxError = pcbIUScale.mmToIU( ARC_TO_SEGMENT_MAX_ERROR_MM );
 }
 
@@ -328,7 +327,6 @@ void STEP_PCB_MODEL::OCCSetMergeMaxDistance( double aDistance )
 {
     // Ensure a minimal value (in mm)
     m_mergeOCCMaxDist = aDistance;
-    BRepBuilderAPI::Precision( m_mergeOCCMaxDist );
 }
 
 
