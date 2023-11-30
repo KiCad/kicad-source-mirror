60ddafa4ff5263b26d17bd19a99806777884e794
Jon Evans <jon@craftyjon.com>
2023-11-30 17:08:37 -0500

    Guard against UNDEFINED_LAYER in GetLayerSet
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/16230
    
    
    (cherry picked from commit 1ff3b5a4370614985840683371ee77105fa0940a)

--- pcbnew/pcb_track.cpp
+++ pcbnew/pcb_track.cpp
@@ -506,6 +506,9 @@ LSET PCB_VIA::GetLayerSet() const
 {
     LSET layermask;
 
+    if( m_layer < PCBNEW_LAYER_ID_START )
+        return layermask;
+
     if( GetViaType() == VIATYPE::THROUGH )
         layermask = LSET::AllCuMask();
     else
