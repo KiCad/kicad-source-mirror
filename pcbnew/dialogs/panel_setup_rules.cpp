25db3032eedd0c157faed76e82af212abc6e35a6
Jeff Young <jeff@rokeby.ie>
2023-12-27 12:08:38 +0000

    Add missing DRC auto-complete token.
    (cherry picked from commit 2ba9e07f0fd1453ec410d077e858bf4dbf24a0ab)

--- pcbnew/dialogs/panel_setup_rules.cpp
+++ pcbnew/dialogs/panel_setup_rules.cpp
@@ -396,6 +396,7 @@ void PANEL_SETUP_RULES::onScintillaCharAdded( wxStyledTextEvent &aEvent )
         else if( sexprs.top() == wxT( "disallow" ) || isDisallowToken( sexprs.top() ) )
         {
             tokens = wxT( "buried_via|"
+                          "footprint|"
                           "graphic|"
                           "hole|"
                           "micro_via|"
