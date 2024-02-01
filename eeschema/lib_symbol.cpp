7664c345e2c8087c6b73754117bc0aa4c45e0176
Seth Hillbrand <seth@kipro-pcb.com>
2024-02-01 09:00:13 +0100

    Revert "Schematic performance enhancements"
    This reverts commit df654ac12f3491cb18a59cebc2de3805279c23e0.

--- eeschema/lib_symbol.cpp
+++ eeschema/lib_symbol.cpp
@@ -1045,7 +1045,6 @@ void LIB_SYMBOL::GetPins( LIB_PINS& aList, int aUnit, int aBodyStyle ) const
 
     LIB_SYMBOL_SPTR            parent = m_parent.lock();
     const LIB_ITEMS_CONTAINER& drawItems = parent ? parent->m_drawings : m_drawings;
-    aList.reserve( drawItems.size( LIB_PIN_T ) );
 
     for( const LIB_ITEM& item : drawItems[LIB_PIN_T] )
     {
