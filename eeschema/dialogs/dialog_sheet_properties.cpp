27911d9b5a4f104b989ecf721f46e0fa92f9ef98
Wayne Stambaugh <stambaughw@gmail.com>
2024-02-17 08:50:22 -0500

    Fix crash when changing sheet file name that contains a highlighted net.
    Performing a full connectivity rebuild after changing a sheet file name
    ensures that there are no stale connectivity object pointers laying around.
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/17011

--- eeschema/dialogs/dialog_sheet_properties.cpp
+++ eeschema/dialogs/dialog_sheet_properties.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2009 Wayne Stambaugh <stambaughw@gmail.com>
- * Copyright (C) 2014-2023 KiCad Developers, see CHANGELOG.txt for contributors.
+ * Copyright (C) 2014-2024 KiCad Developers, see CHANGELOG.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -648,6 +648,9 @@ bool DIALOG_SHEET_PROPERTIES::onSheetFilenameChanged( const wxString& aNewFilena
 
     if( m_clearAnnotationNewItems )
         *m_clearAnnotationNewItems = clearAnnotation;
+
+    // Rebuild the entire connection graph.
+    m_frame->RecalculateConnections( nullptr, GLOBAL_CLEANUP );
 
     return true;
 }
