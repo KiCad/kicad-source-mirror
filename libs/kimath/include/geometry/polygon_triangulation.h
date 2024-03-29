892c5726f5e8c43a556b1a66f7d163ab886fa1f6
Seth Hillbrand <seth@kipro-pcb.com>
2024-03-29 16:46:08 -0700

    Move logging message after vertex check
    When creating the list, we may have fewer than needed vertices, which
    prevents calculating the area
    
    (cherry picked from commit 6c05f2c67c8040b6368667e464278e318d2aad25)

--- libs/kimath/include/geometry/polygon_triangulation.h
+++ libs/kimath/include/geometry/polygon_triangulation.h
@@ -81,10 +81,10 @@ public:
         /// therefore cannot be polygons
         VERTEX* firstVertex = createList( aPoly );
 
-        wxLogTrace( TRIANGULATE_TRACE, "Created list with %f area", firstVertex->area() );
-
         if( !firstVertex || firstVertex->prev == firstVertex->next )
             return true;
+
+        wxLogTrace( TRIANGULATE_TRACE, "Created list with %f area", firstVertex->area() );
 
         firstVertex->updateList();
 
