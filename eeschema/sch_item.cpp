531a8b9db95b1af4de0ab0f85792d735bd2c726d
Seth Hillbrand <seth@kipro-pcb.com>
2024-04-12 14:13:55 -0700

    Revert "Compare function should return int"
    This reverts commit 772798247889ac92221a13e709267348b2df21c1.

--- eeschema/sch_item.cpp
+++ eeschema/sch_item.cpp
@@ -417,18 +417,18 @@ int SCH_ITEM::compare( const SCH_ITEM& aOther, int aCompareFlags ) const
         return m_bodyStyle - aOther.m_bodyStyle;
 
     if( IsPrivate() != aOther.IsPrivate() )
-        return IsPrivate() < aOther.IsPrivate() ? -1 : 1;
+        return IsPrivate() < aOther.IsPrivate();
 
     if( GetPosition().x != aOther.GetPosition().x )
-        return GetPosition().x < aOther.GetPosition().x ? -1 : 1;
+        return GetPosition().x < aOther.GetPosition().x;
 
     if( GetPosition().y != aOther.GetPosition().y )
-        return GetPosition().y < aOther.GetPosition().y ? -1 : 1;
+        return GetPosition().y < aOther.GetPosition().y;
 
     if( aCompareFlags & SCH_ITEM::COMPARE_FLAGS::EQUALITY )
         return 0;
 
-    return m_Uuid < aOther.m_Uuid ? -1 : 1;
+    return m_Uuid < aOther.m_Uuid;
 }
 
 
