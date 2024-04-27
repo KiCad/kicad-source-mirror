2ca07b47afac38d7af04d9d5946783ff6aa5037f
Wayne Stambaugh <stambaughw@gmail.com>
2024-04-27 13:10:22 -0400

    Begin version 8.0.3 development.

--- cmake/KiCadVersion.cmake
+++ cmake/KiCadVersion.cmake
@@ -37,7 +37,7 @@
 # KiCad.
 #
 # Note: This version string should follow the semantic versioning system
-set( KICAD_SEMANTIC_VERSION "8.0.2" )
+set( KICAD_SEMANTIC_VERSION "8.0.2-unknown" )
 
 # Default the version to the semantic version.
 # This is overridden by the git repository tag though (if using git)
