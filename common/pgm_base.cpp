9f1d1fc45ee9a642d56144f263f2c40033f88eaf
Mark Roszko <mark.roszko@gmail.com>
2024-04-29 11:48:22 +0000

    Delete m_locale in the pgm_base destructor instead of manual destroy method
    We need the object in m_locale for a bit more things during program cleanup,
    especially on macOS
    
    The destructor of PGM_BASE is sufficient allowance of lifetime.
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/17369
    
    
    (cherry picked from commit 9ffa5db667103f086baf23c94eb05d2dd8123579)
    
    Co-authored-by: Marek Roszko <mark.roszko@gmail.com>

--- common/pgm_base.cpp
+++ common/pgm_base.cpp
@@ -153,6 +153,9 @@ PGM_BASE::~PGM_BASE()
     }
 
     delete[] m_argvUtf8;
+
+    delete m_locale;
+    m_locale = nullptr;
 }
 
 
@@ -163,10 +166,6 @@ void PGM_BASE::Destroy()
 #ifdef KICAD_USE_SENTRY
     sentry_close();
 #endif
-
-    // unlike a normal destructor, this is designed to be called more than once safely:
-    delete m_locale;
-    m_locale = nullptr;
 
     m_pgm_checker.reset();
 }
