62d4bd7c1d16a1747846b5793af4b90ba4734ad6
Jeff Young <jeff@rokeby.ie>
2024-05-05 17:32:07 +0100

    Exercise more care when comparing doubles.
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/17249

--- pcbnew/pad.cpp
+++ pcbnew/pad.cpp
@@ -874,7 +874,7 @@ void PAD::SetFPRelativeOrientation( const EDA_ANGLE& aAngle )
 }
 
 
-EDA_ANGLE PAD::GetFPRelativeOrientation()
+EDA_ANGLE PAD::GetFPRelativeOrientation() const
 {
     if( FOOTPRINT* parentFP = GetParentFootprint() )
         return GetOrientation() - parentFP->GetOrientation();
