6baceedea491b93336391eb8e160813c011f2f4a
dsa-t <dudesuchamazing@gmail.com>
2024-05-24 14:47:48 +0000

    Switch length tuner settings action scope to global to make the hotkey work.
    (cherry picked from commit 8d001a8031cf6ce24dd3f790575fbf638645432a)
    
    Co-authored-by: Alex Shvartzkop <dudesuchamazing@gmail.com>

--- pcbnew/tools/pcb_actions.cpp
+++ pcbnew/tools/pcb_actions.cpp
@@ -2470,7 +2470,7 @@ TOOL_ACTION PCB_ACTIONS::generatorsShowManager( TOOL_ACTION_ARGS()
 //
 TOOL_ACTION PCB_ACTIONS::lengthTunerSettings( TOOL_ACTION_ARGS()
         .Name( "pcbnew.LengthTuner.Settings" )
-        .Scope( AS_CONTEXT )
+        .Scope( AS_GLOBAL )
         .DefaultHotkey( MD_CTRL + 'L' )
         // Don't be tempted to remove "Modern Toolset only".  It's in the legacy property name.
         .LegacyHotkeyName( "Length Tuning Settings (Modern Toolset only)" )
