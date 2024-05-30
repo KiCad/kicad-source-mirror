877f7daa2bfde3e7f801becb8ef11774955b1fc5
jean-pierre charras <jp.charras@wanadoo.fr>
2024-05-30 15:50:18 +0200

    do not flag an empty string as translatable.
    (cherry picked from commit 1c9e96c89a687263a54b13288b49f1a2baaa17c4)

--- pcbnew/drc/drc_item.cpp
+++ pcbnew/drc/drc_item.cpp
@@ -130,7 +130,7 @@ DRC_ITEM DRC_ITEM::viaDiameter( DRCE_VIA_DIAMETER,
         wxT( "via_diameter" ) );
 
 DRC_ITEM DRC_ITEM::padstack( DRCE_PADSTACK,
-        _( "" ),
+        wxT( "" ),
         wxT( "padstack" ) );
 
 DRC_ITEM DRC_ITEM::padstackInvalid( DRCE_PADSTACK_INVALID,
