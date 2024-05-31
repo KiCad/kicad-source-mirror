2f9b10e379fe636636b554f8abdbbc057a5f34bd
Seth Hillbrand <seth@kipro-pcb.com>
2024-05-31 14:39:31 -0700

    Revert "Add additional 45° shortcuts for sin/cos"
    Needs more testing
    
    This reverts commit e83ca5e91d59053783f0bed2d6ae6a08d1a4203e.

--- libs/kimath/include/geometry/eda_angle.h
+++ libs/kimath/include/geometry/eda_angle.h
@@ -174,10 +174,6 @@ public:
 
         if( test.m_value == 0.0 || test.m_value == 180.0 )
             return 0.0;
-        else if( test.m_value == 45.0 || test.m_value == 135.0 )
-            return M_SQRT1_2;
-        else if( test.m_value == 225.0 || test.m_value == 315.0 )
-            return -M_SQRT1_2;
         else if( test.m_value == 90.0 )
             return 1.0;
         else if( test.m_value == 270.0 )
@@ -197,10 +193,6 @@ public:
             return -1.0;
         else if( test.m_value == 90.0 || test.m_value == 270.0 )
             return 0.0;
-        else if( test.m_value == 45.0 || test.m_value == 315.0 )
-            return M_SQRT2;
-        else if( test.m_value == 135.0 || test.m_value == 225.0 )
-            return -M_SQRT2;
         else
             return cos( AsRadians() );
     }
