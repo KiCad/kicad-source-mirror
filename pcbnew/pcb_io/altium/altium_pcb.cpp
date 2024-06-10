KiCad/kicad-source-mirror

commit 82c2bda52bcaff1253de997dd5db791bec0bb034

Altium PCB import: make sure Polygon fill is contained within the outline.

pcbnew/pcb_io/altium/altium_pcb.cpp
@@ -1991,14 +1991,30 @@ void ALTIUM_PCB::ParsePolygons6Data( const ALTIUM_COMPOUND_FILE&     aAltiumPcbF
             continue;
         }
 
-        std::unique_ptr<ZONE> zone = std::make_unique<ZONE>( m_board );
-        m_polygons.emplace_back( zone.get() );
+        // Altium polygon outlines have thickness, convert it to KiCad's representation.
+        SHAPE_POLY_SET outline( linechain );
+        outline.Inflate( elem.trackwidth / 2, CORNER_STRATEGY::CHAMFER_ACUTE_CORNERS, ARC_HIGH_DEF,
+                         true );
+
+        if( outline.OutlineCount() != 1 && m_reporter )
+        {
+            wxString msg;
+            msg.Printf( _( "Polygon outline count is %d, expected 1.", outline.OutlineCount() ) );
+
+            m_reporter->Report( msg, RPT_SEVERITY_ERROR );
+        }
+
+        if( outline.OutlineCount() == 0 )
+            continue;
+
+        std::unique_ptr<ZONE> zone = std::make_unique<ZONE>(m_board);
+        m_polygons.emplace_back(zone.get());
 
         zone->SetNetCode( GetNetCode( elem.net ) );
         zone->SetPosition( elem.vertices.at( 0 ).position );
         zone->SetLocked( elem.locked );
         zone->SetAssignedPriority( elem.pourindex > 0 ? elem.pourindex : 0 );
-        zone->Outline()->AddOutline( linechain );
+        zone->Outline()->AddOutline( outline.Outline( 0 ) );
 
         HelperSetZoneLayers( *zone, elem.layer );
 

commit 012d3f098fd57aef22e1013393d65e94abc24be1

Fix build error caused by commit 82c2bda52bcaff1253de997dd5db791bec0bb034.

pcbnew/pcb_io/altium/altium_pcb.cpp
@@ -2003,7 +2003,7 @@ void ALTIUM_PCB::ParsePolygons6Data( const ALTIUM_COMPOUND_FILE&     aAltiumPcbF
         if( outline.OutlineCount() != 1 && m_reporter )
         {
             wxString msg;
-            msg.Printf( _( "Polygon outline count is %d, expected 1.", outline.OutlineCount() ) );
+            msg.Printf( _( "Polygon outline count is %d, expected 1." ), outline.OutlineCount() );
 
             m_reporter->Report( msg, RPT_SEVERITY_ERROR );
         }