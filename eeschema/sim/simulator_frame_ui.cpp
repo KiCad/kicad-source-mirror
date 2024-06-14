c633ac26eb31cd31125faae2c25fb818568308fd
Jeff Young <jeff@rokeby.ie>
2024-06-14 16:30:32 +0100

    Don't lose value of cursor checkboxes when running sim.
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/18205

--- eeschema/sim/simulator_frame_ui.cpp
+++ eeschema/sim/simulator_frame_ui.cpp
@@ -2701,7 +2701,10 @@ void SIMULATOR_FRAME_UI::OnSimRefresh( bool aFinal )
     wxString msg;
 
     if( aFinal )
+    {
         applyUserDefinedSignals();
+        updateSignalsGrid();
+    }
 
     // If there are any signals plotted, update them
     if( SIM_TAB::IsPlottable( simType ) )
