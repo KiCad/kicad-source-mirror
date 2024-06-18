e1ffd956e6730c107af6930e692d11d8a0d27398
Jon Evans <jon@craftyjon.com>
2024-06-18 11:59:37 -0400

    Try harder to keep searchable text hidden in SVG exports

--- common/plotters/SVG_plotter.cpp
+++ common/plotters/SVG_plotter.cpp
@@ -851,7 +851,7 @@ void SVG_PLOTTER::Text( const VECTOR2I&        aPos,
 
     fprintf( m_outputFile,
              "textLength=\"%.*f\" font-size=\"%.*f\" lengthAdjust=\"spacingAndGlyphs\"\n"
-             "text-anchor=\"%s\" opacity=\"0\">%s</text>\n",
+             "text-anchor=\"%s\" opacity=\"0\" stroke-opacity=\"0\">%s</text>\n",
              m_precision, sz_dev.x, m_precision, sz_dev.y, hjust, TO_UTF8( XmlEsc( aText ) ) );
 
     if( !aOrient.IsZero() )
