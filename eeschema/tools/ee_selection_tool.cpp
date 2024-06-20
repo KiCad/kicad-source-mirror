KiCad/kicad-source-mirror

commit 994c1c971a24693eb412c39b948c0a3fcc060e76

Respect schematic settings for hidden pins
Fixes https://gitlab.com/kicad/code/kicad/-/issues/17941

(cherry picked from commit b8ccaae403cb8d4d5eb3967ad0452a2e6fde3dc8)

eeschema/tools/ee_selection_tool.cpp
@@ -49,6 +49,7 @@
 #include <sch_junction.h>
 #include <sch_marker.h>
 #include <sch_no_connect.h>
+#include <sch_render_settings.h>
 #include <sch_sheet.h>
 #include <sch_sheet_pin.h>
 #include <lib_shape.h>
@@ -1034,8 +1035,12 @@ OPT_TOOL_EVENT EE_SELECTION_TOOL::autostartEvent( TOOL_EVENT* aEvent, EE_GRID_HE
 
             wxCHECK( pin, OPT_TOOL_EVENT() );
 
-            if( !pin->IsVisible() && !m_frame->eeconfig()->m_Appearance.show_hidden_pins )
+            if( !pin->IsVisible()
+                && !( m_frame->eeconfig()->m_Appearance.show_hidden_pins
+                      || m_frame->GetRenderSettings()->m_ShowHiddenPins ) )
+            {
                 return OPT_TOOL_EVENT();
+            }
         }
 
         newEvt->SetMousePosition( pos );

commit 4ede6f61b21edc0a96a12e03a7c86ba04fa84dc9

Fix build errors introduced in 994c1c971a24693eb412c39b948c0a3fcc060e76

eeschema/tools/ee_selection_tool.cpp
@@ -49,7 +49,6 @@
 #include <sch_junction.h>
 #include <sch_marker.h>
 #include <sch_no_connect.h>
-#include <sch_render_settings.h>
 #include <sch_sheet.h>
 #include <sch_sheet_pin.h>
 #include <lib_shape.h>
@@ -1037,7 +1036,7 @@ OPT_TOOL_EVENT EE_SELECTION_TOOL::autostartEvent( TOOL_EVENT* aEvent, EE_GRID_HE
 
             if( !pin->IsVisible()
                 && !( m_frame->eeconfig()->m_Appearance.show_hidden_pins
-                      || m_frame->GetRenderSettings()->m_ShowHiddenPins ) )
+                      || m_frame->GetRenderSettings()->m_ShowHiddenLibPins ) )
             {
                 return OPT_TOOL_EVENT();
             }