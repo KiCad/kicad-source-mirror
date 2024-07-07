154112d4ccb96d5d6ff83c6e110755e67d2ba217
Mathieu Pilato <pilatomic@gmail.com>
2024-07-07 12:00:47 +0200

    Clear measurement grid while changing tab
    Prevent a tab change while editing measurement from impacting measurements of target tab
    
    (cherry picked from commit 98b4a580598c29f0f42f760556e2ac83b6fe14f8)

--- eeschema/sim/simulator_frame_ui.cpp
+++ eeschema/sim/simulator_frame_ui.cpp
@@ -2399,6 +2399,8 @@ void SIMULATOR_FRAME_UI::updateMeasurementsFromGrid()
 
 void SIMULATOR_FRAME_UI::onPlotChanging( wxAuiNotebookEvent& event )
 {
+    m_measurementsGrid->ClearRows();
+
     event.Skip();
 }
 
