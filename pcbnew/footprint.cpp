3a8a5bb3781691494107a9abd4414ddc9f27e845
Seth Hillbrand <seth@kipro-pcb.com>
2024-07-09 12:09:48 -0700

    Convert courtyards with error allowance
    We convert arcs/circles to polygons with an allowable error.  This means
    that the segments might end up being closer than we expect and we should
    allow for this deviation when checking courtyards
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/18347

--- pcbnew/footprint.cpp
+++ pcbnew/footprint.cpp
@@ -2896,7 +2896,9 @@ void FOOTPRINT::BuildCourtyardCaches( OUTLINE_ERROR_HANDLER* aErrorHandler )
         int width = 0;
 
         // Touching courtyards, or courtyards -at- the clearance distance are legal.
-        m_courtyard_cache_front.Inflate( -1, CORNER_STRATEGY::CHAMFER_ACUTE_CORNERS, maxError );
+        // Use maxError here because that is the allowed deviation when transforming arcs/circles to
+        // polygons.
+        m_courtyard_cache_front.Inflate( -maxError, CORNER_STRATEGY::CHAMFER_ACUTE_CORNERS, maxError );
 
         m_courtyard_cache_front.CacheTriangulation( false );
         auto max = std::max_element( front_width_histogram.begin(), front_width_histogram.end(),
@@ -2925,7 +2927,7 @@ void FOOTPRINT::BuildCourtyardCaches( OUTLINE_ERROR_HANDLER* aErrorHandler )
         int width = 0;
 
         // Touching courtyards, or courtyards -at- the clearance distance are legal.
-        m_courtyard_cache_back.Inflate( -1, CORNER_STRATEGY::CHAMFER_ACUTE_CORNERS, maxError );
+        m_courtyard_cache_back.Inflate( -maxError, CORNER_STRATEGY::CHAMFER_ACUTE_CORNERS, maxError );
 
         m_courtyard_cache_back.CacheTriangulation( false );
         auto max = std::max_element( back_width_histogram.begin(), back_width_histogram.end(),
