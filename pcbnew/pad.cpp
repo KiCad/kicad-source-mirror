f6c8dbe81e62075644f67e841836ddd9e0c9ff0f
Jeff Young <jeff@rokeby.ie>
2024-07-09 18:10:38 +0100

    Fix RecombinePad when pad is rotated.
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/18348
    
    (cherry picked from commit 556efd836bbf07b37fb88da07b1d9f6e37675e01)

--- pcbnew/pad.cpp
+++ pcbnew/pad.cpp
@@ -1820,8 +1820,8 @@ std::vector<PCB_SHAPE*> PAD::Recombine( bool aIsDryRun, int maxError )
             shape->SetFilled( true );
             shape->SetStroke( STROKE_PARAMS( 0, LINE_STYLE::SOLID ) );
             shape->SetPolyShape( existingOutline );
-            shape->Rotate( VECTOR2I( 0, 0 ), - GetOrientation() );
             shape->Move( - ShapePos() );
+            shape->Rotate( VECTOR2I( 0, 0 ), - GetOrientation() );
             AddPrimitive( shape );
         }
     }
