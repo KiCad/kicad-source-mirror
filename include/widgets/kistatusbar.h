869a3ee21f52b52b68c25be046729e809f03eb0b
Mark Roszko <mark.roszko@gmail.com>
2024-08-16 02:21:37 +0000

    Unbind events in KISTATUSBAR destructor

--- include/widgets/kistatusbar.h
+++ include/widgets/kistatusbar.h
@@ -46,7 +46,8 @@ class KICOMMON_API KISTATUSBAR : public wxStatusBar
 public:
     KISTATUSBAR( int aNumberFields, wxWindow* parent, wxWindowID id );
 
-public:
+    ~KISTATUSBAR();
+
     /**
      * Set the text in a field using wxELLIPSIZE_MIDDLE option to adjust the text size
      * to the field size (unfortunately, setting the wxStatusBar style to wxELLIPSIZE_MIDDLE
