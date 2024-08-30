4b035d5b5c3dcbf97418237d305b63d4578d68d1
Alex Shvartzkop <dudesuchamazing@gmail.com>
2024-08-30 06:01:41 +0300

    Altium PCB import: convert arcs with very small angle to segments.
    See mb1791_bdp.zip for example.

--- pcbnew/pcb_io/altium/altium_pcb.cpp
+++ pcbnew/pcb_io/altium/altium_pcb.cpp
@@ -2839,28 +2839,52 @@ void ALTIUM_PCB::ConvertArcs6ToBoardItemOnLayer( const AARC6& aElem, PCB_LAYER_I
 {
     if( IsCopperLayer( aLayer ) && aElem.net != ALTIUM_NET_UNCONNECTED )
     {
-        // TODO: This is not the actual board item. We use it for now to calculate the arc points. This could be improved!
-        PCB_SHAPE shape( nullptr, SHAPE_T::ARC );
-
         EDA_ANGLE includedAngle( aElem.endangle - aElem.startangle, DEGREES_T );
         EDA_ANGLE startAngle( aElem.endangle, DEGREES_T );
+
+        includedAngle.Normalize();
 
         VECTOR2I startOffset = VECTOR2I( KiROUND( startAngle.Cos() * aElem.radius ),
                                          -KiROUND( startAngle.Sin() * aElem.radius ) );
 
-        shape.SetCenter( aElem.center );
-        shape.SetStart( aElem.center + startOffset );
-        shape.SetArcAngleAndEnd( includedAngle.Normalize(), true );
+        if( includedAngle.AsDegrees() >= 0.1 )
+        {
+            // TODO: This is not the actual board item. We use it for now to calculate the arc points. This could be improved!
+            PCB_SHAPE shape( nullptr, SHAPE_T::ARC );
 
-        // Create actual arc
-        SHAPE_ARC shapeArc( shape.GetCenter(), shape.GetStart(), shape.GetArcAngle(), aElem.width );
-        PCB_ARC*  arc = new PCB_ARC( m_board, &shapeArc );
+            shape.SetCenter( aElem.center );
+            shape.SetStart( aElem.center + startOffset );
+            shape.SetArcAngleAndEnd( includedAngle, true );
 
-        arc->SetWidth( aElem.width );
-        arc->SetLayer( aLayer );
-        arc->SetNetCode( GetNetCode( aElem.net ) );
+            // Create actual arc
+            SHAPE_ARC shapeArc( shape.GetCenter(), shape.GetStart(), shape.GetArcAngle(),
+                                aElem.width );
+            PCB_ARC*  arc = new PCB_ARC( m_board, &shapeArc );
 
-        m_board->Add( arc, ADD_MODE::APPEND );
+            arc->SetWidth( aElem.width );
+            arc->SetLayer( aLayer );
+            arc->SetNetCode( GetNetCode( aElem.net ) );
+
+            m_board->Add( arc, ADD_MODE::APPEND );
+        }
+        else
+        {
+            EDA_ANGLE endAngle( aElem.startangle, DEGREES_T );
+
+            VECTOR2I endOffset = VECTOR2I( KiROUND( endAngle.Cos() * aElem.radius ),
+                                           -KiROUND( endAngle.Sin() * aElem.radius ) );
+
+            // Angle is too small, create a track
+            PCB_TRACK* track = new PCB_TRACK( m_board );
+            track->SetStart( aElem.center + startOffset );
+            track->SetEnd( aElem.center + endOffset );
+
+            track->SetWidth( aElem.width );
+            track->SetLayer( aLayer );
+            track->SetNetCode( GetNetCode( aElem.net ) );
+
+            m_board->Add( track, ADD_MODE::APPEND );
+        }
     }
     else
     {
