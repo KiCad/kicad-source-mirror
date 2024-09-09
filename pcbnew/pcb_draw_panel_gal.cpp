7a0803f384853705ef60029bc6c9346fc2ab3114
jean-pierre charras <jp.charras@wanadoo.fr>
2024-09-09 17:07:28 +0200

    PCB_DRAW_PANEL_GAL: Use TARGET_OVERLAY for LAYER_CONFLICTS_SHADOW.
    This is a better option than TARGET_NON_CACHED, and giving better look.

--- pcbnew/pcb_draw_panel_gal.cpp
+++ pcbnew/pcb_draw_panel_gal.cpp
@@ -677,7 +677,9 @@ void PCB_DRAW_PANEL_GAL::setDefaultLayerDeps()
     m_view->SetLayerTarget( LAYER_ANCHOR, KIGFX::TARGET_NONCACHED );
     m_view->SetLayerDisplayOnly( LAYER_ANCHOR );
 
-    m_view->SetLayerTarget( LAYER_CONFLICTS_SHADOW, KIGFX::TARGET_NONCACHED );
+    // Use TARGET_OVERLAY for LAYER_CONFLICTS_SHADOW, it is for items
+    // that may change while the view stays the same.
+    m_view->SetLayerTarget( LAYER_CONFLICTS_SHADOW, KIGFX::TARGET_OVERLAY );
 
     m_view->SetLayerDisplayOnly( LAYER_LOCKED_ITEM_SHADOW );
     m_view->SetLayerDisplayOnly( LAYER_CONFLICTS_SHADOW );
