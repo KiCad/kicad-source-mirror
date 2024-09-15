3774e31db8284e230d2910a48f5c92ba7a523ca8
John Beard <john.j.beard@gmail.com>
2024-09-15 21:08:04 +0100

    Pcbnew: convert to lines should take the width of polygons if it can
    Fixes: https://gitlab.com/kicad/code/kicad/-/issues/18713
    (cherry picked from commit 81c1cd9af171b782acf35107647b680b84016edb)

---
+++ pcbnew/tools/pcb_tool_utils.cpp
@@ -0,0 +1,50 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+
+ * Copyright (C) 2024 KiCad Developers, see AUTHORS.txt for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you may find one here:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * or you may search the http://www.gnu.org website for the version 2 license,
+ * or you may write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#include "tools/pcb_tool_utils.h"
+
+#include <board.h>
+#include <pcb_shape.h>
+#include <pcb_track.h>
+
+std::optional<int> GetBoardItemWidth( const BOARD_ITEM& aItem )
+{
+    switch( aItem.Type() )
+    {
+    case PCB_SHAPE_T:
+    {
+        const PCB_SHAPE& shape = static_cast<const PCB_SHAPE&>( aItem );
+        return shape.GetWidth();
+    }
+
+    case PCB_TRACE_T:
+    {
+        const PCB_TRACK& track = static_cast<const PCB_TRACK&>( aItem );
+        return track.GetWidth();
+    }
+
+    default: break;
+    }
+
+    return std::nullopt;
+}
