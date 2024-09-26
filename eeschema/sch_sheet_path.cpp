KiCad/kicad-source-mirror

commit 83de056de93954d150183b577187fe0ab587b4be

Fix missing schematic symbol instance data.
This fix adds missing symbol instance data that defaults the instance
data to no annotation which will will trigger a annotation request on
any operations that require a fully annotated schematic.

Fixes https://gitlab.com/kicad/code/kicad/-/issues/18486

(cherry picked from commit 0cf11103677583dba25997c9d5d7adfcbbfd15a7)

eeschema/sch_sheet_path.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2017 Jean-Pierre Charras, jp.charras at wanadoo.fr
  * Copyright (C) 2011 Wayne Stambaugh <stambaughw@gmail.com>
- * Copyright (C) 1992-2023 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 1992-2023, 2024 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,6 +23,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+#include <refdes_utils.h>
 #include <sch_screen.h>
 #include <sch_item.h>
 #include <sch_marker.h>
@@ -626,6 +627,33 @@ void SCH_SHEET_PATH::RemoveSymbolInstances( const SCH_SHEET_PATH& aPrefixSheetPa
         // Prefix the hierarchical path of the symbol instance to be removed.
         fullSheetPath = fullSheetPath + currentSheetPath;
         symbol->RemoveInstance( fullSheetPath );
+    }
+}
+
+
+void SCH_SHEET_PATH::CheckForMissingSymbolInstances( const wxString& aProjectName )
+{
+    wxCHECK( !aProjectName.IsEmpty() && LastScreen(), /* void */ );
+
+    for( SCH_ITEM* item : LastScreen()->Items().OfType( SCH_SYMBOL_T ) )
+    {
+        SCH_SYMBOL* symbol = static_cast<SCH_SYMBOL*>( item );
+
+        wxCHECK2( symbol, continue );
+
+        SCH_SYMBOL_INSTANCE symbolInstance;
+
+        if( !symbol->GetInstance( symbolInstance, Path() ) )
+        {
+            wxLogTrace( traceSchSheetPaths, "Adding missing symbol \"%s\" instance data for "
+                        "sheet path '%s'.",
+                        symbol->m_Uuid.AsString(), PathHumanReadable( false ) );
+
+            symbolInstance.m_Reference = UTIL::GetRefDesUnannotated( symbol->GetPrefix() );
+            symbolInstance.m_ProjectName = aProjectName;
+            symbolInstance.m_Path = Path();
+            symbol->AddHierarchicalReference( symbolInstance );
+        }
     }
 }
 
@@ -1335,6 +1363,13 @@ void SCH_SHEET_LIST::AddNewSheetInstances( const SCH_SHEET_PATH& aPrefixSheetPat
         newSheetPath.SetPageNumber( instance.m_PageNumber );
         usedPageNumbers.push_back( instance.m_PageNumber );
     }
+}
+
+
+void SCH_SHEET_LIST::CheckForMissingSymbolInstances( const wxString& aProjectName )
+{
+    for( SCH_SHEET_PATH& sheetPath : *this )
+        sheetPath.CheckForMissingSymbolInstances( aProjectName );
 }
 
 

eeschema/files-io.cpp
@@ -181,7 +181,8 @@ bool SCH_EDIT_FRAME::OpenProjectFiles( const std::vector<wxString>& aFileSet, in
         CreateScreens();
     }
 
-    SCH_IO_MGR::SCH_FILE_T schFileType = SCH_IO_MGR::GuessPluginTypeFromSchPath( fullFileName, KICTL_KICAD_ONLY );
+    SCH_IO_MGR::SCH_FILE_T schFileType = SCH_IO_MGR::GuessPluginTypeFromSchPath( fullFileName,
+                                                                                 KICTL_KICAD_ONLY );
 
     if( schFileType == SCH_IO_MGR::SCH_LEGACY )
     {
@@ -492,13 +493,14 @@ bool SCH_EDIT_FRAME::OpenProjectFiles( const std::vector<wxString>& aFileSet, in
 
             // Restore all of the loaded symbol and sheet instances from the root sheet.
             if( Schematic().RootScreen()->GetFileFormatVersionAtLoad() < 20221002 )
-                sheetList.UpdateSymbolInstanceData( Schematic().RootScreen()->GetSymbolInstances());
+                sheetList.UpdateSymbolInstanceData( Schematic().RootScreen()->GetSymbolInstances() );
 
             if( Schematic().RootScreen()->GetFileFormatVersionAtLoad() < 20221110 )
                 sheetList.UpdateSheetInstanceData( Schematic().RootScreen()->GetSheetInstances());
 
             if( Schematic().RootScreen()->GetFileFormatVersionAtLoad() < 20230221 )
-                for( SCH_SCREEN* screen = schematic.GetFirst(); screen; screen = schematic.GetNext() )
+                for( SCH_SCREEN* screen = schematic.GetFirst(); screen;
+                     screen = schematic.GetNext() )
                     screen->FixLegacyPowerSymbolMismatches();
 
             for( SCH_SCREEN* screen = schematic.GetFirst(); screen; screen = schematic.GetNext() )
@@ -511,8 +513,9 @@ bool SCH_EDIT_FRAME::OpenProjectFiles( const std::vector<wxString>& aFileSet, in
             }
         }
 
-        schematic.PruneOrphanedSymbolInstances( Prj().GetProjectName(), Schematic().GetSheets() );
-        schematic.PruneOrphanedSheetInstances( Prj().GetProjectName(), Schematic().GetSheets() );
+        schematic.PruneOrphanedSymbolInstances( Prj().GetProjectName(), sheetList );
+        schematic.PruneOrphanedSheetInstances( Prj().GetProjectName(), sheetList );
+        sheetList.CheckForMissingSymbolInstances( Prj().GetProjectName() );
 
         Schematic().ConnectionGraph()->Reset();
 

eeschema/sch_sheet_path.h
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2017 Jean-Pierre Charras, jp.charras at wanadoo.fr
  * Copyright (C) 2011 Wayne Stambaugh <stambaughw@gmail.com>
- * Copyright (C) 1992-2023 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 1992-2023, 2024 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -408,6 +408,8 @@ public:
 
     void RemoveSymbolInstances( const SCH_SHEET_PATH& aPrefixSheetPath );
 
+    void CheckForMissingSymbolInstances( const wxString& aProjectName );
+
     bool operator==( const SCH_SHEET_PATH& d1 ) const;
 
     bool operator!=( const SCH_SHEET_PATH& d1 ) const { return !( *this == d1 ) ; }
@@ -689,6 +691,8 @@ public:
     int GetLastVirtualPageNumber() const;
 
     void RemoveSymbolInstances( const SCH_SHEET_PATH& aPrefixSheetPath );
+
+    void CheckForMissingSymbolInstances( const wxString& aProjectName );
 
     bool HasPath( const KIID_PATH& aPath ) const;
 

commit 21dd41eeb2930362d00219627fa5ed4f11c46f1b

Fix annotation issue loading legacy schematics.
Commit 83de056de9 broke loading legacy schematics that are not shared.
Legacy schematics do not contain instance data "AR Path=" entries when
the schematic file was not shared.  The reference and unit information
was only stored in the symbol reference field and unit entries in the
symbol definition.

Fixes https://gitlab.com/kicad/code/kicad/-/issues/18779

eeschema/sch_sheet_path.cpp
@@ -686,7 +686,19 @@ void SCH_SHEET_PATH::CheckForMissingSymbolInstances( const wxString& aProjectNam
                         "sheet path '%s'.",
                         symbol->m_Uuid.AsString(), PathHumanReadable( false ) );
 
-            symbolInstance.m_Reference = UTIL::GetRefDesUnannotated( symbol->GetPrefix() );
+            // Legacy schematics that are not shared do not contain separate instance data.
+            // The symbol refrence and unit are saved in the reference field and unit entries.
+            if( ( LastScreen()->GetRefCount() <= 1 ) &&
+                ( LastScreen()->GetFileFormatVersionAtLoad() <= 20200310 ) )
+            {
+                symbolInstance.m_Reference =
+                        symbol->GetField( REFERENCE_FIELD )->GetShownText( this, true );
+            }
+            else
+            {
+                symbolInstance.m_Reference = UTIL::GetRefDesUnannotated( symbol->GetPrefix() );
+            }
+
             symbolInstance.m_ProjectName = aProjectName;
             symbolInstance.m_Path = Path();
             symbol->AddHierarchicalReference( symbolInstance );