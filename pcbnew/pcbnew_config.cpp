80079c16e83990c7a5f85bd3ef3ec5887cd74d31
jean-pierre charras <jp.charras@wanadoo.fr>
2024-10-10 17:35:19 +0200

    Pcbnew: ensure all local settings are saved on close the board editor.
    From master branch.

--- pcbnew/pcbnew_config.cpp
+++ pcbnew/pcbnew_config.cpp
@@ -4,7 +4,7 @@
  * Copyright (C) 2015 Jean-Pierre Charras, jp.charras at wanadoo.fr
  * Copyright (C) 2012 SoftPLC Corporation, Dick Hollenbeck <dick@softplc.com>
  * Copyright (C) 2012 Wayne Stambaugh <stambaughw@gmail.com>
- * Copyright (C) 1992-2021 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 1992-2024 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -142,6 +142,9 @@ void PCB_EDIT_FRAME::SaveProjectLocalSettings()
         return;
 
     PROJECT_FILE& project = Prj().GetProjectFile();
+
+    // save some local settings like appearance control settings
+    saveProjectSettings();
 
     // TODO: Can this be pulled out of BASE_SCREEN?
     project.m_BoardDrawingSheetFile = BASE_SCREEN::m_DrawingSheetFileName;
