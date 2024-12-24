01fb6c80edb2c971f27e48601eb4c44622e289ba
Mark Roszko <mark.roszko@gmail.com>
2024-12-24 15:24:26 -0500

    Fix exception due to decrementing end() on a list of size 0

--- eeschema/tools/sch_navigate_tool.cpp
+++ eeschema/tools/sch_navigate_tool.cpp
@@ -67,7 +67,7 @@ void SCH_NAVIGATE_TOOL::CleanHistory()
             entry = m_navHistory.erase( entry );
         }
     }
-    if( m_navHistory.size() == 1 )
+    if( m_navHistory.size() <= 1 )
         m_navIndex = m_navHistory.begin();
     else
         m_navIndex = --m_navHistory.end();
