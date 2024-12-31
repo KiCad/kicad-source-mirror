KiCad/kicad-source-mirror

commit 8aa0b3e16cd8f18db404e3909624c0ff09670a95

Convert output dialog to checkbox list.
Also cleans up the RC options dialog.

common/CMakeLists.txt
@@ -115,8 +115,6 @@ set( KICOMMON_SRCS
 
     dialogs/dialog_migrate_settings.cpp
     dialogs/dialog_migrate_settings_base.cpp
-    dialogs/dialog_rc_job.cpp
-    dialogs/dialog_rc_job_base.cpp
 
     widgets/bitmap_button.cpp
     widgets/html_window.cpp
@@ -360,6 +358,8 @@ set( COMMON_DLG_SRCS
     dialogs/dialog_page_settings.cpp
     dialogs/dialog_print_generic.cpp
     dialogs/dialog_print_generic_base.cpp
+    dialogs/dialog_rc_job.cpp
+    dialogs/dialog_rc_job_base.cpp
     dialogs/dialog_text_entry.cpp
     dialogs/dialog_unit_entry.cpp
     dialogs/dialog_unit_entry_base.cpp

common/dialogs/dialog_rc_job_base.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+// C++ code generated with wxFormBuilder (version 4.0.0-0-g0efcecf)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -16,79 +16,72 @@ DIALOG_RC_JOB_BASE::DIALOG_RC_JOB_BASE( wxWindow* parent, wxWindowID id, const w
 	wxBoxSizer* bSizerMain;
 	bSizerMain = new wxBoxSizer( wxVERTICAL );
 
-	m_staticText9 = new wxStaticText( this, wxID_ANY, _("Options"), wxDefaultPosition, wxDefaultSize, 0 );
-	m_staticText9->Wrap( -1 );
-	m_staticText9->SetFont( wxFont( wxNORMAL_FONT->GetPointSize(), wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false, wxEmptyString ) );
-
-	bSizerMain->Add( m_staticText9, 0, wxALL, 5 );
-
-	m_panel9 = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
 	wxFlexGridSizer* fgSizer1;
-	fgSizer1 = new wxFlexGridSizer( 0, 2, 0, 0 );
+	fgSizer1 = new wxFlexGridSizer( 0, 2, 5, 5 );
+	fgSizer1->AddGrowableCol( 1 );
 	fgSizer1->SetFlexibleDirection( wxBOTH );
 	fgSizer1->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
 
-	m_staticText18 = new wxStaticText( m_panel9, wxID_ANY, _("Format"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText18 = new wxStaticText( this, wxID_ANY, _("Format:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText18->Wrap( -1 );
-	fgSizer1->Add( m_staticText18, 0, wxALIGN_CENTER|wxALL, 5 );
+	fgSizer1->Add( m_staticText18, 0, 0, 5 );
 
 	wxArrayString m_choiceFormatChoices;
-	m_choiceFormat = new wxChoice( m_panel9, wxID_ANY, wxDefaultPosition, wxDefaultSize, m_choiceFormatChoices, 0 );
+	m_choiceFormat = new wxChoice( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, m_choiceFormatChoices, 0 );
 	m_choiceFormat->SetSelection( 0 );
-	fgSizer1->Add( m_choiceFormat, 0, wxALL, 5 );
+	fgSizer1->Add( m_choiceFormat, 0, wxALIGN_CENTER_VERTICAL, 5 );
 
-	m_staticText10 = new wxStaticText( m_panel9, wxID_ANY, _("Violations"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText10 = new wxStaticText( this, wxID_ANY, _("Include:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText10->Wrap( -1 );
-	fgSizer1->Add( m_staticText10, 0, wxALIGN_CENTER|wxALL, 5 );
+	fgSizer1->Add( m_staticText10, 0, wxALIGN_CENTER_VERTICAL, 5 );
 
-	m_panel6 = new wxPanel( m_panel9, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
 	wxBoxSizer* bSizer2;
 	bSizer2 = new wxBoxSizer( wxHORIZONTAL );
 
-	m_cbViolationErrors = new wxCheckBox( m_panel6, wxID_ANY, _("Errors"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer2->Add( m_cbViolationErrors, 0, wxALL, 5 );
-
-	m_cbViolationWarnings = new wxCheckBox( m_panel6, wxID_ANY, _("Warnings"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer2->Add( m_cbViolationWarnings, 0, wxALL, 5 );
+	m_cbViolationErrors = new wxCheckBox( this, wxID_ANY, _("Errors"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer2->Add( m_cbViolationErrors, 0, wxALIGN_CENTER_VERTICAL, 10 );
 
 
-	m_panel6->SetSizer( bSizer2 );
-	m_panel6->Layout();
-	bSizer2->Fit( m_panel6 );
-	fgSizer1->Add( m_panel6, 1, wxEXPAND | wxALL, 5 );
+	bSizer2->Add( 40, 0, 0, wxEXPAND, 5 );
 
-	m_textOutputPath = new wxStaticText( m_panel9, wxID_ANY, _("Output File"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_cbViolationWarnings = new wxCheckBox( this, wxID_ANY, _("Warnings"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer2->Add( m_cbViolationWarnings, 0, wxALIGN_CENTER_VERTICAL, 10 );
+
+
+	fgSizer1->Add( bSizer2, 1, wxEXPAND|wxTOP|wxBOTTOM, 4 );
+
+	m_textOutputPath = new wxStaticText( this, wxID_ANY, _("Output file:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_textOutputPath->Wrap( -1 );
-	fgSizer1->Add( m_textOutputPath, 0, wxALIGN_CENTER|wxALL, 5 );
+	fgSizer1->Add( m_textOutputPath, 0, wxALIGN_CENTER, 5 );
 
-	m_textCtrlOutputPath = new wxTextCtrl( m_panel9, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
+	m_textCtrlOutputPath = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	m_textCtrlOutputPath->SetMinSize( wxSize( 350,-1 ) );
 
-	fgSizer1->Add( m_textCtrlOutputPath, 0, wxALL, 5 );
+	fgSizer1->Add( m_textCtrlOutputPath, 1, wxALIGN_CENTER_VERTICAL, 5 );
 
 
-	fgSizer1->Add( 0, 0, 1, wxEXPAND, 5 );
+	bSizerMain->Add( fgSizer1, 1, wxALL|wxEXPAND, 10 );
 
-	m_cbHaltOutput = new wxCheckBox( m_panel9, wxID_ANY, _("Report job failure when violations present"), wxDefaultPosition, wxDefaultSize, 0 );
-	fgSizer1->Add( m_cbHaltOutput, 1, wxALL, 5 );
+	wxBoxSizer* bSizerBottom;
+	bSizerBottom = new wxBoxSizer( wxVERTICAL );
 
-
-	fgSizer1->Add( 0, 0, 1, wxEXPAND, 5 );
-
-	m_cbAllTrackViolations = new wxCheckBox( m_panel9, wxID_ANY, _("Report all errors for each track"), wxDefaultPosition, wxDefaultSize, 0 );
-	fgSizer1->Add( m_cbAllTrackViolations, 0, wxALL, 5 );
+	m_cbHaltOutput = new wxCheckBox( this, wxID_ANY, _("Report job failure when violations present"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizerBottom->Add( m_cbHaltOutput, 0, wxBOTTOM|wxLEFT, 5 );
 
 
-	fgSizer1->Add( 0, 0, 1, wxEXPAND, 5 );
+	bSizerBottom->Add( 0, 3, 0, wxEXPAND, 5 );
 
-	m_cbSchParity = new wxCheckBox( m_panel9, wxID_ANY, _("Test for parity between PCB and schematic"), wxDefaultPosition, wxDefaultSize, 0 );
-	fgSizer1->Add( m_cbSchParity, 0, wxALL, 5 );
+	m_cbAllTrackViolations = new wxCheckBox( this, wxID_ANY, _("Report all errors for each track"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizerBottom->Add( m_cbAllTrackViolations, 0, wxBOTTOM|wxLEFT, 5 );
 
 
-	m_panel9->SetSizer( fgSizer1 );
-	m_panel9->Layout();
-	fgSizer1->Fit( m_panel9 );
-	bSizerMain->Add( m_panel9, 1, wxEXPAND | wxALL, 5 );
+	bSizerBottom->Add( 0, 3, 0, wxEXPAND, 5 );
+
+	m_cbSchParity = new wxCheckBox( this, wxID_ANY, _("Test for parity between PCB and schematic"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizerBottom->Add( m_cbSchParity, 0, wxBOTTOM|wxLEFT, 5 );
+
+
+	bSizerMain->Add( bSizerBottom, 0, wxEXPAND|wxRIGHT|wxLEFT, 5 );
 
 	m_sdbSizer1 = new wxStdDialogButtonSizer();
 	m_sdbSizer1OK = new wxButton( this, wxID_OK );
@@ -97,7 +90,7 @@ DIALOG_RC_JOB_BASE::DIALOG_RC_JOB_BASE( wxWindow* parent, wxWindowID id, const w
 	m_sdbSizer1->AddButton( m_sdbSizer1Cancel );
 	m_sdbSizer1->Realize();
 
-	bSizerMain->Add( m_sdbSizer1, 0, wxEXPAND, 5 );
+	bSizerMain->Add( m_sdbSizer1, 0, wxEXPAND|wxALL, 5 );
 
 
 	this->SetSizer( bSizerMain );

common/dialogs/dialog_rc_job_base.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+// C++ code generated with wxFormBuilder (version 4.0.0-0-g0efcecf)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -20,29 +20,24 @@
 #include <wx/choice.h>
 #include <wx/checkbox.h>
 #include <wx/sizer.h>
-#include <wx/panel.h>
 #include <wx/textctrl.h>
 #include <wx/button.h>
 #include <wx/dialog.h>
 
-#include "kicommon.h"
-
 ///////////////////////////////////////////////////////////////////////////
+
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Class DIALOG_RC_JOB_BASE
 ///////////////////////////////////////////////////////////////////////////////
-class KICOMMON_API DIALOG_RC_JOB_BASE : public DIALOG_SHIM
+class DIALOG_RC_JOB_BASE : public DIALOG_SHIM
 {
 	private:
 
 	protected:
-		wxStaticText* m_staticText9;
-		wxPanel* m_panel9;
 		wxStaticText* m_staticText18;
 		wxChoice* m_choiceFormat;
 		wxStaticText* m_staticText10;
-		wxPanel* m_panel6;
 		wxCheckBox* m_cbViolationErrors;
 		wxCheckBox* m_cbViolationWarnings;
 		wxStaticText* m_textOutputPath;
@@ -60,7 +55,7 @@ class KICOMMON_API DIALOG_RC_JOB_BASE : public DIALOG_SHIM
 
 	public:
 
-		DIALOG_RC_JOB_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxDEFAULT_DIALOG_STYLE );
+		DIALOG_RC_JOB_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
 
 		~DIALOG_RC_JOB_BASE();
 

eeschema/CMakeLists.txt
@@ -212,7 +212,7 @@ set( EESCHEMA_SIM_SRCS
 
     sim/sim_library.cpp
     sim/sim_library_spice.cpp
-        sim/sim_library_ibis.cpp
+    sim/sim_library_ibis.cpp
     sim/sim_lib_mgr.cpp
 
     sim/sim_model_serializer.cpp
@@ -220,7 +220,7 @@ set( EESCHEMA_SIM_SRCS
     sim/sim_model_behavioral.cpp
     sim/sim_model_ideal.cpp
     sim/sim_model_l_mutual.cpp
-        sim/sim_model_ibis.cpp
+    sim/sim_model_ibis.cpp
     sim/sim_model_ngspice.cpp
     sim/sim_model_ngspice_data_bjt.cpp
     sim/sim_model_ngspice_data_bsim1.cpp

common/dialogs/dialog_rc_job.cpp
@@ -74,7 +74,7 @@ void DIALOG_RC_JOB::OnFormatChoice( wxCommandEvent& event )
         switch( selectedFormat )
         {
         case JOB_RC::OUTPUT_FORMAT::REPORT: fn.SetExt( FILEEXT::ReportFileExtension ); break;
-        case JOB_RC::OUTPUT_FORMAT::JSON: fn.SetExt( FILEEXT::JsonFileExtension ); break;
+        case JOB_RC::OUTPUT_FORMAT::JSON:   fn.SetExt( FILEEXT::JsonFileExtension );   break;
         }
 
         m_textCtrlOutputPath->SetValue( fn.GetFullPath() );

eeschema/dialogs/dialog_erc_job_config.cpp
@@ -1,7 +1,7 @@
 #include <dialogs/dialog_erc_job_config.h>
 
 DIALOG_ERC_JOB_CONFIG::DIALOG_ERC_JOB_CONFIG( wxWindow* parent, JOB_SCH_ERC* aJob ) :
-        DIALOG_RC_JOB( parent, aJob, _( "ERC Job Configuration" ) ),
+        DIALOG_RC_JOB( parent, aJob, _( "ERC Job Settings" ) ),
         m_ercJob( aJob )
 {
     m_cbAllTrackViolations->Hide();

commit 9fb8de959c79ddd79f97664da8b9f8cb7d6d6bfc

Fix windows builds broken by 8aa0b3e16cd8f18db404e3909624c0ff09670a95

common/CMakeLists.txt
@@ -115,6 +115,8 @@ set( KICOMMON_SRCS
 
     dialogs/dialog_migrate_settings.cpp
     dialogs/dialog_migrate_settings_base.cpp
+    dialogs/dialog_rc_job.cpp
+    dialogs/dialog_rc_job_base.cpp
 
     widgets/bitmap_button.cpp
     widgets/html_window.cpp
@@ -358,8 +360,6 @@ set( COMMON_DLG_SRCS
     dialogs/dialog_page_settings.cpp
     dialogs/dialog_print_generic.cpp
     dialogs/dialog_print_generic_base.cpp
-    dialogs/dialog_rc_job.cpp
-    dialogs/dialog_rc_job_base.cpp
     dialogs/dialog_text_entry.cpp
     dialogs/dialog_unit_entry.cpp
     dialogs/dialog_unit_entry_base.cpp

common/dialogs/dialog_rc_job_base.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version 4.0.0-0-g0efcecf)
+// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!

common/dialogs/dialog_rc_job_base.fbp
@@ -1,34 +1,36 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <wxFormBuilder_Project>
-  <FileVersion major="1" minor="17"/>
+  <FileVersion major="1" minor="18"/>
   <object class="Project" expanded="true">
-    <property name="class_decoration">; </property>
     <property name="code_generation">C++</property>
-    <property name="disconnect_events">1</property>
-    <property name="disconnect_mode">source_name</property>
-    <property name="disconnect_php_events">0</property>
-    <property name="disconnect_python_events">0</property>
+    <property name="cpp_class_decoration">KICOMMON_API; kicommon.h</property>
+    <property name="cpp_disconnect_events">1</property>
+    <property name="cpp_event_generation">connect</property>
+    <property name="cpp_help_provider">none</property>
+    <property name="cpp_namespace"></property>
+    <property name="cpp_precompiled_header"></property>
+    <property name="cpp_use_array_enum">0</property>
+    <property name="cpp_use_enum">0</property>
     <property name="embedded_files_path">res</property>
     <property name="encoding">UTF-8</property>
-    <property name="event_generation">connect</property>
     <property name="file">dialog_rc_job_base</property>
     <property name="first_id">1000</property>
-    <property name="help_provider">none</property>
-    <property name="image_path_wrapper_function_name"></property>
-    <property name="indent_with_spaces"></property>
     <property name="internationalize">1</property>
+    <property name="lua_skip_events">1</property>
+    <property name="lua_ui_table">UI</property>
     <property name="name">DIALOG_RC_JOB_BASE</property>
-    <property name="namespace"></property>
     <property name="path">.</property>
-    <property name="precompiled_header"></property>
+    <property name="php_disconnect_events">0</property>
+    <property name="php_disconnect_mode">source_name</property>
+    <property name="php_skip_events">1</property>
+    <property name="python_disconnect_events">0</property>
+    <property name="python_disconnect_mode">source_name</property>
+    <property name="python_image_path_wrapper_function_name"></property>
+    <property name="python_indent_with_spaces"></property>
+    <property name="python_skip_events">1</property>
     <property name="relative_path">1</property>
-    <property name="skip_lua_events">1</property>
-    <property name="skip_php_events">1</property>
-    <property name="skip_python_events">1</property>
-    <property name="ui_table">UI</property>
-    <property name="use_array_enum">0</property>
-    <property name="use_enum">0</property>
     <property name="use_microsoft_bom">0</property>
+    <property name="use_native_eol">0</property>
     <object class="Dialog" expanded="true">
       <property name="aui_managed">0</property>
       <property name="aui_manager_style">wxAUI_MGR_DEFAULT</property>

common/dialogs/dialog_rc_job_base.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version 4.0.0-0-g0efcecf)
+// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -24,13 +24,14 @@
 #include <wx/button.h>
 #include <wx/dialog.h>
 
-///////////////////////////////////////////////////////////////////////////
+#include "kicommon.h"
 
+///////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Class DIALOG_RC_JOB_BASE
 ///////////////////////////////////////////////////////////////////////////////
-class DIALOG_RC_JOB_BASE : public DIALOG_SHIM
+class KICOMMON_API DIALOG_RC_JOB_BASE : public DIALOG_SHIM
 {
 	private:
 