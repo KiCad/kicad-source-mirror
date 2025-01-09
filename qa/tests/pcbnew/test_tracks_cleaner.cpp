79419ef2f09ab7f8a402966c54f78ace7c37aca1
Seth Hillbrand <seth@kipro-pcb.com>
2025-01-09 14:28:34 -0800

    Update QA check for 8883
    This actually has a redundant track that was not caught before

--- qa/tests/pcbnew/test_tracks_cleaner.cpp
+++ qa/tests/pcbnew/test_tracks_cleaner.cpp
@@ -69,7 +69,7 @@ BOOST_FIXTURE_TEST_CASE( FailedToCleanRegressionTests, TRACK_CLEANER_TEST_FIXTUR
         { "issue2904",    false,     false,    false,     true,      false,    false,       9    },
         { "issue5093",    false,     false,    false,     false,     true,     false,     117    },
         { "issue7004",    false,     true,     false,     false,     false,    true,       25    },
-        { "issue8883",    true,      true,     true,      true,      false,    true,       80    },
+        { "issue8883",    true,      true,     true,      true,      false,    true,       81    },
         { "issue10916",   false,     false,    true,      false,     false,    false,       0    },
         { "issue19574",   false,     false,    true,      false,     false,    false,       2    }
     };
