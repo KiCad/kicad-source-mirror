6b31bbe9f3bda8f229d3f7d48af522395cef30ae
Alex Shvartzkop <dudesuchamazing@gmail.com>
2025-02-13 14:21:42 +0300

    Don't exit out of length tuning tool on first cancel.
    Otherwise you cannot use same settings on multiple tracks.

--- pcbnew/generators/pcb_tuning_pattern.cpp
+++ pcbnew/generators/pcb_tuning_pattern.cpp
@@ -2312,8 +2312,10 @@ int DRAWING_TOOL::PlaceTuningPattern( const TOOL_EVENT& aEvent )
                 delete m_tuningPattern;
                 m_tuningPattern = nullptr;
             }
-
-            break;
+            else
+            {
+                break;
+            }
         }
         else if( evt->IsMotion() )
         {
