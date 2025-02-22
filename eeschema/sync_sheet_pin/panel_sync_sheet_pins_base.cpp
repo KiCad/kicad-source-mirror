89a5fadcdcc62a9a2691e0130e20f9802c30b846
Graham Keeth <graham.keeth@gmail.com>
2025-02-22 16:38:50 -0500

    improve tooltips in sync sheet pins dialog

--- eeschema/sync_sheet_pin/panel_sync_sheet_pins_base.cpp
+++ eeschema/sync_sheet_pin/panel_sync_sheet_pins_base.cpp
@@ -96,12 +96,12 @@ PANEL_SYNC_SHEET_PINS_BASE::PANEL_SYNC_SHEET_PINS_BASE( wxWindow* parent, wxWind
 	bSizer14 = new wxBoxSizer( wxVERTICAL );
 
 	m_btnUseLabelAsTemplate = new wxBitmapButton( m_panel8, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW|0 );
-	m_btnUseLabelAsTemplate->SetToolTip( _("Change sheet pin to hierarchical label") );
+	m_btnUseLabelAsTemplate->SetToolTip( _("Associate selected sheet pin and hierarchical label using the label name") );
 
 	bSizer14->Add( m_btnUseLabelAsTemplate, 0, wxBOTTOM|wxEXPAND|wxLEFT|wxRIGHT, 5 );
 
 	m_btnUsePinAsTemplate = new wxBitmapButton( m_panel8, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW|0 );
-	m_btnUsePinAsTemplate->SetToolTip( _("Change hierarchical label to sheet pin") );
+	m_btnUsePinAsTemplate->SetToolTip( _("Associate selected sheet pin and hierarchical label using the pin name") );
 
 	bSizer14->Add( m_btnUsePinAsTemplate, 0, wxALL|wxEXPAND, 5 );
 
