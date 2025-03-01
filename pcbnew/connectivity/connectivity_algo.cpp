KiCad/kicad-source-mirror

commit 3d526edc8a0190a5a8f465946de741a5f1c3aac3

More thread safety for TRACKS_CLEANER
Fixes https://gitlab.com/kicad/code/kicad/-/issues/19884

pcbnew/connectivity/connectivity_algo.cpp
@@ -350,8 +350,10 @@ CN_CONNECTIVITY_ALGO::SearchClusters( CLUSTER_SEARCH_MODE aMode, const std::vect
     if( m_itemList.IsDirty() )
         searchConnections();
 
+    std::set<CN_ITEM*> visited;
+
     auto addToSearchList =
-            [&item_set, withinAnyNet, aSingleNet, &aTypes, rootItem ]( CN_ITEM *aItem )
+            [&item_set, withinAnyNet, aSingleNet, &aTypes, rootItem, &visited]( CN_ITEM *aItem )
             {
                 if( withinAnyNet && aItem->Net() <= 0 )
                     return;
@@ -376,8 +378,6 @@ CN_CONNECTIVITY_ALGO::SearchClusters( CLUSTER_SEARCH_MODE aMode, const std::vect
                 if( !found && aItem != rootItem )
                     return;
 
-                aItem->SetVisited( false );
-
                 item_set.insert( aItem );
             };
 
@@ -392,14 +392,14 @@ CN_CONNECTIVITY_ALGO::SearchClusters( CLUSTER_SEARCH_MODE aMode, const std::vect
         CN_ITEM*                    root;
         auto                        it = item_set.begin();
 
-        while( it != item_set.end() && (*it)->Visited() )
+        while( it != item_set.end() && visited.contains( *it ) )
             it = item_set.erase( item_set.begin() );
 
         if( it == item_set.end() )
             break;
 
         root = *it;
-        root->SetVisited( true );
+        visited.insert( root );
 
         Q.clear();
         Q.push_back( root );
@@ -416,9 +416,9 @@ CN_CONNECTIVITY_ALGO::SearchClusters( CLUSTER_SEARCH_MODE aMode, const std::vect
                 if( withinAnyNet && n->Net() != root->Net() )
                     continue;
 
-                if( !n->Visited() && n->Valid() )
+                if( !visited.contains( n ) && n->Valid() )
                 {
-                    n->SetVisited( true );
+                    visited.insert( n );
                     Q.push_back( n );
                 }
             }

pcbnew/dialogs/dialog_cleanup_tracks_and_vias.cpp
@@ -188,10 +188,29 @@ void DIALOG_CLEANUP_TRACKS_AND_VIAS::doCleanup( bool aDryRun )
     BOARD_COMMIT commit( m_parentFrame );
     TRACKS_CLEANER cleaner( m_brd, commit );
 
+    struct FILTER_STATE
+    {
+        bool selectedOnly;
+        int netCodeOnly;
+        wxString netClassOnly;
+        int layerOnly;
+    };
+
+    FILTER_STATE filter_state = {
+            .selectedOnly = m_selectedItemsFilter->GetValue(),
+            .netCodeOnly = m_netFilterOpt->GetValue() ? m_netFilter->GetSelectedNetcode() : -1,
+            .netClassOnly = m_netclassFilterOpt->GetValue()
+                                ? m_netclassFilter->GetStringSelection()
+                                : wxString(),
+            .layerOnly = m_layerFilterOpt->GetValue()
+                             ? m_layerFilter->GetLayerSelection()
+                             : UNDEFINED_LAYER
+            };
+
     cleaner.SetFilter(
-            [&]( BOARD_CONNECTED_ITEM* aItem ) -> bool
+            [&, filter_state]( BOARD_CONNECTED_ITEM* aItem ) -> bool
             {
-                if( m_selectedItemsFilter->GetValue() )
+                if( filter_state.selectedOnly )
                 {
                     if( !aItem->IsSelected() )
                     {
@@ -205,26 +224,23 @@ void DIALOG_CLEANUP_TRACKS_AND_VIAS::doCleanup( bool aDryRun )
                     }
                 }
 
-                if( m_netFilterOpt->GetValue() && m_netFilter->GetSelectedNetcode() >= 0 )
+                if( filter_state.netCodeOnly >= 0 )
                 {
-                    if( aItem->GetNetCode() != m_netFilter->GetSelectedNetcode() )
+                    if( aItem->GetNetCode() != filter_state.netCodeOnly )
                         return true;
                 }
 
-                if( m_netclassFilterOpt->GetValue()
-                        && !m_netclassFilter->GetStringSelection().IsEmpty() )
+                if( !filter_state.netClassOnly.IsEmpty() )
                 {
-                    wxString  filterNetclass = m_netclassFilter->GetStringSelection();
                     NETCLASS* netclass = aItem->GetEffectiveNetClass();
 
-                    if( !netclass->ContainsNetclassWithName( filterNetclass ) )
+                    if( !netclass->ContainsNetclassWithName( filter_state.netClassOnly ) )
                         return true;
                 }
 
-                if( m_layerFilterOpt->GetValue()
-                        && m_layerFilter->GetLayerSelection() != UNDEFINED_LAYER )
+                if( filter_state.layerOnly != UNDEFINED_LAYER )
                 {
-                    if( aItem->GetLayer() != m_layerFilter->GetLayerSelection() )
+                    if( aItem->GetLayer() != filter_state.layerOnly )
                         return true;
                 }
 

commit 51f56ab31ca6b10853d93b53bd86d247d7dda1cb

Fix inadvertent logic bug in SearchClusters
The safety changes in 3d526edc introduced a subtle
bug where aExcludeZones was not actually excluding
zones anymore because its behavior depended on the
item visited flag being set to true by a previous
call to the function.

Fixes https://gitlab.com/kicad/code/kicad/-/issues/20090

pcbnew/connectivity/connectivity_algo.cpp
@@ -390,6 +390,9 @@ CN_CONNECTIVITY_ALGO::SearchClusters( CLUSTER_SEARCH_MODE aMode, bool aExcludeZo
                 if( withinAnyNet && n->Net() != root->Net() )
                     continue;
 
+                if( aExcludeZones && n->Parent()->Type() == PCB_ZONE_T )
+                    continue;
+
                 if( !visited.contains( n ) && n->Valid() )
                 {
                     visited.insert( n );