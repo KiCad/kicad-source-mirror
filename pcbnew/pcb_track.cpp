11df4b32ebe64a53ccbb1069434ced905029794a
Jeff Young <jeff@rokeby.ie>
2025-03-07 13:06:19 +0000

    Fix merge mess-up.
    (cherry picked from commit 727fc9bd064e9349bd01501e210eb55e6b0207f9)

--- pcbnew/pcb_track.cpp
+++ pcbnew/pcb_track.cpp
@@ -1546,11 +1546,11 @@ double PCB_VIA::ViewGetLOD( int aLayer, const KIGFX::VIEW* aView ) const
         }
 
         // Netnames will be shown only if zoom is appropriate
-        return width == 0 ? LOD_HIDE : ( (double) pcbIUScale.mmToIU( 10 ) / width );
+        return lodScaleForThreshold( aView, width, pcbIUScale.mmToIU( 10 ) );
     }
 
     if( !IsCopperLayer( aLayer ) )
-        return (double) pcbIUScale.mmToIU( 0.6 ) / width;
+        return lodScaleForThreshold( aView, width, pcbIUScale.mmToIU( 0.6 ) );
 
     return LOD_SHOW;
 }
