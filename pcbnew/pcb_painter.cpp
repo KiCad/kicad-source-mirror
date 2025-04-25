6873a7531315df98254fe429c84414eeeff7c4d9
Jeff Young <jeff@rokeby.ie>
2025-04-25 09:36:17 +0100

    Solder masks for teardrops on tracks with solder masks.
    Also moves teardrops away from the zone UI: they
    are now always filled and their borders never
    drawn.
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/20744

--- pcbnew/pcb_painter.cpp
+++ pcbnew/pcb_painter.cpp
@@ -2738,6 +2738,9 @@ void PCB_PAINTER::draw( const ZONE* aZone, int aLayer )
     std::deque<VECTOR2D> corners;
     ZONE_DISPLAY_MODE    displayMode = m_pcbSettings.m_ZoneDisplayMode;
 
+    if( aZone->IsTeardropArea() )
+        displayMode = ZONE_DISPLAY_MODE::SHOW_FILLED;
+
     // Draw the outline
     if( !IsZoneFillLayer( aLayer ) )
     {
