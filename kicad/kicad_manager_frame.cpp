9d07784835591171defd5bb2e5855faba2e1f935
Marek Roszko <mark.roszko@gmail.com>
2025-05-24 16:10:52 -0400

    Show [read-only] in the title bar based on the project's state

--- kicad/kicad_manager_frame.cpp
+++ kicad/kicad_manager_frame.cpp
@@ -1052,7 +1052,7 @@ void KICAD_MANAGER_FRAME::ProjectChanged()
 
         title = fn.GetName();
 
-        if( !fn.IsDirWritable() )
+        if( Prj().IsReadOnly() )
             title += wxS( " " ) + _( "[Read Only]" );
     }
     else
