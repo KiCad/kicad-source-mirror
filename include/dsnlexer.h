adb535e6493eb5464604798d0cbc16b43900ee52
Jeff Young <jeff@rokeby.ie>
2025-06-05 13:13:08 +0100

    Initializer safety.

--- include/dsnlexer.h
+++ include/dsnlexer.h
@@ -80,7 +80,6 @@ enum DSN_SYNTAX_T
 class KICOMMON_API DSNLEXER
 {
 public:
-
     /**
      * Initialize a DSN lexer and prepares to read from aFile which is already open and has
      * \a aFilename.
@@ -539,6 +538,7 @@ protected:
         return parseDouble( GetTokenText( aToken ) );
     }
 
+protected:
     bool                iOwnReaders;            ///< On readerStack, should I delete them?
     const char*         start;
     const char*         next;
