d8003365d373779afdb9823cde3f2efe1c951751
Jeff Young <jeff@rokeby.ie>
2025-06-26 11:27:30 -0600

    Fix presumptive cast (KICAD-J24).
    (cherry picked from commit 3424f2bd4cb7a8d02e6bea2c9e8d63d2812c02c0)

--- pcbnew/tools/edit_tool.cpp
+++ pcbnew/tools/edit_tool.cpp
@@ -3193,7 +3193,7 @@ bool EDIT_TOOL::pickReferencePoint( const wxString& aTooltip, const wxString& aS
                                     const wxString& aCanceledMessage, VECTOR2I& aReferencePoint )
 {
     PCB_PICKER_TOOL*        picker = m_toolMgr->GetTool<PCB_PICKER_TOOL>();
-    PCB_EDIT_FRAME&         editFrame = *getEditFrame<PCB_EDIT_FRAME>();
+    PCB_BASE_EDIT_FRAME*    editFrame = getEditFrame<PCB_BASE_EDIT_FRAME>();
     std::optional<VECTOR2I> pickedPoint;
     bool                    done = false;
 
@@ -3202,17 +3202,19 @@ bool EDIT_TOOL::pickReferencePoint( const wxString& aTooltip, const wxString& aS
     /// This allow the option of snapping in the tool
     picker->SetSnapping( true );
 
-    const auto setPickerLayerSet = [&]()
-    {
-        MAGNETIC_SETTINGS& magSettings = *editFrame.GetMagneticItemsSettings();
-        LSET               layerFilter;
-        if( !magSettings.allLayers )
-            layerFilter = LSET( { editFrame.GetActiveLayer() } );
-        else
-            layerFilter = LSET::AllLayersMask();
+    const auto setPickerLayerSet =
+            [&]()
+            {
+                MAGNETIC_SETTINGS* magSettings = editFrame->GetMagneticItemsSettings();
+                LSET               layerFilter;
 
-        picker->SetLayerSet( layerFilter );
-    };
+                if( !magSettings->allLayers )
+                    layerFilter = LSET( { editFrame->GetActiveLayer() } );
+                else
+                    layerFilter = LSET::AllLayersMask();
+
+                picker->SetLayerSet( layerFilter );
+            };
 
     // Initial set
     setPickerLayerSet();
@@ -3281,7 +3283,9 @@ bool EDIT_TOOL::pickReferencePoint( const wxString& aTooltip, const wxString& aS
             evt->SetPassEvent();
         }
         else
+        {
             break;
+        }
     }
 
     // Ensure statusPopup is hidden after use and before deleting it:
@@ -3298,8 +3302,7 @@ bool EDIT_TOOL::pickReferencePoint( const wxString& aTooltip, const wxString& aS
 int EDIT_TOOL::copyToClipboard( const TOOL_EVENT& aEvent )
 {
     CLIPBOARD_IO io;
-    PCB_GRID_HELPER grid( m_toolMgr,
-                          getEditFrame<PCB_BASE_EDIT_FRAME>()->GetMagneticItemsSettings() );
+    PCB_GRID_HELPER grid( m_toolMgr, getEditFrame<PCB_BASE_EDIT_FRAME>()->GetMagneticItemsSettings() );
     TOOL_EVENT      selectReferencePoint( aEvent.Category(), aEvent.Action(),
                                           "pcbnew.InteractiveEdit.selectReferencePoint",
                                           TOOL_ACTION_SCOPE::AS_GLOBAL );
