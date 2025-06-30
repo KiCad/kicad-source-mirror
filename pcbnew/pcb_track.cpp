b35150abf749505b09ca386921ff2312f7e329d2
Jeff Young <jeff@rokeby.ie>
2025-06-29 19:40:59 -0600

    Remove overzealous assert (KICAD-MF5).
    (It is still called when using global Edit Tracks & Vias
    to set vias to netclass defaults.)

--- pcbnew/pcb_track.cpp
+++ pcbnew/pcb_track.cpp
@@ -378,8 +378,6 @@ void PCB_VIA::SetWidth( int aWidth )
 
 int PCB_VIA::GetWidth() const
 {
-    // This is present because of the parent class.  It should never be actually called on a via.
-    wxASSERT_MSG( false, "Warning: PCB_VIA::GetWidth called without a layer argument" );
     return m_padStack.Size( PADSTACK::ALL_LAYERS ).x;
 }
 
