ece360c83536614df0a834086866c574604bc6d4
Jeff Young <jeff@rokeby.ie>
2025-06-30 20:08:27 -0600

    Nullptr safety (KICAD-F5D).
    (cherry picked from commit b179a6cac8250af54c28f7897cb45462575c59c0)

--- eeschema/schematic.h
+++ eeschema/schematic.h
@@ -97,7 +97,10 @@ public:
     SCH_SHEET_LIST BuildUnorderedSheetList() const
     {
         SCH_SHEET_LIST sheets;
-        sheets.BuildSheetList( m_rootSheet, false );
+
+        if( m_rootSheet )
+            sheets.BuildSheetList( m_rootSheet, false );
+
         return sheets;
     }
 
