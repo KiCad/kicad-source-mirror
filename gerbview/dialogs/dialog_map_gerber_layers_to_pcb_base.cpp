21b294e2045e558ca4d5cf12d116c4d7e5cb99e6
Jeff Young <jeff@rokeby.ie>
2025-07-02 22:08:04 -0600

    Don't specify number of rows, wxWidgets will figure it out (KICAD-7HS)
    (and likes to assert if you don't let it.)

--- gerbview/dialogs/dialog_map_gerber_layers_to_pcb_base.cpp
+++ gerbview/dialogs/dialog_map_gerber_layers_to_pcb_base.cpp
@@ -38,7 +38,7 @@ DIALOG_MAP_GERBER_LAYERS_TO_PCB_BASE::DIALOG_MAP_GERBER_LAYERS_TO_PCB_BASE( wxWi
 
 	m_bSizerLayerList->Add( 0, 0, 0, wxRIGHT|wxLEFT, 10 );
 
-	m_flexLeftColumnBoxSizer = new wxFlexGridSizer( 16, 4, 0, 0 );
+	m_flexLeftColumnBoxSizer = new wxFlexGridSizer( 4, 0, 0 );
 	m_flexLeftColumnBoxSizer->AddGrowableCol( 0 );
 	m_flexLeftColumnBoxSizer->AddGrowableCol( 1 );
 	m_flexLeftColumnBoxSizer->AddGrowableCol( 2 );
@@ -52,7 +52,7 @@ DIALOG_MAP_GERBER_LAYERS_TO_PCB_BASE::DIALOG_MAP_GERBER_LAYERS_TO_PCB_BASE( wxWi
 	m_staticlineSep = new wxStaticLine( this, ID_M_STATICLINESEP, wxDefaultPosition, wxDefaultSize, wxLI_VERTICAL );
 	m_bSizerLayerList->Add( m_staticlineSep, 0, wxEXPAND | wxALL, 5 );
 
-	m_flexRightColumnBoxSizer = new wxFlexGridSizer( 16, 4, 0, 0 );
+	m_flexRightColumnBoxSizer = new wxFlexGridSizer( 4, 0, 0 );
 	m_flexRightColumnBoxSizer->AddGrowableCol( 0 );
 	m_flexRightColumnBoxSizer->AddGrowableCol( 1 );
 	m_flexRightColumnBoxSizer->AddGrowableCol( 2 );
