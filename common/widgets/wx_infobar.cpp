69e2c55291f063c2a001abb95f8133da2b1a2643
Mark Roszko <mark.roszko@gmail.com>
2025-07-05 13:45:44 -0400

    Fix crash because the infobar button doesn't have a window yet (because the infobar was not shown yet on Windows), fixes sentry KICAD-X9Z

--- common/widgets/wx_infobar.cpp
+++ common/widgets/wx_infobar.cpp
@@ -400,6 +400,9 @@ wxBitmapButton* WX_INFOBAR::GetCloseButton() const
 {
     wxSizer* sizer = GetSizer();
 
+    if( !sizer )
+        return nullptr;
+
     if( sizer->GetItemCount() == 0 )
         return nullptr;
 
@@ -408,7 +411,7 @@ wxBitmapButton* WX_INFOBAR::GetCloseButton() const
 
     wxSizerItem* item = sizer->GetItem( sizer->GetItemCount() - 1 );
 
-    if( item->GetWindow()->GetId() == ID_CLOSE_INFOBAR )
+    if( item && item->GetWindow() && item->GetWindow()->GetId() == ID_CLOSE_INFOBAR )
         return static_cast<wxBitmapButton*>( item->GetWindow() );
 
     return nullptr;
