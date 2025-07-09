7a91441a82a037aba9efe45ac948af5f8e97de0b
Jeff Young <jeff@rokeby.ie>
2025-07-09 16:13:34 +0100

    Formatting.

--- kicad/dialogs/panel_kicad_launcher.cpp
+++ kicad/dialogs/panel_kicad_launcher.cpp
@@ -36,8 +36,8 @@ PANEL_KICAD_LAUNCHER::PANEL_KICAD_LAUNCHER( wxWindow* aParent ) :
     m_frame = static_cast<KICAD_MANAGER_FRAME*>( aParent->GetParent() );
     CreateLaunchers();
 
-    Bind( wxEVT_SYS_COLOUR_CHANGED,
-          wxSysColourChangedEventHandler( PANEL_KICAD_LAUNCHER::onThemeChanged ), this );
+    Bind( wxEVT_SYS_COLOUR_CHANGED, wxSysColourChangedEventHandler( PANEL_KICAD_LAUNCHER::onThemeChanged ),
+          this );
 }
 
 
@@ -48,23 +48,22 @@ PANEL_KICAD_LAUNCHER::~PANEL_KICAD_LAUNCHER()
     for( wxWindow* window : m_scrolledWindow->GetChildren() )
     {
         if( dynamic_cast<BITMAP_BUTTON*>( window ) != nullptr )
-        {
             window->Unbind( wxEVT_BUTTON, &PANEL_KICAD_LAUNCHER::onLauncherButtonClick, this );
-        }
     }
 
-    Unbind( wxEVT_SYS_COLOUR_CHANGED,
-            wxSysColourChangedEventHandler( PANEL_KICAD_LAUNCHER::onThemeChanged ), this );
+    Unbind( wxEVT_SYS_COLOUR_CHANGED, wxSysColourChangedEventHandler( PANEL_KICAD_LAUNCHER::onThemeChanged ),
+            this );
 }
 
 
 void PANEL_KICAD_LAUNCHER::onLauncherButtonClick( wxCommandEvent& aEvent )
 {
-    // Defocus the button because leaving the large buttons
-    // focused after a click looks out of place in the launcher
+    // Defocus the button because leaving the large buttons focused after a click looks out of place in
+    // the launcher
     m_frame->SetFocus();
-    // Gives a slice of time to update the button state (mandatory on GTK,
-    // useful on MSW to avoid some cosmetic issues).
+
+    // Gives a slice of time to update the button state (mandatory on GTK, useful on MSW to avoid some
+    // cosmetic issues).
     wxSafeYield();
 
     BITMAP_BUTTON*     button = (BITMAP_BUTTON*) aEvent.GetEventObject();
@@ -98,48 +97,48 @@ void PANEL_KICAD_LAUNCHER::CreateLaunchers()
     wxFont helpFont = wxSystemSettings::GetFont( wxSYS_DEFAULT_GUI_FONT );
     helpFont.SetStyle( wxFONTSTYLE_ITALIC );
 
-    auto addLauncher = [&]( const TOOL_ACTION& aAction, BITMAPS aBitmaps, const wxString& aHelpText,
-                            bool enabled = true )
-    {
-        BITMAP_BUTTON* btn = new BITMAP_BUTTON( m_scrolledWindow, wxID_ANY );
-        btn->SetBitmap( KiBitmapBundle( aBitmaps ) );
-        btn->SetDisabledBitmap( KiDisabledBitmapBundle( aBitmaps ) );
-        btn->SetPadding( FromDIP( 4 ) );
-        btn->SetToolTip( aAction.GetTooltip() );
+    auto addLauncher =
+            [&]( const TOOL_ACTION& aAction, BITMAPS aBitmaps, const wxString& aHelpText, bool enabled = true )
+            {
+                BITMAP_BUTTON* btn = new BITMAP_BUTTON( m_scrolledWindow, wxID_ANY );
+                btn->SetBitmap( KiBitmapBundle( aBitmaps ) );
+                btn->SetDisabledBitmap( KiDisabledBitmapBundle( aBitmaps ) );
+                btn->SetPadding( FromDIP( 4 ) );
+                btn->SetToolTip( aAction.GetTooltip() );
 
-        m_scrolledWindow->SetFont( titleFont ); // Use font inheritance to avoid extra SetFont call.
-        wxStaticText* label =
-                new wxStaticText( m_scrolledWindow, wxID_ANY, aAction.GetFriendlyName() );
-        label->SetToolTip( aAction.GetTooltip() );
+                m_scrolledWindow->SetFont( titleFont ); // Use font inheritance to avoid extra SetFont call.
+                wxStaticText* label = new wxStaticText( m_scrolledWindow, wxID_ANY, aAction.GetFriendlyName() );
+                label->SetToolTip( aAction.GetTooltip() );
 
-        m_scrolledWindow->SetFont( helpFont ); // Use font inheritance to avoid extra SetFont call.
-        wxStaticText* help = new wxStaticText( m_scrolledWindow, wxID_ANY, aHelpText );
+                m_scrolledWindow->SetFont( helpFont ); // Use font inheritance to avoid extra SetFont call.
+                wxStaticText* help = new wxStaticText( m_scrolledWindow, wxID_ANY, aHelpText );
 
-        btn->Bind( wxEVT_BUTTON, &PANEL_KICAD_LAUNCHER::onLauncherButtonClick, this );
-        btn->SetClientData( (void*) &aAction );
+                btn->Bind( wxEVT_BUTTON, &PANEL_KICAD_LAUNCHER::onLauncherButtonClick, this );
+                btn->SetClientData( (void*) &aAction );
 
-        // The bug fix below makes this handler active for the entire window width.  Without
-        // any visual feedback that's a bit odd.  Disabling for now.
-        // label->Bind( wxEVT_LEFT_UP, handler );
+                // The bug fix below makes this handler active for the entire window width.  Without any visual
+                // feedback that's a bit odd.  Disabling for now.
+                // label->Bind( wxEVT_LEFT_UP, handler );
 
-        m_toolsSizer->Add( btn, 1, wxALIGN_CENTER_VERTICAL );
+                m_toolsSizer->Add( btn, 1, wxALIGN_CENTER_VERTICAL );
 
-        wxBoxSizer* textSizer = new wxBoxSizer( wxVERTICAL );
+                wxBoxSizer* textSizer = new wxBoxSizer( wxVERTICAL );
 
-        textSizer->Add( label );
-        textSizer->Add( help );
+                textSizer->Add( label );
+                textSizer->Add( help );
 
-        m_toolsSizer->Add( textSizer, 1, wxEXPAND | wxALIGN_CENTER_VERTICAL );
+                m_toolsSizer->Add( textSizer, 1, wxEXPAND | wxALIGN_CENTER_VERTICAL );
 
-        btn->Enable( enabled );
-        if( !enabled )
-        {
-            help->Disable();
-            label->Disable();
-        }
+                btn->Enable( enabled );
 
-        return btn;
-    };
+                if( !enabled )
+                {
+                    help->Disable();
+                    label->Disable();
+                }
+
+                return btn;
+            };
 
     addLauncher( KICAD_MANAGER_ACTIONS::editSchematic, BITMAPS::icon_eeschema,
                  _( "Edit the project schematic" ) );
@@ -163,14 +162,12 @@ void PANEL_KICAD_LAUNCHER::CreateLaunchers()
                  _( "Show tools for calculating resistance, current capacity, etc." ) );
 
     addLauncher( KICAD_MANAGER_ACTIONS::editDrawingSheet, BITMAPS::icon_pagelayout_editor,
-                 _( "Edit drawing sheet borders and title blocks for use in schematics and PCB "
-                    "designs" ) );
+                 _( "Edit drawing sheet borders and title blocks for use in schematics and PCB designs" ) );
 
-    BITMAP_BUTTON* bb =
-            addLauncher( KICAD_MANAGER_ACTIONS::showPluginManager, BITMAPS::icon_pcm,
-                         _( "Manage downloadable packages from KiCad and 3rd party repositories" ),
-                         ( KIPLATFORM::POLICY::GetPolicyBool( POLICY_KEY_PCM )
-                           != KIPLATFORM::POLICY::PBOOL::DISABLED ) );
+    BITMAP_BUTTON* bb = addLauncher( KICAD_MANAGER_ACTIONS::showPluginManager, BITMAPS::icon_pcm,
+                                     _( "Manage downloadable packages from KiCad and 3rd party repositories" ),
+                                     KIPLATFORM::POLICY::GetPolicyBool( POLICY_KEY_PCM )
+                                            != KIPLATFORM::POLICY::PBOOL::DISABLED );
 
     m_frame->SetPcmButton( bb );
 
