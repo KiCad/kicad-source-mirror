db801c8d7e002975cf4b4b5d7fc42edaadd62068
Jeff Young <jeff@rokeby.ie>
2025-08-06 17:22:04 +0100

    Remove stale comment.
    (cherry picked from commit 3f1f9e8e72ced37e9241ec9791403194307b5611)

--- include/board_item.h
+++ include/board_item.h
@@ -284,9 +284,6 @@ public:
     /**
      * Set the layer this item is on.
      *
-     * This method is virtual because some items (in fact: class DIMENSION)
-     * have a slightly different initialization.
-     *
      * @param aLayer The layer number.
      */
     virtual void SetLayer( PCB_LAYER_ID aLayer )
