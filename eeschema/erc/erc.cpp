88c09517f23d5736c85845e177361b13342bea80
JamesJCode <13408010-JamesJCode@users.noreply.gitlab.com>
2025-08-08 14:37:06 +0100

    Ignore empty net class fields in net class ERC

--- eeschema/erc/erc.cpp
+++ eeschema/erc/erc.cpp
@@ -715,7 +715,7 @@ int ERC_TESTER::TestMissingNetclasses()
                             {
                                 wxString netclass = field->GetShownText( &sheet, false );
 
-                                if( !netclass.IsSameAs( defaultNetclass )
+                                if( !netclass.empty() && !netclass.IsSameAs( defaultNetclass )
                                     && !settings->HasNetclass( netclass ) )
                                 {
                                     logError( sheet, item, netclass );
