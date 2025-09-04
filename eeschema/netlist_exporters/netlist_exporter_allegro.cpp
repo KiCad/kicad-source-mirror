9de2548064685a0fb1549f89dc901bd47e8f7633
Mark Roszko <mark.roszko@gmail.com>
2025-09-04 07:08:56 -0400

    Fix typo in allegro output for nets

--- eeschema/netlist_exporters/netlist_exporter_allegro.cpp
+++ eeschema/netlist_exporters/netlist_exporter_allegro.cpp
@@ -711,7 +711,7 @@ void NETLIST_EXPORTER_ALLEGRO::toAllegroPackageProperties()
 
 void NETLIST_EXPORTER_ALLEGRO::toAllegroNets()
 {
-    fmt::print( m_f, "$NET\n" );
+    fmt::print( m_f, "$NETS\n" );
 
     while( !m_netNameNodes.empty() )
     {
