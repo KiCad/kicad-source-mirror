46c8a6ee956efcec2b87728440bfc7b29a44a503
jean-pierre charras <jp.charras@wanadoo.fr>
2025-09-07 18:26:48 +0200

    DIALOG_LIB_FIELDS_TABLE & DIALOG_SYMBOL_FIELDS_TABLE: avoid empty m_viewControlsGrid.
    The fix is more a workaround: if all columns of m_viewControlsGrid are removed by
    the right click menu and the grid is empty, there are no way to re-add a column.
    When happens, this workaround re-add a column when reopening the dialog to avoid
    a empty grid.

--- eeschema/dialogs/dialog_lib_fields_table.cpp
+++ eeschema/dialogs/dialog_lib_fields_table.cpp
@@ -318,6 +318,15 @@ DIALOG_LIB_FIELDS_TABLE::DIALOG_LIB_FIELDS_TABLE( SYMBOL_EDIT_FRAME* parent, DIA
     SYMBOL_EDITOR_SETTINGS::PANEL_LIB_FIELDS_TABLE& cfg = m_parent->libeditconfig()->m_LibFieldEditor;
 
     m_viewControlsGrid->ShowHideColumns( cfg.view_controls_visible_columns );
+    // Ensure at least one column is visible otherwise we cannot add columns
+    // because there is no area to right click to get the menu managing the show/hide columns
+    wxString visible_column = m_viewControlsGrid->GetShownColumnsAsString();;
+
+    if( visible_column.IsEmpty() )
+    {
+        visible_column = wxT( "0" );
+        m_viewControlsGrid->ShowHideColumns( visible_column );
+    }
 
     CallAfter( [this, cfg]()
                {
