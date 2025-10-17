afd240523029041c0c66c27c6b22c3bb39505bed
Alex Shvartzkop <dudesuchamazing@gmail.com>
2025-10-17 21:46:17 +0300

    Support VRML(WRL) mesh data for STL/PLY/U3D/PDF export.

--- pcbnew/exporters/step/step_pcb_model.cpp
+++ pcbnew/exporters/step/step_pcb_model.cpp
@@ -2935,9 +2935,11 @@ bool STEP_PCB_MODEL::getModelLabel( const std::string& aFileNameUTF8, const VECT
                 }
             }
 
-            // VRML models only work when exporting to glTF
-            // Also OCCT < 7.9.0 fail to load most VRML 2.0 models because of Switch nodes
-            if( m_outFmt == OUTPUT_FORMAT::FMT_OUT_GLTF )
+            // VRML models only work when exporting to mesh formats
+            // Also OCCT < 7.9.0 fails to load most VRML 2.0 models because of Switch nodes
+            if( m_outFmt == OUTPUT_FORMAT::FMT_OUT_GLTF || m_outFmt == OUTPUT_FORMAT::FMT_OUT_STL
+                || m_outFmt == OUTPUT_FORMAT::FMT_OUT_PLY || m_outFmt == OUTPUT_FORMAT::FMT_OUT_U3D
+                || m_outFmt == OUTPUT_FORMAT::FMT_OUT_PDF )
             {
                 if( readVRML( doc, aFileNameUTF8.c_str() ) )
                 {
