c1a1bbc96b552fc3655a3cc1abcc922c35603521
Seth Hillbrand <seth@kipro-pcb.com>
2025-10-21 12:05:06 -0700

    Strongly prefer snap points over snap lines
    When moving toward an intersection of snap lines that is also a snap
    point, when the grid is off, the snap will always be closer to one snap
    line or the other before the snap point.  This is problematic for
    snapping to a center point, e.g.
    
    Instead, if you are within the snap range of a snap point, that should
    be your preference.  Zooming in will allow snapping to the snap physical
    point on a line and avoiding the snap point snapping
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/22045

--- pcbnew/tools/pcb_grid_helper.cpp
+++ pcbnew/tools/pcb_grid_helper.cpp
@@ -677,15 +677,22 @@ VECTOR2I PCB_GRID_HELPER::BestSnapAnchor( const VECTOR2I& aOrigin, const LSET& a
 
                 if( nearest && nearest->Distance( aOrigin ) <= snapRange )
                 {
-                    int snapLineDistance = ( *snapLineSnap - aOrigin ).EuclideanNorm();
-                    int anchorDistance = nearest->Distance( aOrigin );
-
-                    // Prefer the anchor if it's closer or within a small margin
-                    if( anchorDistance <= snapLineDistance * ADVANCED_CFG::GetCfg().m_SnapToAnchorMargin )
+                    preferAnchor = true;
+                    wxLogTrace( traceSnap, "    Preferring anchor over snap line (anchorDist=%f, snapRange=%d)",
+                                nearest->Distance( aOrigin ), snapRange );
+                }
+                else
+                {
+                    if( nearest )
                     {
-                        preferAnchor = true;
-                        wxLogTrace( traceSnap, "    Preferring anchor over snap line (anchorDist=%d, snapLineDist=%d)",
-                                    anchorDistance, snapLineDistance );
+                        wxLogTrace( traceSnap, "    Nearest anchor at (%d, %d), distance=%f is out of range (snapRange=%d)",
+                                    nearest->pos.x, nearest->pos.y,
+                                    nearest->Distance( aOrigin ),
+                                    snapRange );
+                    }
+                    else
+                    {
+                        wxLogTrace( traceSnap, "    No nearest anchor to consider" );
                     }
                 }
 
