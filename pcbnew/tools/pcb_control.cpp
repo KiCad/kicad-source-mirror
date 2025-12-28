0b88e2a9ab27f73c9dcc98b1bc1b0dff3da755f6
Seth Hillbrand <seth@kipro-pcb.com>
2025-12-27 19:56:28 -0800

    Do not clear netclasses when appending
    Appending should not replace all content, it should add to the existing
    content, so don't remove data before loading the board to append
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/22089
    
    (cherry picked from commit acab87cadc8aa342773d62ca1df1f43cf07eb766)

--- pcbnew/tools/pcb_control.cpp
+++ pcbnew/tools/pcb_control.cpp
@@ -1534,7 +1534,6 @@ int PCB_CONTROL::AppendBoard( PCB_IO& pi, wxString& fileName )
 
         WX_PROGRESS_REPORTER progressReporter( editFrame, _( "Loading PCB" ), 1 );
 
-        editFrame->GetDesignSettings().m_NetSettings->ClearNetclasses();
         pi.SetProgressReporter( &progressReporter );
         pi.LoadBoard( fileName, brd, &props, nullptr );
     }
