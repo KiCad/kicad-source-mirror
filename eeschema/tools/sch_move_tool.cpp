KiCad/kicad-source-mirror

commit 5eee8f1c5fac01d95d3b309028a50aa8f7cc7ae1

sch_view: update to handle items with children

eeschema/tools/sch_move_tool.cpp
@@ -645,6 +645,13 @@ bool SCH_MOVE_TOOL::doMoveSelection( const TOOL_EVENT& aEvent, SCH_COMMIT* aComm
                         shape->UpdateHatching();
                     }
 
+                    static_cast<SCH_ITEM*>( item )->RunOnChildren(
+                            [&]( SCH_ITEM* schItem )
+                            {
+                                item->SetFlags( IS_MOVING );
+                            },
+                            RECURSE_MODE::RECURSE );
+
                     if( SCH_ITEM* schItem = dynamic_cast<SCH_ITEM*>( item ) )
                         schItem->SetStoredPos( schItem->GetPosition() );
                 }

eeschema/sch_view.cpp
@@ -57,6 +57,40 @@ SCH_VIEW::~SCH_VIEW()
 }
 
 
+void SCH_VIEW::Update( const KIGFX::VIEW_ITEM* aItem, int aUpdateFlags ) const
+{
+    if( aItem->IsSCH_ITEM() )
+    {
+        // The equivalent function in PCB_VIEW doesn't need to do this, but
+        // that's only because RunOnChildren's constness is misleading in the PCB editor;
+        // it doesn't modify target item, but our uses of it modify its children.
+        SCH_ITEM* schItem = const_cast<SCH_ITEM*>( static_cast<const SCH_ITEM*>( aItem ) );
+
+        if( schItem->Type() == SCH_TABLECELL_T )
+        {
+            VIEW::Update( schItem->GetParent() );
+        }
+        else
+        {
+            schItem->RunOnChildren(
+                    [this, aUpdateFlags]( SCH_ITEM* child )
+                    {
+                        VIEW::Update( child, aUpdateFlags );
+                    },
+                    RECURSE_MODE::NO_RECURSE );
+        }
+    }
+
+    VIEW::Update( aItem, aUpdateFlags );
+}
+
+
+void SCH_VIEW::Update( const KIGFX::VIEW_ITEM* aItem ) const
+{
+    SCH_VIEW::Update( aItem, KIGFX::ALL );
+}
+
+
 void SCH_VIEW::Cleanup()
 {
     Clear();

eeschema/sch_view.h
@@ -100,6 +100,9 @@ public:
     SCH_VIEW( SCH_BASE_FRAME* aFrame );
     ~SCH_VIEW();
 
+    void Update( const KIGFX::VIEW_ITEM* aItem, int aUpdateFlags ) const override;
+    void Update( const KIGFX::VIEW_ITEM* aItem ) const override;
+
     void Cleanup();
 
     void DisplaySheet( const SCH_SCREEN* aScreen );

eeschema/tools/sch_selection_tool.cpp
@@ -2422,6 +2422,15 @@ void SCH_SELECTION_TOOL::filterCollectorForHierarchy( SCH_COLLECTOR& aCollector,
 }
 
 
+int SCH_SELECTION_TOOL::updateSelection( const TOOL_EVENT& aEvent )
+{
+    getView()->Update( &m_selection );
+    getView()->Update( &m_enteredGroupOverlay );
+
+    return 0;
+}
+
+
 void SCH_SELECTION_TOOL::InitializeSelectionState( SCH_TABLE* aTable )
 {
     for( SCH_TABLECELL* cell : aTable->GetCells() )
@@ -3273,6 +3282,9 @@ void SCH_SELECTION_TOOL::setTransitions()
 
     Go( &SCH_SELECTION_TOOL::SelectNext,          SCH_ACTIONS::nextNetItem.MakeEvent() );
     Go( &SCH_SELECTION_TOOL::SelectPrevious,      SCH_ACTIONS::previousNetItem.MakeEvent() );
+
+    Go( &SCH_SELECTION_TOOL::updateSelection,     EVENTS::SelectedItemsModified );
+    Go( &SCH_SELECTION_TOOL::updateSelection,     EVENTS::SelectedItemsMoved );
 
     Go( &SCH_SELECTION_TOOL::disambiguateCursor,  EVENTS::DisambiguatePoint );
 }

eeschema/tools/sch_selection_tool.h
@@ -334,6 +334,11 @@ private:
     bool selectionContains( const VECTOR2I& aPoint ) const;
 
     /**
+     * Event handler to update the selection VIEW_ITEM.
+     */
+    int updateSelection( const TOOL_EVENT& aEvent );
+
+    /**
      * Return true if the given item passes the stateful selection filter
      */
     bool itemPassesFilter( EDA_ITEM* aItem );

commit 9b925197b0355eab97f77e19655631e2eef20d8b

Eeschema: Fix crash when moving symbol fields repeatedly
The RunOnChildren lambda was using the outer loop variable instead of
the lambda parameter. Children never had IS_MOVING set, only the parent
got it set repeatedly. After several move operations this inconsistent
state caused crashes.

Bug introduced in 5eee8f1c5f where the lambda parameter was named
'schItem' but the body used 'item'.

Fixes https://gitlab.com/kicad/code/kicad/-/issues/22422

eeschema/tools/sch_move_tool.cpp
@@ -1264,9 +1264,9 @@ void SCH_MOVE_TOOL::initializeMoveOperation( const TOOL_EVENT& aEvent, SCH_SELEC
         }
 
         schItem->RunOnChildren(
-                [&]( SCH_ITEM* unused )
+                [&]( SCH_ITEM* aChild )
                 {
-                    item->SetFlags( IS_MOVING );
+                    aChild->SetFlags( IS_MOVING );
                 },
                 RECURSE_MODE::RECURSE );
 