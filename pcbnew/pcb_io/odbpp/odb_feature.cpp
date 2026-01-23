fa75005bef47f9504e424ca0e75042147b73b3c0
Seth Hillbrand <seth@kipro-pcb.com>
2026-01-22 20:50:25 -0800

    ODB++: Add support for exporting PCB_TABLE elements
    This adds a PCB_TABLE_T case that exports table cell text using the
    existing text export logic using PCB_TEXTBOX objects and
    exports table borders using DrawBorders() to iterate over border
    segments.
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/19993

--- pcbnew/pcb_io/odbpp/odb_feature.cpp
+++ pcbnew/pcb_io/odbpp/odb_feature.cpp
@@ -29,6 +29,7 @@
 #include "odb_defines.h"
 #include "pcb_track.h"
 #include "pcb_textbox.h"
+#include "pcb_table.h"
 #include "zone.h"
 #include "board.h"
 #include "board_design_settings.h"
@@ -776,6 +777,25 @@ void FEATURES_MANAGER::InitFeatureList( PCB_LAYER_ID aLayer, std::vector<BOARD_I
                 add_shape( static_cast<PCB_TEXTBOX*>( item ) );
 
             break;
+
+        case PCB_TABLE_T:
+        {
+            PCB_TABLE* table = static_cast<PCB_TABLE*>( item );
+
+            for( PCB_TABLECELL* cell : table->GetCells() )
+                add_text( cell );
+
+            table->DrawBorders(
+                    [&]( const VECTOR2I& aPt1, const VECTOR2I& aPt2, const STROKE_PARAMS& aStroke )
+                    {
+                        int lineWidth = aStroke.GetWidth();
+
+                        if( lineWidth > 0 )
+                            AddFeatureLine( aPt1, aPt2, lineWidth );
+                    } );
+
+            break;
+        }
 
         case PCB_DIMENSION_T:
         case PCB_TARGET_T:
