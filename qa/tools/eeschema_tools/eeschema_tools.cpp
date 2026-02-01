8006c5517202a55c752c07f311df06f22c6185c2
John Beard <john.j.beard@gmail.com>
2026-02-01 18:27:13 +0000

    QA: Implement a skeleton eeschema_tools utility
    This is an analogue of pcbnew_tools for schematic stuff.
    
    This gives a central place to hang executable utils, such as
    micro-benchmarks, fuzzer targets, etc.

---
+++ qa/tools/eeschema_tools/eeschema_tools.cpp
@@ -0,0 +1,33 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright The KiCad Developers, see AUTHORS.TXT for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <qa_utils/utility_program.h>
+
+#include <wx/init.h>
+
+int main( int argc, char** argv )
+{
+    wxInitialize( argc, argv );
+    KI_TEST::COMBINED_UTILITY c_util;
+
+    int ret = c_util.HandleCommandLine( argc, argv );
+
+    wxUninitialize();
+    return ret;
+}
