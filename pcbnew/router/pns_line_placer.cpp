8759d05a1240753eadf1513d4a1ebda914780487
Seth Hillbrand <seth@kipro-pcb.com>
2026-02-03 08:26:59 -0800

    Fix overlapping track segments
    When routing with "Fix All Segments on Click" disabled, collinear segments
    committed across multiple partial fixes were never merged, resulting in
    redundant overlapping track segments.
    
    The root cause was that simplifyNewLine() was only called when the route
    was complete (realEnd=true), not during partial fixes. This function
    handles merging of collinear segments and removal of contained segments.
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/19538
    
    (cherry picked from commit 30838e1ec8d18a22672f5b9a8258b5877324c3a5)

--- pcbnew/router/pns_line_placer.cpp
+++ pcbnew/router/pns_line_placer.cpp
@@ -1699,7 +1699,7 @@ bool LINE_PLACER::FixRoute( const VECTOR2I& aP, ITEM* aEndItem, bool aForceFinis
     }
 
 
-    if( realEnd && lastItem )
+    if( lastItem )
         simplifyNewLine( m_lastNode, lastItem );
 
     if( !realEnd )
