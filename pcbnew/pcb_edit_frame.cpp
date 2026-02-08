0970fb8b71bbf9e344913326c82f2585a533f0dd
Jeff Young <jeff@rokeby.ie>
2026-02-08 20:29:36 +0000

    A little bit of safety based on 94757f6239542e2d5017367a7af0b8bfc9bcc3ff.
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/22468

--- pcbnew/pcb_edit_frame.cpp
+++ pcbnew/pcb_edit_frame.cpp
@@ -323,7 +323,7 @@ PCB_EDIT_FRAME::PCB_EDIT_FRAME( KIWAY* aKiway, wxWindow* aParent ) :
     m_auimgr.AddPane( m_appearancePanel, EDA_PANE().Name( wxS( "LayersManager" ) )
                       .Right().Layer( 4 )
                       .Caption( _( "Appearance" ) ).PaneBorder( false )
-                      .MinSize( m_appearancePanel->GetMinSize().x, -1 )
+                      .MinSize( m_appearancePanel->GetMinSize().x, FromDIP( 60 ) )
 #ifdef __WXMAC__
                       // Best size for this pane is calculated larger than necessary on wxMac
                       .BestSize( m_appearancePanel->GetMinSize().x, -1 )
@@ -336,7 +336,7 @@ PCB_EDIT_FRAME::PCB_EDIT_FRAME( KIWAY* aKiway, wxWindow* aParent ) :
     m_auimgr.AddPane( m_selectionFilterPanel, EDA_PANE().Name( wxS( "SelectionFilter" ) )
                       .Right().Layer( 4 ).Position( 2 )
                       .Caption( _( "Selection Filter" ) ).PaneBorder( false )
-                      .MinSize( m_selectionFilterPanel->GetMinSize().x, -1  )
+                      .MinSize( m_selectionFilterPanel->GetMinSize().x, FromDIP( 60 )  )
                       .BestSize( m_selectionFilterPanel->GetBestSize().x, -1 )
                       .FloatingSize( m_selectionFilterPanel->GetBestSize() )
                       .CloseButton( false ) );
