b81567d6b0642552b37fd50833f17c0249ce0eee
Damjan <damjanovmail@gmail.com>
2026-05-13 20:00:09 +0200

    Pcbnew: show DRC elapsed time in dialog status bar
    Adds a status bar to the DRC dialog that ticks the elapsed time
    while DRC is running, and shows the final duration when it
    completes or is canceled.
    
    
    (cherry picked from commit d5e36ec6b7858a4669d0cfca764b6925791dc35a)
    
    Co-authored-by: Damjan Prerad <damjan@kipro-pcb.com>

--- pcbnew/dialogs/dialog_drc.h
+++ pcbnew/dialogs/dialog_drc.h
@@ -26,6 +26,7 @@
 
 #pragma once
 
+#include <chrono>
 #include <core/throttle.h>
 #include <wx/htmllbox.h>
 #include <rc_item.h>
@@ -33,6 +34,8 @@
 #include <board.h>
 #include <dialog_drc_base.h>
 #include <widgets/progress_reporter_base.h>
+
+class wxStatusBar;
 
 
 class BOARD_DESIGN_SETTINGS;
@@ -138,5 +141,9 @@ private:
 
     THROTTLE m_updateThrottle;
     THROTTLE m_yieldThrottle;
+
+    wxStatusBar*                          m_drcStatusBar;
+    std::chrono::steady_clock::time_point m_drcStartTime;
+    int                                   m_lastTickSeconds;
 };
 
