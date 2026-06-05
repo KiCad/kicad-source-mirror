0227dad0dae15386a4caca6b4ff0a52b2d07015e
Jeff Young <jeff@rokeby.ie>
2026-06-05 19:55:39 +0100

    Clear arcs before calling boolean op on SHAPE_POLY_SET.
    Fixes KICAD-PX3.

--- pcbnew/pcbexpr_functions.cpp
+++ pcbnew/pcbexpr_functions.cpp
@@ -531,8 +531,8 @@ static SHAPE_POLY_SET getDeflatedZoneOutline( BOARD* aBoard, ZONE* aArea )
     // Cache miss - compute deflated outline
     SHAPE_POLY_SET areaOutline = aArea->Outline()->CloneDropTriangulation();
     areaOutline.ClearArcs();
-    areaOutline.Deflate( aBoard->GetDesignSettings().GetDRCEpsilon(),
-                         CORNER_STRATEGY::ALLOW_ACUTE_CORNERS, ARC_LOW_DEF );
+    areaOutline.Deflate( aBoard->GetDesignSettings().GetDRCEpsilon(), CORNER_STRATEGY::ALLOW_ACUTE_CORNERS,
+                         ARC_LOW_DEF );
 
     // Store in cache
     {
@@ -962,16 +962,13 @@ static void enclosedByAreaFunc( LIBEVAL::CONTEXT* aCtx, void* self )
 
                                 for( PCB_LAYER_ID testLayer : aArea->GetLayerSet() )
                                 {
-                                    fp->TransformPadsToPolySet( itemShape, testLayer, 0,
-                                                                maxError, ERROR_OUTSIDE );
-                                    fp->TransformFPShapesToPolySet( itemShape, testLayer, 0,
-                                                                    maxError, ERROR_OUTSIDE );
+                                    fp->TransformPadsToPolySet( itemShape, testLayer, 0, maxError, ERROR_OUTSIDE );
+                                    fp->TransformFPShapesToPolySet( itemShape, testLayer, 0, maxError, ERROR_OUTSIDE );
                                 }
                             }
                             else
                             {
-                                item->TransformShapeToPolygon( itemShape, layer, 0, maxError,
-                                                               ERROR_OUTSIDE );
+                                item->TransformShapeToPolygon( itemShape, layer, 0, maxError, ERROR_OUTSIDE );
                             }
 
                             if( itemShape.IsEmpty() )
@@ -981,6 +978,7 @@ static void enclosedByAreaFunc( LIBEVAL::CONTEXT* aCtx, void* self )
                             }
                             else
                             {
+                                itemShape.ClearArcs();
                                 itemShape.BooleanSubtract( *aArea->Outline() );
 
                                 enclosedByArea = itemShape.IsEmpty();
