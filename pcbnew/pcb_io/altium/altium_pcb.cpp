5bf1e7826d74c75dfc80cbfb309b37081f475067
Seth Hillbrand <seth@kipro-pcb.com>
2026-07-28 12:53:15 -0700

    Altium: keep soldermask relief on copper regions
    Copper region imports dropped their soldermask and paste mask apertures,
    so a region set to open the mask above it lost the relief.  This now
    emits the mask shapes for F_Cu and B_Cu regions.  Also fixed the
    HelperGetSolderAndPasteMaskExpansions to query the passed record type
    instead of always querying TRACK
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/13750
    
    (cherry picked from commit 88171d494f48e5cbd280d1e85413f0a46f360fd8)

--- pcbnew/pcb_io/altium/altium_pcb.cpp
+++ pcbnew/pcb_io/altium/altium_pcb.cpp
@@ -2678,7 +2678,7 @@ void ALTIUM_PCB::ParseShapeBasedRegions6Data( const ALTIUM_PCB_COMPOUND_FILE&   
             || elem.kind == ALTIUM_REGION_KIND::BOARD_CUTOUT )
         {
             // TODO: implement all different types for footprints
-            ConvertShapeBasedRegions6ToBoardItem( elem );
+            ConvertShapeBasedRegions6ToBoardItem( elem, primitiveIndex );
         }
         else
         {
@@ -2692,7 +2692,7 @@ void ALTIUM_PCB::ParseShapeBasedRegions6Data( const ALTIUM_PCB_COMPOUND_FILE&   
 }
 
 
-void ALTIUM_PCB::ConvertShapeBasedRegions6ToBoardItem( const AREGION6& aElem )
+void ALTIUM_PCB::ConvertShapeBasedRegions6ToBoardItem( const AREGION6& aElem, const int aPrimitiveIndex )
 {
     if( aElem.kind == ALTIUM_REGION_KIND::BOARD_CUTOUT )
     {
@@ -2815,7 +2815,7 @@ void ALTIUM_PCB::ConvertShapeBasedRegions6ToBoardItem( const AREGION6& aElem )
         if( aElem.polygon == ALTIUM_POLYGON_NONE )
         {
             for( PCB_LAYER_ID klayer : GetKicadLayersToIterate( aElem.layer ) )
-                ConvertShapeBasedRegions6ToBoardItemOnLayer( aElem, klayer );
+                ConvertShapeBasedRegions6ToBoardItemOnLayer( aElem, klayer, aPrimitiveIndex );
         }
     }
     else
@@ -2981,8 +2981,8 @@ void ALTIUM_PCB::ConvertShapeBasedRegions6ToFootprintItem( FOOTPRINT*      aFoot
 }
 
 
-void ALTIUM_PCB::ConvertShapeBasedRegions6ToBoardItemOnLayer( const AREGION6& aElem,
-                                                              PCB_LAYER_ID    aLayer )
+void ALTIUM_PCB::ConvertShapeBasedRegions6ToBoardItemOnLayer( const AREGION6& aElem, PCB_LAYER_ID aLayer,
+                                                              const int aPrimitiveIndex )
 {
     SHAPE_LINE_CHAIN linechain;
     HelperShapeLineChainFromAltiumVertices( linechain, aElem.outline );
@@ -3023,6 +3023,36 @@ void ALTIUM_PCB::ConvertShapeBasedRegions6ToBoardItemOnLayer( const AREGION6& aE
     }
 
     m_board->Add( shape.release(), ADD_MODE::APPEND );
+
+    // Guard skips dup mask shapes when a MULTI_LAYER region iterates every copper layer
+    if( aLayer == F_Cu || aLayer == B_Cu )
+    {
+        for( const auto& layerExpansionMask :
+             HelperGetSolderAndPasteMaskExpansions( ALTIUM_RECORD::REGION, aPrimitiveIndex, aElem.layer ) )
+        {
+            const PCB_LAYER_ID maskLayer = layerExpansionMask.first;
+
+            if( ( ( maskLayer == F_Mask || maskLayer == F_Paste ) && aLayer != F_Cu )
+                || ( ( maskLayer == B_Mask || maskLayer == B_Paste ) && aLayer != B_Cu ) )
+            {
+                continue;
+            }
+
+            int expansion = layerExpansionMask.second;
+
+            SHAPE_POLY_SET expandedPolySet = polySet;
+            expandedPolySet.Inflate( expansion, CORNER_STRATEGY::ROUND_ALL_CORNERS, ARC_HIGH_DEF );
+
+            std::unique_ptr<PCB_SHAPE> maskShape = std::make_unique<PCB_SHAPE>( m_board, SHAPE_T::POLY );
+
+            maskShape->SetPolyShape( expandedPolySet );
+            maskShape->SetFilled( true );
+            maskShape->SetLayer( maskLayer );
+            maskShape->SetStroke( STROKE_PARAMS( 0 ) );
+
+            m_board->Add( maskShape.release(), ADD_MODE::APPEND );
+        }
+    }
 }
 
 
@@ -5447,8 +5477,7 @@ std::vector<std::pair<PCB_LAYER_ID, int>> ALTIUM_PCB::HelperGetSolderAndPasteMas
     if( m_extendedPrimitiveInformationMaps.count( aType ) == 0 )
         return {}; // there is nothing to parse
 
-    auto elems =
-            m_extendedPrimitiveInformationMaps[ALTIUM_RECORD::TRACK].equal_range( aPrimitiveIndex );
+    auto elems = m_extendedPrimitiveInformationMaps[aType].equal_range( aPrimitiveIndex );
 
     if( elems.first == elems.second )
         return {}; // there is nothing to parse
