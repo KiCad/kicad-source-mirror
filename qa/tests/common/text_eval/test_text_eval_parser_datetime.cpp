ef5322820a4ee9469c2169bab1b75461751a7289
Jeff Young <jeff@rokeby.ie>
2026-08-14 13:26:25 +0100

    Repair comment.

--- qa/tests/common/text_eval/test_text_eval_parser_datetime.cpp
+++ qa/tests/common/text_eval/test_text_eval_parser_datetime.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright The KiCad Developers, see AUTHORS.TXT for contributors.
  *
- * This program is free softwar        { "@{datestring('2023年12月25日')}", "19716", false },  // Christmas 2023       { "@{datestring('2023年12月25日')}", "19716", false },  // Christmas 2023       { "@{datestring('2023年12월25일')}", "19716", false },  // Christmas 2023; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
