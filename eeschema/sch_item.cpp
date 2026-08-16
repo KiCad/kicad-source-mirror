KiCad/kicad-source-mirror

commit 3075b7a12e5478f1c517a7ecf3541dd28c6f345e

ADDED: fine grain control for Compare Symbol with Library.
Fixes https://gitlab.com/kicad/code/kicad/-/issues/21058

eeschema/sch_item.cpp
@@ -698,7 +698,7 @@ bool SCH_ITEM::operator==( const SCH_ITEM& aOther ) const
     if( Type() != aOther.Type() )
         return false;
 
-    return compare( aOther, SCH_ITEM::COMPARE_FLAGS::EQUALITY ) == 0;
+    return compare( aOther, !COMPARE_FLAGS::UUID ) == 0;
 }
 
 
@@ -713,7 +713,7 @@ bool SCH_ITEM::operator<( const SCH_ITEM& aOther ) const
 
 bool SCH_ITEM::cmp_items::operator()( const SCH_ITEM* aFirst, const SCH_ITEM* aSecond ) const
 {
-    return aFirst->compare( *aSecond, COMPARE_FLAGS::EQUALITY ) < 0;
+    return aFirst->compare( *aSecond, !COMPARE_FLAGS::UUID ) < 0;
 }
 
 
@@ -722,16 +722,19 @@ int SCH_ITEM::compare( const SCH_ITEM& aOther, int aCompareFlags ) const
     if( Type() != aOther.Type() )
         return Type() - aOther.Type();
 
-    if( !( aCompareFlags & SCH_ITEM::COMPARE_FLAGS::UNIT ) && m_unit != aOther.m_unit )
-        return m_unit - aOther.m_unit;
+    if( aCompareFlags & SCH_ITEM::COMPARE_FLAGS::UNIT )
+    {
+        if( m_unit != aOther.m_unit )
+            return m_unit - aOther.m_unit;
 
-    if( !( aCompareFlags & SCH_ITEM::COMPARE_FLAGS::UNIT ) && m_bodyStyle != aOther.m_bodyStyle )
-        return m_bodyStyle - aOther.m_bodyStyle;
+        if( m_bodyStyle != aOther.m_bodyStyle )
+            return m_bodyStyle - aOther.m_bodyStyle;
+    }
 
     if( IsPrivate() != aOther.IsPrivate() )
         return IsPrivate() ? 1 : -1;
 
-    if( !( aCompareFlags & SCH_ITEM::COMPARE_FLAGS::SKIP_TST_POS ) )
+    if( aCompareFlags & COMPARE_FLAGS::POSITION )
     {
         if( GetPosition().x != aOther.GetPosition().x )
             return GetPosition().x - aOther.GetPosition().x;
@@ -740,17 +743,14 @@ int SCH_ITEM::compare( const SCH_ITEM& aOther, int aCompareFlags ) const
             return GetPosition().y - aOther.GetPosition().y;
     }
 
-    if( ( aCompareFlags & SCH_ITEM::COMPARE_FLAGS::EQUALITY )
-        || ( aCompareFlags & SCH_ITEM::COMPARE_FLAGS::ERC ) )
+    if( aCompareFlags & COMPARE_FLAGS::UUID )
     {
-        return 0;
+        if( m_Uuid < aOther.m_Uuid )
+            return -1;
+
+        if( m_Uuid > aOther.m_Uuid )
+            return 1;
     }
-
-    if( m_Uuid < aOther.m_Uuid )
-        return -1;
-
-    if( m_Uuid > aOther.m_Uuid )
-        return 1;
 
     return 0;
 }

eeschema/CMakeLists.txt
@@ -224,6 +224,8 @@ set( EESCHEMA_DLGS
     dialogs/panel_setup_formatting_base.cpp
     dialogs/panel_setup_pinmap.cpp
     dialogs/panel_setup_pinmap_base.cpp
+    dialogs/panel_setup_symbol_parity.cpp
+    dialogs/panel_setup_symbol_parity_base.cpp
     dialogs/panel_symbol_pin_map.cpp
     dialogs/panel_symbol_pin_map_base.cpp
     dialogs/panel_simulator_preferences.cpp

eeschema/dialogs/dialog_sch_import_settings_base.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6a-dirty)
+// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -59,6 +59,9 @@ DIALOG_SCH_IMPORT_SETTINGS_BASE::DIALOG_SCH_IMPORT_SETTINGS_BASE( wxWindow* pare
 
 	m_BomFmtPresetsOpt = new wxCheckBox( this, wxID_ANY, _("BOM format presets"), wxDefaultPosition, wxDefaultSize, 0 );
 	bmiddleSizer->Add( m_BomFmtPresetsOpt, 0, wxBOTTOM|wxLEFT|wxRIGHT, 5 );
+
+	m_SymbolParityOpt = new wxCheckBox( this, wxID_ANY, _("Compare symbol with library settings"), wxDefaultPosition, wxDefaultSize, 0 );
+	bmiddleSizer->Add( m_SymbolParityOpt, 0, wxBOTTOM|wxRIGHT|wxLEFT, 5 );
 
 	m_SeveritiesOpt = new wxCheckBox( this, wxID_ANY, _("Violation severities"), wxDefaultPosition, wxDefaultSize, 0 );
 	bmiddleSizer->Add( m_SeveritiesOpt, 0, wxBOTTOM|wxLEFT|wxRIGHT, 5 );

eeschema/dialogs/dialog_sch_import_settings_base.fbp
@@ -675,6 +675,71 @@
             </object>
             <object class="sizeritem" expanded="true">
               <property name="border">5</property>
+              <property name="flag">wxBOTTOM|wxRIGHT|wxLEFT</property>
+              <property name="proportion">0</property>
+              <object class="wxCheckBox" expanded="true">
+                <property name="BottomDockable">1</property>
+                <property name="LeftDockable">1</property>
+                <property name="RightDockable">1</property>
+                <property name="TopDockable">1</property>
+                <property name="aui_layer">0</property>
+                <property name="aui_name"></property>
+                <property name="aui_position">0</property>
+                <property name="aui_row">0</property>
+                <property name="best_size"></property>
+                <property name="bg"></property>
+                <property name="caption"></property>
+                <property name="caption_visible">1</property>
+                <property name="center_pane">0</property>
+                <property name="checked">0</property>
+                <property name="close_button">1</property>
+                <property name="context_help"></property>
+                <property name="context_menu">1</property>
+                <property name="default_pane">0</property>
+                <property name="dock">Dock</property>
+                <property name="dock_fixed">0</property>
+                <property name="docking">Left</property>
+                <property name="drag_accept_files">0</property>
+                <property name="enabled">1</property>
+                <property name="fg"></property>
+                <property name="floatable">1</property>
+                <property name="font"></property>
+                <property name="gripper">0</property>
+                <property name="hidden">0</property>
+                <property name="id">wxID_ANY</property>
+                <property name="label">Compare symbol with library settings</property>
+                <property name="max_size"></property>
+                <property name="maximize_button">0</property>
+                <property name="maximum_size"></property>
+                <property name="min_size"></property>
+                <property name="minimize_button">0</property>
+                <property name="minimum_size"></property>
+                <property name="moveable">1</property>
+                <property name="name">m_SymbolParityOpt</property>
+                <property name="pane_border">1</property>
+                <property name="pane_position"></property>
+                <property name="pane_size"></property>
+                <property name="permission">public</property>
+                <property name="pin_button">1</property>
+                <property name="pos"></property>
+                <property name="resize">Resizable</property>
+                <property name="show">1</property>
+                <property name="size"></property>
+                <property name="style"></property>
+                <property name="subclass">; ; forward_declare</property>
+                <property name="toolbar_pane">0</property>
+                <property name="tooltip"></property>
+                <property name="validator_data_type"></property>
+                <property name="validator_style">wxFILTER_NONE</property>
+                <property name="validator_type">wxDefaultValidator</property>
+                <property name="validator_variable"></property>
+                <property name="window_extra_style"></property>
+                <property name="window_name"></property>
+                <property name="window_style"></property>
+              </object>
+            </object>
+            <object class="sizeritem" expanded="true">
+              <property name="border">5</property>
               <property name="flag">wxBOTTOM|wxLEFT|wxRIGHT</property>
               <property name="proportion">0</property>
               <object class="wxCheckBox" expanded="true">

eeschema/dialogs/dialog_sch_import_settings_base.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6a-dirty)
+// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -59,6 +59,7 @@ class DIALOG_SCH_IMPORT_SETTINGS_BASE : public DIALOG_SHIM
 		wxCheckBox* m_FieldNameTemplatesOpt;
 		wxCheckBox* m_BomPresetsOpt;
 		wxCheckBox* m_BomFmtPresetsOpt;
+		wxCheckBox* m_SymbolParityOpt;
 		wxCheckBox* m_SeveritiesOpt;
 		wxCheckBox* m_PinMapOpt;
 		wxCheckBox* m_NetClassesOpt;

eeschema/dialogs/dialog_schematic_setup.cpp
@@ -41,6 +41,8 @@
 #include <widgets/wx_infobar.h>
 #include <widgets/wx_progress_reporters.h>
 #include "dialog_schematic_setup.h"
+
+#include "panel_setup_symbol_parity.h"
 #include "panel_template_fieldnames.h"
 
 
@@ -101,6 +103,13 @@ DIALOG_SCHEMATIC_SETUP::DIALOG_SCHEMATIC_SETUP( SCH_EDIT_FRAME* aFrame ) :
                                                    ercSettings.m_ERCSeverities, m_pinToPinError.get() );
             }, _( "Violation Severity" ) );
 
+    m_symbolParityPage = m_treebook->GetPageCount();
+    m_treebook->AddLazySubPage(
+            [this]( wxWindow* aParent ) -> wxWindow*
+            {
+                return new PANEL_SETUP_SYMBOL_PARITY( aParent, m_frame );
+            }, _( "Compare Symbol with Library" ) );
+
     m_pinMapPage = m_treebook->GetPageCount();
     m_treebook->AddLazySubPage(
             [this]( wxWindow* aParent ) -> wxWindow*
@@ -115,8 +124,7 @@ DIALOG_SCHEMATIC_SETUP::DIALOG_SCHEMATIC_SETUP( SCH_EDIT_FRAME* aFrame ) :
             [this]( wxWindow* aParent ) -> wxWindow*
             {
                 SCHEMATIC& schematic = m_frame->Schematic();
-                return new PANEL_SETUP_NETCLASSES( aParent, m_frame,
-                                                   m_frame->Prj().GetProjectFile().NetSettings(),
+                return new PANEL_SETUP_NETCLASSES( aParent, m_frame, m_frame->Prj().GetProjectFile().NetSettings(),
                                                    schematic.GetNetClassAssignmentCandidates(), true );
             }, _( "Net Classes" ) );
 
@@ -219,6 +227,12 @@ void DIALOG_SCHEMATIC_SETUP::onAuxiliaryAction( wxCommandEvent& event )
     {
         static_cast<PANEL_TEMPLATE_FIELDNAMES*>( m_treebook->ResolvePage( m_fieldNameTemplatesPage ) )
                 ->ImportSettingsFrom( &otherSch.Settings().m_TemplateFieldNames );
+    }
+
+    if( importDlg.m_SymbolParityOpt->GetValue() )
+    {
+        static_cast<PANEL_SETUP_SYMBOL_PARITY*>( m_treebook->ResolvePage( m_symbolParityPage ) )
+                ->ImportSettingsFrom( file.m_SchematicSettings->m_SymbolParity );
     }
 
     if( importDlg.m_PinMapOpt->GetValue() )

eeschema/dialogs/dialog_schematic_setup.h
@@ -57,6 +57,7 @@ protected:
     size_t                    m_busesPage;
     size_t                    m_netChainsPage;
     size_t                    m_textVarsPage;
+    size_t                    m_symbolParityPage;
     size_t                    m_severitiesPage;
     size_t                    m_netclassesPage;
     size_t                    m_embeddedFilesPage;

eeschema/dialogs/panel_setup_symbol_parity.cpp
@@ -1,0 +1,95 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright The KiCad Developers, see AUTHORS.txt for contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <sch_edit_frame.h>
+#include <sch_painter.h>
+#include <kiface_base.h>
+#include <panel_setup_symbol_parity.h>
+
+
+PANEL_SETUP_SYMBOL_PARITY::PANEL_SETUP_SYMBOL_PARITY( wxWindow* aWindow, SCH_EDIT_FRAME* aFrame  ) :
+        PANEL_SETUP_SYMBOL_PARITY_BASE( aWindow ),
+        m_frame( aFrame )
+{
+}
+
+
+bool PANEL_SETUP_SYMBOL_PARITY::TransferDataToWindow()
+{
+    SYMBOL_PARITY_SETTINGS& settings = m_frame->Schematic().Settings().m_SymbolParity;
+
+    m_missingFields->SetValue( settings.m_MissingFields );
+    m_extraFields->SetValue( settings.m_ExtraFields );
+    m_fieldTextOpt->SetValue( settings.m_FieldTexts );
+    m_fieldVisibilitiesOpt->SetValue( settings.m_FieldVisibilities );
+    m_fieldStyleOpt->SetValue( settings.m_FieldStyles );
+    m_fieldPositionsOpt->SetValue( settings.m_FieldPositions );
+
+    m_pinVisibilitiesOpt->SetValue( settings.m_PinVisibilities );
+    m_altPinFunctionsOpt->SetValue( settings.m_PinAltFunctions );
+
+    m_excludeFromBoardOpt->SetValue( settings.m_ExcludeFromBoardFlags );
+    m_DNPOpt->SetValue( settings.m_DNPFlags );
+    m_excludeFromBOMOpt->SetValue( settings.m_ExcludeFromBOMFlags );
+    m_excludeFromPosFilesOpt->SetValue( settings.m_ExcludeFromPosFileFlags );
+
+    return true;
+}
+
+
+bool PANEL_SETUP_SYMBOL_PARITY::TransferDataFromWindow()
+{
+    SYMBOL_PARITY_SETTINGS& settings = m_frame->Schematic().Settings().m_SymbolParity;
+
+    settings.m_MissingFields = m_missingFields->GetValue();
+    settings.m_ExtraFields = m_extraFields->GetValue();
+    settings.m_FieldTexts = m_fieldTextOpt->GetValue();
+    settings.m_FieldVisibilities = m_fieldVisibilitiesOpt->GetValue();
+    settings.m_FieldStyles = m_fieldStyleOpt->GetValue();
+    settings.m_FieldPositions = m_fieldPositionsOpt->GetValue();
+
+    settings.m_PinVisibilities = m_pinVisibilitiesOpt->GetValue();
+    settings.m_PinAltFunctions = m_altPinFunctionsOpt->GetValue();
+
+    settings.m_ExcludeFromBoardFlags = m_excludeFromBoardOpt->GetValue();
+    settings.m_DNPFlags = m_DNPOpt->GetValue();
+    settings.m_ExcludeFromBOMFlags = m_excludeFromBOMOpt->GetValue();
+    settings.m_ExcludeFromPosFileFlags = m_excludeFromPosFilesOpt->GetValue();
+
+    return true;
+}
+
+
+void PANEL_SETUP_SYMBOL_PARITY::ImportSettingsFrom( SYMBOL_PARITY_SETTINGS& aSettings )
+{
+    m_missingFields->SetValue( aSettings.m_MissingFields );
+    m_extraFields->SetValue( aSettings.m_ExtraFields );
+    m_fieldTextOpt->SetValue( aSettings.m_FieldTexts );
+    m_fieldVisibilitiesOpt->SetValue( aSettings.m_FieldVisibilities );
+    m_fieldStyleOpt->SetValue( aSettings.m_FieldStyles );
+    m_fieldPositionsOpt->SetValue( aSettings.m_FieldPositions );
+
+    m_pinVisibilitiesOpt->SetValue( aSettings.m_PinVisibilities );
+    m_altPinFunctionsOpt->SetValue( aSettings.m_PinAltFunctions );
+
+    m_excludeFromBoardOpt->SetValue( aSettings.m_ExcludeFromBoardFlags );
+    m_DNPOpt->SetValue( aSettings.m_DNPFlags );
+    m_excludeFromBOMOpt->SetValue( aSettings.m_ExcludeFromBOMFlags);
+    m_excludeFromPosFilesOpt->SetValue( aSettings.m_ExcludeFromPosFileFlags );
+}

commit 13c5e68eec8f809866b1e91dab29f7622f637694

eeschema: mask the SCH_ITEM compare flags with ~, not !
`!COMPARE_FLAGS::UUID` is 0, so operator== and cmp_items ignored unit,
body style and position.  Regression from 3075b7a12e.

eeschema/sch_item.cpp
@@ -698,7 +698,7 @@ bool SCH_ITEM::operator==( const SCH_ITEM& aOther ) const
     if( Type() != aOther.Type() )
         return false;
 
-    return compare( aOther, !COMPARE_FLAGS::UUID ) == 0;
+    return compare( aOther, ~COMPARE_FLAGS::UUID ) == 0;
 }
 
 
@@ -713,7 +713,7 @@ bool SCH_ITEM::operator<( const SCH_ITEM& aOther ) const
 
 bool SCH_ITEM::cmp_items::operator()( const SCH_ITEM* aFirst, const SCH_ITEM* aSecond ) const
 {
-    return aFirst->compare( *aSecond, !COMPARE_FLAGS::UUID ) < 0;
+    return aFirst->compare( *aSecond, ~COMPARE_FLAGS::UUID ) < 0;
 }
 
 