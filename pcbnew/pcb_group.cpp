336c5b359dc64771b96a0b7d0753274f5169ce7c
Jon Evans <jon@craftyjon.com>
2026-08-23 15:47:54 -0400

    API: Support locked and lib_id for groups
    (cherry picked from commit ca3b0bf3aa3e13ee69ed7eb1db641b8e60114a64)

--- pcbnew/pcb_group.cpp
+++ pcbnew/pcb_group.cpp
@@ -53,11 +53,12 @@ PCB_GROUP::PCB_GROUP( BOARD_ITEM* aParent, KICAD_T idtype, PCB_LAYER_ID aLayer )
 
 void PCB_GROUP::Serialize( google::protobuf::Any &aContainer ) const
 {
-    using namespace kiapi::board::types;
-    Group group;
+    using namespace kiapi::common::types;
+    kiapi::board::types::Group group;
 
     group.mutable_id()->set_value( m_Uuid.AsStdString() );
     group.set_name( GetName().ToUTF8() );
+    group.set_locked( IsLocked() ? LockedState::LS_LOCKED : LockedState::LS_UNLOCKED );
 
     for( EDA_ITEM* item : GetItems() )
     {
@@ -67,6 +68,9 @@ void PCB_GROUP::Serialize( google::protobuf::Any &aContainer ) const
 
     if( const BOARD* board = GetBoard() )
         group.mutable_parent()->set_value( board->m_Uuid.AsStdString() );
+
+    if( HasDesignBlockLink() )
+        kiapi::common::PackLibId( group.mutable_lib_id(), GetDesignBlockLibId() );
 
     aContainer.PackFrom( group );
 }
@@ -81,6 +85,7 @@ bool PCB_GROUP::Deserialize( const google::protobuf::Any &aContainer )
 
     SetUuidDirect( KIID( group.id().value() ) );
     SetName( wxString( group.name().c_str(), wxConvUTF8 ) );
+    SetLocked( group.locked() == kiapi::common::types::LockedState::LS_LOCKED );
 
     BOARD* board = GetBoard();
 
@@ -94,6 +99,9 @@ bool PCB_GROUP::Deserialize( const google::protobuf::Any &aContainer )
         if( BOARD_ITEM* item = board->ResolveItem( id, true ) )
             AddItem( item );
     }
+
+    if( group.has_lib_id() )
+        SetDesignBlockLibId( kiapi::common::UnpackLibId( group.lib_id() ) );
 
     return true;
 }
