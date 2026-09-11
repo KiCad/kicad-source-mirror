20967116af2c6665f7ef8351b3e75c8b2a65d648
Damjan <damjan@kipro-pcb.com>
2026-09-11 11:18:38 +0200

    common: open list picker dialogs with an empty filter
    The last filter text was restored into the box, but restoring fires no
    text event. The list was never filtered by it.
    
    Now the filter box is left out of saved dialog state. It starts empty.

--- common/dialogs/eda_list_dialog.cpp
+++ common/dialogs/eda_list_dialog.cpp
@@ -43,6 +43,7 @@ EDA_LIST_DIALOG::EDA_LIST_DIALOG( wxWindow* aParent, const wxString& aTitle, con
     // correct for wxfb not supporting FromDIP
     m_listBox->SetMinSize( FromDIP( m_listBox->GetMinSize() ) );
     m_filterBox->SetHint( _( "Filter" ) );
+    OptOut( m_filterBox );
 
     initDialog( aItemHeaders, aItemList, aPreselectText );
 
@@ -68,6 +69,7 @@ EDA_LIST_DIALOG::EDA_LIST_DIALOG( wxWindow* aParent, const wxString& aTitle, boo
         m_sortList( aSortList )
 {
     m_filterBox->SetHint( _( "Filter" ) );
+    OptOut( m_filterBox );
 }
 
 
