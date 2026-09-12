033cc3a4682c02ddbfb7b01e83a47ccb4a4a3212
John Beard <john.j.beard@gmail.com>
2026-09-12 11:46:37 +0100

    DIALOG_SHIM: Batch events for undo when they arrive in bursts
    Some events cause a cascade of other events (e.g. a copy or paste in a
    grid). These should act as a single undo event. Currently it undoes cell
    by cell. Resolve this by batching undos from a single handler into a
    single UNDO_STEP per handler.
    
    This (still) doesn't handle any case where multiple controls change in
    once handler, because UNDO_STEP only represents a single control at this
    time.
    
    This also theoretically improves performance, e.g. in large grids,
    because you don't need to grab N copies of the grid's entire content.
    
    (cherry picked from commit f7f68121803dc6ce38735e851c7bbd4873ce566f)

--- common/dialog_shim.cpp
+++ common/dialog_shim.cpp
@@ -23,8 +23,9 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+#include "dialog_shim.h"
+
 #include <app_monitor.h>
-#include <dialog_shim.h>
 #include <settings/common_settings.h>
 #include <settings/common_settings_internals.h>
 #include <core/ignore.h>
@@ -1182,15 +1183,45 @@ void DIALOG_SHIM::registerUndoRedoHandlers( wxWindowList& children )
 
 void DIALOG_SHIM::recordControlChange( wxWindow* aCtrl )
 {
-    wxVariant before = m_currentValues[ aCtrl ];
-    wxVariant after = getControlValue( aCtrl );
+    // If we are in an event handler that generates lots of events (e.g. cutting
+    // a range of cells in a grid), we want to coalesce all of those events into a
+    // single undo commit.
+    //
+    // So what we'll do is collect all of the controls that have changes,
+    // and enqueue a single call to flushPendingControlChanges() to be called after the
+    // current event handler (which is calling this function) has finished.
 
-    if( before != after )
+    const auto [it, inserted] = m_controlsWithPendingChanges.insert( aCtrl );
+
+    if( !inserted )
+        return;
+
+    if( m_controlsWithPendingChanges.size() == 1 )
+        CallAfter( &DIALOG_SHIM::flushPendingControlChanges );
+}
+
+
+void DIALOG_SHIM::flushPendingControlChanges()
+{
+    for( wxWindow* const ctrl : m_controlsWithPendingChanges )
     {
-        m_undoStack.push_back( { aCtrl, before, after } );
-        m_redoStack.clear();
-        m_currentValues[ aCtrl ] = after;
+        wxVariant before = m_currentValues[ctrl];
+        wxVariant after = getControlValue( ctrl );
+
+        if( before != after )
+        {
+            // Note this still produces an undo/redo entry per control,
+            // even if changed within a control are combined.
+            // If an event causes a multi-control change, the user will
+            // still have to hit undo multiple times to get back to the
+            // original state.`
+            m_undoStack.push_back( { ctrl, before, after } );
+            m_redoStack.clear();
+            m_currentValues[ctrl] = after;
+        }
     }
+
+    m_controlsWithPendingChanges.clear();
 }
 
 
@@ -1453,6 +1484,8 @@ void DIALOG_SHIM::setControlValue( wxWindow* aCtrl, const wxVariant& aValue )
 
 void DIALOG_SHIM::doUndo()
 {
+    flushPendingControlChanges();
+
     if( m_undoStack.empty() )
         return;
 
@@ -1468,6 +1501,8 @@ void DIALOG_SHIM::doUndo()
 
 void DIALOG_SHIM::doRedo()
 {
+    flushPendingControlChanges();
+
     if( m_redoStack.empty() )
         return;
 
@@ -1682,6 +1717,7 @@ void DIALOG_SHIM::resetUndoRedoForNewContent( wxWindowList& aChildren )
     m_undoStack.clear();
     m_redoStack.clear();
     m_currentValues.clear();
+    m_controlsWithPendingChanges.clear();
     registerUndoRedoHandlers( aChildren );
 }
 
