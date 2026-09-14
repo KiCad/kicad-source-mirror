5b29942d0e2fd750c1b0b2c153b171ed742900ed
Seth Hillbrand <seth@kipro-pcb.com>
2026-09-14 11:23:43 -0700

    Symbol editor: skip children deleted with the base
    Cutting a base symbol together with a symbol derived from it removes the
    derived one along with the base. We then looked that name up again. On
    10.0.5 this crashed in GetDerivedSymbolNames, which dbb096e412 fixed.
    Master still asserts in RemoveSymbol, so we skip it now.
    
    Fixes https://gitlab.com/kicad/code/kicad/-/issues/25492
    
    Assisted by: Claude Code
    
    (cherry picked from commit 4683b9746ef841165c148298668f7ddef6f750e8)

--- eeschema/symbol_editor/symbol_editor.cpp
+++ eeschema/symbol_editor/symbol_editor.cpp
@@ -42,6 +42,7 @@
 #include <sch_io/kicad_sexpr/sch_io_kicad_sexpr.h>
 #include <dialogs/dialog_lib_new_symbol.h>
 #include <eda_list_dialog.h>
+#include <set>
 #include <wx/clipbrd.h>
 #include <wx/filedlg.h>
 #include <wx/log.h>
@@ -1256,8 +1257,14 @@ void SYMBOL_EDIT_FRAME::DeleteSymbolFromLibrary()
     if( toDelete.empty() )
         toDelete.emplace_back( GetTargetLibId() );
 
+    // A derived symbol selected together with its base is already gone once the base is removed
+    std::set<LIB_ID> removedWithBase;
+
     for( LIB_ID& libId : toDelete )
     {
+        if( removedWithBase.count( libId ) )
+            continue;
+
         if( m_libMgr->IsSymbolModified( libId.GetLibItemName(), libId.GetLibNickname() )
             && !IsOK( this, wxString::Format( _( "The symbol '%s' has been modified.\n"
                                                  "Do you want to remove it from the library?" ),
@@ -1298,6 +1305,9 @@ void SYMBOL_EDIT_FRAME::DeleteSymbolFromLibrary()
         }
 
         m_libMgr->RemoveSymbol( libId.GetLibItemName(), libId.GetLibNickname() );
+
+        for( const wxString& derivedName : derived )
+            removedWithBase.emplace( libId.GetLibNickname().wx_str(), derivedName );
     }
 
     m_treePane->GetLibTree()->RefreshLibTree();
