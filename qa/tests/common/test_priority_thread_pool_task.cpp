1b2ab3500cb81d29f6b6a168150ee3ad6f0f5e02
John Beard <john.j.beard@gmail.com>
2026-09-16 20:17:35 +0100

    QA: Remove copied copyright assignments
    The header was copied from an Allegro file that had extra assignments.

--- qa/tests/common/test_priority_thread_pool_task.cpp
+++ qa/tests/common/test_priority_thread_pool_task.cpp
@@ -1,7 +1,6 @@
 /*
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
- * Copyright Quilter
  * Copyright The KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
