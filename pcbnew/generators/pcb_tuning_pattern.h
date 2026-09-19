21f1f53428b13701a685c67bbab4fac12d8894c4
Mark Roszko <mark.roszko@gmail.com>
2026-09-19 18:58:04 -0400

    Fix build failure again

--- pcbnew/generators/pcb_tuning_pattern.h
+++ pcbnew/generators/pcb_tuning_pattern.h
@@ -371,20 +371,7 @@ public:
         return 0;
     }
 
-    void SetNetCode( int aNetCode )
-    {
-        if( BOARD* board = GetBoard() )
-        {
-            if( NETINFO_ITEM* net = board->FindNet( aNetCode ) )
-                m_lastNetName = net->GetNetname();
-            else
-                m_lastNetName.clear();
-        }
-
-        for( BOARD_ITEM* item : GetBoardItems() )
-            if( BOARD_CONNECTED_ITEM* bci = dynamic_cast<BOARD_CONNECTED_ITEM*>( item ) )
-                bci->SetNetCode( aNetCode );
-    }
+    void SetNetCode( int aNetCode );
 
     bool HasSolderMask() const
     {
